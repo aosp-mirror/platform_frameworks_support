androidx/androidx

commit 5506618c80a292ac275d8b0c1046b446c7f58836

Fix issue #6584942 IllegalStateException: Failure saving state...
...active SuggestFragment{419494f0} has cleared index: -1

There were issues when the same fragment was removed and then
added again before completely finishing the remove (such as due
to a running animation).

Two fixes:

- Now when you call FragmentTransaction.replace() and are replacing
  a fragment with the same fragment, this becomes a no-op, to avoid
  visual artifacts in the transition and bad states.
- When we are moving the fragment state up and it is currently
  animating away to the INITIALIZED state, we could end up making
  the fragment inactive as part of the cleanup.  In this case it
  shouldn't be made inactive; we just need to initialize it but
  keep it active since we are going to continue to use it.

Bug: 6584942
Change-Id: I4c37a8e48b115feb0892ef0eb92316d7aeba7b1b

v4/java/android/support/v4/app/BackStackRecord.java
@@ -53,7 +53,7 @@ final class BackStackState implements Parcelable {
         int pos = 0;
         while (op != null) {
             mOps[pos++] = op.cmd;
-            mOps[pos++] = op.fragment.mIndex;
+            mOps[pos++] = op.fragment != null ? op.fragment.mIndex : -1;
             mOps[pos++] = op.enterAnim;
             mOps[pos++] = op.exitAnim;
             mOps[pos++] = op.popEnterAnim;
@@ -99,8 +99,13 @@ final class BackStackState implements Parcelable {
             op.cmd = mOps[pos++];
             if (FragmentManagerImpl.DEBUG) Log.v(FragmentManagerImpl.TAG,
                     "BSE " + bse + " set base fragment #" + mOps[pos]);
-            Fragment f = fm.mActive.get(mOps[pos++]);
-            op.fragment = f;
+            int findex = mOps[pos++];
+            if (findex >= 0) {
+                Fragment f = fm.mActive.get(findex);
+                op.fragment = f;
+            } else {
+                op.fragment = null;
+            }
             op.enterAnim = mOps[pos++];
             op.exitAnim = mOps[pos++];
             op.popEnterAnim = mOps[pos++];
@@ -506,9 +511,11 @@ final class BackStackRecord extends FragmentTransaction implements
                 + " by " + amt);
         Op op = mHead;
         while (op != null) {
-            op.fragment.mBackStackNesting += amt;
-            if (FragmentManagerImpl.DEBUG) Log.v(TAG, "Bump nesting of "
-                    + op.fragment + " to " + op.fragment.mBackStackNesting);
+            if (op.fragment != null) {
+                op.fragment.mBackStackNesting += amt;
+                if (FragmentManagerImpl.DEBUG) Log.v(TAG, "Bump nesting of "
+                        + op.fragment + " to " + op.fragment.mBackStackNesting);
+            }
             if (op.removed != null) {
                 for (int i=op.removed.size()-1; i>=0; i--) {
                     Fragment r = op.removed.get(i);
@@ -568,23 +575,29 @@ final class BackStackRecord extends FragmentTransaction implements
                             Fragment old = mManager.mAdded.get(i);
                             if (FragmentManagerImpl.DEBUG) Log.v(TAG,
                                     "OP_REPLACE: adding=" + f + " old=" + old);
-                            if (old.mContainerId == f.mContainerId) {
-                                if (op.removed == null) {
-                                    op.removed = new ArrayList<Fragment>();
+                            if (f == null || old.mContainerId == f.mContainerId) {
+                                if (old == f) {
+                                    op.fragment = f = null;
+                                } else {
+                                    if (op.removed == null) {
+                                        op.removed = new ArrayList<Fragment>();
+                                    }
+                                    op.removed.add(old);
+                                    old.mNextAnim = op.exitAnim;
+                                    if (mAddToBackStack) {
+                                        old.mBackStackNesting += 1;
+                                        if (FragmentManagerImpl.DEBUG) Log.v(TAG, "Bump nesting of "
+                                                + old + " to " + old.mBackStackNesting);
+                                    }
+                                    mManager.removeFragment(old, mTransition, mTransitionStyle);
                                 }
-                                op.removed.add(old);
-                                old.mNextAnim = op.exitAnim;
-                                if (mAddToBackStack) {
-                                    old.mBackStackNesting += 1;
-                                    if (FragmentManagerImpl.DEBUG) Log.v(TAG, "Bump nesting of "
-                                            + old + " to " + old.mBackStackNesting);
-                                }
-                                mManager.removeFragment(old, mTransition, mTransitionStyle);
                             }
                         }
                     }
-                    f.mNextAnim = op.enterAnim;
-                    mManager.addFragment(f, false);
+                    if (f != null) {
+                        f.mNextAnim = op.enterAnim;
+                        mManager.addFragment(f, false);
+                    }
                 } break;
                 case OP_REMOVE: {
                     Fragment f = op.fragment;
@@ -644,10 +657,12 @@ final class BackStackRecord extends FragmentTransaction implements
                 } break;
                 case OP_REPLACE: {
                     Fragment f = op.fragment;
-                    f.mNextAnim = op.popExitAnim;
-                    mManager.removeFragment(f,
-                            FragmentManagerImpl.reverseTransit(mTransition),
-                            mTransitionStyle);
+                    if (f != null) {
+                        f.mNextAnim = op.popExitAnim;
+                        mManager.removeFragment(f,
+                                FragmentManagerImpl.reverseTransit(mTransition),
+                                mTransitionStyle);
+                    }
                     if (op.removed != null) {
                         for (int i=0; i<op.removed.size(); i++) {
                             Fragment old = op.removed.get(i);

v4/java/android/support/v4/app/Fragment.java
@@ -1266,7 +1266,7 @@ public class Fragment implements ComponentCallbacks, OnCreateContextMenuListener
     public void dump(String prefix, FileDescriptor fd, PrintWriter writer, String[] args) {
         writer.print(prefix); writer.print("mFragmentId=#");
                 writer.print(Integer.toHexString(mFragmentId));
-                writer.print(" mContainerId#=");
+                writer.print(" mContainerId=#");
                 writer.print(Integer.toHexString(mContainerId));
                 writer.print(" mTag="); writer.println(mTag);
         writer.print(prefix); writer.print("mState="); writer.print(mState);

v4/java/android/support/v4/app/FragmentManager.java
@@ -765,11 +765,12 @@ final class FragmentManagerImpl extends FragmentManager {
                 return;
             }
             f.mDeferStart = false;
-            moveToState(f, mCurState, 0, 0);
+            moveToState(f, mCurState, 0, 0, false);
         }
     }
 
-    void moveToState(Fragment f, int newState, int transit, int transitionStyle) {
+    void moveToState(Fragment f, int newState, int transit, int transitionStyle,
+            boolean keepActive) {
         // Fragments that are not currently added will sit in the onCreate() state.
         if (!f.mAdded && newState > Fragment.CREATED) {
             newState = Fragment.CREATED;
@@ -796,7 +797,7 @@ final class FragmentManagerImpl extends FragmentManager {
                 // animation, move to whatever the final state should be once
                 // the animation is done, and then we can proceed from there.
                 f.mAnimatingAway = null;
-                moveToState(f, f.mStateAfterAnimating, 0, 0);
+                moveToState(f, f.mStateAfterAnimating, 0, 0, true);
             }
             switch (f.mState) {
                 case Fragment.INITIALIZING:
@@ -983,7 +984,7 @@ final class FragmentManagerImpl extends FragmentManager {
                                         if (fragment.mAnimatingAway != null) {
                                             fragment.mAnimatingAway = null;
                                             moveToState(fragment, fragment.mStateAfterAnimating,
-                                                    0, 0);
+                                                    0, 0, false);
                                         }
                                     }
                                     @Override
@@ -1040,11 +1041,13 @@ final class FragmentManagerImpl extends FragmentManager {
                                 throw new SuperNotCalledException("Fragment " + f
                                         + " did not call through to super.onDetach()");
                             }
-                            if (!f.mRetaining) {
-                                makeInactive(f);
-                            } else {
-                                f.mActivity = null;
-                                f.mFragmentManager = null;
+                            if (!keepActive) {
+                                if (!f.mRetaining) {
+                                    makeInactive(f);
+                                } else {
+                                    f.mActivity = null;
+                                    f.mFragmentManager = null;
+                                }
                             }
                         }
                     }
@@ -1055,7 +1058,7 @@ final class FragmentManagerImpl extends FragmentManager {
     }
     
     void moveToState(Fragment f) {
-        moveToState(f, mCurState, 0, 0);
+        moveToState(f, mCurState, 0, 0, false);
     }
 
     void moveToState(int newState, boolean always) {
@@ -1077,7 +1080,7 @@ final class FragmentManagerImpl extends FragmentManager {
             for (int i=0; i<mActive.size(); i++) {
                 Fragment f = mActive.get(i);
                 if (f != null) {
-                    moveToState(f, newState, transit, transitStyle);
+                    moveToState(f, newState, transit, transitStyle, false);
                     if (f.mLoaderManager != null) {
                         loadersRunning |= f.mLoaderManager.hasRunningLoaders();
                     }
@@ -1170,7 +1173,7 @@ final class FragmentManagerImpl extends FragmentManager {
             fragment.mAdded = false;
             fragment.mRemoving = true;
             moveToState(fragment, inactive ? Fragment.INITIALIZING : Fragment.CREATED,
-                    transition, transitionStyle);
+                    transition, transitionStyle, false);
         }
     }
     
@@ -1223,7 +1226,7 @@ final class FragmentManagerImpl extends FragmentManager {
                     mNeedMenuInvalidate = true;
                 }
                 fragment.mAdded = false;
-                moveToState(fragment, Fragment.CREATED, transition, transitionStyle);
+                moveToState(fragment, Fragment.CREATED, transition, transitionStyle, false);
             }
         }
     }
@@ -1238,7 +1241,7 @@ final class FragmentManagerImpl extends FragmentManager {
                 if (fragment.mHasMenu && fragment.mMenuVisible) {
                     mNeedMenuInvalidate = true;
                 }
-                moveToState(fragment, mCurState, transition, transitionStyle);
+                moveToState(fragment, mCurState, transition, transitionStyle, false);
             }
         }
     }

aosp-mirror/platform_frameworks_support

commit df92a504222e4e6bfadf04890cf46e2815765e40

Fix FragmentTransaction replace() behavior
According to FragmentTransaction's reference, replace()
should be equivalent to remove()'ing every fragment in
a given container and then adding the specified fragment
to replace those that were removed.

Commit 5506618c80a292ac275d8b0c1046b446c7f58836 broke
this intended behavior in the support library.

This patch makes it so that the removal loop doesn't
end prematurely. It also fixes an issue where the
replace operation would remove fragments regardless
of their container after encountering the first fragment
with a matching container ID.

Relevant issues:
http://b.android.com/43265
http://b.android.com/52112
http://b.android.com/53393
http://b.android.com/68856
http://b.android.com/70803

Change-Id: Ieab4468fa7a2afb63bd5fd87a21bbd95d54d638f
Signed-off-by: Eddie Ringle <eddie.ringle@gmail.com>

v4/java/android/support/v4/app/BackStackRecord.java
@@ -617,12 +617,13 @@ final class BackStackRecord extends FragmentTransaction implements
                 } break;
                 case OP_REPLACE: {
                     Fragment f = op.fragment;
+                    int containerId = f.mContainerId;
                     if (mManager.mAdded != null) {
                         for (int i=0; i<mManager.mAdded.size(); i++) {
                             Fragment old = mManager.mAdded.get(i);
                             if (FragmentManagerImpl.DEBUG) Log.v(TAG,
                                     "OP_REPLACE: adding=" + f + " old=" + old);
-                            if (f == null || old.mContainerId == f.mContainerId) {
+                            if (old.mContainerId == containerId) {
                                 if (old == f) {
                                     op.fragment = f = null;
                                 } else {