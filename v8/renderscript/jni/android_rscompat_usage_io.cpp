80de9ca8a721b48add3a4fca8a2e07e3d1e4fcc5
Miao Wang <miaowang@google.com>
2014-11-06 10:55:44 -0800

    Enable USAGE_IO_OUTPUT and corresponding SetSurface and IOSend functions for in RS Compat lib.
    The USAGE_IO support is provided by a shared lib (libRSSSupportIO)
    dynamically loaded on devices running ICS or higher.
    
    bug 12924018
    
    Change-Id: Id9478f8db0d52dd6891e367dbef0d64a186ca399

---
+++ v8/renderscript/jni/android_rscompat_usage_io.cpp
@@ -0,0 +1,28 @@
+#include <android/log.h>
+#include <android/native_window.h>
+#include <android/native_window_jni.h>
+
+#include <rsEnv.h>
+#include "rsDispatch.h"
+#define LOG_API(...)
+
+void rsdAllocationSetSurface(RsContext rsc, RsAllocation alloc, ANativeWindow *nw);
+void rsdAllocationIoSend(RsContext rsc, RsAllocation alloc);
+
+extern "C" void AllocationSetSurface(JNIEnv *_env, jobject _this, RsContext con, RsAllocation alloc, jobject sur, bool useNative, dispatchTable dispatchTab)
+{
+    LOG_API("nAllocationSetSurface, con(%p), alloc(%p), surface(%p)",
+            con, alloc, sur);
+
+    ANativeWindow* s;
+    if (sur != 0) {
+        s = ANativeWindow_fromSurface(_env, sur);
+    }
+    if (useNative) {
+        dispatchTab.AllocationSetSurface(con, alloc, s);
+    } else {
+        rsdAllocationSetSurface(con, alloc, s);
+    }
+
+}
+
