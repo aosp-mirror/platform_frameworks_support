e7307dbc72ac147b6d07c968437644f9d18b15fc
Neil Fuller <nfuller@google.com>
2015-08-07 16:16:35 +0100

    Modify the interface of TransportMediatorJellybeanMR2 to fix build issues
    Bug: 18049859
    Change-Id: Ie9237d6f1f6c657f3dd7aa4bf14b010f720aa627

--- v4/jellybean-mr2/android/support/v4/media/TransportMediatorJellybeanMR2.java
+++ v4/jellybean-mr2/android/support/v4/media/TransportMediatorJellybeanMR2.java
@@ -28,9 +28,7 @@ import android.view.KeyEvent;
 import android.view.View;
 import android.view.ViewTreeObserver;
 
-class TransportMediatorJellybeanMR2
-        implements RemoteControlClient.OnGetPlaybackPositionListener,
-        RemoteControlClient.OnPlaybackPositionUpdateListener {
+class TransportMediatorJellybeanMR2 {
     final Context mContext;
     final AudioManager mAudioManager;
     final View mTargetView;
@@ -75,7 +73,20 @@ class TransportMediatorJellybeanMR2
             mTransportCallback.handleAudioFocusChange(focusChange);
         }
     };
-
+    final RemoteControlClient.OnGetPlaybackPositionListener mGetPlaybackPositionListener
+            = new RemoteControlClient.OnGetPlaybackPositionListener() {
+                @Override
+                public long onGetPlaybackPosition() {
+                    return TransportMediatorJellybeanMR2.this.onGetPlaybackPosition();
+                }
+            };
+    final RemoteControlClient.OnPlaybackPositionUpdateListener mPlaybackPositionUpdateListener
+            = new RemoteControlClient.OnPlaybackPositionUpdateListener() {
+                public void onPlaybackPositionUpdate(long newPositionMs) {
+                    TransportMediatorJellybeanMR2.this.onPlaybackPositionUpdate(newPositionMs);
+                }
+            };
+ 
     PendingIntent mPendingIntent;
     RemoteControlClient mRemoteControl;
     boolean mFocused;
@@ -112,8 +123,8 @@ class TransportMediatorJellybeanMR2
         mPendingIntent = PendingIntent.getBroadcast(mContext, 0, mIntent,
                 PendingIntent.FLAG_CANCEL_CURRENT);
         mRemoteControl = new RemoteControlClient(mPendingIntent);
-        mRemoteControl.setOnGetPlaybackPositionListener(this);
-        mRemoteControl.setPlaybackPositionUpdateListener(this);
+        mRemoteControl.setOnGetPlaybackPositionListener(mGetPlaybackPositionListener);
+        mRemoteControl.setPlaybackPositionUpdateListener(mPlaybackPositionUpdateListener);
     }
 
     void gainFocus() {
@@ -145,12 +156,10 @@ class TransportMediatorJellybeanMR2
         }
     }
 
-    @Override
     public long onGetPlaybackPosition() {
         return mTransportCallback.getPlaybackPosition();
     }
 
-    @Override
     public void onPlaybackPositionUpdate(long newPositionMs) {
         mTransportCallback.playbackPositionUpdate(newPositionMs);
     }
