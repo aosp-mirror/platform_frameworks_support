0cbdefc7d877fd701c6c1eb3a56d14ce0c9765d4
Thilo Schuchort <t.schuchort@googlemail.com>
2017-01-15 14:03:20 +0100

    remove final in ItemAnimator dispatch methods
    remove final keyword of dispatchAnimation*
    methods in RecyclerView ItemAnimator to make
    it composable.
    
    Test: ran existing tests. all passed..
    
    Change-Id: Ia5619d9f07cf52d414296af9e43b0330e9d05039
    Signed-off-by: Thilo Schuchort <t.schuchort@googlemail.com>

--- v7/recyclerview/src/android/support/v7/widget/RecyclerView.java
+++ v7/recyclerview/src/android/support/v7/widget/RecyclerView.java
@@ -11645,7 +11645,7 @@ public class RecyclerView extends ViewGroup implements ScrollingView, NestedScro
          * @param viewHolder The ViewHolder whose animation is finished.
          * @see #onAnimationFinished(ViewHolder)
          */
-        public final void dispatchAnimationFinished(ViewHolder viewHolder) {
+        public void dispatchAnimationFinished(ViewHolder viewHolder) {
             onAnimationFinished(viewHolder);
             if (mListener != null) {
                 mListener.onAnimationFinished(viewHolder);
@@ -11686,7 +11686,7 @@ public class RecyclerView extends ViewGroup implements ScrollingView, NestedScro
          * @param viewHolder The ViewHolder whose animation is starting.
          * @see #onAnimationStarted(ViewHolder)
          */
-        public final void dispatchAnimationStarted(ViewHolder viewHolder) {
+        public void dispatchAnimationStarted(ViewHolder viewHolder) {
             onAnimationStarted(viewHolder);
         }
 
@@ -11788,7 +11788,7 @@ public class RecyclerView extends ViewGroup implements ScrollingView, NestedScro
          * This method should be called by ItemAnimator implementations to notify
          * any listeners that all pending and active item animations are finished.
          */
-        public final void dispatchAnimationsFinished() {
+        public void dispatchAnimationsFinished() {
             final int count = mFinishedListeners.size();
             for (int i = 0; i < count; ++i) {
                 mFinishedListeners.get(i).onAnimationsFinished();
