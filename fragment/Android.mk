4d0ab18cb3043b90cb1e4348865b2aefad48ca6f
Nan Zhang <nanzhang@google.com>
2017-08-21 11:10:13 -0700

    Change android-support-media-compat to static lib
    bug: b/64308460
    Test: m clean && m java
    Change-Id: I1b3ac401f0a1cc8ad59d15aa83fe2ba14efc4e7c

--- fragment/Android.mk
+++ fragment/Android.mk
@@ -41,8 +41,10 @@ LOCAL_SHARED_ANDROID_LIBRARIES := \
     android-support-compat \
     android-support-core-ui \
     android-support-core-utils \
-    android-support-media-compat \
+    android-support-media-compat
+LOCAL_STATIC_ANDROID_LIBRARIES := \
     android-support-annotations
+
 LOCAL_JAR_EXCLUDE_FILES := none
 LOCAL_JAVA_LANGUAGE_VERSION := 1.7
 LOCAL_AAPT_FLAGS := --add-javadoc-annotation doconly
