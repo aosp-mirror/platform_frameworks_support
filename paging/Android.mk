43dfe53c84f11374c09f7a7d7f558cd01296b6a7
Allen <calderwoodra@google.com>
2017-12-21 18:45:52 -0800

    Added Android.mk files for Room Persistence.
    Test: make
    Change-Id: I5368426550393126cf0be398badf3bb9b0da3362

---
+++ paging/Android.mk
@@ -0,0 +1,14 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := paging-common
+LOCAL_MODULE_TAGS := optional
+LOCAL_SDK_VERSION := current
+
+LOCAL_SRC_FILES := $(call all-java-files-under, common/src/main/)
+
+LOCAL_STATIC_ANDROID_LIBRARIES := android-support-v4 \
+
+include $(BUILD_STATIC_JAVA_LIBRARY)
+include $(CLEAR_VARS)
