bc6a8a209ea63388451d03789206078deb22c8c1
Scott Kennedy <skennedy27@gmail.com>
2018-05-07 11:38:29 -0700

    Prevent an NPE in BottomSheetBehavior
    Similar to I25847ce70437d33b864b0d1b27a28d215a7c1ab3, this prevents
    an NPE if mNestedScrollingChildRef has not set yet.
    
    Test: Existing unit tests
    Change-Id: Ic9758a0943d375a68e0a25c2570997c506441c51

--- design/src/android/support/design/widget/BottomSheetBehavior.java
+++ design/src/android/support/design/widget/BottomSheetBehavior.java
@@ -296,7 +296,7 @@ public class BottomSheetBehavior<V extends View> extends CoordinatorLayout.Behav
         // We have to handle cases that the ViewDragHelper does not capture the bottom sheet because
         // it is not the top most view of its parent. This is not necessary when the touch event is
         // happening over the scrolling content as nested scrolling logic handles that case.
-        View scroll = mNestedScrollingChildRef.get();
+        View scroll = mNestedScrollingChildRef != null ? mNestedScrollingChildRef.get() : null;
         return action == MotionEvent.ACTION_MOVE && scroll != null &&
                 !mIgnoreEvents && mState != STATE_DRAGGING &&
                 !parent.isPointInChildBounds(scroll, (int) event.getX(), (int) event.getY()) &&
