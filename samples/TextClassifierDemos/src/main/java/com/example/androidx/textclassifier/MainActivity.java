dff6931430f892a7bd568e21076aa3e8608f3c7f
Tony Mak <tonymak@google.com>
2018-06-06 16:42:13 +0100

    Sample projects for textclassifier
    BUG: 72307474
    
    Test: ./gradlew textclassifier-demos:installDebug
    
    Change-Id: I01355506ea932d7fc2d4bfedddeff0ee285aac74

---
+++ samples/TextClassifierDemos/src/main/java/com/example/androidx/textclassifier/MainActivity.java
@@ -0,0 +1,115 @@
+/*
+ * Copyright 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.androidx.textclassifier;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.textclassifier.SmartLinkify;
+import androidx.textclassifier.TextClassificationContext;
+import androidx.textclassifier.TextClassificationManager;
+import androidx.textclassifier.TextClassifier;
+import androidx.textclassifier.TextLinksParams;
+
+/**
+ * Main activity.
+ */
+public class MainActivity extends AppCompatActivity {
+    private static final int DEFAULT = 0;
+    private static final int CUSTOM = 1;
+
+    private TextClassifier mTextClassifier;
+
+    private EditText mInput;
+
+    private TextView mStatusTextView;
+
+    private TextClassificationManager mTextClassificationManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mTextClassificationManager = TextClassificationManager.of(this);
+        mTextClassifier = createTextClassifier();
+
+        setContentView(R.layout.activity_main);
+        mInput = findViewById(R.id.textView_input);
+        mStatusTextView = findViewById(R.id.textView_tc);
+        findViewById(R.id.button_generate_links).setOnClickListener(v -> smartLinkify());
+        findViewById(R.id.create_new_tc).setOnClickListener(view -> {
+            mTextClassifier = createTextClassifier();
+            updateStatusText();
+        });
+        findViewById(R.id.destroy).setOnClickListener(v -> destroyTextClassifier());
+
+        updateStatusText();
+        setupSpinner();
+    }
+
+    private void setupSpinner() {
+        Spinner spinner = findViewById(R.id.textclassifier_spinner);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int pos, long l) {
+                if (pos == DEFAULT) {
+                    mTextClassificationManager.setTextClassifierFactory(null);
+                } else {
+                    mTextClassificationManager.setTextClassifierFactory(
+                            textClassificationContext ->
+                                    new SimpleTextClassifier(
+                                            MainActivity.this, textClassificationContext));
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+    }
+
+    @Nullable
+    private TextClassifier createTextClassifier() {
+        TextClassifier textClassifier = mTextClassificationManager.createTextClassifier(
+                new TextClassificationContext.Builder(
+                        getPackageName(), TextClassifier.WIDGET_TYPE_UNKNOWN).build());
+        return textClassifier;
+    }
+
+    private void updateStatusText() {
+        mStatusTextView.setText(
+                mTextClassifier.getClass().getName()
+                        + " is in use. Destroyed: " + mTextClassifier.isDestroyed());
+    }
+
+    private void smartLinkify() {
+        TextLinksParams textLinksParams = new TextLinksParams.Builder().build();
+        SmartLinkify.addLinksAsync(mInput, textLinksParams);
+    }
+
+    private void destroyTextClassifier() {
+        mTextClassifier.destroy();
+        updateStatusText();
+    }
+}
