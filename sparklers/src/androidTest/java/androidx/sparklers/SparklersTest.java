cfc7a50d53001dc3c531d6943ff0875cdf8739d5
Jason Monk <jmonk@android.com>
2018-07-23 22:08:39 -0400

    Add sparklers: prototype of a library for getting up to date code
    Allows a library to be compiled in locally or loaded from another
    APK depending on policy specified by the caller.
    
    Test: :sparklers:connectedCheck
    Bug: 111854819
    Change-Id: I09e4c011476d5e02945d1a090d556cca101729a1

---
+++ sparklers/src/androidTest/java/androidx/sparklers/SparklersTest.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.sparklers;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertNotNull;
+import static junit.framework.Assert.assertTrue;
+
+import static org.junit.Assert.assertNotEquals;
+
+import android.content.Context;
+
+import androidx.sparklers.test.R;
+import androidx.test.InstrumentationRegistry;
+import androidx.test.filters.SmallTest;
+import androidx.test.runner.AndroidJUnit4;
+
+import dalvik.system.PathClassLoader;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+@SmallTest
+public class SparklersTest {
+
+    private final Context mContext = InstrumentationRegistry.getContext();
+
+    @Test
+    public void testPolicyBuilder() throws SparkException {
+        String vers = "1.1.0";
+        LoadPolicy policy = new LoadPolicy.Builder("test.library")
+                .setLocalVersion(vers)
+                .setLoadMode(LoadPolicy.ONLY_LOAD)
+                .setMinVersion("1.0.0")
+                .disableVersion("1.0.0-alpha02")
+                .disableVersion("1.0.0-alpha03")
+                .build();
+
+        assertEquals(vers, policy.getLocalVersion());
+        assertEquals("1.0.0", policy.getMinVersion());
+        assertEquals("test.library", policy.getLibrary());
+        assertEquals(LoadPolicy.ONLY_LOAD, policy.getLoadMode());
+        assertEquals(2, policy.getDisabledVersions().size());
+        assertTrue(policy.getDisabledVersions().contains("1.0.0-alpha02"));
+        assertTrue(policy.getDisabledVersions().contains("1.0.0-alpha03"));
+    }
+
+    @Test
+    public void testPolicyXml() throws SparkException {
+        String vers = "1.1.0";
+        LoadPolicy policy = LoadPolicy.readLoadPolicy(mContext, R.xml.test_policy);
+
+        assertEquals(vers, policy.getLocalVersion());
+        assertEquals("1.0.0", policy.getMinVersion());
+        assertEquals("test.library", policy.getLibrary());
+        assertEquals(LoadPolicy.ONLY_LOAD, policy.getLoadMode());
+        assertEquals(2, policy.getDisabledVersions().size());
+        assertTrue(policy.getDisabledVersions().contains("1.0.0-alpha02"));
+        assertTrue(policy.getDisabledVersions().contains("1.0.0-alpha03"));
+    }
+
+    @Test
+    public void testLoad() throws Exception {
+        LoadPolicy policy = LoadPolicy.readLoadPolicy(mContext, R.xml.slice_policy);
+        Spark spark = Sparklers.loadSpark(mContext, policy);
+        assertNotNull(spark);
+        assertEquals(Spark.SOURCE_LOADED, spark.getSourceType());
+        assertNotEquals(getClass().getClassLoader(), spark.getClassLoader());
+        Runnable r = (Runnable) new PathClassLoader(mContext.getApplicationInfo().sourceDir,
+                spark.getClassLoader()).loadClass("androidx.sparklers.LoadedClassCheck")
+                .getConstructor().newInstance();
+        r.run();
+    }
+}
