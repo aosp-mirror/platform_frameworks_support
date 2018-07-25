e6cf33f140b39d698571eac9b9ca67fc1b546904
Chris Craik <ccraik@google.com>
2018-07-25 14:18:15 -0700

    AOT-compile benchmarks with adb injection
    AOT isn't very representative of real world performance, but it does
    significantly improve performance stability.
    
    Bug: 72877822
    Test: ./gradlew benchmark:cC
    Change-Id: Ifcb39720fc678f09fc575914846efbbc766e02de

--- buildSrc/src/main/kotlin/androidx/build/SupportAndroidLibraryPlugin.kt
+++ buildSrc/src/main/kotlin/androidx/build/SupportAndroidLibraryPlugin.kt
@@ -21,6 +21,7 @@ import com.android.build.gradle.LibraryExtension
 import com.android.build.gradle.LibraryPlugin
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.logging.LogLevel
 import org.gradle.kotlin.dsl.apply
 
 /**
@@ -39,6 +40,8 @@ class SupportAndroidLibraryPlugin : Plugin<Project> {
         project.afterEvaluate {
             val library = project.extensions.findByType(LibraryExtension::class.java)
                     ?: return@afterEvaluate
+
+            project.injectCompilationForBenchmarks(library, supportLibraryExtension)
 
             VersionFileWriterTask.setUpAndroidLibrary(project, library)
 
@@ -69,5 +72,32 @@ class SupportAndroidLibraryPlugin : Plugin<Project> {
                 ?: throw Exception("Failed to find Android extension")
 
         project.configureLint(library.lintOptions, supportLibraryExtension)
+    }
+}
+
+/**
+ * For benchmarks, inject an extra adb command to AOT compile the APK after install.
+ *
+ * This is disgusting, but seems to be the only way to AOT-compile an APK without essentially
+ * reimplementing connectedCheck. If AGP adds support for this in some other way, we can avoid this.
+ *
+ * Additionally, AOT isn't a great solution here, since it's not very representative of real world.
+ * Ideally we'd use profile-driven compilation, but that requires significantly more steps. However
+ * AOT does give us much better performance stability.
+ */
+private fun Project.injectCompilationForBenchmarks(
+    extension: LibraryExtension,
+    supportLibraryExtension: SupportLibraryExtension
+) {
+    if (name.endsWith("-benchmark")) {
+        logger.log(LogLevel.WARN,
+                "Will use ADB command injection to force AOT-compile suspected benchmark")
+        val group = supportLibraryExtension.mavenGroup
+
+        // NOTE: we assume here that all benchmarks have package name $groupname.benchmark.test
+        val inject = "cmd package compile -m speed -f $group.benchmark.test"
+        // NOTE: we assume here that all benchmarks have apk name $projectname-debug-androidTest.apk
+        val options = "/data/local/tmp/$name-debug-androidTest.apk && $inject #".split(" ")
+        extension.adbOptions.setInstallOptions(*options.toTypedArray())
     }
 }
