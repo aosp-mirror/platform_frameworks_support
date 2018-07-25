427fd3aae63ed0003c6db5a78a09527ff61589c6
Julia Tuttle <juliatuttle@google.com>
2018-07-25 10:44:40 -0400

    Slices: Add SliceItem.getSanitizedText
    This lets the support library remove Spans that it doesn't want to
    render because they would break layout or theming.
    
    (getText still returns the unmodified text, so apps can use support
    library to find Slices and then render them with whatever subset of
    formatting their views are able to handle.)
    
    Test: Added SliceTest.testProhibitedSpan
    Change-Id: I08ec60a9f2cf92cddaec4a30a9da6945fdff0376

--- slices/core/src/androidTest/java/androidx/slice/SliceTestProvider.java
+++ slices/core/src/androidTest/java/androidx/slice/SliceTestProvider.java
@@ -24,6 +24,10 @@ import static android.app.slice.Slice.HINT_TITLE;
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.net.Uri;
+import android.text.SpannableStringBuilder;
+import android.text.Spanned;
+import android.text.SpannedString;
+import android.text.style.AbsoluteSizeSpan;
 
 import androidx.core.graphics.drawable.IconCompat;
 import androidx.slice.Slice.Builder;
@@ -32,7 +36,6 @@ import androidx.test.filters.SdkSuppress;
 
 @SdkSuppress(minSdkVersion = 19)
 public class SliceTestProvider extends androidx.slice.SliceProvider {
-
     @Override
     public boolean onCreateSliceProvider() {
         getContext().getPackageName();
@@ -50,6 +53,17 @@ public class SliceTestProvider extends androidx.slice.SliceProvider {
                 return b.addSubSlice(new Slice.Builder(b).build(), "subslice").build();
             case "/text":
                 return new Slice.Builder(sliceUri).addText("Expected text", "text").build();
+            case "/prohibited_span": {
+                SpannableStringBuilder spannableStringBuilder = new SpannableStringBuilder();
+                spannableStringBuilder.append("Expected text");
+                spannableStringBuilder.setSpan(
+                        new AbsoluteSizeSpan(1000),
+                        0, spannableStringBuilder.length(),
+                        Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+                return new Slice.Builder(sliceUri)
+                        .addText(new SpannedString(spannableStringBuilder), "text")
+                        .build();
+            }
             case "/icon":
                 return new Slice.Builder(sliceUri).addIcon(
                         IconCompat.createWithResource(getContext(), R.drawable.size_48x48),
