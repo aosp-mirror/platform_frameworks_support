0c1a60b01be8e0e3b4b79d09a052437f6ce10d90
Jake Wharton <jakew@google.com>
2018-07-26 00:29:13 -0400

    Remove some abstraction from LocaleListCompat
    LocaleListHelper was half unused and only used in the compat implementation so it was inlined there. The interface duplicated LocaleListCompat API so it was removed and implementations now directly extend LocaleListCompat.
    
    This refactor removes 42 methods from a non-shrunk APK.
    
    Test: ./gradlew :core:cC
    Change-Id: I54dc7a90d4d5a18bf56fed125cd12527e53010bd

---
+++ compat/src/main/java/androidx/core/os/LocaleListPlatformImpl.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.core.os;
+
+import android.os.LocaleList;
+
+import androidx.annotation.IntRange;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+
+import java.util.Locale;
+
+@RequiresApi(24)
+class LocaleListPlatformImpl extends LocaleListCompat {
+    private final LocaleList mLocaleList;
+
+    LocaleListPlatformImpl(LocaleList localeList) {
+        mLocaleList = localeList;
+    }
+
+    @Override
+    public Object unwrap() {
+        return mLocaleList;
+    }
+
+    @Override
+    public Locale get(int index) {
+        return mLocaleList.get(index);
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return mLocaleList.isEmpty();
+    }
+
+    @Override
+    @IntRange(from = 0)
+    public int size() {
+        return mLocaleList.size();
+    }
+
+    @Override
+    @IntRange(from = -1)
+    public int indexOf(Locale locale) {
+        return mLocaleList.indexOf(locale);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return mLocaleList.equals(((LocaleListCompat) other).unwrap());
+    }
+
+    @Override
+    public int hashCode() {
+        return mLocaleList.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return mLocaleList.toString();
+    }
+
+    @Override
+    public String toLanguageTags() {
+        return mLocaleList.toLanguageTags();
+    }
+
+    @Nullable
+    @Override
+    public Locale getFirstMatch(String[] supportedLocales) {
+        if (mLocaleList != null) {
+            return mLocaleList.getFirstMatch(supportedLocales);
+        }
+        return null;
+    }
+}
