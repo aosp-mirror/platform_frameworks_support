256c97e34889f6c75d63e321f84d05ee9d41840e
shepshapard <shepshapard@google.com>
2018-07-30 16:13:16 -0700

    Fix bug where NestedScrollView isn't handling nested flings.
    Bug: None.
    Test: None yet.
    
    Change-Id: I01ebc7d297125159f80f488f38a10a9d732c3336

--- compat/src/androidTest/java/androidx/core/widget/NestedScrollViewInteractionTest.java
+++ compat/src/androidTest/java/androidx/core/widget/NestedScrollViewInteractionTest.java
@@ -16,9 +16,10 @@
 
 package androidx.core.widget;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.Mockito.doReturn;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.ArgumentMatchers.anyFloat;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
 
 import android.content.Context;
@@ -33,16 +34,17 @@ import androidx.core.view.NestedScrollingChild3;
 import androidx.core.view.NestedScrollingParent3;
 import androidx.core.view.ViewCompat;
 import androidx.test.InstrumentationRegistry;
-import androidx.test.filters.LargeTest;
+import androidx.test.filters.SmallTest;
 import androidx.test.runner.AndroidJUnit4;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InOrder;
 import org.mockito.Mockito;
 
 @RunWith(AndroidJUnit4.class)
-@LargeTest
+@SmallTest
 public class NestedScrollViewInteractionTest {
 
     private NestedScrollingSpyView mParentSpy;
@@ -78,12 +80,6 @@ public class NestedScrollViewInteractionTest {
                 View.MeasureSpec.makeMeasureSpec(1000, View.MeasureSpec.EXACTLY);
         mParentSpy.measure(measureSpecWidth, measureSpecHeight);
         mParentSpy.layout(0, 0, 1000, 1000);
-
-        // Enable nested scrolling
-
-        doReturn(true)
-                .when(mParentSpy)
-                .onStartNestedScroll(any(View.class), any(View.class), anyInt(), anyInt());
     }
 
     @Test
@@ -116,8 +112,60 @@ public class NestedScrollViewInteractionTest {
                 new int[]{0, 0});
     }
 
+    @Test
+    public void uiFingerDown_parentHasNestedScrollingChildWithTypeTouch() {
+        MotionEvent down = MotionEvent.obtain(0, 0, MotionEvent.ACTION_DOWN, 500, 500, 0);
+
+        mNestedScrollView.dispatchTouchEvent(down);
+
+        assertThat(mParentSpy.mHasNestedScrollingChild, is(true));
+        assertThat(mParentSpy.mNestedScrollingType, is(ViewCompat.TYPE_TOUCH));
+    }
+
+    @Test
+    public void uiFingerUp_parentDoesNotHaveNestedScrollingChild() {
+        MotionEvent down = MotionEvent.obtain(0, 0, MotionEvent.ACTION_DOWN, 500, 500, 0);
+        MotionEvent up = MotionEvent.obtain(0, 100, MotionEvent.ACTION_UP, 500, 500, 0);
+        mNestedScrollView.dispatchTouchEvent(down);
+
+        mNestedScrollView.dispatchTouchEvent(up);
+
+        assertThat(mParentSpy.mHasNestedScrollingChild, is(false));
+    }
+
+    @Test
+    public void uiFling_parentHasNestedScrollingChildWithTypeFling() {
+        int[] targetFlingTimeAndDistance = getTargetFlingTimeAndDistance();
+        MotionEvent[] motionEvents = generateMotionEvents(targetFlingTimeAndDistance);
+        runFlingOnNestedScrollView(motionEvents);
+
+        assertThat(mParentSpy.mHasNestedScrollingChild, is(true));
+        assertThat(mParentSpy.mNestedScrollingType, is(ViewCompat.TYPE_NON_TOUCH));
+    }
+
+    @Test
+    public void uiFling_callsNestedFlingsCorrectly() {
+        int[] targetFlingTimeAndDistance = getTargetFlingTimeAndDistance();
+        MotionEvent[] motionEvents = generateMotionEvents(targetFlingTimeAndDistance);
+        runFlingOnNestedScrollView(motionEvents);
+
+        InOrder inOrder = Mockito.inOrder(mParentSpy);
+        inOrder.verify(mParentSpy).onNestedPreFling(
+                eq(mNestedScrollView),
+                eq(0f),
+                anyFloat());
+        inOrder.verify(mParentSpy).onNestedFling(
+                eq(mNestedScrollView),
+                eq(0f),
+                anyFloat(),
+                eq(true));
+    }
+
     public class NestedScrollingSpyView extends FrameLayout implements NestedScrollingChild3,
             NestedScrollingParent3 {
+
+        public boolean mHasNestedScrollingChild;
+        public int mNestedScrollingType;
 
         public NestedScrollingSpyView(Context context) {
             super(context);
@@ -126,7 +174,9 @@ public class NestedScrollViewInteractionTest {
         @Override
         public boolean onStartNestedScroll(@NonNull View child, @NonNull View target, int axes,
                 int type) {
-            return false;
+            mHasNestedScrollingChild = true;
+            mNestedScrollingType = type;
+            return true;
         }
 
         @Override
@@ -137,7 +187,7 @@ public class NestedScrollViewInteractionTest {
 
         @Override
         public void onStopNestedScroll(@NonNull View target, int type) {
-
+            mHasNestedScrollingChild = false;
         }
 
         @Override
@@ -188,6 +238,49 @@ public class NestedScrollViewInteractionTest {
         public void dispatchNestedScroll(int dxConsumed, int dyConsumed, int dxUnconsumed,
                 int dyUnconsumed, @Nullable int[] offsetInWindow, int type,
                 @NonNull int[] consumed) {
+        }
+    }
+
+    private int[] getTargetFlingTimeAndDistance() {
+        ViewConfiguration configuration =
+                ViewConfiguration.get(InstrumentationRegistry.getContext());
+        int touchSlop = configuration.getScaledTouchSlop();
+        int mMinimumVelocity = configuration.getScaledMinimumFlingVelocity();
+        int mMaximumVelocity = configuration.getScaledMaximumFlingVelocity();
+
+        int targetVelocitySeconds = ((mMaximumVelocity - mMinimumVelocity) / 2) + mMinimumVelocity;
+        int targetDistanceTraveled = touchSlop * 2;
+        int targetTimePassed = (targetDistanceTraveled * 1000) / targetVelocitySeconds;
+
+        return new int[]{targetTimePassed, targetDistanceTraveled};
+    }
+
+    private MotionEvent[] generateMotionEvents(int[] targetFlingTimeAndDistance) {
+        int targetTimePassed = targetFlingTimeAndDistance[0];
+        int targetDistanceTraveled = targetFlingTimeAndDistance[1];
+
+        MotionEvent down = MotionEvent.obtain(0, 0, MotionEvent.ACTION_DOWN, 500, 500, 0);
+        MotionEvent move = MotionEvent.obtain(
+                0,
+                targetTimePassed,
+                MotionEvent.ACTION_MOVE,
+                500,
+                500 + targetDistanceTraveled,
+                0);
+        MotionEvent up = MotionEvent.obtain(
+                0,
+                targetTimePassed,
+                MotionEvent.ACTION_UP,
+                500,
+                500 + targetDistanceTraveled,
+                0);
+
+        return new MotionEvent[]{down, move, up};
+    }
+
+    private void runFlingOnNestedScrollView(MotionEvent[] motionEvents) {
+        for (MotionEvent motionEvent : motionEvents) {
+            mNestedScrollView.dispatchTouchEvent(motionEvent);
         }
     }
 }
