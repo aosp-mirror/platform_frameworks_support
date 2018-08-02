0c320c33ba1ad6d4f5d1e016a0ee80d36d337856
Daniel Santiago Rivera <danysantiago@google.com>
2018-08-02 09:27:01 -0700

    Fix back stack inconsistency when detecting back pressed.
    FragmentNavigator's back stack was becoming inconsistent with the
    FragmentManager's back stack due to in-between multiple pending
    operations callbacks occurring that would cause the difference between
    the new count and the final count to be equal and thus causing the
    amount of pending operations not to be decreased.
    
    This changes takes a slightly different strategy, instead of counting
    pending operations, we just flag that we have pending operations and we
    don't consider them done until the FragmentManager's back stack is equal
    to the Navigator's back stack. Consuming back stack callbacks as long
    as there are pending operations so that we don't accidentally detect a
    back press. Since a back press causes a popBackStackImmediate() we have
    confidence pending operations will be executed causing the Navigator to
    reset the flag to later then detect the back press pop.
    
    Bug: 111884848
    Bug: 111907708
    Test: FragmentNavigatorTest, additionally, manually verified fix works
          on both sample projects provided.
    
    Change-Id: I51be4d6a974ecbeeaa1f63d764edabb230ae85e2

--- navigation/fragment/src/main/java/androidx/navigation/fragment/FragmentNavigator.java
+++ navigation/fragment/src/main/java/androidx/navigation/fragment/FragmentNavigator.java
@@ -17,12 +17,12 @@
 package androidx.navigation.fragment;
 
 import android.content.Context;
-import android.content.res.Resources;
 import android.content.res.TypedArray;
 import android.os.Bundle;
 import android.support.annotation.IdRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.annotation.RestrictTo;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
@@ -36,6 +36,7 @@ import androidx.navigation.Navigator;
 import androidx.navigation.NavigatorProvider;
 
 import java.util.ArrayDeque;
+import java.util.Iterator;
 
 /**
  * Navigator that navigates through {@link FragmentTransaction fragment transactions}. Every
@@ -54,26 +55,24 @@ public class FragmentNavigator extends Navigator<FragmentNavigator.Destination> 
     @SuppressWarnings("WeakerAccess") /* synthetic access */
     ArrayDeque<Integer> mBackStack = new ArrayDeque<>();
     @SuppressWarnings("WeakerAccess") /* synthetic access */
-    int mLastBackStackCount = 1;
-    @SuppressWarnings("WeakerAccess") /* synthetic access */
-    int mPendingBackStackOperations = 0;
+    boolean mIsPendingBackStackOperation = false;
 
     private final FragmentManager.OnBackStackChangedListener mOnBackStackChangedListener =
             new FragmentManager.OnBackStackChangedListener() {
 
                 @Override
                 public void onBackStackChanged() {
+                    // If we have pending operations made by us then consume this change. Moreover,
+                    // if our back stack is equal to the fragment manager's back stack then we are
+                    // done waiting for operations.
+                    if (mIsPendingBackStackOperation) {
+                        mIsPendingBackStackOperation = !isBackStackEqual();
+                        return;
+                    }
+
                     // The initial Fragment won't be on the back stack, so the
                     // real count of destinations is the back stack entry count + 1
                     int newCount = mFragmentManager.getBackStackEntryCount() + 1;
-                    if (mPendingBackStackOperations > 0
-                            && newCount <= mLastBackStackCount + mPendingBackStackOperations) {
-                        // This was an expected back stack operation caused by a navigate() call
-                        mPendingBackStackOperations -= newCount - mLastBackStackCount;
-                        mLastBackStackCount = newCount;
-                        return;
-                    }
-                    mLastBackStackCount = newCount;
                     if (newCount < mBackStack.size()) {
                         // Handle cases where the user hit the system back button
                         while (mBackStack.size() > newCount) {
@@ -115,6 +114,7 @@ public class FragmentNavigator extends Navigator<FragmentNavigator.Destination> 
         boolean popped = false;
         if (mFragmentManager.getBackStackEntryCount() > 0) {
             mFragmentManager.popBackStack();
+            mIsPendingBackStackOperation = true;
             popped = true;
         }
         mBackStack.removeLast();
@@ -127,17 +127,6 @@ public class FragmentNavigator extends Navigator<FragmentNavigator.Destination> 
     @Override
     public Destination createDestination() {
         return new Destination(this);
-    }
-
-    @NonNull
-    private String getBackStackName(@IdRes int destinationId) {
-        // This gives us the resource name if it exists,
-        // or just the destinationId if it doesn't exist
-        try {
-            return mContext.getResources().getResourceName(destinationId);
-        } catch (Resources.NotFoundException e) {
-            return Integer.toString(destinationId);
-        }
     }
 
     @SuppressWarnings("deprecation")
@@ -186,13 +175,13 @@ public class FragmentNavigator extends Navigator<FragmentNavigator.Destination> 
                 // remove it from the back stack and put our replacement
                 // on the back stack in its place
                 mFragmentManager.popBackStack();
-                ft.addToBackStack(getBackStackName(destId));
-                mPendingBackStackOperations++;
+                ft.addToBackStack(Integer.toString(destId));
+                mIsPendingBackStackOperation = true;
             }
             backStackEffect = BACK_STACK_UNCHANGED;
         } else {
-            ft.addToBackStack(getBackStackName(destId));
-            mPendingBackStackOperations++;
+            ft.addToBackStack(Integer.toString(destId));
+            mIsPendingBackStackOperation = true;
             backStackEffect = BACK_STACK_DESTINATION_ADDED;
         }
         ft.setReorderingAllowed(true);
@@ -226,9 +215,38 @@ public class FragmentNavigator extends Navigator<FragmentNavigator.Destination> 
                 for (int destId : backStack) {
                     mBackStack.add(destId);
                 }
-                mLastBackStackCount = mBackStack.size();
             }
         }
+    }
+
+    /**
+     * Checks if this FragmentNavigator's back stack is equal to the FragmentManager's back stack.
+     *
+     * @hide
+     */
+    @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
+    @SuppressWarnings("WeakerAccess") /* synthetic access */
+    boolean isBackStackEqual() {
+        int fragmentBackStackCount = mFragmentManager.getBackStackEntryCount();
+        // Initial fragment won't be on the FragmentManager's back stack so +1 its count.
+        if (mBackStack.size() != fragmentBackStackCount + 1) {
+            return false;
+        }
+
+        // From top to bottom verify destination ids match in both back stacks/
+        Iterator<Integer> backStackIterator = mBackStack.descendingIterator();
+        int fragmentBackStackIndex = fragmentBackStackCount - 1;
+        while (backStackIterator.hasNext() && fragmentBackStackIndex >= 0) {
+            int destId = backStackIterator.next();
+            int fragmentDestId = Integer.valueOf(mFragmentManager
+                    .getBackStackEntryAt(fragmentBackStackIndex--)
+                    .getName());
+            if (destId != fragmentDestId) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
     /**
