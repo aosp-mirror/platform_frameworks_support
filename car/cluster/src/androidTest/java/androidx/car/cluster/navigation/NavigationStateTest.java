4b24ea113680110dbf24e83f7b5bbd2d501f139f
Roberto Perez <robertoalexis@google.com>
2018-08-01 19:57:17 -0700

    Initial version of the navigation state API schema.
    Bug: 111891759
    Test: TBD
    Change-Id: I4e240c5d372f0ba594e6c2d014ee3993b97736b4

---
+++ car/cluster/src/androidTest/java/androidx/car/cluster/navigation/NavigationStateTest.java
@@ -0,0 +1,100 @@
+/*
+ * Copyright 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.car.cluster.navigation;
+
+import static org.junit.Assert.assertEquals;
+
+import android.os.Bundle;
+import android.os.Parcel;
+
+import androidx.test.filters.SmallTest;
+import androidx.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Tests for {@link NavigationState} serialization
+ */
+@RunWith(AndroidJUnit4.class)
+@SmallTest
+public class NavigationStateTest {
+    /**
+     * Tests that all the objects in the androidx.car.navigation.schema package are serializable
+     * and deserializable, and all content is maintained.
+     */
+    @Test
+    public void schemaIsSerializableAndDeserializable() {
+        String bundleKey = "DATA";
+        NavigationState state = createSampleState();
+
+        // Set the state into a {@link Bundle}
+        Bundle in = new Bundle();
+        in.putParcelable(bundleKey, state.toParcelable());
+
+        // Serialize it into a {@link Parcel}
+        Parcel parcel = Parcel.obtain();
+        in.writeToParcel(parcel, 0);
+        parcel.setDataPosition(0);
+
+        // Deserialize it back to {@link Bundle}
+        Bundle out = new Bundle();
+        out.setClassLoader(NavigationState.class.getClassLoader());
+        out.readFromParcel(parcel);
+
+        // Recover the state and assert
+        NavigationState result = NavigationState.fromParcelable(out.getParcelable(bundleKey));
+        assertEquals(state, result);
+    }
+
+    private NavigationState createSampleState() {
+        List<Lane> lanes = Arrays.asList(
+                new Lane()
+                        .setHightlightedDirection(
+                                new LaneDirection(LaneDirection.Shape.U_TURN, Side.LEFT))
+                        .setDirections(Arrays.asList(
+                                new LaneDirection(LaneDirection.Shape.NORMAL_TURN,
+                                        Side.LEFT),
+                                new LaneDirection(LaneDirection.Shape.STRAIGHT,
+                                        Side.NO_SIDE))),
+                new Lane()
+                        .setDirections(Arrays.asList(
+                                new LaneDirection(LaneDirection.Shape.STRAIGHT,
+                                        Side.NO_SIDE))));
+        List<Step> steps = Arrays.asList(
+                new Step()
+                        .setManeuver(new Maneuver(Maneuver.Type.DEPART, 0))
+                        .setDistance(new Distance(10, 10000, Distance.Unit.METERS))
+                        .setLanes(lanes),
+                new Step()
+                        .setManeuver(new Maneuver(Maneuver.Type.TURN_NORMAL_LEFT, 0))
+                        .setDistance(new Distance(15, 15000, Distance.Unit.METERS)));
+        List<Destination> destinations = Arrays.asList(
+                new Destination("Home")
+                        .setDistance(new Distance(1230, 1200, Distance.Unit.KILOMETERS))
+        );
+        return new NavigationState()
+                .setStatus(new ProducerStatus(ProducerStatus.Code.OK, ""))
+                .setMode(NavigationState.NavigationMode.TURN_BY_TURN)
+                .setSegment(new Segment("Main St."))
+                .setSteps(steps)
+                .setDestinations(destinations);
+    }
+}
---
+++ car/cluster/src/main/java/androidx/car/cluster/navigation/Destination.java
@@ -0,0 +1,177 @@
+/*
+ * Copyright 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.car.cluster.navigation;
+
+import static androidx.annotation.RestrictTo.Scope.LIBRARY_GROUP;
+
+import android.location.Location;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.RestrictTo;
+import androidx.core.util.Preconditions;
+import androidx.versionedparcelable.ParcelField;
+import androidx.versionedparcelable.VersionedParcelable;
+import androidx.versionedparcelable.VersionedParcelize;
+
+import java.time.ZonedDateTime;
+import java.util.Objects;
+
+/**
+ * Final or intermediate stop in a navigation route.
+ */
+@VersionedParcelize
+public final class Destination implements VersionedParcelable {
+    @ParcelField(1)
+    String mTitle = "";
+    @ParcelField(2)
+    String mAddress = "";
+    @ParcelField(3)
+    Distance mDistance;
+    @ParcelField(4)
+    Time mEta;
+    @ParcelField(5)
+    Location mLocation;
+
+    /**
+     * Used by {@link VersionedParcelable}
+     *
+     * @hide
+     */
+    @RestrictTo(LIBRARY_GROUP)
+    Destination() {
+    }
+
+    /**
+     * Creates a destination
+     *
+     * @param title Name of the destination (formatted for the current user's locale)
+     */
+    public Destination(@NonNull String title) {
+        mTitle = Preconditions.checkNotNull(title);
+    }
+
+    /**
+     * Sets the destination address.
+     *
+     * @see #getAddress()
+     */
+    public Destination setAddress(@NonNull String address) {
+        mAddress = Preconditions.checkNotNull(address);
+        return this;
+    }
+
+    /**
+     * Sets the distance to destination
+     *
+     * @see #getDistance()
+     */
+    public Destination setDistance(@Nullable Distance distance) {
+        mDistance = distance;
+        return this;
+    }
+
+    /**
+     * Sets the estimated time of arrival to destination
+     *
+     * @see #getEta()
+     */
+    public Destination setEta(@Nullable ZonedDateTime eta) {
+        mEta = eta != null ? new Time(eta) : null;
+        return this;
+    }
+
+    /**
+     * Sets the location
+     *
+     * @see #getLocation()
+     */
+    public Destination setLocation(@Nullable Location location) {
+        mLocation = location;
+        return this;
+    }
+
+    /**
+     * Name of the destination (formatted for the current user's locale), or empty if destination
+     * name is unknown.
+     */
+    @NonNull
+    public String getTitle() {
+        return mTitle;
+    }
+
+    /**
+     * Destination address (formatted for the current user's locale), or empty if there is no
+     * address associated with this destination.
+     */
+    @NonNull
+    public String getAddress() {
+        return mAddress;
+    }
+
+    /**
+     * Distance from the current position to this destination, or null if distance is unknown.
+     */
+    @Nullable
+    public Distance getDistance() {
+        return mDistance;
+    }
+
+    /**
+     * Estimated time of arrival to this destination, or null if estimated time of arrival is
+     * unknown.
+     */
+    @Nullable
+    public Time getEta() {
+        return mEta;
+    }
+
+    /**
+     * Geo-location of this destination, or null if location is unknown.
+     */
+    @Nullable
+    public Location getLocation() {
+        return mLocation;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Destination that = (Destination) o;
+        return Objects.equals(mTitle, that.mTitle)
+                && Objects.equals(mAddress, that.mAddress)
+                && Objects.equals(mDistance, that.mDistance)
+                && Objects.equals(mLocation, that.mLocation)
+                && Objects.equals(mEta, that.mEta);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(mTitle, mAddress, mDistance, mLocation, mEta);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("{title: %s, address: %s, distance: %s, location: %s, eta: %s}",
+                mTitle, mAddress, mDistance, mLocation, mEta);
+    }
+}
