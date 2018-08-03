ec43673a6588126cf833f5c7490fbedf5651dc14
Jason Monk <jmonk@google.com>
2018-08-03 19:33:21 -0400

    RemoteCallback Proposal
    Proposal for new way to handle PendingIntents, see README.md and sample
    code, then move on to javadocs and IMPL.md.
    
    Test: :remotecallback:connectedCheck
    Change-Id: I740c0122bc8798cb51f0b3620254b5fa2fe36e5c

---
+++ remotecallback/src/main/java/androidx/remotecallback/AppWidgetProviderWithCallbacks.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package androidx.remotecallback;
+
+import static androidx.remotecallback.BroadcastReceiverWithCallbacks.ACTION_BROADCAST_CALLBACK;
+import static androidx.remotecallback.RemoteCallback.TYPE_RECEIVER;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+
+/**
+ * Version of {@link AppWidgetProvider} that implements a {@link CallbackReceiver}.
+ *
+ * @param <T> Should be specified as the root class (e.g. class X extends
+ *           AppWidgetProviderWithCallbacks\<X>)
+ */
+public class AppWidgetProviderWithCallbacks<T extends CallbackReceiver> extends
+        AppWidgetProvider implements CallbackReceiver<T> {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (ACTION_BROADCAST_CALLBACK.equals(intent.getAction())) {
+            CallbackHandlerRegistry.sInstance.ensureInitialized(getClass());
+
+            CallbackHandlerRegistry.sInstance.invokeCallback(context, this, intent);
+        } else {
+            super.onReceive(context, intent);
+        }
+    }
+
+    @Override
+    public T createRemoteCallback() {
+        CallbackHandlerRegistry.sInstance.ensureInitialized(getClass());
+        return CallbackHandlerRegistry.sInstance.getAndResetStub(getClass());
+    }
+
+    @Override
+    public PendingIntent toPendingIntent(Context context) {
+        return toRemoteCallback(context).toPendingIntent();
+    }
+
+    @Override
+    public RemoteCallback toRemoteCallback(Context context) {
+        if (!(this instanceof CallbackHandlerRegistry.RemoteCallStub)) {
+            throw new IllegalStateException(
+                    "toRemoteCallback() can only be called in a createRemoteCallback() chain.");
+        }
+        Intent intent = new Intent(ACTION_BROADCAST_CALLBACK);
+        // Superclass because this is being executed in the generated stub.
+        String clsName = getClass().getSuperclass().getName();
+        intent.setComponent(new ComponentName(context.getPackageName(), clsName));
+        Bundle args = CallbackHandlerRegistry.sInstance.getArgs(
+                (CallbackHandlerRegistry.RemoteCallStub) this);
+        intent.putExtras(args);
+        return new RemoteCallback(context, TYPE_RECEIVER, intent, clsName, args);
+    }
+}
---
+++ remotecallback/src/main/java/androidx/remotecallback/CallbackHandlerRegistry.java
@@ -0,0 +1,210 @@
+/*
+ * Copyright 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.remotecallback;
+
+import static androidx.remotecallback.RemoteCallback.EXTRA_METHOD;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.collection.ArrayMap;
+
+import java.lang.reflect.InvocationTargetException;
+
+/**
+ * The holder for callbacks that are tagged with {@link RemoteCallable}.
+ * Note: This should only be referenced by generated code, there is no reason to reference this
+ * otherwise.
+ */
+public class CallbackHandlerRegistry {
+    static final CallbackHandlerRegistry sInstance = new CallbackHandlerRegistry();
+    private static final String TAG = "CallbackHandlerRegistry";
+
+    private final ArrayMap<Class<? extends CallbackReceiver>, ClsHandler> mClsLookup =
+            new ArrayMap<>();
+
+    <T extends CallbackReceiver> Bundle getArgs(RemoteCallStub stub) {
+        Bundle b = stub.getArguments();
+        b.putString(EXTRA_METHOD, stub.getMethod());
+        return b;
+    }
+
+    <T extends CallbackReceiver> T getAndResetStub(Class<? extends CallbackReceiver> cls) {
+        RemoteCallStub stub = findStub(cls);
+        stub.reset();
+        return (T) stub;
+    }
+
+    <T extends CallbackReceiver> void ensureInitialized(Class<T> cls) {
+        if (!mClsLookup.containsKey(cls)) {
+            runInit(cls);
+        }
+    }
+
+    /**
+     * Trigger a call to a callback using arguments that were generated with
+     * {@link RemoteCallback#getArgumentBundle()}.
+     */
+    public <T extends CallbackReceiver> void invokeCallback(Context context, T receiver,
+                Intent intent) {
+        invokeCallback(context, receiver, intent.getExtras());
+    }
+
+    /**
+     * Trigger a call to a callback using arguments that were generated with
+     * {@link RemoteCallback#getArgumentBundle()}.
+     */
+    public <T extends CallbackReceiver> void invokeCallback(Context context, T receiver,
+                Bundle bundle) {
+        ArrayMap<String, CallbackHandler<? extends CallbackReceiver>> map =
+                findMap(receiver.getClass());
+        if (map == null) {
+            Log.e(TAG, "No map found for " + receiver.getClass().getName());
+            return;
+        }
+        String method = bundle.getString(EXTRA_METHOD);
+        @SuppressWarnings("unchecked")
+        CallbackHandler<T> callbackHandler = (CallbackHandler<T>) map.get(method);
+        if (callbackHandler == null) {
+            Log.e(TAG, "No handler found for " + method + " on " + receiver.getClass().getName());
+            return;
+        }
+        callbackHandler.executeCallback(context, receiver, bundle);
+    }
+
+    private ArrayMap<String, CallbackHandler<? extends CallbackReceiver>> findMap(Class<?> aClass) {
+        ClsHandler map = mClsLookup.get(aClass);
+        if (map != null) {
+            return map.mHandlers;
+        }
+        if (aClass.getSuperclass() != null) {
+            return findMap(aClass.getSuperclass());
+        }
+        return null;
+    }
+
+    private RemoteCallStub findStub(Class<?> aClass) {
+        ClsHandler map = mClsLookup.get(aClass);
+        if (map != null) {
+            return map.mCallStub;
+        }
+        if (aClass.getSuperclass() != null) {
+            return findStub(aClass.getSuperclass());
+        }
+        return null;
+    }
+
+    private <T extends CallbackReceiver> void runInit(Class<T> cls) {
+        try {
+            // This is the only bit of reflection/keeping that needs to exist, one init class
+            // per callback receiver.
+            ClsHandler clsHandler = new ClsHandler();
+            mClsLookup.put(cls, clsHandler);
+            clsHandler.mCallStub =
+                    (RemoteCallStub) findInitClass(cls).getDeclaredConstructor().newInstance();
+        } catch (InstantiationException e) {
+            Log.e(TAG, "Unable to initialize " + cls.getName(), e);
+        } catch (IllegalAccessException e) {
+            Log.e(TAG, "Unable to initialize " + cls.getName(), e);
+        } catch (InvocationTargetException e) {
+            Log.e(TAG, "Unable to initialize " + cls.getName(), e);
+        } catch (NoSuchMethodException e) {
+            Log.e(TAG, "Unable to initialize " + cls.getName(), e);
+        } catch (ClassNotFoundException e) {
+            Log.e(TAG, "Unable to initialize " + cls.getName(), e);
+        }
+    }
+
+    private <T extends CallbackReceiver> void registerHandler(Class<T> cls, String method,
+            CallbackHandler<T> handler) {
+        ClsHandler map = mClsLookup.get(cls);
+        if (map == null) {
+            throw new IllegalStateException("registerHandler called before init was run");
+        }
+        map.mHandlers.put(method, handler);
+    }
+
+    @SuppressWarnings("unchecked")
+    private static Class<? extends Runnable> findInitClass(Class<? extends CallbackReceiver> cls)
+            throws ClassNotFoundException {
+        String pkg = cls.getPackage().getName();
+        String c = String.format("%s.%sInitializer", pkg, cls.getSimpleName());
+        return (Class<? extends Runnable>) Class.forName(c, false, cls.getClassLoader());
+    }
+
+    /**
+     * Registers a callback handler to be executed when a given PendingIntent is fired
+     * for a {@link RemoteCallback}.
+     * Note: This should only be called by generated code, there is no reason to reference this
+     * otherwise.
+     */
+    public static <T extends CallbackReceiver> void registerCallbackHandler(Class<T> cls,
+            String method, CallbackHandler<T> handler) {
+        sInstance.registerHandler(cls, method, handler);
+    }
+
+    private static class ClsHandler {
+        final ArrayMap<String, CallbackHandler<? extends CallbackReceiver>> mHandlers =
+                new ArrayMap<>();
+        RemoteCallStub mCallStub;
+    }
+
+    /**
+     * The interface used to trigger a callback when the pending intent is fired.
+     * Note: This should only be referenced by generated code, there is no reason to reference
+     * this otherwise.
+     * @param <T> The receiver type for this callback handler.
+     */
+    public interface CallbackHandler<T extends CallbackReceiver> {
+        /**
+         * Executes a callback given a Bundle of aurgements.
+         * Note: This should only be called by generated code, there is no reason to reference this
+         * otherwise.
+         */
+        void executeCallback(Context context, T receiver, Bundle arguments);
+    }
+
+    /**
+     * Interface implemented by generated RemoteCallable stubs.
+     * Note: This should only be referenced by generated code, there is no reason to reference
+     * this otherwise.
+     */
+    public interface RemoteCallStub {
+        /**
+         * Reset the state for a new remote call.
+         * Note: This should only be referenced by generated code, there is no reason to reference
+         * this otherwise.
+         */
+        void reset();
+
+        /**
+         * Get the method that was called.
+         * Note: This should only be referenced by generated code, there is no reason to reference
+         * this otherwise.
+         */
+        String getMethod();
+
+        /**
+         * Get the arguments that were called with the method.
+         * Note: This should only be referenced by generated code, there is no reason to reference
+         * this otherwise.
+         */
+        Bundle getArguments();
+    }
+}
