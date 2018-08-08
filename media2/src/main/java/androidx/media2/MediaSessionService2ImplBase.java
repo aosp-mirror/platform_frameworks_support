3feac92606ef6d76bbb21a0cf573f5dd9fc14ca1
Jaewan Kim <jaewan@google.com>
2018-08-08 14:51:48 +0900

    MediaSessionService2: Allows to add multiple MediaSession2
    Bug: 109837955
    Test: Run all media, media2, media-widget tests.
    Change-Id: If989567c7462cf926455fcfcb4fc2350dc4fd93d

--- media2/src/main/java/androidx/media2/MediaSessionService2ImplBase.java
+++ media2/src/main/java/androidx/media2/MediaSessionService2ImplBase.java
@@ -18,14 +18,23 @@ package androidx.media2;
 
 import android.app.Notification;
 import android.content.Intent;
+import android.os.Binder;
 import android.os.IBinder;
+import android.os.RemoteException;
 import android.util.Log;
 
 import androidx.annotation.GuardedBy;
+import androidx.collection.ArrayMap;
 import androidx.core.app.NotificationManagerCompat;
 import androidx.media.MediaBrowserServiceCompat;
 import androidx.media2.MediaSessionService2.MediaNotification;
 import androidx.media2.MediaSessionService2.MediaSessionService2Impl;
+
+import java.lang.ref.WeakReference;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.Executor;
 
 /**
  * Implementation of {@link MediaSessionService2}.
@@ -36,76 +45,245 @@ class MediaSessionService2ImplBase implements MediaSessionService2Impl {
 
     private final Object mLock = new Object();
     @GuardedBy("mLock")
-    private MediaSession2 mSession;
+    MediaSessionService2Stub mStub;
+    @GuardedBy("mLock")
+    MediaSessionService2 mInstance;
+    @GuardedBy("mLock")
+    MainHandlerExecutor mExecutor;
+    @GuardedBy("mLock")
+    private Map<String, MediaSession2> mSessions = new ArrayMap<>();
 
     MediaSessionService2ImplBase() {
     }
 
     @Override
-    public void onCreate(final MediaSessionService2 service) {
-        MediaSession2 session;
+    public void onCreate(MediaSessionService2 service) {
         synchronized (mLock) {
-            session = mSession;
+            mInstance = service;
+            mStub = new MediaSessionService2Stub(this);
+            mExecutor = new MainHandlerExecutor(service);
         }
-        if (session == null) {
-            session = service.onCreateSession();
-            synchronized (mLock) {
-                mSession = session;
-            }
-        }
-
-        session.getCallback().setOnHandleForegroundServiceListener(
-                new MediaSession2.SessionCallback.OnHandleForegroundServiceListener() {
-                    @Override
-                    public void onHandleForegroundService(int state) {
-                        if (state == MediaPlayerConnector.PLAYER_STATE_IDLE
-                                || state == MediaPlayerConnector.PLAYER_STATE_ERROR) {
-                            service.stopForeground(false /* removeNotification */);
-                            return;
-                        }
-
-                        // state is PLAYER_STATE_PLAYING or PLAYER_STATE_PAUSE.
-                        MediaNotification mediaNotification = service.onUpdateNotification();
-                        if (mediaNotification == null) {
-                            return;
-                        }
-
-                        int notificationId = mediaNotification.getNotificationId();
-                        Notification notification = mediaNotification.getNotification();
-
-                        NotificationManagerCompat manager = NotificationManagerCompat.from(service);
-                        manager.notify(notificationId, notification);
-                        service.startForeground(notificationId, notification);
-                    }
-                });
     }
 
     @Override
     public IBinder onBind(Intent intent) {
-        final MediaSession2 session = getSession();
-        if (session == null) {
-            Log.w(TAG, "Session hasn't created");
+        final MediaSessionService2 service = mInstance;
+        if (service == null) {
+            Log.w(TAG, "Service hasn't created before onBind()");
             return null;
         }
         switch (intent.getAction()) {
-            case MediaSessionService2.SERVICE_INTERFACE:
-                return session.getSessionBinder();
-            case MediaBrowserServiceCompat.SERVICE_INTERFACE:
+            case MediaSessionService2.SERVICE_INTERFACE: {
+                final MediaSessionService2Stub stub = mStub;
+                if (stub == null) {
+                    return null;
+                }
+                return stub.asBinder();
+            }
+            case MediaBrowserServiceCompat.SERVICE_INTERFACE: {
+                final MediaSession2 session = service.onGetSession();
+                addSession(session);
+                // Return a specific session's legacy binder although the Android framework caches
+                // the returned binder here and next binding request may reuse cached binder even
+                // after the session is closed.
+                // Disclaimer: Although MediaBrowserCompat can only get the session that initially
+                // set, it doesn't make things bad. Such limitation had been there between
+                // MediaBrowserCompat and MediaBrowserServiceCompat.
                 return session.getLegacyBrowerServiceBinder();
+            }
         }
         return null;
     }
 
     @Override
-    public MediaNotification onUpdateNotification() {
+    public void onDestroy() {
+        synchronized (mLock) {
+            if (mExecutor != null) {
+                mExecutor.clear();
+                mExecutor = null;
+            }
+            mInstance = null;
+            if (mStub != null) {
+                mStub.close();
+                mStub = null;
+            }
+        }
+    }
+
+    @Override
+    public void addSession(final MediaSession2 session) {
+        if (session == null) {
+            throw new IllegalArgumentException("session shouldn't be null");
+        }
+        final MediaSession2 old;
+        synchronized (mLock) {
+            old = mSessions.get(session.getId());
+            if (old != null && old != session) {
+                // TODO(b/112114183): Also check the uniqueness before sessions're returned by
+                //                    onGetSession.
+                throw new IllegalArgumentException("Session ID should be unique.");
+            }
+            mSessions.put(session.getId(), session);
+        }
+        if (old == null) {
+            // TODO: Check whether the session is registered in multiple sessions.
+            session.getCallback().setForegroundServiceEventCallback(
+                    new MediaSession2.SessionCallback.ForegroundServiceEventCallback() {
+                        @Override
+                        public void onPlayerStateChanged(int state) {
+                            final MediaSessionService2 service = mInstance;
+                            if (service == null) {
+                                return;
+                            }
+                            if (isPlaybackStopped(state)) {
+                                List<MediaSession2> sessions = getSessions();
+                                boolean areAllStopped = true;
+                                for (int i = 0; i < sessions.size(); i++) {
+                                    MediaSession2 session = sessions.get(i);
+                                    if (!isPlaybackStopped(session.getPlayerState())) {
+                                        areAllStopped = false;
+                                        break;
+                                    }
+                                }
+                                if (areAllStopped) {
+                                    service.stopForeground(false /* removeNotification */);
+                                }
+                                return;
+                            }
+                            // state is PLAYER_STATE_PLAYING or PLAYER_STATE_PAUSE.
+                            MediaNotification mediaNotification =
+                                    service.onUpdateNotification(session);
+                            if (mediaNotification == null) {
+                                return;
+                            }
+
+                            int notificationId = mediaNotification.getNotificationId();
+                            Notification notification = mediaNotification.getNotification();
+
+                            NotificationManagerCompat manager =
+                                    NotificationManagerCompat.from(service);
+                            manager.notify(notificationId, notification);
+                            service.startForeground(notificationId, notification);
+                        }
+
+                        @Override
+                        public void onSessionClosed() {
+                            removeSession(session);
+                        }
+                    });
+        }
+    }
+
+    @SuppressWarnings("WeakerAccess") /* synthetic access */
+    static boolean isPlaybackStopped(int state) {
+        return state == MediaPlayerConnector.PLAYER_STATE_IDLE
+                || state == MediaPlayerConnector.PLAYER_STATE_ERROR;
+    }
+
+    @Override
+    public void removeSession(MediaSession2 session) {
+        if (session == null) {
+            throw new IllegalArgumentException("session shouldn't be null");
+        }
+        synchronized (mLock) {
+            mSessions.remove(session.getId());
+        }
+    }
+
+    @Override
+    public MediaNotification onUpdateNotification(MediaSession2 session) {
         // May supply default implementation later
         return null;
     }
 
     @Override
-    public MediaSession2 getSession() {
+    public List<MediaSession2> getSessions() {
+        List<MediaSession2> list = new ArrayList<>();
         synchronized (mLock) {
-            return mSession;
+            list.addAll(mSessions.values());
+        }
+        return list;
+    }
+
+    private static final class MediaSessionService2Stub extends IMediaSessionService2.Stub
+            implements AutoCloseable {
+        private final WeakReference<MediaSessionService2ImplBase> mServiceImpl;
+
+        MediaSessionService2Stub(final MediaSessionService2ImplBase serviceImpl) {
+            mServiceImpl = new WeakReference<>(serviceImpl);
+        }
+
+        @Override
+        public void connect(final IMediaController2 caller, final String packageName) {
+            final MediaSessionService2ImplBase serviceImpl = mServiceImpl.get();
+            if (serviceImpl == null) {
+                if (DEBUG) {
+                    Log.d(TAG, "ServiceImpl isn't available");
+                }
+                return;
+            }
+            final Executor executor = serviceImpl.mExecutor;
+            if (executor == null) {
+                if (DEBUG) {
+                    Log.d(TAG, "ServiceImpl.mExecutor isn't available");
+                }
+                return;
+            }
+            final int pid = Binder.getCallingPid();
+            final int uid = Binder.getCallingUid();
+            executor.execute(new Runnable() {
+                @Override
+                public void run() {
+                    boolean shouldNotifyDisconnected = true;
+                    try {
+                        final MediaSessionService2ImplBase serviceImpl = mServiceImpl.get();
+                        if (serviceImpl == null) {
+                            if (DEBUG) {
+                                Log.d(TAG, "ServiceImpl isn't available");
+                            }
+                            return;
+                        }
+                        final MediaSessionService2 service = serviceImpl.mInstance;
+                        if (service == null) {
+                            if (DEBUG) {
+                                Log.d(TAG, "Service isn't available");
+                            }
+                            return;
+                        }
+                        final MediaSession2 session;
+                        try {
+                            session = service.onGetSession();
+                            service.addSession(session);
+                            shouldNotifyDisconnected = false;
+
+                            session.handleControllerConnectionFromService(caller, packageName,
+                                    pid, uid);
+                        } catch (Exception e) {
+                            // Don't propagate exception in service to the controller.
+                            Log.w(TAG, "Failed to add a session to session service", e);
+                        }
+                    } finally {
+                        // Trick to call onDisconnected() in one place.
+                        if (shouldNotifyDisconnected) {
+                            if (DEBUG) {
+                                Log.d(TAG, "Service has destroyed prematurely."
+                                        + " Rejecting connection");
+                            }
+                            try {
+                                caller.onDisconnected();
+                            } catch (RemoteException e) {
+                                // Controller may be died prematurely.
+                                // Not an issue because we'll ignore it anyway.
+                            }
+                        }
+                    }
+                }
+            });
+        }
+
+        @Override
+        public void close() {
+            mServiceImpl.clear();
         }
     }
 }
