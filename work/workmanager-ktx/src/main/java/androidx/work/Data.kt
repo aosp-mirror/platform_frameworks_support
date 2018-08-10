a625bc5c6e170432d3050fcb1817ba159198a9e9
Rahul Ravikumar <rahulrav@google.com>
2018-08-10 11:35:52 -0700

    Add workDataOf() factory function which produces a Data.
    * Deprecated the extension function `Map.toWorkData()`
    * Ran `./gradlew updateApi`
    
    Test: Added unit test for factory function.
    Change-Id: I3768a63d63f4b6b23cf2a66ede2cda42d6903b27
    Fixes: b/112423628

--- work/workmanager-ktx/src/main/java/androidx/work/Data.kt
+++ work/workmanager-ktx/src/main/java/androidx/work/Data.kt
@@ -22,8 +22,24 @@ package androidx.work
 /**
  * Converts a [Map] to a [Data] object.
  */
+@Deprecated("Use workDataOf() instead.")
 inline fun <V> Map<String, V>.toWorkData(): Data {
     val dataBuilder = Data.Builder()
     dataBuilder.putAll(this)
+    return dataBuilder.build()
+}
+
+/**
+ * Converts a list of pairs to a [Data] object.
+ *
+ * If multiple pairs have the same key, the resulting map will contain the value
+ * from the last of those pairs.
+ *
+ * Entries of the map are iterated in the order they were specified.
+ */
+inline fun workDataOf(vararg pairs: Pair<String, Any>): Data {
+    val valueMap = mutableMapOf(pairs = *pairs)
+    val dataBuilder = Data.Builder()
+    dataBuilder.putAll(valueMap)
     return dataBuilder.build()
 }
