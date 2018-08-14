1800e10f759af25f5ff786822933a5a80ae5fd9e
Yao, Yuxing <yaoyx@google.com>
2018-08-14 15:59:56 -0700

    Support CharSequence in subheader and seekbar list item.
    Bug: 112601467
    Test: ./gradlew :car:connectedCheck
    Change-Id: I240f870952a13133c02c527e447d738213585658

--- car/core/src/main/java/androidx/car/widget/SubheaderListItem.java
+++ car/core/src/main/java/androidx/car/widget/SubheaderListItem.java
@@ -20,6 +20,7 @@ import static java.lang.annotation.RetentionPolicy.SOURCE;
 
 import android.car.drivingstate.CarUxRestrictions;
 import android.content.Context;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.TextView;
 
@@ -53,14 +54,12 @@ public class SubheaderListItem extends ListItem<SubheaderListItem.ViewHolder> {
 
     private final List<ViewBinder<ViewHolder>> mBinders = new ArrayList<>();
 
-    private String mText;
+    private CharSequence mText;
 
-    public SubheaderListItem(Context context, String text) {
-
+    public SubheaderListItem(@NonNull Context context, @NonNull CharSequence text) {
         mContext = context;
         mText = text;
         mTextStartMarginType = TEXT_START_MARGIN_TYPE_NONE;
-
         markDirty();
     }
 
@@ -97,7 +96,10 @@ public class SubheaderListItem extends ListItem<SubheaderListItem.ViewHolder> {
     /**
      * Sets the text to be displayed.
      */
-    public void setText(String text) {
+    public void setText(@NonNull CharSequence text) {
+        if (TextUtils.isEmpty(text)) {
+            throw new IllegalArgumentException("Text for subheader cannot be null.");
+        }
         mText = text;
         markDirty();
     }
