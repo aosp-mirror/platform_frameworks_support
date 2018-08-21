472ec6123e20bb096b32155ddc521a22a7f69eb0
Doris Liu <tianliu@google.com>
2018-08-21 10:57:11 -0700

    New animation test rule and tests
    The new test rule allows developers to manually advance the animation
    clock.
    
    Tests are added for ObjectAnimator and AnimatorSet
    
    Also improved the path float array generation for API < 26 to reduce
    the number of entries for when a path contains lines.
    
    BUG:112591808
    Test: this CL
    
    Change-Id: Iac0eb4580efbd814cc82c264225e2261a91cda36

---
+++ animation/testing/src/main/java/androidx/animation/AnimationTestRule.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.animation;
+
+import android.os.Looper;
+
+import org.junit.rules.TestRule;
+import org.junit.runner.Description;
+import org.junit.runners.model.Statement;
+
+/**
+ * TODO: Mention caveat: 1) setFrameDelay/getFrameDelay will not be sensible.
+ * 2) advanceTimeBy/getCurrentTime needs to be called on the same thread as where anims are started
+ *
+ */
+public class AnimationTestRule implements TestRule {
+    private final TestHandler mTestHandler = new TestHandler();
+    @Override
+    public Statement apply(final Statement base, Description description) {
+        return new Statement() {
+            @Override
+            public void evaluate() throws Throwable {
+                AnimationHandler.setTestHandler(mTestHandler);
+                try {
+                    base.evaluate();
+                } finally {
+                    AnimationHandler.setTestHandler(null);
+                }
+            }
+        };
+    }
+    /**
+     * This needs to be called on the same thread as start().
+     * @param timeDelta
+     */
+    public void advanceTimeBy(long timeDelta) {
+        if (Looper.myLooper() == null) {
+            // Throw an exception
+        }
+        mTestHandler.advanceTimeBy(timeDelta);
+    }
+    /**
+     * This needs to be called on the same thread as start().
+     */
+    public long getCurrentTime() {
+        if (Looper.myLooper() == null) {
+            // Throw an exception
+        }
+        return mTestHandler.getCurrentTime();
+    }
+}
+
