266f62970b16a7091214ab1df0b642d22e8ae061
shepshapard <shepshapard@google.com>
2018-08-22 15:50:58 -0700

    Implement NestedScrollingParent3 in CoordinatorLayout
    This includes adding a new CoordinatorLayout.Behavior method
    to allow Behaviors to report back how much scroll distance
    they consumed during the onNestedScroll pass.
    
    This change is non-breaking and backwards compatable as well.
    
    Bug: 69326291
    Bug: 65129787
    Bug: 67345200
    Test: androidx.coordinatorlayout.widget.CoordinatorLayoutNestedScrollingParentCompatTest
          androidx.coordinatorlayout.widget.CoordinatorLayoutNestedScrollingParentTest
    
    Change-Id: I690b607dc5066dbb097b000123a7ef237fee4000

--- buildSrc/src/main/kotlin/androidx/build/LibraryVersions.kt
+++ buildSrc/src/main/kotlin/androidx/build/LibraryVersions.kt
@@ -195,4 +195,9 @@ object LibraryVersions {
      * Version code for RecyclerView
      */
     val RECYCLERVIEW = Version("1.1.0-alpha01")
+
+    /**
+     * Version code for RecyclerView
+     */
+    val COORDINATORLAYOUT = Version("1.1.0-alpha01")
 }
