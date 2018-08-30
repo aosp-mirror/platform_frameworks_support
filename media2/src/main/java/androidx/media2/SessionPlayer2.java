aaca5145f7f93dda94b6752bd877fbffcd12e0ff
Jaewan Kim <jaewan@google.com>
2018-08-30 02:13:28 -0700

    MediaSession2: Initial commit of new MediaItem2
    Previoulsy MediaItem2 had contained previous DataSourceDesc2,
    and new MediaItem2 would  DataSourceDesc2.
    
    In this CL, the MediaItemDesc2 is temporarily added because
      - We don't want to touch public API signatures until all
        SessionPlayer2 related APIs are unhidden, but
        VersionedParcelable seemed to have issues with inheritance.
      - New MediaItem2's Builder needs to be generic class and name
        conflicts with existing one.
    
    Bug: 113375123
    Test: ./gradlew media2:check
    Change-Id: I1c6f67def7c12e34741f032be38f36a28bf5fa49

--- media2/src/main/java/androidx/media2/SessionPlayer2.java
+++ media2/src/main/java/androidx/media2/SessionPlayer2.java
@@ -211,27 +211,27 @@ public abstract class SessionPlayer2 implements AutoCloseable {
     public abstract float getPlaybackSpeed();
 
     // APIs from the MediaPlaylistAgent
-    public abstract @NonNull ListenableFuture<CommandResult2> setPlaylist(List<MediaItem2> list,
+    public abstract @NonNull ListenableFuture<CommandResult2> setPlaylist(List<MediaItemDesc2> list,
             MediaMetadata2 metadata);
 
-    public abstract @NonNull ListenableFuture<CommandResult2> setPlayItemDesc(MediaItem2 desc,
+    public abstract @NonNull ListenableFuture<CommandResult2> setPlayItemDesc(MediaItemDesc2 desc,
             MediaMetadata2 metadata);
 
     public abstract @NonNull ListenableFuture<CommandResult2> addPlaylistItem(int index,
-            MediaItem2 desc);
+            MediaItemDesc2 desc);
 
     public abstract @NonNull ListenableFuture<CommandResult2> removePlaylistItem(
-            MediaItem2 desc);
+            MediaItemDesc2 desc);
 
     public abstract @NonNull ListenableFuture<CommandResult2> replacePlaylistItem(int index,
-            MediaItem2 desc);
+            MediaItemDesc2 desc);
 
     public abstract @NonNull ListenableFuture<CommandResult2> skipToPreviousItem();
 
     public abstract @NonNull ListenableFuture<CommandResult2> skipToNextItem();
 
     public abstract @NonNull ListenableFuture<CommandResult2> skipToPlaylistItem(
-            @NonNull MediaItem2 desc);
+            @NonNull MediaItemDesc2 desc);
 
     public abstract @NonNull ListenableFuture<CommandResult2> updatePlaylistMetadata(
             @Nullable MediaMetadata2 metadata);
@@ -242,7 +242,7 @@ public abstract class SessionPlayer2 implements AutoCloseable {
     public abstract @NonNull ListenableFuture<CommandResult2> setShuffleMode(
             @ShuffleMode int shuffleMode);
 
-    public abstract @Nullable List<MediaItem2> getPlaylist();
+    public abstract @Nullable List<MediaItemDesc2> getPlaylist();
 
     public abstract @Nullable MediaMetadata2 getPlaylistMetadata();
 
@@ -251,7 +251,7 @@ public abstract class SessionPlayer2 implements AutoCloseable {
     public abstract @ShuffleMode int getShuffleMode();
 
     // APIs previously in the grey area (neither player connector / playlist agent)
-    public abstract @Nullable MediaItem2 getCurrentMediaItem();
+    public abstract @Nullable MediaItemDesc2 getCurrentMediaItem();
 
     // Listeners / Callback related
     // Intentionally final not to allow developers to change the behavior
@@ -313,7 +313,7 @@ public abstract class SessionPlayer2 implements AutoCloseable {
         }
 
         public void onBufferingStateChanged(@NonNull SessionPlayer2 player,
-                @Nullable MediaItem2 desc, @BuffState int buffState) {
+                @Nullable MediaItemDesc2 desc, @BuffState int buffState) {
         }
 
         public void onPlaybackSpeedChanged(@NonNull SessionPlayer2 player,
@@ -324,7 +324,7 @@ public abstract class SessionPlayer2 implements AutoCloseable {
         }
 
         // Callbacks from MediaPlaylistAgent
-        public void onPlaylistChanged(@NonNull SessionPlayer2 player, List<MediaItem2> list,
+        public void onPlaylistChanged(@NonNull SessionPlayer2 player, List<MediaItemDesc2> list,
                 @Nullable MediaMetadata2 metadata) {
         }
 
