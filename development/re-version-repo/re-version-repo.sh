7720fd7f8e2851d225c8aed93fccbce06ef5a2be
Jeff Gaston <jeffrygaston@google.com>
2018-09-04 19:14:14 -0400

    Script that takes a .zip of a Maven repo and rewrites the versions inside it to a new version
    Bug: 113873226
    
    Test: ./re-version-repo.sh ~/jetified-mdc-1.0.0-rc1.zip 1.0.0-rc01 1.0.0-rc02
    
    Change-Id: Idfcb58b91b280c0dca32791cbc73bd6333b2ba6f

---
+++ development/re-version-repo/re-version-repo.sh
@@ -0,0 +1,90 @@
+set -e
+
+# parse args
+inputFile="$1"
+oldVersion="$2"
+newVersion="$3"
+
+function usage() {
+  echo "re-version-repo.sh takes a .zip of a Maven repo and rewrites the versions inside it to a new version
+
+Usage: re-version-repo.sh <input-zip> <old-version> <new-version>
+"
+  exit 1
+}
+
+if [ "${inputFile}" == "" ]; then
+  usage
+fi
+if [ "${oldVersion}" == "" ]; then
+  usage
+fi
+if [ "${newVersion}" == "" ]; then
+  usage
+fi
+
+# setup
+if stat "${inputFile}" > /dev/null 2>/dev/null; then
+  echo
+else
+  echo "Input file ${inputFile} does not exist"
+  exit 1
+fi
+inputFile="$(readlink -f ${inputFile})"
+
+if echo "${inputFile}" | grep -v "${oldVersion}" >/dev/null; then
+  echo "The text '${oldVersion}' does not appear in the name of the file, ${inputFile}."
+  echo "This is required as a sanity check (and also facilitates computing the output file name)"
+  exit 1
+fi
+outputFile="$(echo ${inputFile} | sed "s/${oldVersion}/${newVersion}/g")"
+
+tempDir="/tmp/repo"
+rm "${tempDir}" -rf
+mkdir -p "${tempDir}"
+cd "${tempDir}"
+
+# unzip dir
+echo
+echo unzipping "${inputFile}"
+unzippedDir="${tempDir}/unzipped"
+unzip -q "${inputFile}" -d "${unzippedDir}"
+cd "${unzippedDir}"
+
+# make new dirs for new files
+echo
+oldDirs="$(find -type d)"
+newDirs="$(echo ${oldDirs} | sed "s/${oldVersion}/${newVersion}/g")"
+echo "Making new dirs: ${newDirs}"
+echo "${newDirs}" | xargs --no-run-if-empty mkdir -p
+
+# move every file
+echo
+echo moving files
+oldFiles="$(find -type f)"
+moveCommand=""
+for oldFile in ${oldFiles}; do
+  if echo "${oldFile}" | grep "${oldVersion}">/dev/null; then
+    newFile="$(echo ${oldFile} | sed "s/${oldVersion}/${newVersion}/g")"
+    echo "moving ${oldFile} -> ${newFile}"
+    mv "${oldFile}" "${newFile}"
+  fi
+done
+
+# remove old dirs
+echo
+obsoleteDirs="$(find -type d | grep ${oldVersion} | grep -v ${newVersion} | sort -r)"
+echo "Removing dirs: ${obsoleteDirs}"
+echo "${obsoleteDirs}" | xargs -n 1 --no-run-if-empty rmdir
+
+# rewrite .pom files
+echo
+echo rewriting poms
+find -name "*.pom" | xargs sed -i "s/${oldVersion}/${newVersion}/g"
+
+echo
+echo rezipping
+rm -f "${outputFile}"
+zip -qr "${outputFile}" .
+
+echo "Done transforming ${inputFile} (${oldVersion}) ->  ${outputFile} (${newVersion})"
