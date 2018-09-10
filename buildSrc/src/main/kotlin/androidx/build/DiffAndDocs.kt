ff239e2e06f0f4f3fcf5e35edaebc63ecfa6895e
Oussama Ben Abdelbaki <obenabde@google.com>
2018-09-10 11:24:50 -0400

    Support for testing different dependency versions
    AndroidX libraries should now declare their dependencies as a module
    with a fixed version instead of a local project (e.g
    "androidx.collection:collection:1.0.0" instead of ":collection".) The
    version specified should be the minimum supported version the library
    depends on.
    
    If project A contains any (prebuilt) dependencies of the form
    androidx.B:B:version, then A will be built twice: once depending on
    (the prebuilt) androidx.B:B:version , and once depending on
    (the project) ":B", in an effort to check the compatibility expected
    by the SemVer policy for B.
    
    Test: manual testing.
    Test: testing to ensure that we only substitute fixed version with
    current projects that actually exist and are within the same major
    release.
    
    Bug: 112531649
    Change-Id: Ic3539e2bbf552ebcc4559c34f8724e9659c968f1

--- buildSrc/src/main/kotlin/androidx/build/DiffAndDocs.kt
+++ buildSrc/src/main/kotlin/androidx/build/DiffAndDocs.kt
@@ -285,9 +285,8 @@ object DiffAndDocs {
     ) {
 
         registerPrebuilts(extension)
-
         library.libraryVariants.all { variant ->
-            if (variant.name == "release") {
+            if (variant.name == "minDepVersionsRelease") {
                 // include R.file generated for prebuilts
                 rules.filter { it.resolve(extension)?.strategy is Prebuilts }.forEach { rule ->
                     docsTasks[rule.name]?.include { fileTreeElement ->
