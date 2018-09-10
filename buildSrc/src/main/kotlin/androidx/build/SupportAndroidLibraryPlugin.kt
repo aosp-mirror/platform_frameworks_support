f57628e2795d0de66da91c6e305f70bbf0b6d059
Oussama Ben Abdelbaki <obenabde@google.com>
2018-09-10 11:24:50 -0400

    Support for testing dependency version ranges
    Any androidx library can now declare to be compatible with a range
    of versions of another androidx library via standard Gradle version
    ranges. These ranges may look like:
    
    dependency_group:dependency_name:[x.y.z, a.b.c)
    
    We approximate the testing of this compatibility by creating a task
    that builds the library against the earliest version of its
    dependencies, and another task that builds the library against the
    latest released version of its dependencies.
    
    Test: manual testing with ranges
    Test: setting minimum dependencies to non existant libraries both
    in the build dependencies and in code with the support library
    plugin and getting the expected errors.
    
    Bug: 112531649
    Change-Id: Ic3539e2bbf552ebcc4559c34f8724e9659c968f1

--- buildSrc/src/main/kotlin/androidx/build/SupportAndroidLibraryPlugin.kt
+++ buildSrc/src/main/kotlin/androidx/build/SupportAndroidLibraryPlugin.kt
@@ -21,9 +21,11 @@ import com.android.build.gradle.LibraryExtension
 import com.android.build.gradle.LibraryPlugin
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.artifacts.ConfigurationContainer
 import org.gradle.api.logging.LogLevel
 import org.gradle.api.plugins.JavaBasePlugin
 import org.gradle.kotlin.dsl.apply
+import org.gradle.kotlin.dsl.get
 
 /**
  * Support library specific com.android.library plugin that sets common configurations needed for
@@ -51,7 +53,26 @@ class SupportAndroidLibraryPlugin : Plugin<Project> {
                 DiffAndDocs.registerAndroidProject(project, library, supportLibraryExtension)
             }
 
+            project.configurations.all { configuration ->
+                configuration.resolutionStrategy.eachDependency { dep ->
+                    val target = dep.target
+                    // Enforce inclusive exclusive Androidx dependency version range specification.
+                    if (artifactSupportsSemVer(target.group) && isDependencyRange(target.version) &&
+                            !isSupportedDependencyRange(target.version)) {
+                            throw IllegalArgumentException(
+                                    "Androidx dependency version ranges can only be specified" +
+                                            " in the format [x.y.z, a.b.c) " +
+                                            "(i.e inclusive on the left and exclusive on the" +
+                                            " right. The + operator" +
+                                            " is not yet supported")
+                    }
+                }
+            }
+
             library.libraryVariants.all { libraryVariant ->
+                if (libraryVariant.flavorName.contains("minDepVersions")) {
+                    useMinimumDependencyVersions(project.configurations)
+                }
                 if (libraryVariant.getBuildType().getName().equals("debug")) {
                     @Suppress("DEPRECATION")
                     val javaCompile = libraryVariant.javaCompile
@@ -70,6 +91,14 @@ class SupportAndroidLibraryPlugin : Plugin<Project> {
 
         val library = project.extensions.findByType(LibraryExtension::class.java)
                 ?: throw Exception("Failed to find Android extension")
+
+        library.flavorDimensions("version")
+        library.productFlavors {
+            it.create("minDepVersions")
+            it.get("minDepVersions").dimension = "version"
+            it.create("maxDepVersions")
+            it.get("maxDepVersions").dimension = "version"
+        }
 
         project.configureLint(library.lintOptions, supportLibraryExtension)
     }
@@ -113,4 +142,41 @@ private fun Project.injectCompilationForBenchmarks(
         val options = "/data/local/tmp/$name-debug-androidTest.apk && $inject #"
         extension.adbOptions.setInstallOptions(*options.split(" ").toTypedArray())
     }
+}
+
+/**
+ * Goes through all the dependencies in the passed in configurations and finds each dependency
+ * depending on an androidx library. Then For any that is specified as a version range, overrides
+ * the resolution strategy for this dependency to resolve to the earliest version (i.e if the
+ * version range is [x.y.z, a.b.c) then make the dependency resolve to x.y.z.)
+ */
+private fun useMinimumDependencyVersions(configurations: ConfigurationContainer) {
+    configurations.all { configuration ->
+        configuration.resolutionStrategy.eachDependency { dep ->
+                if (artifactSupportsSemVer(dep.target.group) &&
+                        isSupportedDependencyRange(dep.target.version)) {
+                    // Set dependency version to the first value in the range (i.e the minimum
+                    // dependency, this is a hard set so if the version does not exist the build will
+                    // error)
+                    dep.useVersion(dep.target.version.removePrefix("[")
+                        .split(",")[0])
+                }
+            }
+        }
+    }
+
+private fun artifactSupportsSemVer(artifactGroup: String): Boolean {
+    return artifactGroup.startsWith("androidx.")
+}
+
+private fun isDependencyRange(version: String): Boolean {
+    return ((version.startsWith("[") || version.startsWith("(")) &&
+            (version.endsWith("]") || version.endsWith(")"))) ||
+            version.contains("+")
+}
+
+private fun isSupportedDependencyRange(version: String): Boolean {
+    if (!isDependencyRange(version)) return false
+    if (!version.startsWith("[") || !version.endsWith(")")) return false
+    return true
 }
