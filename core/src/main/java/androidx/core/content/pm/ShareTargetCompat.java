bc0be6bdc5358aa6e8303442880c1800a48f2e9f
Mehdi Alizadeh <mett@google.com>
2018-09-13 14:46:46 -0700

    Xml parsers and serializers
    Test: it builds
    
    Change-Id: I40a9c2b0c4269dcd397464eaf517325b2fbe73bd

---
+++ core/src/main/java/androidx/core/content/pm/ShareTargetCompat.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.core.content.pm;
+
+import android.content.Intent;
+
+import androidx.annotation.NonNull;
+
+class ShareTargetCompat {
+    // TODO: Consider if ShareTarget needs to have multiple dataTypes, intents, categories
+    // TODO: Add getters for members
+    // TODO: Use Builder to create new ShareTargetCompat instances
+
+    public String mDataType;
+    public Intent mIntent;
+    public String mCategory;
+
+    public ShareTargetCompat(@NonNull String dataType,@NonNull Intent intent,@NonNull String category) {
+        mDataType = dataType;
+        mIntent = intent;
+        mCategory = category;
+    }
+
+    public String toString() {
+        return "dataType=" + mDataType + " intent=" + mIntent.toString() + " category=" + mCategory;
+    }
+}
