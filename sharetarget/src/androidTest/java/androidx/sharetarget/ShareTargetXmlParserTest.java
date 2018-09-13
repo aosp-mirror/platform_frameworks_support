15cf5619640949fe313f41bf7d0a2628ed4a6e31
Mehdi Alizadeh <mett@google.com>
2018-09-13 14:47:38 -0700

    Use ShortcutManagerCompat to push direct share targets
    Test: it builds
    
    Change-Id: Ib4dfbec8a511cbda60fbe8047c4241302b69a1c7

--- sharetarget/src/androidTest/java/androidx/sharetarget/ShareTargetXmlParserTest.java
+++ sharetarget/src/androidTest/java/androidx/sharetarget/ShareTargetXmlParserTest.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package androidx.core.content.pm;
+package androidx.sharetarget;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
