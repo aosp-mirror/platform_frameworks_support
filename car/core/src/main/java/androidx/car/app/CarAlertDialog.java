dff1a7fffdc3e7487f75d812522f638a787d92ec
Obadah Diar Bakerli <obadah@google.com>
2018-09-20 11:45:13 -0700

    Added enter/exit animations for the Car Alert Dialog
    Change-Id: Iabea455b4a257146c0bbcff9f72360cb85b4a2c4
    Fixes: 115653850
    Test: Tested on device with sample app

--- car/core/src/main/java/androidx/car/app/CarAlertDialog.java
+++ car/core/src/main/java/androidx/car/app/CarAlertDialog.java
@@ -16,6 +16,7 @@
 
 package androidx.car.app;
 
+import android.animation.ValueAnimator;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.res.Resources;
@@ -28,6 +29,7 @@ import android.view.TouchDelegate;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -80,6 +82,25 @@ public class CarAlertDialog extends Dialog {
         mBottomPadding = res.getDimensionPixelSize(R.dimen.car_padding_4);
         mButtonMinWidth = res.getDimensionPixelSize(R.dimen.car_button_min_width);
         mButtonSpacing = res.getDimensionPixelSize(R.dimen.car_padding_4);
+
+        Window window = this.getWindow();
+
+        window.getAttributes().windowAnimations = R.style.AlertDialog;
+
+        // Background dim animation. Blur
+        TypedValue outValue = new TypedValue();
+        res.getValue(R.dimen.car_dialog_background_dim, outValue, true);
+        float dimAmount = outValue.getFloat();
+
+        ValueAnimator backgroundDimAnimator = ValueAnimator.ofFloat(0f, dimAmount);
+        backgroundDimAnimator.setDuration(res.getInteger(R.integer.car_dialog_enter_duration_ms));
+        WindowManager.LayoutParams lp = window.getAttributes();
+
+        backgroundDimAnimator.addUpdateListener(animation -> {
+            lp.dimAmount = (float) animation.getAnimatedValue();
+            window.setAttributes(lp);
+        });
+        backgroundDimAnimator.start();
     }
 
     @Override
