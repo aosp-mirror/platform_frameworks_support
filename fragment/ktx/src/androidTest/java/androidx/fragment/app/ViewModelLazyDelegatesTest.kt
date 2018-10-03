22557bb65c3afa36253d9f3a0364281796b2aa9a
Sergey Vasilinets <sergeyv@google.com>
2018-10-03 16:45:17 +0100

    Introduce viewModels().
    Now to create vm field, you can simply by:
    val vm: MyViewModel by viewModels()
    
    Test: ViewModelLazyDelegatesTest
    Change-Id: If795009933707c917dcbd51499f3a39056184451

---
+++ fragment/ktx/src/androidTest/java/androidx/fragment/app/ViewModelLazyDelegatesTest.kt
@@ -0,0 +1,50 @@
+package androidx.fragment.app
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.test.annotation.UiThreadTest
+import androidx.test.filters.MediumTest
+import androidx.test.rule.ActivityTestRule
+import com.google.common.truth.Truth.assertThat
+import org.junit.Rule
+import org.junit.Test
+
+@MediumTest
+class ViewModelDelegatesTest {
+    @get:Rule val activityRule = ActivityTestRule<TestVMActivity>(TestVMActivity::class.java)
+
+    @UiThreadTest
+    @Test fun vmInitialization() {
+        val activity = activityRule.activity
+        assertThat(activity.vm).isNotNull()
+        assertThat(activity.factoryVM.prop).isEqualTo("activity")
+        val fragment = TestVMFragment()
+        activity.supportFragmentManager.commitNow { add(fragment, "tag") }
+        assertThat(fragment.vm).isNotNull()
+        assertThat(fragment.factoryVM.prop).isEqualTo("fragment")
+    }
+}
+
+class TestVMFragment : Fragment() {
+    val vm: TestViewModel by viewModels()
+    val factoryVM: TestFactorizedViewModel by viewModels(TestVMFactory("fragment"))
+}
+
+class TestVMActivity : FragmentActivity() {
+    val vm: TestViewModel by viewModels()
+    val factoryVM: TestFactorizedViewModel by viewModels(TestVMFactory("activity"))
+}
+
+class TestViewModel : ViewModel()
+
+class TestFactorizedViewModel(val prop: String) : ViewModel()
+
+class TestVMFactory(val prop: String) : ViewModelProvider.Factory {
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass != TestFactorizedViewModel::class.java) {
+            throw IllegalArgumentException()
+        }
+        return TestFactorizedViewModel(prop) as T
+    }
+}
