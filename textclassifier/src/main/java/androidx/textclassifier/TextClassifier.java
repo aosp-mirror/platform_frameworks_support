7e151e99a5878ad1a0e8fab90862ef4b4a95aadd
Tony Mak <tonymak@google.com>
2018-10-09 15:20:43 +0100

    Introduce TextClasifier.close() to release underlying resource
    Test: ./gradlew textclassifier:connectedCheck --info --daemon
    
    BUG: 72307474
    Change-Id: Ie4093a7901b38f8c7638e16d401ad31e6d557ac2

--- textclassifier/src/main/java/androidx/textclassifier/TextClassifier.java
+++ textclassifier/src/main/java/androidx/textclassifier/TextClassifier.java
@@ -222,6 +222,16 @@ public abstract class TextClassifier {
     public void onSelectionEvent(@NonNull SelectionEvent event) {
     }
 
+    /**
+     * Closes this text classifier, relinquishing any underlying resources, if any.
+     * <p>
+     * <strong>Note:</strong> If this text classifier is set to {@link TextClassificationManager}
+     * via {@link TextClassificationManager#setTextClassifier(TextClassifier)}, you should unset
+     * it before closing the textclassifier.
+     */
+    public void close() {
+    }
+
     /** @hide */
     @RestrictTo(RestrictTo.Scope.LIBRARY)
     static void ensureNotOnMainThread() {
