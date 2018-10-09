2905a13ac73d07146ae326b15d1fb3ab456abe05
Louis Pullen-Freilich <lpf@google.com>
2018-10-09 15:27:52 +0100

    Generifies findPreference() to return <T extends Preference>
    This helps avoid boilerplate casting when interacting with Preferences
    in a hierarchy.
    
    Bug: b/36881810
    Test: Manually verified
    Change-Id: I2e129b1289fe7f673e31bab70293d0d1e991c9fe

--- preference/src/main/java/androidx/preference/DialogPreference.java
+++ preference/src/main/java/androidx/preference/DialogPreference.java
@@ -267,6 +267,6 @@ public abstract class DialogPreference extends Preference {
          * @return The {@link Preference} with the key, or {@code null}
          * @see PreferenceGroup#findPreference(CharSequence)
          */
-        Preference findPreference(CharSequence key);
+        <T extends Preference> T findPreference(CharSequence key);
     }
 }
