9d302844f6db32fafb21d7a1b748d8211fb9b580
Jin Seok Park <jinpark@google.com>
2018-10-24 17:16:40 +0900

    Call onPlaylistChanged when metadata is updated
    If a metadata of a media item is updated and the item is included
    inside the current playlist, notify the controllers by calling
    onPlaylistChanged callback.
    
    Bug: 117587959
    Test: ./gradlew :media-widget:connectedCheck
          && :media2:connectedCheck
    
    Change-Id: Ia7abe0d5d6d48c444d3731863202aed65b652195

--- media2/src/main/java/androidx/media2/MediaSession2ImplBase.java
+++ media2/src/main/java/androidx/media2/MediaSession2ImplBase.java
@@ -1159,11 +1159,15 @@ class MediaSession2ImplBase implements MediaSession2Impl {
     private static class SessionPlayerCallback extends SessionPlayer2.PlayerCallback {
         private final WeakReference<MediaSession2ImplBase> mSession;
         private MediaItem2 mMediaItem;
-        private final MediaItemCallback mMediaItemCallback;
+        private List<MediaItem2> mList;
+        private final MediaItemCallback mCallbackForCurrentMediaItemChanged;
+        private final MediaItemCallback mCallbackForPlaylistChanged;
 
         SessionPlayerCallback(MediaSession2ImplBase session) {
             mSession = new WeakReference<>(session);
-            mMediaItemCallback = new MediaItemCallback(session);
+            mCallbackForCurrentMediaItemChanged = new MediaItemCallback(session);
+            mCallbackForPlaylistChanged = new MediaItemCallback(session);
+            mList = new ArrayList<>();
         }
 
         @Override
@@ -1174,10 +1178,10 @@ class MediaSession2ImplBase implements MediaSession2Impl {
             }
             synchronized (session.mLock) {
                 if (mMediaItem != null) {
-                    mMediaItem.unregisterCallback(mMediaItemCallback);
+                    mMediaItem.unregisterCallback(mCallbackForCurrentMediaItemChanged);
                 }
                 if (item != null)  {
-                    item.registerCallback(mMediaItemCallback);
+                    item.registerCallback(mCallbackForCurrentMediaItemChanged);
                 }
                 mMediaItem = item;
             }
@@ -1246,7 +1250,24 @@ class MediaSession2ImplBase implements MediaSession2Impl {
         @Override
         public void onPlaylistChanged(final SessionPlayer2 player, final List<MediaItem2> list,
                 final MediaMetadata2 metadata) {
-            // TODO: Add code for changing metadata of media item in a playlist
+            final MediaSession2ImplBase session = getSession();
+            if (session == null || session.getPlayer() != player || player == null) {
+                return;
+            }
+            synchronized (session.mLock) {
+                if (mList != null) {
+                    for (int i = 0; i < mList.size(); i++) {
+                        mList.get(i).unregisterCallback(mCallbackForPlaylistChanged);
+                    }
+                }
+                if (list != null) {
+                    for (int i = 0; i < list.size(); i++) {
+                        list.get(i).registerCallback(mCallbackForPlaylistChanged);
+                    }
+                }
+                mList = list;
+            }
+
             dispatchRemoteControllerTask(player, new RemoteControllerCallbackTask() {
                 @Override
                 public void run(ControllerCb callback) throws RemoteException {
@@ -1410,7 +1431,23 @@ class MediaSession2ImplBase implements MediaSession2Impl {
                 });
                 return;
             }
-            // TODO: Add code for changing metadata of media item in a playlist
+
+            final List<MediaItem2> list = session.getPlaylist();
+            if (list == null) {
+                return;
+            }
+            for (int i = 0; i < list.size(); i++) {
+                if (list.get(i).equals(item)) {
+                    session.dispatchRemoteControllerCallbackTask(
+                            new RemoteControllerCallbackTask() {
+                                @Override
+                                public void run(ControllerCb callback) throws RemoteException {
+                                    callback.onPlaylistChanged(list, session.getPlaylistMetadata());
+                                }
+                            });
+                    return;
+                }
+            }
         }
 
         private MediaSession2ImplBase getSession() {
