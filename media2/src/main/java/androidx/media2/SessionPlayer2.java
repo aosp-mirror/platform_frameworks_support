1803908daa7dae079195c9b258efa70f42b5a5d3
Jin Seok Park <jinpark@google.com>
2018-10-26 14:54:48 +0900

    Update PlayerCallback javadoc
    Bug: 118402771
    Test: Comment change only
    Change-Id: I8dd512d29904c1e54a1c178e737f42bc63f340e4

--- media2/src/main/java/androidx/media2/SessionPlayer2.java
+++ media2/src/main/java/androidx/media2/SessionPlayer2.java
@@ -115,6 +115,8 @@ import java.util.concurrent.Executor;
  * <li> You can call {@link #seekTo(long)} from the {@link #PLAYER_STATE_PAUSED}. In these cases, if
  * you are playing a video stream and the requested position is valid, one video frame may be
  * displayed.
+ * <li> When updating UI for state changes, make sure to respond to {@link PlayerCallback}
+ * invocations instead of {@link PlayerResult} objects.
  * </ul>
  *
  * <h3 id="Invalid_States">Invalid method calls</h3>
