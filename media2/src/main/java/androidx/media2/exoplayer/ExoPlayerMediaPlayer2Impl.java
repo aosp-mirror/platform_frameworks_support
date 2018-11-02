0ef7d6020532ad3e55b70ee3cdf01ea0f73f407f
Andrew Lewis <andrewlewis@google.com>
2018-11-02 17:04:28 +0000

    Fix calling methods after close()
    Calling methods after close would block the calling thread indefinitely
    because the handler thread was no longer active.
    
    Fix this behavior so we throw an exception instead.
    
    Bug: 80232248
    Test: Checked main thread not blocked in support-media-demos after close
    Change-Id: Icc04ea53b5291033b40fba059ad3b8499fb058c9

--- media2/src/main/java/androidx/media2/exoplayer/ExoPlayerMediaPlayer2Impl.java
+++ media2/src/main/java/androidx/media2/exoplayer/ExoPlayerMediaPlayer2Impl.java
@@ -772,6 +772,7 @@ public final class ExoPlayerMediaPlayer2Impl extends MediaPlayer2
         });
     }
 
+
     /**
      * Runs the specified callable on the player thread, blocking the calling thread until a result
      * is returned.
@@ -781,7 +782,7 @@ public final class ExoPlayerMediaPlayer2Impl extends MediaPlayer2
      */
     private <T> T runPlayerCallableBlocking(final Callable<T> callable) {
         final ResolvableFuture<T> future = ResolvableFuture.create();
-        mTaskHandler.post(new Runnable() {
+        boolean success = mTaskHandler.post(new Runnable() {
             @Override
             public void run() {
                 try {
@@ -791,6 +792,7 @@ public final class ExoPlayerMediaPlayer2Impl extends MediaPlayer2
                 }
             }
         });
+        Preconditions.checkState(success);
         try {
             T result;
             boolean wasInterrupted = false;
