d4fb2a5e59e208bd5f0372a5bd6414b99a55cf50
Julia Tuttle <juliatuttle@google.com>
2018-11-06 12:56:35 -0500

    Slices: Render selection subslices.
    Test: ./gradlew :slice-{core,builders,view}:connectedCheck
    Change-Id: Ib97ac0df930303d3712360c75835beb3c484c02e

--- slices/view/src/main/java/androidx/slice/widget/RowContent.java
+++ slices/view/src/main/java/androidx/slice/widget/RowContent.java
@@ -215,6 +215,7 @@ public class RowContent extends SliceContent {
                 || mSubtitleItem != null
                 || mEndItems.size() > 0
                 || mRange != null
+                || mSelection != null
                 || isDefaultSeeMore());
     }
 
