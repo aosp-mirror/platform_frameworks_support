aaa4f1ee40185d696a606ba5a7fc733cdb33c325
Julia Tuttle <juliatuttle@google.com>
2018-11-06 12:56:35 -0500

    Slices: Render selection subslices.
    Test: ./gradlew :slice-{core,builders,view}:connectedCheck
    Change-Id: Ib97ac0df930303d3712360c75835beb3c484c02e

--- slices/builders/src/main/java/androidx/slice/builders/impl/ListBuilderImpl.java
+++ slices/builders/src/main/java/androidx/slice/builders/impl/ListBuilderImpl.java
@@ -45,6 +45,7 @@ import static androidx.slice.builders.ListBuilder.INFINITY;
 import static androidx.slice.builders.ListBuilder.LARGE_IMAGE;
 import static androidx.slice.core.SliceHints.SUBTYPE_MILLIS;
 import static androidx.slice.core.SliceHints.SUBTYPE_MIN;
+import static androidx.slice.core.SliceHints.SUBTYPE_SELECTION;
 
 import android.app.PendingIntent;
 import android.net.Uri;
@@ -226,7 +227,7 @@ public class ListBuilderImpl extends TemplateBuilderImpl implements ListBuilder 
     @Override
     public void addSelection(SelectionBuilder builder) {
         SelectionBuilderImpl impl = new SelectionBuilderListImpl(createChildBuilder(), builder);
-        getBuilder().addSubSlice(impl.build());
+        getBuilder().addSubSlice(impl.build(), SUBTYPE_SELECTION);
     }
 
     /**
