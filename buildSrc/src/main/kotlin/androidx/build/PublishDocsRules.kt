026b5302ce1f40d54a3b02cc87117fa5c6b3f812
Sumir Kataria <sumir@google.com>
2018-11-06 22:47:55 -0800

    Publish docs for WorkManager alpha11.
    Stop ignoring the coroutines module.
    
    Test: N/A
    Change-Id: I79d66178d0ed8e51c87c8163811f4d636c01013b

--- buildSrc/src/main/kotlin/androidx/build/PublishDocsRules.kt
+++ buildSrc/src/main/kotlin/androidx/build/PublishDocsRules.kt
@@ -95,8 +95,7 @@ val RELEASE_RULE = docsRules("public", false) {
     prebuilts(LibraryGroups.ARCH_CORE, "2.0.0")
     prebuilts(LibraryGroups.PAGING, "2.1.0-beta01")
     prebuilts(LibraryGroups.NAVIGATION, "1.0.0-alpha07")
-    ignore(LibraryGroups.WORKMANAGER, "work-coroutines")
-    prebuilts(LibraryGroups.WORKMANAGER, "1.0.0-alpha10")
+    prebuilts(LibraryGroups.WORKMANAGER, "1.0.0-alpha11")
     default(Ignore)
 }
 
