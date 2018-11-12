d70b3289612f4f506dcfb3c02b1f133def53d7c9
Yigit Boyar <yboyar@google.com>
2018-11-12 13:45:00 -0800

    WIP dependency test repro
    Change-Id: I7f6d04ab008c503928a9e9ee0f8b93093de20ebe

---
+++ dependencytest/src/test/java/com/example/android/dependencytest/ExampleUnitTest.kt
@@ -0,0 +1,18 @@
+package com.example.android.dependencytest
+
+import androidx.lifecycle.LiveData
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        val liveData = object : LiveData<String>() {}
+    }
+}
