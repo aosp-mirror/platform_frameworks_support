883d6729b347e2ca5fa46262e1b831ccb00a8589
Jeff Gaston <jeffrygaston@google.com>
2018-11-13 16:31:16 -0500

    Tracking RestrictTo APIs too
    Adding a public method with a RestrictTo annotation now gets tracked like a public method.
    This is to facilitate binary compatibility between libraries in the same group using methods marked with RestrictTo(LIBRARY_GROUP).
    
    Test: ./gradlew :core:updateApi && ls core/api/system_current.txt
    Test: rm -f core/api/system_current.txt && ./gradlew :core:checkApi || echo ok
    Test: rm -f core/api/system_1.1.0-alpha01.txt && ./gradlew :core:checkApi || echo ok
    Test: echo 'package androidx.core.internal.view;
      import static androidx.annotation.RestrictTo.Scope.LIBRARY_GROUP;
      import androidx.annotation.RestrictTo;
      @RestrictTo(LIBRARY_GROUP)
      public interface SupportSubMenu2 {
        public void TestMe();
      }' > core/src/main/java/androidx/core/internal/view/SupportSubMenu2.java && ./gradlew :core:checkApi || echo ok
    Test: diff core/api/system_1.1.0-alpha01.txt core/api/system_current.txt
    Test: stat navigation/ui/api && echo failed || echo ok
    Test: echo 'package newpackage {
            @RestrictTo(androidx.annotation.RestrictTo.Scope.LIBRARY_GROUP) public class NewClass  {
              method public void NewMethod();
            }
          }' >> core/api/system_1.0.0.txt && ./gradlew :core:checkApi || echo ok
    
    Bug: 87457009
    
    Change-Id: I1ad4a50cee8afe179a3b0a912c1b40a858b10468

---
+++ regenerateReleaseApis.py
@@ -0,0 +1,92 @@
+#!/usr/bin/python
+import subprocess
+import os
+import shutil
+
+def runShell(text):
+  print("Running '" + text + "'")
+  try:
+    subprocess.check_call(["bash", "-c", text])
+    return 0
+  except subprocess.CalledProcessError as e:
+    return e.returncode
+
+def runShellAndGetOutput(text):
+  print("Running '" + text + "'")
+  return subprocess.check_output(["bash", "-c", text + " 2>&1 || true"])
+
+
+def findMissingApiFilePaths():
+  print("Checking for missing api file paths:")
+  missingApiFilePaths = []
+  output = runShellAndGetOutput("./gradlew checkApi --continue")
+  lines = output.split("\n")
+  for line in lines:
+    marker = " is not a file"
+    if line.endswith(marker):
+      prefix = line[:-len(marker)]
+      if prefix[0] == "/":
+        missingApiFilePaths.append(prefix)
+      else:
+        print(output)
+        raise Exception("Could not parse line '" + line + "'")
+  print(output)
+  print("Missing api file paths: " + str(missingApiFilePaths))
+  return missingApiFilePaths
+
+def getSourceDirForApiFilepath(apiFilePath):
+  if not "/api/" in apiFilePath:
+    raise Exception("Illegal api filePath: " + apiFilePath)
+  apiDir = os.path.dirname(apiFilePath)
+  projectDir = os.path.dirname(apiDir)
+  sourceDir = os.path.join(projectDir, "src")
+  return sourceDir
+
+def checkoutOldCode(apiFilePath):
+  sourceDir = getSourceDirForApiFilepath(apiFilePath)
+  refCommit = "3478ef7cbc6b27d8c1497d76c3ffff688771380e"
+  runShell("git checkout " + refCommit + " -- " + sourceDir)
+
+def checkoutNewCode(apiFilePath):
+  sourceDir = getSourceDirForApiFilepath(apiFilePath)
+  runShell("git reset")
+  runShell("git checkout HEAD -- " + sourceDir)
+  runShell("cd " + sourceDir + " && git clean -fd")
+
+def regenerateOldApiFilePathsInParallel(apiFilePaths):
+  for apiFilePath in apiFilePaths:
+    checkoutOldCode(apiFilePath)
+  runShell("./gradlew updateApi --continue || true")
+  for desiredApiFilePath in apiFilePaths:
+    apiDir = os.path.dirname(desiredApiFilePath)
+    createdApiFilePath = os.path.join(apiDir, "system_current.txt")
+    if os.path.isfile(createdApiFilePath):
+      shutil.copyfile(createdApiFilePath, desiredApiFilePath)
+  for apiFilePath in apiFilePaths:
+    checkoutNewCode(apiFilePath)
+
+def regenerateOldApiFilePathsInSeries(apiFilePaths):
+  for path in apiFilePaths:
+    regenerateOldApiFilePathsInParallel([path])
+
+def updateOldApiFilesRepeatedly():
+  even = True
+  while True:
+    missingApiFilePaths = findMissingApiFilePaths()
+    if len(missingApiFilePaths) < 1:
+      print("No api file paths missing")
+      return
+    if even:
+      # in most cases we want to regenerate the api files in parallel because it's faster
+      regenerateOldApiFilePathsInParallel(missingApiFilePaths)
+    else:
+      # in some cases we want to regenerate the api files in series because there might be a build failure when building one of them
+      regenerateOldApiFilePathsInSeries(missingApiFilePaths)
+    even = not even
+
+def main():
+  updateOldApiFilesRepeatedly()
+  runShell("./gradlew updateApi")
+  print("Done")
+
+main()
