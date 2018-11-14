7a2317862744f51117b8b828b803e50e5786f657
Kyunglyul Hyun <klhyun@google.com>
2018-11-14 14:59:37 +0900

    MediaRouteCastDialog: Make the whole row clickable
    Whole row can be tapped to add route to the group.
    To opt out, the check button should be tapped as previously.
    
    Bug: 119531857
    Test: run v7 support demo manually
    Change-Id: Id376903b778fef97d5672bb7f1c65c73c7e8c7eb

--- mediarouter/src/main/java/androidx/mediarouter/app/MediaRouteCastDialog.java
+++ mediarouter/src/main/java/androidx/mediarouter/app/MediaRouteCastDialog.java
@@ -1141,6 +1141,21 @@ public class MediaRouteCastDialog extends AppCompatDialog {
                 mLayoutAnimationDurationMs = res.getInteger(
                         R.integer.mr_cast_volume_slider_layout_animation_duration_ms);
                 mAccelerateDecelerateInterpolator = new AccelerateDecelerateInterpolator();
+
+                mItemView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        if (!mCheckBox.isChecked()) {
+                            view.setEnabled(false);
+                            mCheckBox.setChecked(true);
+                            mCheckBox.setEnabled(false);
+                            mImageView.setVisibility(View.INVISIBLE);
+                            mProgressBar.setVisibility(View.VISIBLE);
+                            mRoute.selectIntoGroup();
+                            animateLayoutHeight(mVolumeSliderLayout, mExpandedLayoutHeight);
+                        }
+                    }
+                });
             }
 
             boolean isSelected(MediaRouter.RouteInfo route) {
@@ -1184,6 +1199,7 @@ public class MediaRouteCastDialog extends AppCompatDialog {
 
                     // Set enabled states of views, height of volume slider layout and alpha value
                     // of itemView.
+                    mItemView.setEnabled(enabled && !selected);
                     mCheckBox.setEnabled(enabled);
                     mMuteButton.setEnabled(enabled);
                     mVolumeSlider.setEnabled(enabled);
