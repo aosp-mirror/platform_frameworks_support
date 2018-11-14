9ba46779a6f51cff044214a1a9be774da9c79117
Kyunglyul Hyun <klhyun@google.com>
2018-11-14 14:59:37 +0900

    MediaRouteCastDialog: Make the whole row clickable
    Whole row can be tapped to add/remove route.
    
    Bug: 119531857
    Test: run v7 support demo manually
    Change-Id: Id376903b778fef97d5672bb7f1c65c73c7e8c7eb

--- mediarouter/src/main/java/androidx/mediarouter/app/MediaRouteCastDialog.java
+++ mediarouter/src/main/java/androidx/mediarouter/app/MediaRouteCastDialog.java
@@ -1069,18 +1069,18 @@ public class MediaRouteCastDialog extends AppCompatDialog {
             final View.OnClickListener mCheckBoxClickListener = new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    // If user clicked unchecked checkbox, isChecked returns true.
-                    if (((CheckBox) v).isChecked()) {
-                        mCheckBox.setEnabled(false);
+                    mCheckBox.setEnabled(false);
+                    mItemView.setEnabled(false);
+                    if (isSelected(mRoute)) {
+                        mCheckBox.setChecked(false);
+                        mRoute.unselectFromGroup();
+                        animateLayoutHeight(mVolumeSliderLayout, mCollapsedLayoutHeight);
+                    } else {
+                        mCheckBox.setChecked(true);
                         mImageView.setVisibility(View.INVISIBLE);
                         mProgressBar.setVisibility(View.VISIBLE);
                         mRoute.selectIntoGroup();
                         animateLayoutHeight(mVolumeSliderLayout, mExpandedLayoutHeight);
-                    } else {
-                        // If user clicked checked checkbox, isChecked returns false.
-                        mCheckBox.setEnabled(false);
-                        mRoute.unselectFromGroup();
-                        animateLayoutHeight(mVolumeSliderLayout, mCollapsedLayoutHeight);
                     }
                 }
             };
@@ -1184,9 +1184,11 @@ public class MediaRouteCastDialog extends AppCompatDialog {
 
                     // Set enabled states of views, height of volume slider layout and alpha value
                     // of itemView.
+                    mItemView.setEnabled(enabled);
                     mCheckBox.setEnabled(enabled);
                     mMuteButton.setEnabled(enabled);
                     mVolumeSlider.setEnabled(enabled);
+                    mItemView.setOnClickListener(mCheckBoxClickListener);
                     mCheckBox.setOnClickListener(mCheckBoxClickListener);
                     setLayoutHeight(mVolumeSliderLayout, selected
                             ? mExpandedLayoutHeight : mCollapsedLayoutHeight);
