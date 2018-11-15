c8dc26567fa3f80cf6c1531c0b28869bb8ebd7f6
Jin Seok Park <jinpark@google.com>
2018-11-15 11:40:30 +0900

    MCImplLegacy: Return existing current media item index
    Test: ./media/version-compat-tests/runtest.sh 1
    Change-Id: I5d4b44889603158995abdb3ca84ea4ded07749fe

--- media2/src/main/java/androidx/media2/MediaController.java
+++ media2/src/main/java/androidx/media2/MediaController.java
@@ -894,6 +894,9 @@ public class MediaController implements AutoCloseable {
      * Gets the previous item index in the playlist. The returned value can be outdated after
      * {@link ControllerCallback#onCurrentMediaItemChanged(MediaController, MediaItem)} or
      * {@link ControllerCallback#onPlaylistChanged(MediaController, List, MediaMetadata)} is called.
+     * <p>
+     * When connected to {@link android.support.v4.media.session.MediaSessionCompat},
+     * this value will return -1.
      *
      * @return the index of previous item in playlist, or -1 if previous media item does not exist
      * or playlist hasn't been set.
@@ -906,6 +909,9 @@ public class MediaController implements AutoCloseable {
      * Gets the next item index in the playlist. The returned value can be outdated after
      * {@link ControllerCallback#onCurrentMediaItemChanged(MediaController, MediaItem)} or
      * {@link ControllerCallback#onPlaylistChanged(MediaController, List, MediaMetadata)} is called.
+     * <p>
+     * When connected to {@link android.support.v4.media.session.MediaSessionCompat},
+     * this value will return -1.
      *
      * @return the index of next item in playlist, or -1 if next media item does not exist or
      * playlist hasn't been set.
