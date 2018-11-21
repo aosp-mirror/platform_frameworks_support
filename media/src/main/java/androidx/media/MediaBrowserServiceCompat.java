82ed24221657251d125d38cfec8db31f31b27c27
Jaewan Kim <jaewan@google.com>
2018-11-21 17:42:52 +0900

    Media: Use PID information from the controller
    Currently, MediaBrowserServiceCompat and Media2 had used
    Binder#getCallingPid() to get the caller's PID. However,
    Binder#getCallingPid() always returns 0 for oneway calls from a remote
    process while MediaBrowserServiceCompat and Media2's only uses oneway
    calls.
    
    This CL does two things to address the PID issue.
      1. Caller sends the PID information.
         There's no other way to get caller PID information for oneway
         calls. Note that PID may not be available if caller didn't sent
         the PID (e.g. MediaBrowserCompat compiled with the older supportlib
         library wouldn't send PID information)
      2. Skips PID check in RemoteUserInfo#equals() when PID is unknown.
    
    Bug: 111418518, Bug: 111418519
    Test: $ ./gradlew media:connectedCheck media2:connectedCheck
          $ media/version-compat-tests/runtest.sh 4
          $ media2/version-compat-tests/runtest.sh 1
    Change-Id: I8733a4d8538bcb482c177240bdbd60e6625c3d5d

--- media/src/main/java/androidx/media/MediaBrowserServiceCompat.java
+++ media/src/main/java/androidx/media/MediaBrowserServiceCompat.java
@@ -42,6 +42,7 @@ import static androidx.media.MediaBrowserProtocol.DATA_RESULT_RECEIVER;
 import static androidx.media.MediaBrowserProtocol.DATA_ROOT_HINTS;
 import static androidx.media.MediaBrowserProtocol.DATA_SEARCH_EXTRAS;
 import static androidx.media.MediaBrowserProtocol.DATA_SEARCH_QUERY;
+import static androidx.media.MediaBrowserProtocol.EXTRA_CALLING_PID;
 import static androidx.media.MediaBrowserProtocol.EXTRA_CLIENT_VERSION;
 import static androidx.media.MediaBrowserProtocol.EXTRA_MESSENGER_BINDER;
 import static androidx.media.MediaBrowserProtocol.EXTRA_SERVICE_VERSION;
@@ -50,6 +51,7 @@ import static androidx.media.MediaBrowserProtocol.SERVICE_MSG_ON_CONNECT;
 import static androidx.media.MediaBrowserProtocol.SERVICE_MSG_ON_CONNECT_FAILED;
 import static androidx.media.MediaBrowserProtocol.SERVICE_MSG_ON_LOAD_CHILDREN;
 import static androidx.media.MediaBrowserProtocol.SERVICE_VERSION_CURRENT;
+import static androidx.media.MediaSessionManager.RemoteUserInfo.UNKNOWN_PID;
 
 import android.app.Service;
 import android.content.Context;
@@ -179,6 +181,7 @@ public abstract class MediaBrowserServiceCompat extends Service {
     private @interface ResultFlags {
     }
 
+    final ArrayList<ConnectionRecord> mPendingConnections = new ArrayList<>();
     final ArrayMap<IBinder, ConnectionRecord> mConnections = new ArrayMap<>();
     ConnectionRecord mCurConnection;
     final ServiceHandler mHandler = new ServiceHandler();
@@ -347,6 +350,7 @@ public abstract class MediaBrowserServiceCompat extends Service {
         public BrowserRoot onGetRoot(
                 String clientPackageName, int clientUid, Bundle rootHints) {
             Bundle rootExtras = null;
+            int clientPid = UNKNOWN_PID;
             if (rootHints != null && rootHints.getInt(EXTRA_CLIENT_VERSION, 0) != 0) {
                 rootHints.remove(EXTRA_CLIENT_VERSION);
                 mMessenger = new Messenger(mHandler);
@@ -360,16 +364,24 @@ public abstract class MediaBrowserServiceCompat extends Service {
                 } else {
                     mRootExtrasList.add(rootExtras);
                 }
+                clientPid = rootHints.getInt(EXTRA_CALLING_PID, UNKNOWN_PID);
+                rootHints.remove(EXTRA_CALLING_PID);
             }
+            ConnectionRecord connection = new ConnectionRecord(
+                    clientPackageName, clientPid, clientUid, rootHints, null);
             // We aren't sure whether this connection request would be accepted.
             // Temporarily set mCurConnection just to make getCurrentBrowserInfo() working.
-            mCurConnection = new ConnectionRecord(clientPackageName, -1, clientUid, rootHints,
-                    null);
+            mCurConnection = connection;
             BrowserRoot root = MediaBrowserServiceCompat.this.onGetRoot(
                     clientPackageName, clientUid, rootHints);
             mCurConnection = null;
             if (root == null) {
                 return null;
+            }
+            if (mMessenger != null) {
+                // Keeps the connection request from the MediaBrowserCompat to reuse the package
+                // name here.
+                mPendingConnections.add(connection);
             }
             if (rootExtras == null) {
                 rootExtras = root.getExtras();
@@ -729,7 +741,14 @@ public abstract class MediaBrowserServiceCompat extends Service {
             Bundle data = msg.getData();
             data.setClassLoader(MediaBrowserCompat.class.getClassLoader());
             data.putInt(DATA_CALLING_UID, Binder.getCallingUid());
-            data.putInt(DATA_CALLING_PID, Binder.getCallingPid());
+            int pid = Binder.getCallingPid();
+            if (pid != 0) {
+                // Override PID if we can get it through getCallingPid().
+                data.putInt(DATA_CALLING_PID, pid);
+            } else if (data.getLong(DATA_CALLING_PID, UNKNOWN_PID) == UNKNOWN_PID) {
+                // Explicitly put UNKNOWN_PID if it's missing or doesn't have value with Long type.
+                data.putInt(DATA_CALLING_PID, UNKNOWN_PID);
+            }
             return super.sendMessageAtTime(msg, uptimeMillis);
         }
 
@@ -1064,8 +1083,27 @@ public abstract class MediaBrowserServiceCompat extends Service {
                     // Clear out the old subscriptions. We are getting new ones.
                     mConnections.remove(b);
 
-                    final ConnectionRecord connection = new ConnectionRecord(pkg, pid, uid,
-                            rootHints, callbacks);
+                    ConnectionRecord connection = null;
+                    // pkg and pid are set only when the MediaBrowserCompat has sent the
+                    // information, but we cannot expect so for MediaBrowserCompat compiled with
+                    // the older version. Do the best effort to guess.
+                    if (TextUtils.isEmpty(pkg) || pid <= 0) {
+                        // Use heuristic to pick
+                        for (ConnectionRecord pendingConnection : mPendingConnections) {
+                            if (pendingConnection.uid == uid) {
+                                // Do not assign pendingConnection directly because it doesn't have
+                                // callback information.
+                                connection = new ConnectionRecord(pendingConnection.pkg,
+                                        pendingConnection.pid, pendingConnection.uid,
+                                        rootHints, callbacks);
+                                mPendingConnections.remove(pendingConnection);
+                                break;
+                            }
+                        }
+                    }
+                    if (connection == null) {
+                        connection = new ConnectionRecord(pkg, pid, uid, rootHints, callbacks);
+                    }
                     mConnections.put(b, connection);
                     try {
                         b.linkToDeath(connection, 0);
