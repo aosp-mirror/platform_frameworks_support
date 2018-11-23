7b9eaec1cbdb9a21ae0ee35d41f0c0ab23e794ec
Andrew Lewis <andrewlewis@google.com>
2018-11-23 12:41:37 +0000

    Check track index in testChangeSubtitleTrack
    Test: ran test in Android Studio
    Change-Id: I0326d0666aa078a92a480a6bd82bfd82c8340cde

--- media2/src/androidTest/java/androidx/media2/MediaPlayer2Test.java
+++ media2/src/androidTest/java/androidx/media2/MediaPlayer2Test.java
@@ -1936,10 +1936,12 @@ public class MediaPlayer2Test extends MediaPlayer2TestBase {
         // Waits until at least two captions are fired. Timeout is 2.5 sec.
         selectSubtitleTrack(0);
         assertTrue(mOnSubtitleDataCalled.waitForCountedSignals(2, 2500) >= 2);
+        assertEquals(1, mPlayer.getSelectedTrack(MediaPlayer2.TrackInfo.MEDIA_TRACK_TYPE_SUBTITLE));
 
         mOnSubtitleDataCalled.reset();
         selectSubtitleTrack(1);
         assertTrue(mOnSubtitleDataCalled.waitForCountedSignals(2, 2500) >= 2);
+        assertEquals(2, mPlayer.getSelectedTrack(MediaPlayer2.TrackInfo.MEDIA_TRACK_TYPE_SUBTITLE));
 
         mPlayer.reset();
     }
