f0bb1b26655bca16d7ace4d9f704430ca9c5980b
Jason Monk <jmonk@android.com>
2018-11-27 12:57:02 -0500

    Support remotecallbacks for SliceProviders
    Test: :slice-core:connectedCheck
    Bug: 112728978
    Change-Id: Ia2614b98cc969694517fbdbe53bc5421c35463a6

--- slices/core/src/main/java/androidx/slice/compat/SliceProviderWrapperContainer.java
+++ slices/core/src/main/java/androidx/slice/compat/SliceProviderWrapperContainer.java
@@ -35,7 +35,10 @@ import android.util.Log;
 import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.annotation.RestrictTo;
+import androidx.remotecallback.CallbackHandlerRegistry;
+import androidx.remotecallback.ProviderRelayReceiver;
 import androidx.slice.SliceConvert;
+import androidx.slice.SliceProviderWithCallbacks;
 
 import java.util.Collection;
 import java.util.Set;
@@ -107,6 +110,12 @@ public class SliceProviderWrapperContainer {
                         checkPermissions(uri);
                     }
                 }
+            }
+            if (ProviderRelayReceiver.METHOD_PROVIDER_CALLBACK.equals(method)
+                    && (mSliceProvider instanceof SliceProviderWithCallbacks)) {
+                CallbackHandlerRegistry.sInstance.invokeCallback(getContext(),
+                        (SliceProviderWithCallbacks) mSliceProvider, extras);
+                return null;
             }
             return super.call(method, arg, extras);
         }
