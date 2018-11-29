654d21971095f5551f2699d3ae4de9d42af563e8
Ian Lake <ilake@google.com>
2018-11-29 15:01:38 -0800

    Add an @InflatesRes annotation to ComponentActivity, Fragment
    Allow ComponentActivity subclasses and Fragments
    to use an @InflatesRes annotation to mark what
    layout they intend to inflate.
    
    This provides a single source of truth for both
    tooling and the runtime to rely on.
    
    Test: new InflatesResTest, updated existing tests
    BUG: 77492502
    Change-Id: If5b1d117aae1e2d449c6e5fb050723744eb8e609

--- fragment/src/androidTest/java/androidx/fragment/app/FragmentLifecycleTest.java
+++ fragment/src/androidTest/java/androidx/fragment/app/FragmentLifecycleTest.java
@@ -46,6 +46,7 @@ import android.view.ViewGroup;
 import android.view.Window;
 import android.widget.TextView;
 
+import androidx.activity.InflatesRes;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.core.app.ActivityCompat;
@@ -1972,16 +1973,11 @@ public class FragmentLifecycleTest {
         }
     }
 
+    @InflatesRes(R.layout.nested_retained_inflated_fragment_parent)
     public static class RetainedInflatedParentFragment extends Fragment {
-        @Nullable
-        @Override
-        public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
-                @Nullable Bundle savedInstanceState) {
-            return inflater.inflate(R.layout.nested_retained_inflated_fragment_parent, container,
-                    false);
-        }
     }
 
+    @InflatesRes(R.layout.nested_inflated_fragment_child)
     public static class RetainedInflatedChildFragment extends Fragment {
 
         int mOnInflateCount = 0;
@@ -1997,13 +1993,6 @@ public class FragmentLifecycleTest {
                 @Nullable Bundle savedInstanceState) {
             super.onInflate(context, attrs, savedInstanceState);
             mOnInflateCount++;
-        }
-
-        @Nullable
-        @Override
-        public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
-                @Nullable Bundle savedInstanceState) {
-            return inflater.inflate(R.layout.nested_inflated_fragment_child, container, false);
         }
     }
 }
