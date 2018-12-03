c15c366caa5a1f47dc4adebbd20bc0b3cca3b1cb
Yuichi Araki <yaraki@google.com>
2018-12-03 11:40:46 +0900

    Deprecate OnConflictStrategy.ROLLBACK and FAIL
    OnConflictStrategy.ROLLBACK was never working properly. On conflict, it
    always fails with SQLiteException since Room tries to end the
    transaction which has already been rolled back by the conflict. In
    addition, framework has a bug in handling of transaction around ROLLBACK
    (b/120397728), so it is impossible to make this work as expected.
    
    Instead, ABORT has always worked like ROLLBACK. This is because the
    INSERT throws SQLiteConstraintException on conflict, so the wrapping
    transaction is ended without being marked as successful and thus rolled
    back. We could change this to work more like SQLite's original ABORT
    (keep preceding changes in the transaction), but it is too risky to
    change the behavior of ABORT since it is the default option of @Insert
    and @Update.  This CL just clarifies the behavior in Javadoc.
    
    OnConflictStrategy.FAIL is no different from ABORT because of the
    wrapping transaction. We should just remove it for clarity.
    
    REPLACE and IGNORE are working fine.
    
    Bug: 117266738
    Test: OnConflictStrategyTest
    Change-Id: Id62920d531afe4644d9d37ffc823a65132b54b4f

--- room/common/src/main/java/androidx/room/OnConflictStrategy.java
+++ room/common/src/main/java/androidx/room/OnConflictStrategy.java
@@ -24,9 +24,6 @@ import java.lang.annotation.Retention;
 
 /**
  * Set of conflict handling strategies for various {@link Dao} methods.
- * <p>
- * Check <a href="https://sqlite.org/lang_conflict.html">SQLite conflict documentation</a> for
- * details.
  */
 @Retention(SOURCE)
 @IntDef({OnConflictStrategy.REPLACE, OnConflictStrategy.ROLLBACK, OnConflictStrategy.ABORT,
@@ -38,15 +35,20 @@ public @interface OnConflictStrategy {
     int REPLACE = 1;
     /**
      * OnConflict strategy constant to rollback the transaction.
+     * @deprecated This was never working properly. Use {@link #ABORT} to roll back the transaction.
      */
+    @Deprecated
     int ROLLBACK = 2;
     /**
-     * OnConflict strategy constant to abort the transaction.
+     * OnConflict strategy constant to abort the transaction. <em>The transaction is rolled
+     * back.</em>
      */
     int ABORT = 3;
     /**
      * OnConflict strategy constant to fail the transaction.
+     * @deprecated Use {@link #ABORT}.
      */
+    @Deprecated
     int FAIL = 4;
     /**
      * OnConflict strategy constant to ignore the conflict.
