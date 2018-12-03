6ca4ae3b27125d5690a5a00156c180737a0d9ac9
Jason Chiu <chiujason@google.com>
2018-12-03 18:36:57 +0800

    Added support for showing the title items of the first row on the start
    Add a new API to show the title items of the first row on the start by presenter.
    
    Test: ./gradlew slice-view:connectedCheck
    Change-Id: If18dbc1e47cfb1e9f3de9c0475b1241d50eadfb9
    Fixes: 119639527

--- slices/view/src/main/java/androidx/slice/widget/SliceView.java
+++ slices/view/src/main/java/androidx/slice/widget/SliceView.java
@@ -160,6 +160,7 @@ public class SliceView extends ViewGroup implements Observer<Slice>, View.OnClic
     private boolean mShowActions = false;
     private boolean mShowLastUpdated = true;
     private boolean mCurrentSliceLoggedVisible = false;
+    private boolean mShowTitleItems = false;
     private boolean mShowHeaderDivider = false;
     private boolean mShowActionDividers = false;
 
@@ -469,6 +470,9 @@ public class SliceView extends ViewGroup implements Observer<Slice>, View.OnClic
             mCurrentView.resetView();
         }
         mListContent = mSliceMetadata != null ? mSliceMetadata.getListContent() : null;
+        if (mShowTitleItems) {
+            showTitleItems(true);
+        }
         if (mShowHeaderDivider) {
             showHeaderDivider(true);
         }
@@ -631,6 +635,16 @@ public class SliceView extends ViewGroup implements Observer<Slice>, View.OnClic
      */
     public @SliceMode int getMode() {
         return mViewPolicy.getMode();
+    }
+
+    /**
+     * Whether this view should show title items of the first row on the start.
+     */
+    public void showTitleItems(boolean enabled) {
+        mShowTitleItems = true;
+        if (mListContent != null) {
+            mListContent.showTitleItems(enabled);
+        }
     }
 
     /**
