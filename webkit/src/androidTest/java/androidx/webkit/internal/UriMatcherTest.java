0e20a657613c8768a2e7673670044bd332a5376e
Hazem Ashmawy <hazems@google.com>
2019-01-09 15:00:37 +0000

    [WebView Support Library] Fork UriMatcher
    Fork UriMatcher From android.content.UriMatcher to:
    - Support matching schemes (like http:// or https://).
    - Return a handler object instead of int code.
    - Add support to the wild card "**" for nested paths.
    
    Used in WebViewAssetLoader in aosp/835923
    
    Bug: 120096543
    Test: Added unit test
    Change-Id: Ic9adf310068f515b3230f9290ede1cfda461c2c9

---
+++ webkit/src/androidTest/java/androidx/webkit/internal/UriMatcherTest.java
@@ -0,0 +1,120 @@
+/*
+ * Copyright (C) 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.webkit.internal;
+
+import android.net.Uri;
+
+import androidx.test.filters.SmallTest;
+import androidx.test.runner.AndroidJUnit4;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/*
+ * Test cases for the forked UriMatcher.
+ */
+@RunWith(AndroidJUnit4.class)
+public class UriMatcherTest {
+
+    @Test
+    @SmallTest
+    public void testFullHttpUrlMatching() {
+        final Object pathMatch = new Object();
+        final Object rootMatch = new Object();
+        UriMatcher<Object> matcher = new UriMatcher<>(rootMatch);
+        matcher.addURI("http", "org.chromium", "/some/path", pathMatch);
+
+        Assert.assertEquals(pathMatch, matcher.match(Uri.parse("http://org.chromium/some/path")));
+        Assert.assertEquals(pathMatch, matcher.match(Uri.parse("http://org.chromium/some/path?asd")));
+        Assert.assertEquals(pathMatch, matcher.match(Uri.parse("http://org.chromium/some/path#asd")));
+
+        Assert.assertNull(matcher.match(Uri.parse("https://org.chromium/some/path")));
+        Assert.assertNull(matcher.match(Uri.parse("http://org.chromium/somepath")));
+        Assert.assertNull(matcher.match(Uri.parse("http://org.chromium/")));
+        Assert.assertNull(matcher.match(Uri.parse("http://org.chromium/some/other")));
+        Assert.assertNull(matcher.match(Uri.parse("http://org.chromium/some/path/other")));
+
+        Assert.assertEquals(rootMatch, matcher.match(Uri.parse("http:")));
+        Assert.assertEquals(rootMatch, matcher.match(Uri.parse("https:")));
+    }
+
+    @Test
+    @SmallTest
+    public void testFullHttpsUrlMatching() {
+        final Object pathMatch = new Object();
+        final Object rootMatch = new Object();
+        UriMatcher<Object> matcher = new UriMatcher<>(rootMatch);
+        matcher.addURI("https", "org.chromium", "/some/path", pathMatch);
+
+        Assert.assertEquals(pathMatch, matcher.match(Uri.parse("https://org.chromium/some/path")));
+        Assert.assertEquals(pathMatch, matcher.match(Uri.parse("https://org.chromium/some/path?asd")));
+        Assert.assertEquals(pathMatch, matcher.match(Uri.parse("https://org.chromium/some/path#asd")));
+
+        Assert.assertNull(matcher.match(Uri.parse("http://org.chromium/some/path")));
+        Assert.assertNull(matcher.match(Uri.parse("https://org.chromium/somepath")));
+        Assert.assertNull(matcher.match(Uri.parse("https://org.chromium/")));
+        Assert.assertNull(matcher.match(Uri.parse("https://org.chromium/some/other")));
+        Assert.assertNull(matcher.match(Uri.parse("https://org.chromium/some/path/other")));
+
+        Assert.assertEquals(rootMatch, matcher.match(Uri.parse("http:")));
+        Assert.assertEquals(rootMatch, matcher.match(Uri.parse("https:")));
+    }
+
+    @Test
+    @SmallTest
+    public void testHttpUrlSingleStarMatching() {
+        final Object pathMatch = new Object();
+        final Object rootMatch = new Object();
+        UriMatcher<Object> matcher = new UriMatcher<>(rootMatch);
+        matcher.addURI("http", "org.chromium", "/path/*", pathMatch);
+
+        Assert.assertEquals(pathMatch, matcher.match(Uri.parse("http://org.chromium/path/a")));
+        Assert.assertEquals(pathMatch, matcher.match(Uri.parse("http://org.chromium/path/a?asd")));
+        Assert.assertEquals(pathMatch, matcher.match(Uri.parse("http://org.chromium/path/a#asd")));
+
+        Assert.assertNull(matcher.match(Uri.parse("https://org.chromium/pa")));
+        Assert.assertNull(matcher.match(Uri.parse("http://org.chromium/path")));
+        Assert.assertNull(matcher.match(Uri.parse("http://org.chromium/path?asd")));
+        Assert.assertNull(matcher.match(Uri.parse("http://org.chromium/path#asd")));
+        Assert.assertNull(matcher.match(Uri.parse("http://org.chromium/path/a/b/c")));
+        Assert.assertNull(matcher.match(Uri.parse("http://org.chromium/other/path")));
+    }
+
+    @Test
+    @SmallTest
+    public void testHttpUrlDoubleStarMatching() {
+        final Object pathMatch = new Object();
+        final Object rootMatch = new Object();
+        UriMatcher<Object> matcher = new UriMatcher<>(rootMatch);
+        matcher.addURI("http", "org.chromium", "/path/**", pathMatch);
+
+        Assert.assertEquals(pathMatch, matcher.match(Uri.parse("http://org.chromium/path/a")));
+        Assert.assertEquals(pathMatch, matcher.match(Uri.parse("http://org.chromium/path/a/b/c")));
+        Assert.assertEquals(pathMatch, matcher.match(Uri.parse("http://org.chromium/path/a?asd")));
+        Assert.assertEquals(pathMatch, matcher.match(Uri.parse("http://org.chromium/path/a#asd")));
+        Assert.assertEquals(pathMatch, matcher.match(Uri.parse("http://org.chromium/path/a/b?asd")));
+        Assert.assertEquals(pathMatch, matcher.match(Uri.parse("http://org.chromium/path/a/b#asd")));
+
+        Assert.assertNull(matcher.match(Uri.parse("https://org.chromium/pa")));
+        Assert.assertNull(matcher.match(Uri.parse("http://org.chromium/path")));
+        Assert.assertNull(matcher.match(Uri.parse("http://org.chromium/path?asd")));
+        Assert.assertNull(matcher.match(Uri.parse("http://org.chromium/path#asd")));
+        Assert.assertNull(matcher.match(Uri.parse("http://org.chromium/other/path")));
+    }
+}
+
