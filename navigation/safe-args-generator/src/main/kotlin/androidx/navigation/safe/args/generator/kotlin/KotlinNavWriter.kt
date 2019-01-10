de27fbc8905ce957a836be3f328ee34c23570eb4
Daniel Santiago Rivera <danysantiago@google.com>
2019-01-10 12:59:41 -0800

    Safe Args in Kotlin - KotlinNavWriter
    Finalized the KotlinNavWriter that will generate Kotlin files. The API
    for the Kotlin version of the Directions and Args classes is different
    than the Java version. The main difference being the lack of builders
    and setter. Args and NavDirections classes are immutable data classes
    and default values are default parameter values with the idea that
    invocations are done with named parameters.
    
    This change also allows users to apply a version of the Safe Args Gradle
    Plugin that will generate code in Kotlin. The id of the plugin is
    'androidx.navigation.safeargs.kotlin'.
    
    Bug: 110263087
    Test: ./gradlew navigation:navigation-safe-args-gradle-plugin:test \
                    navigation:navigation-safe-args-generator:test
    
    Change-Id: Ifa0ec7e4bbadd2d274e04dd9ade8a2d619685bd3

--- navigation/safe-args-generator/src/main/kotlin/androidx/navigation/safe/args/generator/kotlin/KotlinNavWriter.kt
+++ navigation/safe-args-generator/src/main/kotlin/androidx/navigation/safe/args/generator/kotlin/KotlinNavWriter.kt
@@ -17,9 +17,22 @@
 package androidx.navigation.safe.args.generator.kotlin
 
 import androidx.navigation.safe.args.generator.NavWriter
+import androidx.navigation.safe.args.generator.ObjectArrayType
+import androidx.navigation.safe.args.generator.ObjectType
+import androidx.navigation.safe.args.generator.ext.toCamelCase
+import androidx.navigation.safe.args.generator.ext.toCamelCaseAsVar
+import androidx.navigation.safe.args.generator.models.Action
 import androidx.navigation.safe.args.generator.models.Destination
+import androidx.navigation.safe.args.generator.models.accessor
+import com.squareup.kotlinpoet.AnnotationSpec
 import com.squareup.kotlinpoet.ClassName
 import com.squareup.kotlinpoet.FileSpec
+import com.squareup.kotlinpoet.FunSpec
+import com.squareup.kotlinpoet.KModifier
+import com.squareup.kotlinpoet.ParameterSpec
+import com.squareup.kotlinpoet.PropertySpec
+import com.squareup.kotlinpoet.TypeSpec
+import com.squareup.kotlinpoet.asTypeName
 
 class KotlinNavWriter(private val useAndroidX: Boolean = false) : NavWriter {
 
@@ -31,9 +44,104 @@ class KotlinNavWriter(private val useAndroidX: Boolean = false) : NavWriter {
             ?: throw IllegalStateException("Destination with actions must have name")
         val className = ClassName(destName.packageName(), "${destName.simpleName()}Directions")
 
+        val actionTypes = destination.actions.map { action ->
+            action to generateDirectionTypeSpec(action)
+        }
+
+        val actionsFunSpec = actionTypes.map { (action, actionTypeSpec) ->
+            val typeName = ClassName("", actionTypeSpec.name!!)
+            val parameters = action.args.map { arg ->
+                ParameterSpec.builder(
+                    name = arg.sanitizedName,
+                    type = arg.type.typeName().copy(nullable = arg.isNullable)
+                ).apply {
+                    arg.defaultValue?.let {
+                        defaultValue(it.write())
+                    }
+                }.build()
+            }
+            FunSpec.builder(action.id.javaIdentifier.toCamelCaseAsVar()).apply {
+                returns(typeName)
+                addParameters(parameters)
+                if (action.args.isEmpty()) {
+                    addStatement("return %T", typeName)
+                } else {
+                    addStatement(
+                        "return %T(${parameters.joinToString(", ") { it.name }})",
+                        typeName
+                    )
+                }
+            }.build()
+        }
+
+        val typeSpec = TypeSpec.classBuilder(className)
+            .addTypes(actionTypes.map { (_, type) -> type })
+            .addType(
+                TypeSpec.companionObjectBuilder()
+                    .addFunctions(actionsFunSpec)
+                    .build()
+            )
+            .build()
+
         return FileSpec.builder(className.packageName, className.simpleName)
+            .addType(typeSpec)
             .build()
             .toCodeFile()
+    }
+
+    internal fun generateDirectionTypeSpec(action: Action): TypeSpec {
+        val className = ClassName("", action.id.javaIdentifier.toCamelCase())
+
+        val getActionIdFunSpec = FunSpec.builder("getActionId")
+            .addModifiers(KModifier.OVERRIDE)
+            .returns(Int::class)
+            .addStatement("return %N", action.id.accessor())
+            .build()
+
+        val getArgumentsFunSpec = FunSpec.builder("getArguments").apply {
+            addModifiers(KModifier.OVERRIDE)
+            if (action.args.any { it.type is ObjectType }) {
+                addAnnotation(
+                    AnnotationSpec.builder(Suppress::class)
+                        .addMember("%S", "CAST_NEVER_SUCCEEDS")
+                        .build()
+                )
+            }
+            returns(BUNDLE_CLASSNAME)
+            val resultVal = "__result"
+            addStatement("val %L = %T()", resultVal, BUNDLE_CLASSNAME)
+            action.args.forEach { arg ->
+                arg.type.addBundlePutStatement(this, arg, resultVal, arg.sanitizedName)
+            }
+            addStatement("return %L", resultVal)
+        }.build()
+
+        val constructorFunSpec = FunSpec.constructorBuilder()
+            .addModifiers(KModifier.INTERNAL)
+            .addParameters(action.args.map { arg ->
+                ParameterSpec.builder(
+                    name = arg.sanitizedName,
+                    type = arg.type.typeName().copy(nullable = arg.isNullable)
+                ).build()
+            })
+            .build()
+
+        return if (action.args.isEmpty()) {
+            TypeSpec.objectBuilder(className)
+        } else {
+            TypeSpec.classBuilder(className)
+                .addModifiers(KModifier.DATA)
+                .primaryConstructor(constructorFunSpec)
+                .addProperties(action.args.map { arg ->
+                    PropertySpec.builder(
+                        arg.sanitizedName,
+                        arg.type.typeName().copy(nullable = arg.isNullable)
+                    ).initializer(arg.sanitizedName).build()
+                })
+        }.addSuperinterface(NAV_DIRECTION_CLASSNAME)
+            .addFunction(getActionIdFunSpec)
+            .addFunction(getArgumentsFunSpec)
+            .build()
     }
 
     override fun generateArgsCodeFile(destination: Destination): KotlinCodeFile {
@@ -41,7 +149,106 @@ class KotlinNavWriter(private val useAndroidX: Boolean = false) : NavWriter {
             ?: throw IllegalStateException("Destination with actions must have name")
         val className = ClassName(destName.packageName(), "${destName.simpleName()}Args")
 
+        val constructorFunSpec = FunSpec.constructorBuilder()
+            .addParameters(destination.args.map { arg ->
+                ParameterSpec.builder(
+                    name = arg.sanitizedName,
+                    type = arg.type.typeName().copy(nullable = arg.isNullable)
+                ).apply { arg.defaultValue?.let { defaultValue(it.write()) } }.build()
+            })
+            .build()
+
+        val toBundleFunSpec = FunSpec.builder("toBundle").apply {
+            if (destination.args.any { it.type is ObjectType }) {
+                addAnnotation(
+                    AnnotationSpec.builder(Suppress::class)
+                        .addMember("%S", "CAST_NEVER_SUCCEEDS")
+                        .build()
+                )
+            }
+            returns(BUNDLE_CLASSNAME)
+            val resultVal = "result"
+            addStatement("val %L = %T()", resultVal, BUNDLE_CLASSNAME)
+            destination.args.forEach { arg ->
+                arg.type.addBundlePutStatement(this, arg, resultVal, "this.${arg.sanitizedName}")
+            }
+            addStatement("return %L", resultVal)
+        }.build()
+
+                val fromBundleFunSpec = FunSpec.builder("fromBundle").apply {
+            if (destination.args.any { it.type is ObjectArrayType }) {
+                addAnnotation(
+                    AnnotationSpec.builder(Suppress::class)
+                        .addMember("%S", "UNCHECKED_CAST")
+                        .build()
+                )
+            }
+            returns(className)
+            val bundleParamName = "bundle"
+            addParameter(bundleParamName, BUNDLE_CLASSNAME)
+            addStatement(
+                "%L.setClassLoader(%T::class.java.classLoader)",
+                bundleParamName,
+                className
+            )
+            val tempVariables = destination.args.map { arg ->
+                val tempVal = "__${arg.sanitizedName}"
+                addStatement(
+                    "val %L : %T",
+                    tempVal,
+                    arg.type.typeName().copy(nullable = arg.type.allowsNullable())
+                )
+                beginControlFlow("if (%L.containsKey(%S))", bundleParamName, arg.name)
+                arg.type.addBundleGetStatement(this, arg, tempVal, bundleParamName)
+                if (arg.type.allowsNullable() && !arg.isNullable) {
+                    beginControlFlow("if (%L == null)", tempVal).apply {
+                        addStatement(
+                            "throw·%T(%S)",
+                            IllegalArgumentException::class.asTypeName(),
+                            "Argument \"${arg.name}\" is marked as non-null but was passed a " +
+                                    "null value."
+                        )
+                    }
+                    endControlFlow()
+                }
+                nextControlFlow("else")
+                val defaultValue = arg.defaultValue
+                if (defaultValue != null) {
+                    addStatement("%L = %L", tempVal, arg.defaultValue.write())
+                } else {
+                    addStatement(
+                        "throw·%T(%S)",
+                        IllegalArgumentException::class.asTypeName(),
+                        "Required argument \"${arg.name}\" is missing and does not have an " +
+                                "android:defaultValue"
+                    )
+                }
+                endControlFlow()
+                return@map tempVal
+            }
+            addStatement("return %T(${tempVariables.joinToString(", ") { it }})", className)
+        }.build()
+
+        val typeSpec = TypeSpec.classBuilder(className)
+            .addSuperinterface(NAV_ARGS_CLASSNAME)
+            .addModifiers(KModifier.DATA)
+            .primaryConstructor(constructorFunSpec)
+            .addProperties(destination.args.map { arg ->
+                PropertySpec.builder(
+                    arg.sanitizedName,
+                    arg.type.typeName().copy(nullable = arg.isNullable)
+                ).initializer(arg.sanitizedName).build()
+            })
+            .addFunction(toBundleFunSpec)
+            .addType(
+                TypeSpec.companionObjectBuilder()
+                    .addFunction(fromBundleFunSpec)
+                    .build()
+            )
+            .build()
+
         return FileSpec.builder(className.packageName, className.simpleName)
+            .addType(typeSpec)
             .build()
             .toCodeFile()
     }
