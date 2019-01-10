8293edc239a18d010db810df6f1b30066edb5c19
Daniel Santiago Rivera <danysantiago@google.com>
2019-01-10 12:59:41 -0800

    Safe Args in Kotlin - KotlinNavWriter
    Finalized the KotlinNavWriter that will generate Kotlin files. The API
    for the Kotlin version of the Directions and Args classes is different
    than the Java version. The main difference being the lack of builders
    and setter. Args and NavDirections classes are immutable data classes
    and default values are default parameter values with the idea that
    invocations are done with named parameters.
    
    This change also allows users to apply a version of the Safe Args Gradle
    Plugin that will generate code in Kotlin. The id of the plugin is
    'androidx.navigation.safeargs.kotlin'.
    
    Bug: 110263087
    Test: ./gradlew navigation:navigation-safe-args-gradle-plugin:test \
                    navigation:navigation-safe-args-generator:test
    
    Change-Id: Ifa0ec7e4bbadd2d274e04dd9ade8a2d619685bd3

---
+++ navigation/safe-args-generator/src/tests/test-data/expected/kotlin_nav_writer_test/MainFragmentArgs.kt
@@ -0,0 +1,143 @@
+package a.b
+
+import android.content.pm.ActivityInfo
+import android.os.Bundle
+import android.os.Parcelable
+import androidx.navigation.NavArgs
+import java.io.Serializable
+import java.lang.IllegalArgumentException
+import java.lang.UnsupportedOperationException
+import java.nio.file.AccessMode
+import kotlin.Array
+import kotlin.Boolean
+import kotlin.Float
+import kotlin.FloatArray
+import kotlin.Int
+import kotlin.String
+import kotlin.Suppress
+
+data class MainFragmentArgs(
+    val main: String,
+    val optional: Int = -1,
+    val reference: Int = a.b.R.drawable.background,
+    val floatArg: Float = 1F,
+    val floatArrayArg: FloatArray,
+    val objectArrayArg: Array<ActivityInfo>,
+    val boolArg: Boolean = true,
+    val optionalParcelable: ActivityInfo? = null,
+    val enumArg: AccessMode = AccessMode.READ
+) : NavArgs {
+    @Suppress("UNCHECKED_CAST")
+    constructor(bundle: Bundle) : this(bundle.run {
+        setClassLoader(MainFragmentArgs::class.java.classLoader)
+        if (containsKey("main")) {
+            getString("main")
+                ?: throw IllegalArgumentException("Argument \"main\" is marked as non-null but was passed a null value.")
+        } else {
+            throw IllegalArgumentException("Required argument \"main\" is missing and does not have an android:defaultValue")
+        }
+    }
+    , bundle.run {
+        setClassLoader(MainFragmentArgs::class.java.classLoader)
+        if (containsKey("optional")) {
+            getInt("optional")
+        } else {
+            -1
+        }
+    }
+    , bundle.run {
+        setClassLoader(MainFragmentArgs::class.java.classLoader)
+        if (containsKey("reference")) {
+            getInt("reference")
+        } else {
+            a.b.R.drawable.background
+        }
+    }
+    , bundle.run {
+        setClassLoader(MainFragmentArgs::class.java.classLoader)
+        if (containsKey("floatArg")) {
+            getFloat("floatArg")
+        } else {
+            1F
+        }
+    }
+    , bundle.run {
+        setClassLoader(MainFragmentArgs::class.java.classLoader)
+        if (containsKey("floatArrayArg")) {
+            getFloatArray("floatArrayArg")
+                ?: throw IllegalArgumentException("Argument \"floatArrayArg\" is marked as non-null but was passed a null value.")
+        } else {
+            throw IllegalArgumentException("Required argument \"floatArrayArg\" is missing and does not have an android:defaultValue")
+        }
+    }
+    , bundle.run {
+        setClassLoader(MainFragmentArgs::class.java.classLoader)
+        if (containsKey("objectArrayArg")) {
+            getParcelableArray("objectArrayArg") as Array<ActivityInfo>?
+                ?: throw IllegalArgumentException("Argument \"objectArrayArg\" is marked as non-null but was passed a null value.")
+        } else {
+            throw IllegalArgumentException("Required argument \"objectArrayArg\" is missing and does not have an android:defaultValue")
+        }
+    }
+    , bundle.run {
+        setClassLoader(MainFragmentArgs::class.java.classLoader)
+        if (containsKey("boolArg")) {
+            getBoolean("boolArg")
+        } else {
+            true
+        }
+    }
+    , bundle.run {
+        setClassLoader(MainFragmentArgs::class.java.classLoader)
+        if (containsKey("optionalParcelable")) {
+            if (Parcelable::class.java.isAssignableFrom(ActivityInfo::class.java) ||
+                    Serializable::class.java.isAssignableFrom(ActivityInfo::class.java)) {
+                get("optionalParcelable") as ActivityInfo?
+            } else {
+                throw UnsupportedOperationException(ActivityInfo::class.java.name +
+                        " must implement Parcelable or Serializable or must be an Enum.")
+            }
+        } else {
+            null
+        }
+    }
+    , bundle.run {
+        setClassLoader(MainFragmentArgs::class.java.classLoader)
+        if (containsKey("enumArg")) {
+            if (Parcelable::class.java.isAssignableFrom(AccessMode::class.java) ||
+                    Serializable::class.java.isAssignableFrom(AccessMode::class.java)) {
+                get("enumArg") as AccessMode?
+            } else {
+                throw UnsupportedOperationException(AccessMode::class.java.name +
+                        " must implement Parcelable or Serializable or must be an Enum.")
+            }
+                ?: throw IllegalArgumentException("Argument \"enumArg\" is marked as non-null but was passed a null value.")
+        } else {
+            AccessMode.READ
+        }
+    }
+    )
+
+    @Suppress("CAST_NEVER_SUCCEEDS")
+    fun toBundle(): Bundle {
+        val result = Bundle()
+        result.putString("main", this.main)
+        result.putInt("optional", this.optional)
+        result.putInt("reference", this.reference)
+        result.putFloat("floatArg", this.floatArg)
+        result.putFloatArray("floatArrayArg", this.floatArrayArg)
+        result.putParcelableArray("objectArrayArg", this.objectArrayArg)
+        result.putBoolean("boolArg", this.boolArg)
+        if (Parcelable::class.java.isAssignableFrom(ActivityInfo::class.java)) {
+            result.putParcelable("optionalParcelable", this.optionalParcelable as Parcelable?)
+        } else if (Serializable::class.java.isAssignableFrom(ActivityInfo::class.java)) {
+            result.putSerializable("optionalParcelable", this.optionalParcelable as Serializable?)
+        }
+        if (Parcelable::class.java.isAssignableFrom(AccessMode::class.java)) {
+            result.putParcelable("enumArg", this.enumArg as Parcelable)
+        } else if (Serializable::class.java.isAssignableFrom(AccessMode::class.java)) {
+            result.putSerializable("enumArg", this.enumArg as Serializable)
+        }
+        return result
+    }
+}
