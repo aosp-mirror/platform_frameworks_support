7480c52b69fd2ee0d224b58ba29c439a6257acfb
Yigit Boyar <yboyar@google.com>
2019-01-11 16:25:12 -0800

    WIP lifecycle scopes
    Bug: 122740811
    Test: gw :life:integ:l-t-k:cC :life:integ:l-t-k:test
    
    Change-Id: Id565518d9191f21335c793e77d3509b3f3779bbb

---
+++ lifecycle/common/ktx/src/main/java/androidx/lifecycle/Lifecycle.kt
@@ -0,0 +1,140 @@
+/*
+ * Copyright 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.lifecycle
+
+import kotlinx.coroutines.CancellableContinuation
+import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.suspendCancellableCoroutine
+import kotlinx.coroutines.withContext
+import java.util.concurrent.locks.ReentrantLock
+import kotlin.concurrent.withLock
+import kotlin.coroutines.CoroutineContext
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
+
+private val SCOPE_LOCK = ReentrantLock()
+
+/**
+ * [CoroutineScope] tied to this [Lifecycle].
+ *
+ * This scope will be canceled when the [Lifecycle] is destroyed.
+ *
+ * This scope is bound to  [Dispatchers.Main]
+ */
+val Lifecycle.scope: CoroutineScope
+    get() {
+        return (this.mInternalScope as? LifecycleCoroutineScope) ?: SCOPE_LOCK.withLock {
+            (this.mInternalScope as? LifecycleCoroutineScope) ?: LifecycleCoroutineScope(
+                this,
+                Job() + Dispatchers.Main
+            ).also {
+                this.mInternalScope = it
+            }
+        }
+    }
+
+internal class LifecycleCoroutineScope(
+    lifecycle: Lifecycle,
+    context: CoroutineContext
+) : CoroutineScope, DefaultLifecycleObserver {
+    override val coroutineContext: CoroutineContext = context
+
+    init {
+        // in case we are initialized on a non-main thread, make a best effort check before
+        // we return the scope. This is not sync but if developer is launching on a non-main
+        // dispatcher, they cannot be 100% sure anyways.
+        if (!lifecycle.currentState.isAtLeast(Lifecycle.State.INITIALIZED)) {
+            coroutineContext.cancel()
+        } else {
+            launch(Dispatchers.Main) {
+                if (lifecycle.currentState.isAtLeast(Lifecycle.State.INITIALIZED)) {
+                    lifecycle.addObserver(this@LifecycleCoroutineScope)
+                } else {
+                    coroutineContext.cancel()
+                }
+            }
+        }
+    }
+
+    override fun onDestroy(owner: LifecycleOwner) {
+        owner.lifecycle.removeObserver(this)
+        coroutineContext.cancel()
+    }
+}
+
+/**
+ * Runs the given [block] on [Dispatchers.Main] when the [Lifecycle] is at least in the
+ * [Lifecycle.State.STARTED] state. If the [Lifecycle] is already destroyed, this method will throw
+ * [LifecycleDestroyedException] which is a specific type of [CancellationException].
+ *
+ * If the [Lifecycle] is not started yet, this method will suspend until the [Lifecycle] starts.
+ * If it gets destroyed before it starts, this method will fail with [LifecycleDestroyedException].
+ *
+ * You can call this method in any thread but the provided [block] will always be run on the main
+ * dispatcher.
+ */
+suspend inline fun <OUT> Lifecycle.whenStarted(crossinline block: () -> OUT): OUT =
+    withContext(Dispatchers.Main) {
+        if (currentState.isAtLeast(Lifecycle.State.STARTED)) {
+            // fast path
+            block()
+        } else if (currentState == Lifecycle.State.DESTROYED) {
+            throw LifecycleDestroyedException()
+        } else {
+            suspendCancellableCoroutine<OUT> { cancellable ->
+                addObserver(object : DefaultLifecycleObserver {
+                    override fun onDestroy(owner: LifecycleOwner) {
+                        removeObserver(this)
+                        cancellable.cancel(
+                            LifecycleDestroyedException()
+                        )
+                    }
+
+                    override fun onStart(owner: LifecycleOwner) {
+                        removeObserver(this)
+                        runCatching(cancellable, block)
+                    }
+                })
+            }
+        }
+    }
+
+@PublishedApi
+internal inline fun <OUT> runCatching(
+    cancellable: CancellableContinuation<OUT>,
+    crossinline block: () -> OUT
+) {
+    try {
+        cancellable.resume(block())
+    } catch (t: Throwable) {
+        if (cancellable.isActive) {
+            cancellable.resumeWithException(t)
+        }
+    }
+}
+
+/**
+ * Thrown when a coroutine is cancelled because it is scoped to a [Lifecycle] that got destroyed.
+ */
+class LifecycleDestroyedException : CancellationException(
+    "Attached Lifecycle is destroyed"
+)
---
+++ lifecycle/integration-tests/kotlintestapp/src/androidTest/java/androidx/lifecycle/test/LifecycleWhenStartedTest.kt
@@ -0,0 +1,145 @@
+/*
+ * Copyright 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.lifecycle.test
+
+import androidx.lifecycle.Lifecycle.State.CREATED
+import androidx.lifecycle.Lifecycle.State.DESTROYED
+import androidx.lifecycle.Lifecycle.State.RESUMED
+import androidx.lifecycle.Lifecycle.State.STARTED
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.kotlintestapp.TestActivity
+import androidx.lifecycle.whenStarted
+import androidx.test.ext.junit.rules.activityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import androidx.test.filters.SmallTest
+import com.google.common.truth.Truth.assertThat
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.async
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
+import org.junit.After
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import java.util.concurrent.CancellationException
+
+@ExperimentalCoroutinesApi
+@SmallTest
+@RunWith(AndroidJUnit4::class)
+class LifecycleWhenStartedTest {
+    private val testScope = CoroutineScope(Job() + Dispatchers.Default)
+
+    @JvmField
+    @Rule
+    val rule = activityScenarioRule<TestActivity>()
+
+    @After
+    fun cancelScope() {
+        testScope.cancel()
+    }
+
+    @Test
+    fun alreadyResumed() = runBlocking {
+        rule.scenario.moveToState(RESUMED)
+        assertThat(
+            activity().whenStarted {
+                true
+            }
+        ).isTrue()
+    }
+
+    @Test
+    fun alreadyStarted() = runBlocking {
+        rule.scenario.moveToState(STARTED)
+        assertThat(
+            activity().whenStarted {
+                true
+            }
+        ).isTrue()
+    }
+
+    @Test
+    fun alreadyDestroyed() = runBlocking {
+        val owner = activity() // grab it before destroying
+        rule.scenario.moveToState(DESTROYED)
+        try {
+            owner.whenStarted {
+                throw AssertionError("should've been cancelled")
+            }
+        } catch (cancellation: CancellationException) {
+        }
+    }
+
+    @Test
+    fun startOnMainThread() = runBlocking {
+        rule.scenario.moveToState(RESUMED)
+        val owner = activity()
+        assertThat(
+            testScope.async(Dispatchers.Main) {
+                owner.whenStarted {
+                    true
+                }
+            }.await()
+        ).isTrue()
+    }
+
+    @Test
+    @LargeTest
+    fun waitUntilStarted(): Unit = runBlocking {
+        rule.scenario.moveToState(CREATED)
+        val action = testScope.async {
+            activity().whenStarted {
+                true
+            }
+        }
+        delay(2_000) // wait to ensure it is not running
+        assertThat(action.isActive).isTrue()
+        rule.scenario.moveToState(RESUMED)
+        assertThat(action.await()).isTrue()
+    }
+
+    @Test
+    @LargeTest
+    fun waitUntilDestroyed(): Unit = runBlocking {
+        rule.scenario.moveToState(CREATED)
+        val action = testScope.async {
+            activity().whenStarted {
+                throw AssertionError("should've never run")
+            }
+        }
+        // We don't have an API to check if all dispatchers are IDLE so we'll simply delay
+        // until a better API is available.
+        delay(2_000)
+        assertThat(action.isActive).isTrue()
+        rule.scenario.moveToState(DESTROYED)
+        action.join()
+        assertThat(action.isCancelled).isTrue()
+    }
+
+    private fun activity(): LifecycleOwner {
+        lateinit var owner: LifecycleOwner
+        rule.scenario.onActivity {
+            owner = it
+        }
+        return owner
+    }
+}
