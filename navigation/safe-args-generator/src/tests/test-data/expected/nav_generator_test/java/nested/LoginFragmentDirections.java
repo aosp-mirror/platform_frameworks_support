db2add52eab1f09622508ea9963e83b3c0f816c3
Daniel Santiago Rivera <danysantiago@google.com>
2019-01-23 15:46:00 -0800

    Add missing import of parent Directions class in generated Safe Args.
    When generating a parent direction action method in a child directions
    class,correctly import the parent direction if its located in another
    package. Both JavaPoet and KotlinPoet generate import statements but the
    NavWriters were incorrectly creating the class name of the parent type,
    specifically it missed the package name which caused the import to be
    missing.
    
    Bug: 123307342
    Test: ./gradlew navigation:navigation-safe-args-gradle-plugin:test \
                    navigation:navigation-safe-args-generator:test
    
    Change-Id: Ibeda7a7ec6f31bb1c860dfe1f5cd2a7e7ea6d47d

--- navigation/safe-args-generator/src/tests/test-data/expected/nav_generator_test/java/nested/LoginFragmentDirections.java
+++ navigation/safe-args-generator/src/tests/test-data/expected/nav_generator_test/java/nested/LoginFragmentDirections.java
@@ -1,24 +1,9 @@
-/*
- * Copyright 2019 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-package foo.flavor;
+package foo.flavor.account;
 
 import android.support.annotation.NonNull;
 import androidx.navigation.ActionOnlyNavDirections;
 import androidx.navigation.NavDirections;
+import foo.LoginDirections;
 
 public class LoginFragmentDirections {
     private LoginFragmentDirections() {
