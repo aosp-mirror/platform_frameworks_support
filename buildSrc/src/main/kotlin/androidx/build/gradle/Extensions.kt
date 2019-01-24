75735ca6fd0cf4f3b5efcc4fc824132a65228447
Louis Pullen-Freilich <lpf@google.com>
2019-01-24 18:48:57 +0000

    Updates annotations for tests previously marked small/medium
    This cl does not cover tests previously annotated as @LargeTest that can
    be bumped down, nor does it touch tests that do not currently have any
    annotations.  There's a lot of variance in test timing, so some of these
    tests may still move around a lot, but this should as an initial pass
    cut down presubmit times a lot.
    
    SmallTest = < 0.2 seconds
    MediumTest = < 1 second
    LargeTest = > 1 second
    
    Bug: b/123402142
    Test: ./gradlew connectedCheck --daemon -Pandroid.testInstrumentationRunnerArguments.size=small -Pandroid.testInstrumentationRunnerArguments.timeout_msec=200 --continue
    Change-Id: Id9eae3cf720329c0788e27a062d543cc6a2f9852

--- buildSrc/src/main/kotlin/androidx/build/gradle/Extensions.kt
+++ buildSrc/src/main/kotlin/androidx/build/gradle/Extensions.kt
@@ -19,6 +19,8 @@ package androidx.build.gradle
 import org.gradle.api.Project
 import org.gradle.api.plugins.ExtensionContainer
 
+val test = 0
+
 inline fun <reified T : Any> ExtensionContainer.getByType(): T = getByType(T::class.java)
 
 val Project.isRoot get() = this == rootProject
