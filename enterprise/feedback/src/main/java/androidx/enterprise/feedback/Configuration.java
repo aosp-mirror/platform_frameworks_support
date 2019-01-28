6f4b2d077ae945bfe0f54cd585d51814b153dc16
Jonathan Scott <scottjonathan@google.com>
2019-01-28 15:50:36 +0000

    Fix potential race conditions in BufferedServiceConnection.
    We know that the callbacks in ServiceConnection will always be called on the main thread - but do not know which thread will be used for the calls to BufferedServiceConnection.
    
    Test: ./gradlew test
    Change-Id: I8c62d83d7e9d5f765c2697f4e49f7005e207ad8d

---
+++ enterprise/feedback/src/main/java/androidx/enterprise/feedback/Configuration.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.enterprise.feedback;
+
+import androidx.annotation.NonNull;
+
+import com.google.auto.value.AutoValue;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Configuration values for the {@link KeyedAppStatesReporter} instance.
+ */
+@AutoValue
+public abstract class Configuration {
+    private static final int EXECUTOR_IDLE_ALIVE_TIME_SECS = 20;
+    // Create a no-args constructor so it doesn't appear in current.txt
+    Configuration() { }
+
+    /** Create a {@link ConfigurationBuilder}. */
+    @NonNull
+    public static ConfigurationBuilder builder() {
+        return new AutoValue_Configuration.Builder();
+    }
+
+    static Configuration createDefaultConfiguration() {
+        return builder()
+            .setExecutor(createExecutorService())
+            .build();
+    }
+
+    /**
+     * Creates an {@link ExecutorService} which has no persistent background thread, and ensures
+     * tasks will run in submit order.
+     */
+    private static ExecutorService createExecutorService() {
+        return new ThreadPoolExecutor(
+            /* corePoolSize= */ 0,
+            /* maximumPoolSize= */ 1,
+            EXECUTOR_IDLE_ALIVE_TIME_SECS,
+            TimeUnit.SECONDS,
+            new LinkedBlockingQueue<Runnable>() /* Not used */);
+    }
+
+    /**
+     * Set the executor to use.
+     *
+     * <p>To set a custom executor, the executor must run all {@link Runnable} instances on the same
+     * thread, serially.
+     *
+     * <p>If this method is not called, the reporter will run on a newly-created thread.
+     * This newly-created thread will be cleaned up and recreated as necessary when idle.
+     */
+    @NonNull
+    public abstract Executor executor();
+
+    /** The builder for {@link Configuration}. */
+    @AutoValue.Builder
+    public abstract static class ConfigurationBuilder {
+        // Create a no-args constructor so it doesn't appear in current.txt
+        ConfigurationBuilder() { }
+
+        /** Set {@link Configuration#executor()}. */
+        @NonNull
+        public abstract ConfigurationBuilder setExecutor(@NonNull Executor executor);
+
+        /** Instantiate the {@link Configuration}. */
+        public abstract Configuration build();
+    }
+}
