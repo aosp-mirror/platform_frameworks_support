bc10bd4eed7947990af79f6c2785c8e83cac1932
Ian Lake <ilake@google.com>
2019-01-29 14:52:09 -0800

    Add a default implementation of set/getFragmentFactory
    Instead of throwing an UnsupportedOperationException,
    provide a default implementation for those cases where
    libraries have directly extended FragmentManager and
    don't rely on FragmentManagerImpl.
    
    Test: ./gradlew bOS
    Change-Id: Id47406ab3626809793298da689cb9bd4c3945008
    Fixes: 123291927

--- fragment/src/main/java/androidx/fragment/app/FragmentManager.java
+++ fragment/src/main/java/androidx/fragment/app/FragmentManager.java
@@ -112,6 +112,8 @@ public abstract class FragmentManager {
         public void onBackStackChanged();
     }
 
+    private FragmentFactory mFragmentFactory = null;
+
     /**
      * Start a series of edit operations on the Fragments associated with
      * this FragmentManager.
@@ -389,15 +391,12 @@ public abstract class FragmentManager {
 
     /**
      * Set a {@link FragmentFactory} for this FragmentManager that will be used
-     * to create new Fragment instances from this point onward. Any child
-     * FragmentManager that does not have a custom FragmentFactory set will also use
-     * this same FragmentFactory.
+     * to create new Fragment instances from this point onward.
      *
      * @param fragmentFactory the factory to use to create new Fragment instances
      */
     public void setFragmentFactory(@NonNull FragmentFactory fragmentFactory) {
-        throw new UnsupportedOperationException(
-                "This method must be overridden to provide an implementation");
+        mFragmentFactory = fragmentFactory;
     }
 
     /**
@@ -407,8 +406,10 @@ public abstract class FragmentManager {
      */
     @NonNull
     public FragmentFactory getFragmentFactory() {
-        throw new UnsupportedOperationException(
-                "This method must be overridden to provide an implementation");
+        if (mFragmentFactory == null) {
+            mFragmentFactory = new FragmentFactory();
+        }
+        return mFragmentFactory;
     }
 
     /**
