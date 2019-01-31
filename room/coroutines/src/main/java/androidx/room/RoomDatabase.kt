f214084dbb64b1d08e98a56d6b08455dd980cdc4
Daniel Santiago Rivera <danysantiago@google.com>
2019-01-31 12:57:58 -0800

    Room Suspending Transaction
    Create an API for performing database transaction with coroutines. When
    starting a transaction coroutine a thread of the database query executor
    is hold for performing all database operations within the coroutine. It
    is recommended that all DAO functions invoked are also suspend
    functions. In the case they are blocking DAO methods, then invoking
    those on a different context other than the transaction context will
    throw an exception, preventing possible deadlocks.
    
    Bug: 120854786
    Test: ./gradlew room:room-compiler:test
                    room:integration-tests:room-testapp-kotlin:cC
                    room:integration-tests:room-testapp:cC
    
    Change-Id: I30f76db29a3882968f43e1dd1c1038b5afd7f299

---
+++ room/coroutines/src/main/java/androidx/room/RoomDatabase.kt
@@ -0,0 +1,172 @@
+/*
+ * Copyright 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.room
+
+import androidx.annotation.RestrictTo
+import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Runnable
+import kotlinx.coroutines.asContextElement
+import kotlinx.coroutines.asCoroutineDispatcher
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.suspendCancellableCoroutine
+import kotlinx.coroutines.withContext
+import java.util.concurrent.BlockingQueue
+import java.util.concurrent.Executor
+import java.util.concurrent.LinkedBlockingQueue
+import java.util.concurrent.RejectedExecutionException
+import java.util.concurrent.atomic.AtomicBoolean
+import kotlin.coroutines.CoroutineContext
+import kotlin.coroutines.coroutineContext
+import kotlin.coroutines.resume
+
+/**
+ * Calls the specified suspending [block] in a database transaction. The transaction will be
+ * marked as successful unless an exception is thrown in the suspending [block].
+ *
+ * Performing blocking database operations is not permitted in a coroutine scope other than the
+ * one received by the suspending block. It is recommended that all [Dao] function invoked within
+ * the [block] be suspending functions.
+ */
+suspend fun <R> RoomDatabase.runSuspendingTransaction(block: suspend CoroutineScope.() -> R): R {
+    // Use inherited transaction context if available, this allows nested suspending transactions.
+    val transactionContext =
+        coroutineContext[TransactionElement]?.transactionContext ?: createTransactionContext(this)
+    return withContext(transactionContext) {
+        val transactionElement = coroutineContext[TransactionElement]!!
+        try {
+            beginTransaction()
+            try {
+                // Wrap suspending block in a new scope to wait for any child coroutine.
+                val result = coroutineScope {
+                    block.invoke(this)
+                }
+                setTransactionSuccessful()
+                return@withContext result
+            } finally {
+                endTransaction()
+            }
+        } finally {
+            val inTransaction = try { inTransaction() } catch (ex: RuntimeException) { false }
+            if (!inTransaction) {
+                // Last transaction, release transaction thread.
+                transactionElement.release()
+            }
+        }
+    }
+}
+
+/**
+ * Creates a [CoroutineContext] for performing database operations within a coroutine transaction.
+ *
+ * The context is a combination of a dispatcher, a [TransactionElement] and a thread local element.
+ *
+ * * The dispatcher will dispatch coroutines to a single thread that is taken over from the Room
+ * query executor. If the coroutine context is switched, suspending DAO functions will be able to
+ * dispatch to the transaction thread.
+ *
+ * * The [TransactionElement] serves as an indicator for inherited context, meaning, if there is a
+ * switch of context, suspending DAO methods will be able to use the indicator to dispatch the
+ * database operation to the transaction thread.
+ *
+ * * The thread local element serves as a second indicator and marks threads that are used to
+ * execute coroutines within the coroutine transaction, more specifically it allows us to identify
+ * if a blocking DAO method is invoked within the transaction coroutine. The value itself is
+ * currently meaningless, for now all we care is if its present or not.
+ */
+private suspend fun createTransactionContext(db: RoomDatabase): CoroutineContext {
+    val isReleased = AtomicBoolean(false)
+    val runnableQueue = LinkedBlockingQueue<Runnable>()
+    try {
+        // Suspend until we acquire a thread from the query executor, preventing the queue from
+        // being filled until we are ready to dispatch.
+        suspendCancellableCoroutine<Unit> { continuation ->
+            try {
+                db.queryExecutor.execute {
+                    // thread acquired, resume coroutine
+                    continuation.resume(Unit)
+                    do {
+                        val block = runnableQueue.take()
+                        if (block == TransactionElement.RELEASE) {
+                            break
+                        } else {
+                            block.run()
+                        }
+                    } while (true)
+                }
+            } catch (ex: RejectedExecutionException) {
+                // Couldn't acquire a thread, cancel coroutine.
+                continuation.cancel(
+                    IllegalStateException(
+                        "Unable to acquire a thread to perform the database transaction.", ex)
+                )
+            }
+        }
+    } catch (cancellation: CancellationException) {
+        // We got cancelled while waiting to acquire a thread, we can't stop our attempt to acquire
+        // a thread, but we can mark it as released so once it gets acquired it is quickly released.
+        isReleased.set(true)
+        runnableQueue.put(TransactionElement.RELEASE)
+        throw cancellation
+    }
+    val dispatcher = Executor { command ->
+        if (isReleased.get()) {
+            // Thread has been released, reject execution to allow dispatcher to resume finally
+            // blocks and cancel the coroutine.
+            throw RejectedExecutionException()
+        } else {
+            runnableQueue.put(command)
+        }
+    }.asCoroutineDispatcher()
+    val transactionElement = TransactionElement(dispatcher, isReleased, runnableQueue)
+    val threadLocalElement =
+        db.suspendingTransactionId.asContextElement(System.identityHashCode(dispatcher))
+    return dispatcher + transactionElement + threadLocalElement
+}
+
+/**
+ * A [CoroutineContext.Element] that indicates there is an on-going database transaction.
+ *
+ * @hide
+ */
+@RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
+internal class TransactionElement(
+    internal val transactionContext: CoroutineContext,
+    private val transactionReleasedIndicator: AtomicBoolean,
+    private val transactionQueue: BlockingQueue<Runnable>
+) : CoroutineContext.Element {
+
+    companion object Key : CoroutineContext.Key<TransactionElement>
+
+    override val key: CoroutineContext.Key<TransactionElement>
+        get() = TransactionElement
+
+    fun release() {
+        if (transactionReleasedIndicator.get()) {
+            throw IllegalStateException("Release was already called on this transaction.")
+        } else {
+            transactionReleasedIndicator.set(true)
+            transactionQueue.put(RELEASE)
+        }
+    }
+
+    object RELEASE : Runnable {
+        override fun run() {
+            throw IllegalStateException("The release runnable should never be run.")
+        }
+    }
+}
