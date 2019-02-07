9d65b2726afe81de80f4e279b5a30e736359b34f
Anthony Chen <ajchen@google.com>
2019-02-07 11:10:51 -0800

    Add a tab bar view.
    The tab bar will keep track of what tab is currently selected and style
    it appropriately.
    
    Otherwise, it is left up to the user to pass the number of items, the
    tab content and to hook up a listener for when a tab is selected.
    
    Bug: 123646457
    Test: ./gradlew car:connectedCheck
    Test: Verify on newly created demo activity.
    Change-Id: I3dc4cd0eb67553010b0133416b96ef9deaf15149

---
+++ car/core/src/androidTest/java/androidx/car/widget/CarTabBarViewTest.java
@@ -0,0 +1,376 @@
+/*
+ * Copyright (C) 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.car.widget;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.doesNotExist;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withAlpha;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withParent;
+import static androidx.test.espresso.matcher.ViewMatchers.withTagKey;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+import static junit.framework.TestCase.assertFalse;
+import static junit.framework.TestCase.assertTrue;
+
+import static org.hamcrest.CoreMatchers.allOf;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.content.res.Resources;
+import android.graphics.drawable.Icon;
+import android.util.TypedValue;
+
+import androidx.car.test.R;
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.MediumTest;
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.rule.ActivityTestRule;
+
+import org.junit.Assume;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Unit tests for {@link CarTabBarViewTest}.
+ */
+@RunWith(AndroidJUnit4.class)
+@MediumTest
+public class CarTabBarViewTest {
+    @Rule
+    public ActivityTestRule<CarTabBarViewTestActivity> mActivityRule =
+            new ActivityTestRule<>(CarTabBarViewTestActivity.class);
+
+    private Context mContext;
+    private CarTabBarView mTabBarView;
+
+    /** The alpha for a tab that is considered selected. */
+    private float mSelectedAlpha;
+
+    /** The alpha for a tab that is not currently selected. */
+    private float mInactiveAlpha;
+
+    @Before
+    public void setUp() {
+        Assume.assumeTrue(isAutoDevice());
+
+        Activity activity = mActivityRule.getActivity();
+        mTabBarView = activity.findViewById(R.id.car_tab_bar);
+        mContext = activity;
+
+        Resources res = mContext.getResources();
+        TypedValue outValue = new TypedValue();
+        res.getValue(R.dimen.selected_tab_alpha, outValue, true);
+        mSelectedAlpha = outValue.getFloat();
+
+        res.getValue(R.dimen.inactive_tab_alpha, outValue, true);
+        mInactiveAlpha = outValue.getFloat();
+    }
+
+    @Test
+    public void testMinimumHeight_fixedHeight() throws Throwable {
+        // Set all widgets to null - tab bar should still be minimum-height tall.
+        mActivityRule.runOnUiThread(() -> mTabBarView.setTabs(null));
+
+        InstrumentationRegistry.getInstrumentation().waitForIdleSync();
+
+        int expected = mContext.getResources().getDimensionPixelSize(R.dimen.car_app_bar_height);
+        assertThat(mTabBarView.getHeight(), is(equalTo(expected)));
+    }
+
+    @Test
+    public void testNullTabs_noTabsAdded() throws Throwable {
+        mActivityRule.runOnUiThread(() -> mTabBarView.setTabs(null));
+        onView(withId(R.id.car_tab)).check(doesNotExist());
+    }
+
+    @Test
+    public void testSingleTab_addedAsChild() throws Throwable {
+        CarTabItem tab = new CarTabItem.Builder()
+                .setIcon(Icon.createWithResource(mContext, android.R.drawable.sym_def_app_icon))
+                .setText("text")
+                .build();
+
+        mActivityRule.runOnUiThread(() -> mTabBarView.setTabs(Arrays.asList(tab)));
+
+        onView(withId(R.id.car_tab)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void testSingleTab_hasTextContent() throws Throwable {
+        String tabText = "tabText";
+
+        CarTabItem tab = new CarTabItem.Builder()
+                .setText(tabText)
+                .build();
+
+        mActivityRule.runOnUiThread(() -> mTabBarView.setTabs(Arrays.asList(tab)));
+
+        onView(allOf(withId(R.id.car_tab_text), withParent(withId(R.id.car_tab))))
+                .check(matches(withText(tabText)))
+                .check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void testSingleTab_emptyText() throws Throwable {
+        CarTabItem tab = new CarTabItem.Builder().build();
+
+        mActivityRule.runOnUiThread(() -> mTabBarView.setTabs(Arrays.asList(tab)));
+
+        onView(allOf(withId(R.id.car_tab_text), withParent(withId(R.id.car_tab))))
+                .check(matches(not(isDisplayed())));
+    }
+
+    @Test
+    public void testSingleTab_hasIcon() throws Throwable {
+        CarTabItem tab = new CarTabItem.Builder()
+                .setIcon(Icon.createWithResource(mContext, android.R.drawable.sym_def_app_icon))
+                .build();
+
+        mActivityRule.runOnUiThread(() -> mTabBarView.setTabs(Arrays.asList(tab)));
+
+        onView(allOf(withId(R.id.car_tab_icon), withParent(withId(R.id.car_tab))))
+                .check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void testSingleTab_emptyIcon() throws Throwable {
+        CarTabItem tab = new CarTabItem.Builder().build();
+
+        mActivityRule.runOnUiThread(() -> mTabBarView.setTabs(Arrays.asList(tab)));
+
+        onView(allOf(withId(R.id.car_tab_icon), withParent(withId(R.id.car_tab))))
+                .check(matches(not(isDisplayed())));
+    }
+
+    @Test
+    public void testMultipleTabs_addsTabsAsChildren() throws Throwable {
+        List<CarTabItem> tabs = new ArrayList<>();
+
+        int numOfTabs = 5;
+        for (int i = 0; i < numOfTabs; i++) {
+            Icon icon = Icon.createWithResource(mContext, android.R.drawable.sym_def_app_icon);
+            tabs.add(new CarTabItem.Builder()
+                    .setIcon(icon)
+                    .setText("text")
+                    .build());
+        }
+
+        mActivityRule.runOnUiThread(() -> mTabBarView.setTabs(tabs));
+
+        for (int i = 0; i < numOfTabs; i++) {
+            assertTabAtPositionIsDisplayed(/* position= */ i);
+        }
+    }
+
+    @Test
+    public void testTabSelection_firstTabHighlightedByDefault() throws Throwable {
+        List<CarTabItem> tabs = new ArrayList<>();
+
+        int numOfTabs = 3;
+        for (int i = 0; i < numOfTabs; i++) {
+            Icon icon = Icon.createWithResource(mContext, android.R.drawable.sym_def_app_icon);
+            tabs.add(new CarTabItem.Builder()
+                    .setIcon(icon)
+                    .setText("text")
+                    .build());
+        }
+
+        mActivityRule.runOnUiThread(() -> mTabBarView.setTabs(tabs));
+
+        assertTabIsDisplayedWithAlpha(/* position= */ 0, mSelectedAlpha);
+        assertTabIsDisplayedWithAlpha(/* position= */ 1, mInactiveAlpha);
+        assertTabIsDisplayedWithAlpha(/* position= */ 2, mInactiveAlpha);
+    }
+
+    @Test
+    public void testTabSelection_highlightsSelectedTabAfterClick() throws Throwable {
+        List<CarTabItem> tabs = new ArrayList<>();
+
+        int numOfTabs = 3;
+        for (int i = 0; i < numOfTabs; i++) {
+            Icon icon = Icon.createWithResource(mContext, android.R.drawable.sym_def_app_icon);
+            tabs.add(new CarTabItem.Builder()
+                    .setIcon(icon)
+                    .setText("text")
+                    .build());
+        }
+
+        mActivityRule.runOnUiThread(() -> mTabBarView.setTabs(tabs));
+
+        // Click the tab at position 1.
+        onView(allOf(withId(R.id.car_tab), withTagKey(R.id.car_tab_position, is(1))))
+                .perform(click());
+
+        // Verify position 1 is selected.
+        assertTabIsDisplayedWithAlpha(/* position= */ 0, mInactiveAlpha);
+        assertTabIsDisplayedWithAlpha(/* position= */ 1, mSelectedAlpha);
+        assertTabIsDisplayedWithAlpha(/* position= */ 2, mInactiveAlpha);
+    }
+
+    @Test
+    public void testTabSelection_manuallySetSelectedTab() throws Throwable {
+        List<CarTabItem> tabs = new ArrayList<>();
+
+        int numOfTabs = 3;
+        for (int i = 0; i < numOfTabs; i++) {
+            Icon icon = Icon.createWithResource(mContext, android.R.drawable.sym_def_app_icon);
+            tabs.add(new CarTabItem.Builder()
+                    .setIcon(icon)
+                    .setText("text")
+                    .build());
+        }
+
+        // Manually set the tab at position 1 to be selected.
+        mActivityRule.runOnUiThread(() -> {
+            mTabBarView.setSelectedTab(1);
+            mTabBarView.setTabs(tabs);
+        });
+
+        // Verify position 1 is selected.
+        assertTabIsDisplayedWithAlpha(/* position= */ 0, mInactiveAlpha);
+        assertTabIsDisplayedWithAlpha(/* position= */ 1, mSelectedAlpha);
+        assertTabIsDisplayedWithAlpha(/* position= */ 2, mInactiveAlpha);
+    }
+
+    @Test
+    public void testTabSelection_manuallySetSelectedTab_outOfBounds() throws Throwable {
+        List<CarTabItem> tabs = new ArrayList<>();
+
+        int numOfTabs = 3;
+        for (int i = 0; i < numOfTabs; i++) {
+            Icon icon = Icon.createWithResource(mContext, android.R.drawable.sym_def_app_icon);
+            tabs.add(new CarTabItem.Builder()
+                    .setIcon(icon)
+                    .setText("text")
+                    .build());
+        }
+
+        // Manually set the selected tab to be a position greater than the number of tabs.
+        mActivityRule.runOnUiThread(() -> {
+            mTabBarView.setSelectedTab(numOfTabs);
+            mTabBarView.setTabs(tabs);
+        });
+
+        // No tab should be selected.
+        assertTabIsDisplayedWithAlpha(/* position= */ 0, mInactiveAlpha);
+        assertTabIsDisplayedWithAlpha(/* position= */ 1, mInactiveAlpha);
+        assertTabIsDisplayedWithAlpha(/* position= */ 2, mInactiveAlpha);
+    }
+
+    @Test
+    public void testTabSelection_manuallySetSelectedTabAndClickPerformed() throws Throwable {
+        List<CarTabItem> tabs = new ArrayList<>();
+
+        int numOfTabs = 3;
+        for (int i = 0; i < numOfTabs; i++) {
+            Icon icon = Icon.createWithResource(mContext, android.R.drawable.sym_def_app_icon);
+            tabs.add(new CarTabItem.Builder()
+                    .setIcon(icon)
+                    .setText("text")
+                    .build());
+        }
+
+        // Manually set the selected tab to be position 1.
+        mActivityRule.runOnUiThread(() -> {
+            mTabBarView.setSelectedTab(1);
+            mTabBarView.setTabs(tabs);
+        });
+
+        // However, perform a click on position 2.
+        onView(allOf(withId(R.id.car_tab), withTagKey(R.id.car_tab_position, is(2))))
+                .perform(click());
+
+        // Verify position 2 selected.
+        assertTabIsDisplayedWithAlpha(/* position= */ 0, mInactiveAlpha);
+        assertTabIsDisplayedWithAlpha(/* position= */ 1, mInactiveAlpha);
+        assertTabIsDisplayedWithAlpha(/* position= */ 2, mSelectedAlpha);
+    }
+
+    @Test
+    public void testTabSelection_triggersTabListener() throws Throwable {
+        List<CarTabItem> tabs = new ArrayList<>();
+
+        int numOfTabs = 3;
+        for (int i = 0; i < numOfTabs; i++) {
+            Icon icon = Icon.createWithResource(mContext, android.R.drawable.sym_def_app_icon);
+            tabs.add(new CarTabItem.Builder()
+                    .setIcon(icon)
+                    .setText("text")
+                    .build());
+        }
+
+        // 3 tabs, so create this boolean array with 3 entries.
+        boolean[] clicked = new boolean[] {false, false, false};
+
+        mActivityRule.runOnUiThread(() -> {
+            mTabBarView.setTabs(tabs);
+            mTabBarView.setOnTabSelectListener(position -> clicked[position] = true);
+        });
+
+        // Perform a click on position 1.
+        onView(allOf(withId(R.id.car_tab), withTagKey(R.id.car_tab_position, is(1))))
+                .perform(click());
+
+        // Should only have triggered the listener with position 1.
+        assertFalse(clicked[0]);
+        assertTrue(clicked[1]);
+        assertFalse(clicked[2]);
+    }
+
+    /** Asserts the tab at the given position is currently being displayed. */
+    private void assertTabAtPositionIsDisplayed(int position) {
+        // All tabs have the id "car_tab" and should have a tag with the key "car_tab_position" and
+        // a value corresponding to its position.
+        ViewInteraction tabView = onView(allOf(
+                withId(R.id.car_tab),
+                withTagKey(R.id.car_tab_position, is(position))));
+
+        tabView.check(matches(isDisplayed()));
+    }
+
+    /** Asserts that the tab at the given position has the given alpha. */
+    private void assertTabIsDisplayedWithAlpha(int position, float alpha) {
+        ViewInteraction tabView = onView(allOf(
+                withId(R.id.car_tab),
+                withTagKey(R.id.car_tab_position, is(position)),
+                withAlpha(alpha)));
+
+        tabView.check(matches(isDisplayed()));
+    }
+
+    /** Returns {@code true} if the testing device has the automotive feature flag. */
+    private boolean isAutoDevice() {
+        PackageManager packageManager = mActivityRule.getActivity().getPackageManager();
+        return packageManager.hasSystemFeature(PackageManager.FEATURE_AUTOMOTIVE);
+    }
+}
---
+++ car/core/src/main/java/androidx/car/widget/CarTabBarView.java
@@ -0,0 +1,353 @@
+/*
+ * Copyright 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.car.widget;
+
+import static java.lang.annotation.RetentionPolicy.SOURCE;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.content.res.TypedArray;
+import android.graphics.drawable.Icon;
+import android.text.TextUtils;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.util.TypedValue;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.IntDef;
+import androidx.annotation.IntRange;
+import androidx.annotation.Nullable;
+import androidx.car.R;
+
+import java.lang.annotation.Retention;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * A view that represents selectable tabs for building car applications. The tab bar will
+ * automatically keep track of the last selected tab and will update visually when a tab is
+ * selected.
+ *
+ * <p>By default, the tabs are laid out starting from the starting end of the tab bar. The first
+ * tab is considered selected. To change the configuration of these defaults, call
+ * {@link #setTabAlignment(int)} and {@link #setSelectedTab(int)} respectively.
+ *
+ * <p>Pass a {@link OnTabSelectedListener} to this class to be notified of when tabs are selected.
+ * Setting a listener will not affect the visual indicator of the selected tab.
+ *
+ * <p>The tab bar has a fixed height of {@code R.dimen.car_app_bar_height}.
+ */
+public class CarTabBarView extends ViewGroup {
+    private static final String TAG = "CarTabBarView";
+
+    /** The key for a tag on a tab that will store the value of its position. */
+    private static final int POSITION_TAG_KEY = R.id.car_tab_position;
+
+    private final List<CarTabItem> mTabItems = new ArrayList<>();
+    private final List<View> mTabViews = new ArrayList<>();
+    private final int mHeight;
+    private final int mHorizontalMargins;
+    private final int mTabSeparatingMargin;
+
+    private final float mSelectedTabAlpha;
+    private final float mInactiveTabAlpha;
+
+    /** The tab that is currently selected. Only one tab can be selected at a time. */
+    private int mSelectedTab;
+
+    /**
+     * A convenience field for the total width taken by the tabs. This value also includes the
+     * separating margins between the buttons.
+     */
+    private int mWidthTakenByTabs;
+
+    @TabAlignment private int mTabAlignment = TabAlignment.START;
+
+    @Nullable private OnTabSelectedListener mTabListener;
+
+    /**
+     * Callback invoked when a tab is selected.
+     */
+    public interface OnTabSelectedListener {
+        /**
+         * Called when a tab has been selected by the user.
+         *
+         * @param position A 0-based position indicating the tab that has been selected.
+         */
+        void onTabSelected(int position);
+    }
+
+    /**
+     * Specifies the horizontal alignment of the individual tabs within the tab bar.
+     */
+    @IntDef({TabAlignment.START, TabAlignment.CENTER})
+    @Retention(SOURCE)
+    public @interface TabAlignment {
+        /** Lay out the tabs from the starting end of the view. */
+        int START = 0;
+
+        /** Center the tabs horizontally within the tab bar. */
+        int CENTER = 1;
+    }
+
+    public CarTabBarView(Context context) {
+        this(context, /* attrs= */ null);
+    }
+
+    public CarTabBarView(Context context, AttributeSet attrs) {
+        this(context, attrs, R.attr.carTabBarStyle);
+    }
+
+    public CarTabBarView(Context context, AttributeSet attrs, int defStyleAttr) {
+        this(context, attrs, defStyleAttr, R.style.Widget_Car_CarTabBar);
+    }
+
+    public CarTabBarView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+
+        Resources res = getResources();
+        mHeight = res.getDimensionPixelSize(R.dimen.car_app_bar_height);
+        mHorizontalMargins = res.getDimensionPixelSize(R.dimen.car_keyline_1);
+        mTabSeparatingMargin = res.getDimensionPixelSize(R.dimen.car_padding_4);
+
+        TypedValue outValue = new TypedValue();
+        res.getValue(R.dimen.selected_tab_alpha, outValue, true);
+        mSelectedTabAlpha = outValue.getFloat();
+
+        res.getValue(R.dimen.inactive_tab_alpha, outValue, true);
+        mInactiveTabAlpha = outValue.getFloat();
+
+        TypedArray a = context.obtainStyledAttributes(
+                attrs, R.styleable.CarTabBarView, defStyleAttr, defStyleRes);
+
+        try {
+            setTabAlignment(a.getInt(
+                    R.styleable.CarTabBarView_carTabAlignment, TabAlignment.START));
+        } finally {
+            a.recycle();
+        }
+    }
+
+    /**
+     * Sets the horizontal alignment of the tab bar. If not set, the default alignment is
+     * {@link TabAlignment#START}.
+     *
+     * @param tabAlignment The alignment of the tabs.
+     * @see TabAlignment
+     */
+    public void setTabAlignment(@TabAlignment int tabAlignment) {
+        mTabAlignment = tabAlignment;
+        requestLayout();
+    }
+
+    /**
+     * Sets the listener that will be invoked when a tab is selected.
+     *
+     * @param listener The listener that will be invoked or {@code null} to clear any previously
+     *                 set listeners.
+     */
+    public void setOnTabSelectListener(@Nullable OnTabSelectedListener listener) {
+        mTabListener = listener;
+    }
+
+    /**
+     * Manually set the tab that is considered selected. Selected tabs will have an visual
+     * indication of selection.
+     *
+     * <p>If the given position is more than the number of tabs, then no tab will have the visual
+     * indication of being selected.
+     *
+     * <p>Note that the value set here is not necessarily the value that is returned by
+     * {@link #getSelectedTab()}. The reason for this is that the selected tab will automatically
+     * update as the tabs are interacted with.
+     *
+     * <p>The default value for this is 0.
+     *
+     * @param position A 0-based position of the currently selected tab. Passing a value of 0, for
+     *                 example, will result in the first tab being selected.
+     */
+    public void setSelectedTab(@IntRange(from = 0) int position) {
+        mSelectedTab = Math.max(position, 0);
+        updateSelectedTab();
+    }
+
+    /**
+     * Returns the position of the tab that is currently selected.
+     *
+     * <p>This value will not necessarily match the value passed to {@link #setSelectedTab(int)}.
+     * It will always match the last-selected tab, as determined by user interaction.
+     *
+     * @return The position of the last-selected tab.
+     */
+    @IntRange(from = 0)
+    public int getSelectedTab() {
+        return mSelectedTab;
+    }
+
+    /**
+     * Sets the tabs that will be shown on this tab bar.
+     *
+     * @param tabItems The tabs on the tab bar or {@code null} to clear the tabs.
+     */
+    public void setTabs(@Nullable List<CarTabItem> tabItems) {
+        mTabItems.clear();
+        mTabViews.clear();
+        removeAllViews();
+
+        if (tabItems == null) {
+            requestLayout();
+            return;
+        }
+
+        mTabItems.addAll(tabItems);
+
+        for (int i = 0, size = mTabItems.size(); i < size; i++) {
+            View tab = createAndBindTabView(mTabItems.get(i));
+            tab.setTag(POSITION_TAG_KEY, i);
+
+            tab.setOnClickListener(v -> {
+                int position = (int) v.getTag(POSITION_TAG_KEY);
+
+                if (mTabListener != null) {
+                    mTabListener.onTabSelected(position);
+                }
+                setSelectedTab(position);
+            });
+
+            mTabViews.add(tab);
+            addView(tab);
+        }
+
+        updateSelectedTab();
+        requestLayout();
+    }
+
+    /**
+     * Creates and returns a view that represents the tab based on the information in the given
+     * {@code CarTabItem}.
+     */
+    private View createAndBindTabView(CarTabItem tabItem) {
+        View tab = LayoutInflater.from(getContext())
+                .inflate(R.layout.tab_view, /* root= */ this, /* attachToRoot= */ false);
+
+        ImageView iconView = tab.findViewById(R.id.car_tab_icon);
+
+        Icon icon = tabItem.getIcon();
+        iconView.setImageIcon(icon);
+        iconView.setVisibility(icon == null ? GONE : VISIBLE);
+
+        TextView tabTextView = tab.findViewById(R.id.car_tab_text);
+        CharSequence text = tabItem.getText();
+        tabTextView.setText(text);
+        tabTextView.setVisibility(TextUtils.isEmpty(text) ? GONE : VISIBLE);
+
+        return tab;
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        mWidthTakenByTabs = 0;
+        int desiredHeight = getPaddingTop() + mHeight + getPaddingBottom();
+
+        if (mTabViews == null || mTabViews.isEmpty()) {
+            setMeasuredDimension(0, resolveSize(desiredHeight, heightMeasureSpec));
+            return;
+        }
+
+        // Add starting and ending margins to begin with and any padding.
+        int widthTaken = 2 * mHorizontalMargins + getPaddingStart() + getPaddingEnd();
+
+        // Each tab should take up as much width as needed, but be constrained by the height of
+        // the tab view.
+        int wrapMeasureSpec = MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED);
+        int tabHeightMeasureSpec = MeasureSpec.makeMeasureSpec(mHeight, MeasureSpec.AT_MOST);
+
+        for (View tabView : mTabViews) {
+            tabView.measure(wrapMeasureSpec, tabHeightMeasureSpec);
+            mWidthTakenByTabs += tabView.getMeasuredWidth();
+        }
+
+        // Add spacing between each tab.
+        mWidthTakenByTabs += (mTabViews.size() - 1) * mTabSeparatingMargin;
+        widthTaken += mWidthTakenByTabs;
+
+        setMeasuredDimension(
+                resolveSize(widthTaken, widthMeasureSpec),
+                resolveSize(desiredHeight, heightMeasureSpec));
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        if (mTabViews == null || mTabViews.isEmpty()) {
+            return;
+        }
+
+        int height = bottom - top - getPaddingTop() - getPaddingBottom();
+
+        // The horizontal width available to lay out the tabs.
+        int contentWidth =
+                right - left - getPaddingStart() - getPaddingEnd() - (2 * mHorizontalMargins);
+        int layoutLeft = getPaddingStart() + mHorizontalMargins;
+
+        switch (mTabAlignment) {
+            case TabAlignment.START:
+                layOutTabsFromLeft(layoutLeft, height);
+                break;
+            case TabAlignment.CENTER:
+                layoutLeft += (contentWidth - mWidthTakenByTabs) / 2;
+                layOutTabsFromLeft(layoutLeft, height);
+                break;
+            default:
+                Log.e(TAG, "Unknown tab alignment set: " + mTabAlignment);
+        }
+    }
+
+    /** Updates the visual indication of which tab is currently selected. */
+    private void updateSelectedTab() {
+        int numOfTabs = mTabViews.size();
+
+        if (mSelectedTab >= numOfTabs) {
+            return;
+        }
+
+        for (int i = 0; i < numOfTabs; i++) {
+            mTabViews.get(i).setAlpha(i == mSelectedTab
+                    ? mSelectedTabAlpha
+                    : mInactiveTabAlpha);
+        }
+    }
+
+    /**
+     * Lays out all the views in {@link #mTabViews} from the given {@code left} position.
+     *
+     * @param left The starting left position.
+     * @param height The current height of the tab bar.
+     */
+    private void layOutTabsFromLeft(int left, int height) {
+        for (View tabView : mTabViews) {
+            int top = getPaddingTop() + ((height - tabView.getMeasuredHeight()) / 2);
+            tabView.layout(left, top, left + tabView.getMeasuredWidth(),
+                    top + tabView.getMeasuredHeight());
+
+            left += tabView.getMeasuredWidth() + mTabSeparatingMargin;
+        }
+    }
+}
