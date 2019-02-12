44dccb5aaa09aa316fa9a1e2ffacddbfaaf2069b
Rahul Ravikumar <rahulrav@google.com>
2019-02-12 15:41:35 -0800

    Emulate flex time for AlarmManager implementation.
    Test: Added unit tests
    Fixes: b/124274584
    
    Ran integraton tests on API 22.
    
    Change-Id: I197f193dc2fb24b260cb5baf095b8d2bb028931f

--- work/workmanager/src/main/java/androidx/work/impl/WorkerWrapper.java
+++ work/workmanager/src/main/java/androidx/work/impl/WorkerWrapper.java
@@ -167,7 +167,15 @@ public class WorkerWrapper implements Runnable {
 
             if (mWorkSpec.isPeriodic() || mWorkSpec.isBackedOff()) {
                 long now = System.currentTimeMillis();
-                if (now < mWorkSpec.calculateNextRunTime()) {
+                // Allow first run of a PeriodicWorkRequest when flex is applicable
+                // (when using AlarmManager) to go through. This is because when periodStartTime=0;
+                // calculateNextRunTime() always > now.
+                boolean isFirstRunWhenFlexApplicable =
+                        Build.VERSION.SDK_INT < WorkManagerImpl.MIN_JOB_SCHEDULER_API_LEVEL
+                                && mWorkSpec.intervalDuration != mWorkSpec.flexDuration
+                                && mWorkSpec.periodStartTime == 0;
+
+                if (!isFirstRunWhenFlexApplicable && now < mWorkSpec.calculateNextRunTime()) {
                     Logger.get().debug(TAG,
                             String.format(
                                     "Delaying execution for %s because it is being executed "
