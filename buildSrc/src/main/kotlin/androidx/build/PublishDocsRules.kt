ad8aae81553a0da30ad5ba16072874940912a00c
Yigit Boyar <yboyar@google.com>
2019-02-13 23:55:22 -0800

    Lifecycle Dispatcher
    This CL adds Lifecycle#whenStarted, #whenResumed, #whenCreated
    methods which receive a block and execute the block only if
    the Lifecycle is in desired state.
    
    The block is automatically suspended if the lifecycle goes out
    of the desired state and it is cancelled if the Lifecycle is
    destroyed.
    
    Bug: 122740811
    Test: PausingLifecycleDispatcherTest
    Change-Id: Ib1211c0f8a0ead42f793a671a233f3b43c557e07

--- buildSrc/src/main/kotlin/androidx/build/PublishDocsRules.kt
+++ buildSrc/src/main/kotlin/androidx/build/PublishDocsRules.kt
@@ -67,6 +67,7 @@ val RELEASE_RULE = docsRules("public", false) {
     ignore(LibraryGroups.LIFECYCLE.group, "lifecycle-livedata-core-ktx")
     ignore(LibraryGroups.LIFECYCLE.group, "lifecycle-compiler")
     ignore(LibraryGroups.LIFECYCLE.group, "lifecycle-common-eap")
+    ignore(LibraryGroups.LIFECYCLE.group, "lifecycle-runtime-eap")
     prebuilts(LibraryGroups.LIFECYCLE, "2.1.0-alpha02")
     prebuilts(LibraryGroups.LOADER, "1.1.0-alpha01")
     prebuilts(LibraryGroups.LOCALBROADCASTMANAGER, "1.1.0-alpha01")
