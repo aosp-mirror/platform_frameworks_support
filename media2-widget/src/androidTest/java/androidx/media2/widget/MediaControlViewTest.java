4c7cf7d9dade943c0c9533d84118855c62f76349
Insun Kang <insun@google.com>
2019-02-22 11:28:44 +0900

    Fix MediaControlViewTest failures on Nexus5/API 21
    Bug: 125518887
    Test: ./gradlew :media2-widget:connectedAndroidTest
    Change-Id: I22f4bce09f62941729b0fe5922870b06fb9244b0

--- media2-widget/src/androidTest/java/androidx/media2/widget/MediaControlViewTest.java
+++ media2-widget/src/androidTest/java/androidx/media2/widget/MediaControlViewTest.java
@@ -20,8 +20,8 @@ import static android.content.Context.KEYGUARD_SERVICE;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.matcher.ViewMatchers.isCompletelyDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static androidx.test.espresso.matcher.ViewMatchers.withParent;
 
 import static org.hamcrest.CoreMatchers.allOf;
 import static org.junit.Assert.assertEquals;
@@ -147,8 +147,7 @@ public class MediaControlViewTest {
             }
         });
         assertTrue(latchForPausedState.await(WAIT_TIME_MS, TimeUnit.MILLISECONDS));
-        onView(allOf((withId(R.id.pause)),
-                withParent(withId(R.id.full_transport_controls)))).perform(click());
+        onView(allOf(withId(R.id.pause), isCompletelyDisplayed())).perform(click());
         assertTrue(latchForPlayingState.await(WAIT_TIME_MS, TimeUnit.MILLISECONDS));
     }
 
@@ -181,8 +180,7 @@ public class MediaControlViewTest {
             }
         });
         assertTrue(latchForPausedState.await(WAIT_TIME_MS, TimeUnit.MILLISECONDS));
-        onView(allOf((withId(R.id.ffwd)),
-                withParent(withId(R.id.full_transport_controls)))).perform(click());
+        onView(allOf(withId(R.id.ffwd), isCompletelyDisplayed())).perform(click());
         assertTrue(latchForFfwd.await(WAIT_TIME_MS, TimeUnit.MILLISECONDS));
     }
 
@@ -230,8 +228,7 @@ public class MediaControlViewTest {
             }
         });
         assertTrue(latchForFfwd.await(WAIT_TIME_MS, TimeUnit.MILLISECONDS));
-        onView(allOf((withId(R.id.rew)),
-                withParent(withId(R.id.full_transport_controls)))).perform(click());
+        onView(allOf(withId(R.id.rew), isCompletelyDisplayed())).perform(click());
         assertTrue(latchForRew.await(WAIT_TIME_MS, TimeUnit.MILLISECONDS));
     }
 
