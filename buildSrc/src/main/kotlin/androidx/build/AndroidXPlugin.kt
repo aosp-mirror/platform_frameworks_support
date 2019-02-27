abd69b068c785b06d919738208fce830e459e618
Oussama Ben Abdelbaki <obenabde@google.com>
2019-02-27 13:38:27 -0500

    Support for enforcing same versions for same group libraries
    Test: ./gradlew checkSameVersionLibraryGroupsTask
    Test: call the above and play around with versions and booleans
    Bug: 125913606
    Change-Id: I75a4ef3683e4eab073fc314dc86b86efe18d3967

--- buildSrc/src/main/kotlin/androidx/build/AndroidXPlugin.kt
+++ buildSrc/src/main/kotlin/androidx/build/AndroidXPlugin.kt
@@ -186,6 +186,10 @@ class AndroidXPlugin : Plugin<Project> {
 
         val jacocoUberJar = Jacoco.createUberJarTask(this)
         buildOnServerTask.dependsOn(jacocoUberJar)
+        val checkSameVersionLibraryGroupsTask = project.tasks.create(
+            CHECK_SAME_VERSION_LIBRARY_GROUPS,
+            CheckSameVersionLibraryGroupsTask::class.java)
+        buildOnServerTask.dependsOn(checkSameVersionLibraryGroupsTask)
 
         project.createClockLockTasks()
 
@@ -323,6 +327,7 @@ class AndroidXPlugin : Plugin<Project> {
         const val BUILD_TEST_APKS = "buildTestApks"
         const val CHECK_RELEASE_READY_TASK = "checkReleaseReady"
         const val CHECK_NO_WARNINGS_TASK = "checkNoWarnings"
+        const val CHECK_SAME_VERSION_LIBRARY_GROUPS = "checkSameVersionLibraryGroups"
     }
 }
 
