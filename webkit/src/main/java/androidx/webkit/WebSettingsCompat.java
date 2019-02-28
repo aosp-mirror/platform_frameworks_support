8bc2dff00ef07a5676ac77d7bf59b9f598006b54
Christian O. Andersson <cricke@google.com>
2019-02-28 09:16:37 +0000

    [WebView Support Library] Adding an Error Page Suppression API
    We wish to provide a means of optionally suppressing the display of the
    webview error page so that applications that wish to display their own
    error content may do so without presenting a poor user experience.
    
    Test: ./gradlew webkit:connectedCheck --info -Pandroid.testInstrumentationRunnerArguments.class=androidx.webkit.WebSettingsCompatTest#testSuppressedErrorPage
    Design doc: https://docs.google.com/document/d/188BbgvTdhKf8bXikCL-JMduEclJOd9GxhKu79hmSVdo/edit
    Change-Id: Ided79cfacede785fe0cf762fee45abb971ebea22

--- webkit/src/main/java/androidx/webkit/WebSettingsCompat.java
+++ webkit/src/main/java/androidx/webkit/WebSettingsCompat.java
@@ -223,6 +223,57 @@ public class WebSettingsCompat {
         }
     }
 
+    /**
+     * Sets whether the WebView’s internal error page should be suppressed or displayed
+     * for bad navigations. True means suppressed (not shown), false means it will be
+     * displayed.
+     * The default is false.
+     *
+     * @param suppressed whether the WebView should suppress it’s internal error page
+     *
+     * TODO(cricke): unhide
+     * @hide
+     */
+    @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
+    @SuppressLint("NewApi")
+    @RequiresFeature(name = WebViewFeature.SUPPRESS_ERROR_PAGE,
+            enforcement = "androidx.webkit.WebViewFeature#isFeatureSupported")
+    public static void setWillSuppressErrorPage(WebSettings webSettings, boolean suppressed) {
+        WebViewFeatureInternal webviewFeature =
+                WebViewFeatureInternal.getFeature(WebViewFeature.SUPPRESS_ERROR_PAGE);
+        if (webviewFeature.isSupportedByWebView()) {
+            getAdapter(webSettings).setWillSuppressErrorPage(suppressed);
+        } else {
+            throw WebViewFeatureInternal.getUnsupportedOperationException();
+        }
+    }
+
+
+    /**
+     * Gets whether the WebView’s internal error page will be suppressed or displayed
+     *
+     * @see #setWillSuppressErrorPage
+     * @return true if the WebView will suppress it’s internal error page
+     *
+     * TODO(cricke): unhide
+     * @hide
+     */
+    @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
+    @SuppressLint("NewApi")
+    @RequiresFeature(name = WebViewFeature.SUPPRESS_ERROR_PAGE,
+            enforcement = "androidx.webkit.WebViewFeature#isFeatureSupported")
+    public static boolean willSuppressErrorPage(WebSettings webSettings) {
+        WebViewFeatureInternal webviewFeature =
+                WebViewFeatureInternal.getFeature(WebViewFeature.SUPPRESS_ERROR_PAGE);
+        if (webviewFeature.isSupportedByWebView()) {
+            return getAdapter(webSettings).willSuppressErrorPage();
+        } else {
+            throw WebViewFeatureInternal.getUnsupportedOperationException();
+        }
+    }
+
+
+
     private static WebSettingsAdapter getAdapter(WebSettings webSettings) {
         return WebViewGlueCommunicator.getCompatConverter().convertSettings(webSettings);
     }
