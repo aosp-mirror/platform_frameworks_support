edfa08466572792f44fc4622a371efc927cbd4b2
Ram Parameswaran <rampara@google.com>
2019-02-28 15:01:01 -0800

    Update UXR helper to provide a way to retreive current UXR.
    Bug: 123698055
    Test: ./gradlew :car:connectedCheck
    
    Change-Id: I5d041c69d8b440fc0ce5f1204651b303a87fd1df

--- car/core/src/main/java/androidx/car/util/CarUxRestrictionsHelper.java
+++ car/core/src/main/java/androidx/car/util/CarUxRestrictionsHelper.java
@@ -54,7 +54,7 @@ public class CarUxRestrictionsHelper {
         }
         mListener = listener;
         mCar = Car.createCar(context, mServiceConnection);
-    };
+    }
 
     /**
      * Starts monitoring any changes in {@link CarUxRestrictions}.
@@ -101,6 +101,26 @@ public class CarUxRestrictionsHelper {
             // Do nothing.
             Log.w(TAG, "stop(); cannot disconnect from Car.");
         }
+    }
+
+    /**
+     * Get the current UX restrictions {@link CarUxRestrictions} in place.
+     *
+     * @return current UX restrictions that is in effect or {@code null} if the current UX
+     * restrictions cannot be obtained.
+     */
+    @Nullable
+    public CarUxRestrictions getCurrentCarUxRestrictions() {
+        try {
+            if (mCarUxRestrictionsManager != null) {
+                return new CarUxRestrictions(
+                        mCarUxRestrictionsManager.getCurrentCarUxRestrictions());
+            }
+        } catch (CarNotConnectedException e) {
+            // Do nothing.
+            Log.w(TAG, "getCurrentCarUxRestrictions(); cannot get current UX restrictions.");
+        }
+        return null;
     }
 
     private final ServiceConnection mServiceConnection = new ServiceConnection() {
