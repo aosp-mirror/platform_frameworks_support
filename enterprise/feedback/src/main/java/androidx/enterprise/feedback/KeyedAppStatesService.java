53357035df47dc175fc60bfd7df126a4bbae6167
Jonathan Scott <scottjonathan@google.com>
2019-03-08 13:09:57 +0000

    Change to use .obj instead of .setData for backwards compatability.
    Test: ./gradlew test
    Change-Id: I51c4bdefdd27b06d03a088bceec56e830d9f310f

--- enterprise/feedback/src/main/java/androidx/enterprise/feedback/KeyedAppStatesService.java
+++ enterprise/feedback/src/main/java/androidx/enterprise/feedback/KeyedAppStatesService.java
@@ -135,7 +135,14 @@ public abstract class KeyedAppStatesService extends Service {
 
         private static Collection<ReceivedKeyedAppState> extractReceivedKeyedAppStates(
                 Message message, String packageName, long timestamp) {
-            Bundle bundle = message.getData();
+            Bundle bundle;
+
+            try {
+                bundle = (Bundle) message.obj;
+            } catch (ClassCastException e) {
+                Log.e(LOG_TAG, "Could not extract state bundles from message");
+                return Collections.emptyList();
+            }
 
             if (bundle == null) {
                 Log.e(LOG_TAG, "Could not extract state bundles from message");
