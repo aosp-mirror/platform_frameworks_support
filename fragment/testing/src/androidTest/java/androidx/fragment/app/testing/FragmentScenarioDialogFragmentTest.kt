483a5868d705fddd3450380f5dee9423e54fc5f3
Yuki Hamada <hummer@google.com>
2019-03-08 16:41:14 -0800

    Add tests for FragmentScenario with DialogFragment
    FragmentScenarioDialogFragmentTest demonstrates how you can write tests against
    DialogFragment. Note that FragmentScenario does not use DialogFragment#show()
    but it adds the fragment to the manager manually. The show() method is just a
    convenient wrapper around executing the transaction, there is no difference
    in functionality on the displayed DialogFragment between show() and
    launchFragment(). Also note that, if you use launchFragmentInContainer(),
    DialogFragment#onCreateDialog() is never be called. In order to make your
    fragment work with container, you should use onCreateView instead.
    
    Test: fragment-testing:connectedCheck
    Fixes: 127907652
    Change-Id: I54c007fe1be756cfc1a2b21744c9f0c313153a0f

---
+++ fragment/testing/src/androidTest/java/androidx/fragment/app/testing/FragmentScenarioDialogFragmentTest.kt
@@ -0,0 +1,248 @@
+/*
+ * Copyright (C) 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.fragment.app.testing
+
+import androidx.lifecycle.Lifecycle.State
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import com.google.common.truth.Truth.assertThat
+import org.junit.Test
+import org.junit.runner.RunWith
+
+/**
+ * Tests for FragmentScenario's implementation against DialogFragment.
+ */
+@RunWith(AndroidJUnit4::class)
+@LargeTest
+class FragmentScenarioDialogFragmentTest {
+    @Test
+    fun launchFragment() {
+        with(launchFragment<SimpleDialogFragment>()) {
+            onFragment { fragment ->
+                assertThat(fragment.lifecycle.currentState).isEqualTo(State.RESUMED)
+                assertThat(fragment.dialog).isNotNull()
+            }
+            onView(withText("my button")).check(matches(isDisplayed()))
+        }
+    }
+
+    @Test
+    fun launchFragmentInContainer() {
+        with(launchFragmentInContainer<SimpleDialogFragment>()) {
+            onFragment { fragment ->
+                assertThat(fragment.lifecycle.currentState).isEqualTo(State.RESUMED)
+                // We show SimpleDialogFragment in container so dialog is not created.
+                assertThat(fragment.dialog).isNull()
+            }
+            onView(withText("my button")).check(matches(isDisplayed()))
+        }
+    }
+
+    @Test
+    fun fromResumedToCreated() {
+        with(launchFragment<SimpleDialogFragment>()) {
+            moveToState(State.CREATED)
+            onFragment { fragment ->
+                assertThat(fragment.lifecycle.currentState).isEqualTo(State.CREATED)
+            }
+        }
+    }
+
+    @Test
+    fun fromResumedToStarted() {
+        with(launchFragment<SimpleDialogFragment>()) {
+            moveToState(State.STARTED)
+            onFragment { fragment ->
+                assertThat(fragment.lifecycle.currentState).isEqualTo(State.STARTED)
+            }
+        }
+    }
+
+    @Test
+    fun fromResumedToResumed() {
+        with(launchFragment<SimpleDialogFragment>()) {
+            moveToState(State.RESUMED)
+            onFragment { fragment ->
+                assertThat(fragment.lifecycle.currentState).isEqualTo(State.RESUMED)
+            }
+        }
+    }
+
+    @Test
+    fun fromResumedToDestroyed() {
+        with(launchFragment<SimpleDialogFragment>()) {
+            moveToState(State.DESTROYED)
+        }
+    }
+
+    @Test
+    fun fromCreatedToCreated() {
+        with(launchFragment<SimpleDialogFragment>()) {
+            moveToState(State.CREATED)
+            moveToState(State.CREATED)
+            onFragment { fragment ->
+                assertThat(fragment.lifecycle.currentState).isEqualTo(State.CREATED)
+            }
+        }
+    }
+
+    @Test
+    fun fromCreatedToStarted() {
+        with(launchFragment<SimpleDialogFragment>()) {
+            moveToState(State.CREATED)
+            moveToState(State.STARTED)
+            onFragment { fragment ->
+                assertThat(fragment.lifecycle.currentState).isEqualTo(State.STARTED)
+            }
+        }
+    }
+
+    @Test
+    fun fromCreatedToResumed() {
+        with(launchFragment<SimpleDialogFragment>()) {
+            moveToState(State.CREATED)
+            moveToState(State.RESUMED)
+            onFragment { fragment ->
+                assertThat(fragment.lifecycle.currentState).isEqualTo(State.RESUMED)
+            }
+        }
+    }
+
+    @Test
+    fun fromCreatedToDestroyed() {
+        with(launchFragment<SimpleDialogFragment>()) {
+            moveToState(State.CREATED)
+            moveToState(State.DESTROYED)
+        }
+    }
+
+    @Test
+    fun fromStartedToCreated() {
+        with(launchFragment<SimpleDialogFragment>()) {
+            moveToState(State.STARTED)
+            moveToState(State.CREATED)
+            onFragment { fragment ->
+                assertThat(fragment.lifecycle.currentState).isEqualTo(State.CREATED)
+            }
+        }
+    }
+
+    @Test
+    fun fromStartedToStarted() {
+        with(launchFragment<SimpleDialogFragment>()) {
+            moveToState(State.STARTED)
+            moveToState(State.STARTED)
+            onFragment { fragment ->
+                assertThat(fragment.lifecycle.currentState).isEqualTo(State.STARTED)
+            }
+        }
+    }
+
+    @Test
+    fun fromStartedToResumed() {
+        with(launchFragment<SimpleDialogFragment>()) {
+            moveToState(State.STARTED)
+            moveToState(State.RESUMED)
+            onFragment { fragment ->
+                assertThat(fragment.lifecycle.currentState).isEqualTo(State.RESUMED)
+            }
+        }
+    }
+
+    @Test
+    fun fromStartedToDestroyed() {
+        with(launchFragment<SimpleDialogFragment>()) {
+            moveToState(State.STARTED)
+            moveToState(State.DESTROYED)
+        }
+    }
+
+    @Test
+    fun fromDestroyedToDestroyed() {
+        with(launchFragment<SimpleDialogFragment>()) {
+            moveToState(State.DESTROYED)
+            moveToState(State.DESTROYED)
+        }
+    }
+
+    @Test
+    fun recreateCreatedFragment() {
+        var numOfInstantiation = 0
+        with(launchFragment {
+            ++numOfInstantiation
+            SimpleDialogFragment()
+        }) {
+            assertThat(numOfInstantiation).isEqualTo(1)
+            moveToState(State.CREATED)
+            recreate()
+            assertThat(numOfInstantiation).isEqualTo(2)
+            onFragment { fragment ->
+                assertThat(fragment.lifecycle.currentState).isEqualTo(State.CREATED)
+            }
+        }
+    }
+
+    @Test
+    fun recreateStartedFragment() {
+        var numOfInstantiation = 0
+        with(launchFragment {
+            ++numOfInstantiation
+            SimpleDialogFragment()
+        }) {
+            assertThat(numOfInstantiation).isEqualTo(1)
+            moveToState(State.STARTED)
+            recreate()
+            assertThat(numOfInstantiation).isEqualTo(2)
+            onFragment { fragment ->
+                assertThat(fragment.lifecycle.currentState).isEqualTo(State.STARTED)
+            }
+        }
+    }
+
+    @Test
+    fun recreateResumedFragment() {
+        var numOfInstantiation = 0
+        with(launchFragment {
+            ++numOfInstantiation
+            SimpleDialogFragment()
+        }) {
+            assertThat(numOfInstantiation).isEqualTo(1)
+            recreate()
+            assertThat(numOfInstantiation).isEqualTo(2)
+            onFragment { fragment ->
+                assertThat(fragment.lifecycle.currentState).isEqualTo(State.RESUMED)
+            }
+        }
+    }
+
+    @Test
+    fun dismissDialog() {
+        with(launchFragment<SimpleDialogFragment>()) {
+            onFragment { fragment ->
+                assertThat(fragment.lifecycle.currentState).isEqualTo(State.RESUMED)
+                fragment.dismiss()
+                fragment.requireFragmentManager().executePendingTransactions()
+            }
+        }
+        onView(withText("my button")).check(doesNotExist())
+    }
+}
