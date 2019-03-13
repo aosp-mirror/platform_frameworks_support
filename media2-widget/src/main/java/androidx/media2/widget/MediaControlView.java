068a8a704e993d1e7a98ddee4db166d1ac505181
Insun Kang <insun@google.com>
2019-03-13 09:27:39 +0900

    MCV: Setting CenterView's acutal visibility according to the UxState
    Bug: 127870423
    Test: manually (./gradlew :support-media-demos:installDebug)
    Change-Id: I6a908ba5573a02528ff33c7af73e1299af53a996

--- media2-widget/src/main/java/androidx/media2/widget/MediaControlView.java
+++ media2-widget/src/main/java/androidx/media2/widget/MediaControlView.java
@@ -467,8 +467,10 @@ public class MediaControlView extends ViewGroup {
                 sizeType != SIZE_TYPE_MINIMAL ? View.VISIBLE : View.INVISIBLE);
         mMinimalFullScreenButton.setVisibility(
                 sizeType == SIZE_TYPE_MINIMAL ? View.VISIBLE : View.INVISIBLE);
-        mCenterView.setVisibility(
-                sizeType != SIZE_TYPE_FULL ? View.VISIBLE : View.INVISIBLE);
+        if (mUxState == UX_STATE_ALL_VISIBLE) {
+            mCenterView.setVisibility(
+                    sizeType != SIZE_TYPE_FULL ? View.VISIBLE : View.INVISIBLE);
+        }
 
         final int childLeft = getPaddingLeft();
         final int childRight = childLeft + width;
@@ -688,6 +690,13 @@ public class MediaControlView extends ViewGroup {
                     mCenterView.setVisibility(View.VISIBLE);
                 }
                 mMinimalFullScreenView.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                if (mSizeType != SIZE_TYPE_FULL) {
+                    mCenterView.setVisibility(View.VISIBLE);
+                }
             }
         });
 
