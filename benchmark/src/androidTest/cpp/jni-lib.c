aa7e28255b21ee5473073b0beeae1bb63c060966
Dustin Lam <dustinlam@google.com>
2019-03-13 20:25:44 -0700

    Add jni benchmark
    Test: benchmark:cC
    
    Fixes 127974258
    
    Change-Id: I2bc90a769e2d8f33f6af92880e74f15d31f6c754

---
+++ benchmark/src/androidTest/cpp/jni-lib.c
@@ -0,0 +1,28 @@
+/*
+ * Copyright (C) 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+#include <jni.h>
+
+/* This is a trivial JNI example where we use a native method
+ * to return a new VM String. See the corresponding Java source
+ * file located at:
+ *
+ *   hello-jni/app/src/main/java/com/androidx/benchmark/HelloJni.java
+ */
+JNIEXPORT jint JNICALL
+Java_androidx_benchmark_JniLib_add(JNIEnv *env, jobject instance, jint a, jint b) {
+    return a + b;
+}
