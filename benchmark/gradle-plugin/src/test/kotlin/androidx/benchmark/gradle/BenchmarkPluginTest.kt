fa3ff08fb36492d26c1308ff074cf2ca6381f0af
Dustin Lam <dustinlam@google.com>
2019-03-13 18:24:44 -0700

    Benchmark gradle plugin setup
    * Removed dependency on commons-io
    * Only apply lock / unlock task to root project
    * Include lockClocks.sh as part of the distributed .jar
    * Fix adb script
    * Do not throw errors if already rooted
    * Add a simple apply test
    * Split SdkUtil out and add tests
    * Use android gradle plugin to find sdk_dir
    
    Test: ./gradlew benchmark:benchmark-gradle-plugin:check
    Change-Id: I87cbca45bd59d22d424ec4f9516e67768b22ff88

---
+++ benchmark/gradle-plugin/src/test/kotlin/androidx/benchmark/gradle/BenchmarkPluginTest.kt
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.benchmark.gradle
+
+import org.gradle.testfixtures.ProjectBuilder
+import org.junit.Assert
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+@RunWith(JUnit4::class)
+class BenchmarkPluginTest {
+
+    @Test
+    fun applyPlugin() {
+        val project = ProjectBuilder.builder().build()
+        project.apply { it.plugin("androidx.benchmark") }
+
+        Assert.assertNotNull(project.tasks.findByPath("lockClocks"))
+        Assert.assertNotNull(project.tasks.findByPath("unlockClocks"))
+    }
+}
