8c9772429aa0a9a9d655e8479e4771ed2d04ca9e
Dustin Lam <dustinlam@google.com>
2019-03-13 18:24:44 -0700

    Benchmark gradle plugin setup
    * Removed dependency on commons-io
    * Only apply lock / unlock task to root project
    * Include lockClocks.sh as part of the distributed .jar
    * Fix adb script
    * Do not throw errors if already rooted
    * Add a simple apply test
    * Split SdkUtil out and add tests
    * Added env override + test
    
    Test: ./gradlew benchmark:benchmark-gradle-plugin:uploadArchives
    Change-Id: I87cbca45bd59d22d424ec4f9516e67768b22ff88

---
+++ benchmark/gradle-plugin/src/test/kotlin/androidx/benchmark/gradle/SdkUtilTest.kt
@@ -0,0 +1,120 @@
+/*
+ * Copyright 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.benchmark.gradle
+
+import org.gradle.testfixtures.ProjectBuilder
+import org.junit.After
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.TemporaryFolder
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+import java.util.Collections
+
+@RunWith(JUnit4::class)
+class SdkUtilTest {
+
+    @get:Rule
+    val testProjectDir = TemporaryFolder()
+
+    companion object {
+        const val BENCHMARK_PLUGIN_ID = "androidx.benchmark"
+        const val SYSTEM_ENV_ANDROID_HOME = "ANDROID_HOME"
+        const val SYSTEM_PROPERTY_ANDROID_HOME = "android.home"
+    }
+
+    lateinit var systemEnvMap: MutableMap<String, String>
+    private var oldAndroidHomeEnvValue: String? = null
+    private var oldAndroidHomePropValue: String? = null
+
+    @Before
+    fun setUp() {
+        oldAndroidHomePropValue = System.clearProperty(SYSTEM_PROPERTY_ANDROID_HOME)
+        oldAndroidHomeEnvValue = System.getenv(SYSTEM_ENV_ANDROID_HOME)
+
+        for (cl in Collections::class.java.declaredClasses) {
+            if ("java.util.Collections\$UnmodifiableMap" == cl.name) {
+                val field = cl.getDeclaredField("m")
+                field.isAccessible = true
+                systemEnvMap = field.get(System.getenv()) as MutableMap<String, String>
+            }
+        }
+        systemEnvMap.remove(SYSTEM_ENV_ANDROID_HOME)
+    }
+
+    @After
+    fun tearDown() {
+        if (oldAndroidHomePropValue == null) {
+            System.clearProperty(SYSTEM_PROPERTY_ANDROID_HOME)
+        } else {
+            System.setProperty(SYSTEM_PROPERTY_ANDROID_HOME, oldAndroidHomePropValue)
+        }
+
+        if (oldAndroidHomeEnvValue != null) {
+            systemEnvMap[SYSTEM_ENV_ANDROID_HOME] = oldAndroidHomeEnvValue!!
+        }
+    }
+
+    @Test
+    fun getSdkPathFromLocalProps() {
+        testProjectDir.root.mkdirs()
+
+        val localPropsFile = testProjectDir.newFile("local.properties")
+        localPropsFile.createNewFile()
+        localPropsFile.writeText("sdk.dir=/usr/test/location")
+
+        val project = ProjectBuilder.builder()
+            .withProjectDir(testProjectDir.root)
+            .build()
+        project.apply { it.plugin(BENCHMARK_PLUGIN_ID) }
+
+        Assert.assertEquals("/usr/test/location", SdkUtil.getSdkPath(project).path)
+    }
+
+    @Test
+    fun getSdkPathFromSystemEnv() {
+        testProjectDir.root.mkdirs()
+
+        val project = ProjectBuilder.builder()
+            .withProjectDir(testProjectDir.root)
+            .build()
+
+        systemEnvMap[SYSTEM_ENV_ANDROID_HOME] = "/usr/system/env/location"
+        System.setProperty(SYSTEM_PROPERTY_ANDROID_HOME, "/usr/system/env/location")
+        Assert.assertEquals("/usr/system/env/location", SdkUtil.getSdkPath(project).path)
+    }
+
+    @Test
+    fun getSdkPathFromSystemProperty() {
+        testProjectDir.root.mkdirs()
+
+        val project = ProjectBuilder.builder()
+            .withProjectDir(testProjectDir.root)
+            .build()
+
+        System.setProperty(SYSTEM_PROPERTY_ANDROID_HOME, "/usr/system/prop/location")
+        Assert.assertEquals("/usr/system/prop/location", SdkUtil.getSdkPath(project).path)
+    }
+
+    @Test(expected = Exception::class)
+    fun getSdkPathThrowsWhenMissing() {
+        val project = ProjectBuilder.builder().build()
+        SdkUtil.getSdkPath(project).path
+    }
+}
