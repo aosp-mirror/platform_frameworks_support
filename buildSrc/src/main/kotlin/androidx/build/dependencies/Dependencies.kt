cc8019c79cfb231bf10eec30d934c04bf4ce8062
Daniel Santiago Rivera <danysantiago@google.com>
2019-03-20 10:35:05 -0700

    Update arch core-runtime pinned dependency to 2.0.1.
    Test: ./gradlew bOS
    Change-Id: I4f7c8c94caa0f0e00efe06126be4ecd8a6178d2b

--- buildSrc/src/main/kotlin/androidx/build/dependencies/Dependencies.kt
+++ buildSrc/src/main/kotlin/androidx/build/dependencies/Dependencies.kt
@@ -87,9 +87,9 @@ const val ARCH_LIFECYCLE_LIVEDATA =
         "androidx.lifecycle:lifecycle-livedata:2.0.0"
 const val ARCH_LIFECYCLE_VIEWMODEL = "androidx.lifecycle:lifecycle-viewmodel:2.0.0"
 const val ARCH_LIFECYCLE_EXTENSIONS = "androidx.lifecycle:lifecycle-extensions:2.0.0"
-const val ARCH_CORE_COMMON = "androidx.arch.core:core-common:2.0.0@jar"
-const val ARCH_CORE_RUNTIME = "androidx.arch.core:core-runtime:2.0.0"
-const val ARCH_CORE_TESTING = "androidx.arch.core:core-testing:2.0.0"
+const val ARCH_CORE_COMMON = "androidx.arch.core:core-common:2.0.1@jar"
+const val ARCH_CORE_RUNTIME = "androidx.arch.core:core-runtime:2.0.1"
+const val ARCH_CORE_TESTING = "androidx.arch.core:core-testing:2.0.1"
 
 const val SAFE_ARGS_ANDROID_GRADLE_PLUGIN = "com.android.tools.build:gradle:3.3.0"
 const val SAFE_ARGS_KOTLIN_GRADLE_PLUGIN = "org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.20"
