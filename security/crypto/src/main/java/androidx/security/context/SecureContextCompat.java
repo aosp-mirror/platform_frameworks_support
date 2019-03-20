6b7c21d804d39c3256c578a1f74cdaaf847fb6f0
Jon Markoff <jmarkoff@google.com>
2019-03-19 17:24:16 -0700

    Added SecureSharedPreferences, and File encryption using Tink.
    Updated to include Tink and included changes discussed.
    Updated api files.
    Basic test runs.
    
    Test: ./gradlew buildOnServer
    Change-Id: Ib44a253d177825e34926990d6887257b967b97f1

--- security/crypto/src/main/java/androidx/security/context/SecureContextCompat.java
+++ security/crypto/src/main/java/androidx/security/context/SecureContextCompat.java
@@ -17,19 +17,17 @@
 
 package androidx.security.context;
 
-import android.annotation.TargetApi;
-import android.app.KeyguardManager;
 import android.content.Context;
-import android.os.Build;
+import android.content.SharedPreferences;
 
 import androidx.annotation.NonNull;
 import androidx.security.SecureConfig;
+import androidx.security.content.SecureSharedPreferencesCompat;
 import androidx.security.crypto.FileCipher;
 
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.concurrent.Executor;
 
 /**
  * Class that provides access to an encrypted file input and output streams, as well as encrypted
@@ -41,19 +39,6 @@ public class SecureContextCompat {
 
     private Context mContext;
     private SecureConfig mSecureConfig;
-
-    private static final String DEFAULT_FILE_ENCRYPTION_KEY = "default_encryption_key";
-
-    /**
-     * A listener for getting access to encrypted files. Required when the key requires a
-     * Biometric Prompt.
-     */
-    public interface EncryptedFileInputStreamListener {
-        /**
-         * @param inputStream
-         */
-        void onEncryptedFileInput(@NonNull FileInputStream inputStream);
-    }
 
     public SecureContextCompat(@NonNull Context context) {
         this(context, SecureConfig.getDefault());
@@ -69,29 +54,13 @@ public class SecureContextCompat {
      * package for reading.
      *
      * @param name The name of the file to open; can not contain path separators.
-     * @throws IOException
-     */
-    public void openEncryptedFileInput(@NonNull String name,
-                                       @NonNull Executor executor,
-            @NonNull EncryptedFileInputStreamListener listener) throws IOException {
-        new FileCipher(name, mContext.openFileInput(name), mSecureConfig, executor, listener);
-    }
-
-    /**
-     * Open a private encrypted file associated with this Context's application package for
-     * writing. Creates the file if it doesn't already exist.
-     * <p>
-     * The written file will be encrypted with the default keyPairAlias.
-     *
-     * @param name The name of the file to open; can not contain path separators.
-     * @param mode Operating mode.
-     * @return The resulting {@link FileOutputStream}.
+     * @return the encrypted file input stream
      * @throws IOException
      */
     @NonNull
-    public FileOutputStream openEncryptedFileOutput(@NonNull String name, int mode)
-            throws IOException {
-        return openEncryptedFileOutput(name, mode, DEFAULT_FILE_ENCRYPTION_KEY);
+    public FileInputStream openEncryptedFileInput(@NonNull String name) throws IOException {
+        return new FileCipher(name, mContext.openFileInput(name), mSecureConfig, mContext)
+                .getFileInputStream();
     }
 
     /**
@@ -102,31 +71,30 @@ public class SecureContextCompat {
      *
      * @param name         The name of the file to open; can not contain path separators.
      * @param mode         Operating mode.
-     * @param keyPairAlias The alias of the KeyPair used for encryption, the KeyPair will be
-     *                     created if it does not exist.
      * @return The resulting {@link FileOutputStream}.
      * @throws IOException
      */
     @NonNull
-    public FileOutputStream openEncryptedFileOutput(@NonNull String name, int mode,
-            @NonNull String keyPairAlias)
+    public FileOutputStream openEncryptedFileOutput(@NonNull String name, int mode)
             throws IOException {
-        FileCipher fileCipher = new FileCipher(keyPairAlias,
-                mContext.openFileOutput(name, mode), mSecureConfig);
+        FileCipher fileCipher = new FileCipher(name, mContext.openFileOutput(name, mode),
+                mSecureConfig, mContext);
         return fileCipher.getFileOutputStream();
     }
 
-
     /**
-     * Checks to see if the device is locked.
+     * Gets a SharedPreferences object that internally handles encryption/decryption.
      *
-     * @return true if the device is locked, false otherwise.
+     * @param name The name of the preferences file
+     * @param mode Operating mode, should use MODE_PRIVATE
+     * @return A shared preferences object that handles encryption.
      */
-    @TargetApi(Build.VERSION_CODES.LOLLIPOP_MR1)
-    public boolean deviceLocked() {
-        KeyguardManager keyGuardManager =
-                (KeyguardManager) mContext.getSystemService(Context.KEYGUARD_SERVICE);
-        return keyGuardManager.isDeviceLocked();
+    @NonNull
+    public SharedPreferences getSharedPreferences(@NonNull String name, int mode,
+            @NonNull String keyAlias) {
+        return new SecureSharedPreferencesCompat(keyAlias,
+                mContext.getSharedPreferences(name, mode),
+                mSecureConfig, mContext);
     }
 
 }
