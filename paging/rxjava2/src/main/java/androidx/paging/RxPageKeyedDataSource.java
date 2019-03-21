22e3cf4e8c8f0c6c4cc89bc0469a373de50c265e
Dustin Lam <dustinlam@google.com>
2019-03-21 07:46:09 -0700

    Add rx subclasses of datasources
    * Convert everything back to java :(
    * Testing flow through rx to lf
    
    Test: ./gradlew paging:paging-rxjava2-ktx:cC
    Test: ./gradlew paging:paging-rxjava2-ktx:test
    Fixes: 128979277
    Change-Id: I5658a9b9cbf909c60049c8612af0b2fdd5269941

---
+++ paging/rxjava2/src/main/java/androidx/paging/RxPageKeyedDataSource.java
@@ -0,0 +1,166 @@
+/*
+ * Copyright 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.paging;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.List;
+
+import io.reactivex.Single;
+import io.reactivex.SingleObserver;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
+
+abstract class RxPageKeyedDataSource<Key, Value> extends PageKeyedDataSource<Key, Value> {
+    public static class LoadInitialResponse<Key, Value> {
+        @NonNull
+        @SuppressWarnings("WeakerAccess")
+        public final List<Value> result;
+        public final int position;
+        @Nullable
+        @SuppressWarnings("WeakerAccess")
+        public final Key previousPageKey;
+        @Nullable
+        @SuppressWarnings("WeakerAccess")
+        public final Key nextPageKey;
+
+        public LoadInitialResponse(
+                @NonNull List<Value> result,
+                int position,
+                @Nullable Key previousPageKey,
+                @Nullable Key nextPageKey) {
+            this.result = result;
+            this.position = position;
+            this.previousPageKey = previousPageKey;
+            this.nextPageKey = nextPageKey;
+        }
+    }
+
+    public static class LoadBeforeResponse<Key, Value> {
+        @NonNull
+        public final List<Value> data;
+        @Nullable
+        @SuppressWarnings("WeakerAccess")
+        public final Key adjacentPageKey;
+
+        public LoadBeforeResponse(@NonNull List<Value> data, @Nullable Key adjacentPageKey) {
+            this.data = data;
+            this.adjacentPageKey = adjacentPageKey;
+        }
+    }
+
+    public static class LoadAfterResponse<Key, Value> {
+        @NonNull
+        public final List<Value> data;
+        @Nullable
+        @SuppressWarnings("WeakerAccess")
+        public final Key adjacentPageKey;
+
+        public LoadAfterResponse(@NonNull List<Value> data, @Nullable Key adjacentPageKey) {
+            this.data = data;
+            this.adjacentPageKey = adjacentPageKey;
+        }
+    }
+
+    @Override
+    public void loadInitial(
+            @NonNull LoadInitialParams<Key> params,
+            @NonNull final LoadInitialCallback<Key, Value> callback
+    ) {
+        loadInitial(params)
+                .subscribeOn(Schedulers.from(getExecutor()))
+                .subscribe(new SingleObserver<LoadInitialResponse<Key, Value>>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+                        // Ignored
+                    }
+
+                    @Override
+                    public void onSuccess(LoadInitialResponse<Key, Value> response) {
+                        callback.onResult(
+                                response.result,
+                                response.position,
+                                response.result.size(),
+                                response.previousPageKey,
+                                response.nextPageKey
+                        );
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        callback.onError(e);
+                    }
+                });
+    }
+
+    @Override
+    public void loadBefore(
+            @NonNull LoadParams<Key> params,
+            @NonNull final LoadCallback<Key, Value> callback
+    ) {
+        loadBefore(params)
+                .subscribeOn(Schedulers.from(getExecutor()))
+                .subscribe(new SingleObserver<LoadBeforeResponse<Key, Value>>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+                        // Ignored
+                    }
+
+                    @Override
+                    public void onSuccess(LoadBeforeResponse<Key, Value> response) {
+                        callback.onResult(response.data, response.adjacentPageKey);
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        callback.onError(e);
+                    }
+                });
+    }
+
+    @Override
+    public void loadAfter(
+            @NonNull LoadParams<Key> params,
+            @NonNull final LoadCallback<Key, Value> callback
+    ) {
+        loadAfter(params)
+                .subscribeOn(Schedulers.from(getExecutor()))
+                .subscribe(new SingleObserver<LoadAfterResponse<Key, Value>>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+                    }
+
+                    @Override
+                    public void onSuccess(LoadAfterResponse<Key, Value> response) {
+                        callback.onResult(response.data, response.adjacentPageKey);
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        callback.onError(e);
+                    }
+                });
+    }
+
+    public abstract Single<LoadInitialResponse<Key, Value>> loadInitial(
+            LoadInitialParams<Key> params);
+
+    public abstract Single<LoadBeforeResponse<Key, Value>> loadBefore(LoadParams<Key> params);
+
+    public abstract Single<LoadAfterResponse<Key, Value>> loadAfter(LoadParams<Key> params);
+}
---
+++ paging/rxjava2/src/test/java/androidx/paging/RxPositionalDataSourceTest.kt
@@ -0,0 +1,162 @@
+/*
+ * Copyright 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.paging
+
+import androidx.paging.ListenablePositionalDataSource.InitialResult
+import androidx.paging.ListenablePositionalDataSource.LoadInitialParams
+import androidx.paging.ListenablePositionalDataSource.LoadRangeParams
+import androidx.paging.ListenablePositionalDataSource.RangeResult
+import androidx.test.filters.SmallTest
+import io.reactivex.Single
+import io.reactivex.functions.Consumer
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.CoreMatchers.equalTo
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+import java.util.concurrent.ExecutionException
+import java.util.concurrent.TimeUnit
+
+@RunWith(JUnit4::class)
+@SmallTest
+class RxPositionalDataSourceTest {
+    private lateinit var trivialDataSource: RxPositionalDataSource<Int>
+    private lateinit var perpetuallyLoadingDataSource: RxPositionalDataSource<Int>
+    private lateinit var errorDataSource: RxPositionalDataSource<Int>
+
+    @Before
+    fun setUp() {
+        // Trivial example with position == value.
+        trivialDataSource = object : RxPositionalDataSource<Int>() {
+            override fun loadInitial(params: LoadInitialParams): Single<List<Int>> {
+                val start = params.requestedStartPosition
+                val end = params.requestedStartPosition + params.requestedLoadSize - 1
+                return Single.just((start..end).toList())
+            }
+
+            override fun loadRange(params: LoadRangeParams): Single<List<Int>> {
+                val start = params.startPosition
+                val end = params.startPosition + params.loadSize - 1
+                return Single.just((start..end).toList())
+            }
+        }
+
+        perpetuallyLoadingDataSource = object : RxPositionalDataSource<Int>() {
+            override fun loadInitial(params: LoadInitialParams): Single<List<Int>> = Single.never()
+
+            override fun loadRange(params: LoadRangeParams): Single<List<Int>> = Single.never()
+        }
+
+        errorDataSource = object : RxPositionalDataSource<Int>() {
+            override fun loadInitial(
+                params: LoadInitialParams
+            ): Single<List<Int>> = Single.error(NotImplementedError())
+
+            override fun loadRange(
+                params: LoadRangeParams
+            ): Single<List<Int>> = Single.error(NotImplementedError())
+        }
+
+        PagedListTestUtil.initDataSource(trivialDataSource)
+        PagedListTestUtil.initDataSource(perpetuallyLoadingDataSource)
+        PagedListTestUtil.initDataSource(errorDataSource)
+    }
+
+    @Test
+    fun loadInitial_empty() {
+        val testParams = LoadInitialParams(0, 0, 5, false)
+        val actual = trivialDataSource
+            .loadInitial(testParams)
+            .get()
+
+        assertThat(
+            actual,
+            equalTo(InitialResult<Int>(List(0) { it }, 0, 0))
+        )
+    }
+
+    @Test
+    fun loadInitial_simple() {
+        val testParams = LoadInitialParams(0, 10, 5, false)
+        val actual = trivialDataSource
+            .loadInitial(testParams)
+            .get()
+
+        assertThat(
+            actual,
+            equalTo(InitialResult<Int>((0..9).toList(), 0, 10))
+        )
+    }
+
+    @Test
+    fun loadRange_empty() {
+        val testParams = LoadRangeParams(5, 0)
+        val actual = trivialDataSource
+            .loadRange(testParams)
+            .get()
+
+        assertThat(
+            actual,
+            equalTo(RangeResult<Int>(List(0) { it }))
+        )
+    }
+
+    @Test
+    fun loadRange_simple() {
+        val testParams = LoadRangeParams(0, 10)
+        val actual = trivialDataSource
+            .loadRange(testParams)
+            .get()
+
+        assertThat(
+            actual,
+            equalTo(RangeResult<Int>((0..9).toList()))
+        )
+    }
+
+    @Test
+    fun dispose() {
+        var didSucceed = false
+        val testParams = PositionalDataSource.LoadRangeParams(0, 10)
+        val testSubscription = perpetuallyLoadingDataSource
+            .loadRange(testParams)
+            .subscribe(Consumer<List<Int>> { didSucceed = true })
+
+        testSubscription.dispose()
+        assertThat(testSubscription.isDisposed, `is`(true))
+        assertThat(didSucceed, `is`(false))
+    }
+
+    @Test
+    fun cancel() {
+        val testParams = PositionalDataSource.LoadRangeParams(0, 10)
+        trivialDataSource
+            .loadRange(testParams)
+            .test(true)
+            .assertComplete()
+    }
+
+    @Test(expected = ExecutionException::class)
+    fun error() {
+        val testParams = LoadRangeParams(0, 10)
+        errorDataSource
+            .loadRange(testParams)
+            .get(100, TimeUnit.MILLISECONDS)
+    }
+}
