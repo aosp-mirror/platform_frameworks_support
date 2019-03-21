c9d1954162a3a9e8b9f61a18021b8f5bf4fdc674
Ian Lake <ilake@google.com>
2019-03-21 14:19:27 -0700

    Remove argument Bundle from FragmentFactory#instantiate
    Provide a clearer separation between what
    instantiate() is supposed to do (just
    construct the class instance) versus what is
    the responsibility of the caller (set the
    arguments on the resulting Fragment).
    
    As arguments can change at any time, it doesn't
    make as much sense to use them at instantiation
    time.
    
    Test: tests still pass
    Fixes: 128836103
    Change-Id: I8018f4963ace5e58144ed1596c3b1e0b46638a7e

--- fragment/src/main/java/androidx/fragment/app/FragmentFactory.java
+++ fragment/src/main/java/androidx/fragment/app/FragmentFactory.java
@@ -16,10 +16,7 @@
 
 package androidx.fragment.app;
 
-import android.os.Bundle;
-
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.collection.SimpleArrayMap;
 
 import java.lang.reflect.InvocationTargetException;
@@ -103,22 +100,16 @@ public class FragmentFactory {
      * Create a new instance of a Fragment with the given class name. This uses
      * {@link #loadFragmentClass(ClassLoader, String)} and the empty
      * constructor of the resulting Class by default.
-     * <p>
-     * Note that this method is <strong>not</strong> responsible for calling
-     * {@link Fragment#setArguments(Bundle)} on the returned Fragment instance.
      *
      * @param classLoader The default classloader to use for instantiation
      * @param className The class name of the fragment to instantiate.
-     * @param args The Fragment's arguments, if any. This will be <code>null</code> when
-     *             inflating the Fragment from XML.
      * @return Returns a new fragment instance.
-     * @throws InstantiationException If there is a failure in instantiating
+     * @throws Fragment.InstantiationException If there is a failure in instantiating
      * the given fragment class.  This is a runtime exception; it is not
      * normally expected to happen.
      */
     @NonNull
-    public Fragment instantiate(@NonNull ClassLoader classLoader, @NonNull String className,
-                                @SuppressWarnings("unused") @Nullable Bundle args) {
+    public Fragment instantiate(@NonNull ClassLoader classLoader, @NonNull String className) {
         try {
             Class<? extends Fragment> cls = loadFragmentClass(classLoader, className);
             return cls.getConstructor().newInstance();
