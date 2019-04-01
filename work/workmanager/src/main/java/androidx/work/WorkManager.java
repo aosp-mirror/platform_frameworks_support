71dabb02a8e908718846a8058576edb1fdeb70fe
Sumir Kataria <sumir@google.com>
2019-04-01 16:33:01 -0700

    On-demand initialization for WorkManager.
    This CL introduces WorkManager#getInstance(Context) as the preferred
    method and deprecates the argument-less version.  This method performs
    on-demand initialization of WorkManager if it hasn't been initialized
    previously by looking to see if the application Context implements
    Configuration.Provider and querying it for the Configuration.
    
    The exact order of initialization checks are now as follows:
    
    1. Through WorkManagerInitializer if it's enabled.
    2. Otherwise, through a WorkManager#initialize in Application#onCreate
       or ContentProvider#onCreate if specified by the developer.
    3. Otherwise, through WorkManager#getInstance(Context) if the
       application Context implements Configuration.Provider.  Using
       WorkManager#getInstance() at this point results in an exception as
       it does not contain the necessary information to initialize.
    4. Otherwise, throws an IllegalStateException.
    
    This change allows WorkManager to be setup in a manner where you can
    only allow it to be initialized when it is needed.  This is good for
    apps that care about the critical path initialization needed by
    WorkManager; in addition, all of WorkManager's own BroadcastReceivers
    and Services are set up to try to auto-initialize if needed.  Note
    that this initialization can still happen on the main thread.
    
    Bug: 127497100
    Test: Ran integration tests with integration app doing on-demand init.
    Change-Id: I30930d6445c0e9c5f08eaaab554963aaac99b3c9

--- work/workmanager/src/main/java/androidx/work/WorkManager.java
+++ work/workmanager/src/main/java/androidx/work/WorkManager.java
@@ -52,7 +52,7 @@ import java.util.concurrent.TimeUnit;
  *
  * <pre>
  * {@code
- * WorkManager workManager = WorkManager.getInstance();
+ * WorkManager workManager = WorkManager.getInstance(Context);
  * workManager.enqueue(new OneTimeWorkRequest.Builder(FooWorker.class).build());}</pre>
  *
  * A {@link WorkRequest} has an associated id that can be used for lookups and observation as
@@ -134,7 +134,9 @@ public abstract class WorkManager {
      * @throws IllegalStateException If WorkManager is not initialized properly.  This is most
      *         likely because you disabled the automatic initialization but forgot to manually
      *         call {@link WorkManager#initialize(Context, Configuration)}.
+     * @deprecated Call {@link WorkManager#getInstance(Context)} instead.
      */
+    @Deprecated
     public static @NonNull WorkManager getInstance() {
         WorkManager workManager = WorkManagerImpl.getInstance();
         if (workManager == null) {
@@ -148,6 +150,26 @@ public abstract class WorkManager {
     }
 
     /**
+     * Retrieves the {@code default} singleton instance of {@link WorkManager}.
+     *
+     * @param context A {@link Context} for on-demand initialization.
+     * @return The singleton instance of {@link WorkManager}; this may be {@code null} in unusual
+     *         circumstances where you have disabled automatic initialization and have failed to
+     *         manually call {@link #initialize(Context, Configuration)}.
+     * @throws IllegalStateException If WorkManager is not initialized properly.  This is most
+     *         likely because you disabled the automatic initialization but forgot to manually
+     *         call {@link WorkManager#initialize(Context, Configuration)}.
+     */
+    public static @NonNull WorkManager getInstance(@NonNull Context context) {
+        WorkManager workManager = WorkManagerImpl.getInstance(context);
+        if (workManager == null) {
+            throw new IllegalStateException("TODO");
+        } else {
+            return workManager;
+        }
+    }
+
+    /**
      * Used to do a one-time initialization of the {@link WorkManager} singleton with a custom
      * {@link Configuration}.  By default, this method should not be called because WorkManager is
      * automatically initialized.  To initialize WorkManager yourself, please follow these steps:
@@ -155,7 +177,7 @@ public abstract class WorkManager {
      * <li>Disable {@code androidx.work.impl.WorkManagerInitializer} in your manifest.
      * <li>Invoke this method in {@code Application#onCreate} or a {@code ContentProvider}. Note
      * that this method <b>must</b> be invoked in one of these two places or you risk getting a
-     * {@code NullPointerException} in {@link #getInstance()}.
+     * {@code NullPointerException} in {@link #getInstance(Context)}.
      * </ul></p>
      * <p>
      * This method throws an exception if it is called multiple times.
