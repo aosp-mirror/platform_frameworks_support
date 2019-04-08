15a42259992eacb0d9cd72d30daee975a370919c
Sumir Kataria <sumir@google.com>
2019-04-08 10:21:40 -0700

    RxWorker API tweaks.
    1. Change onStopped to final.  It should not be overriden.
    2. Add @NonNull to getBackgroundScheduler and createWork return
       types.
    
    Fixes: 124522105
    Test: Ran existing tests.
    Change-Id: I8b7a3130042040d748da17f8236f98b300505619

--- work/workmanager-rxjava2/src/main/java/androidx/work/RxWorker.java
+++ work/workmanager-rxjava2/src/main/java/androidx/work/RxWorker.java
@@ -91,7 +91,7 @@ public abstract class RxWorker extends ListenableWorker {
      *
      * @return The default {@link Scheduler}.
      */
-    protected Scheduler getBackgroundScheduler() {
+    protected @NonNull Scheduler getBackgroundScheduler() {
         return Schedulers.from(getBackgroundExecutor());
     }
 
@@ -115,10 +115,10 @@ public abstract class RxWorker extends ListenableWorker {
      * @return a {@code Single<Result>} that represents the work.
      */
     @MainThread
-    public abstract Single<Result> createWork();
+    public abstract @NonNull Single<Result> createWork();
 
     @Override
-    public void onStopped() {
+    public final void onStopped() {
         super.onStopped();
         final SingleFutureAdapter observer = mSingleFutureObserverAdapter;
         if (observer != null) {
