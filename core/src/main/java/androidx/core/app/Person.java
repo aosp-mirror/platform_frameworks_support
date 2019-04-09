b2cc896a54e1d77e2e81cb53cd77e2bd61fc6fdd
Tony Mak <tonymak@google.com>
2019-04-09 18:37:00 +0100

    Person is now VersionedParcelable
    ConversationActions.Message is depend on Person.
    So, to make ConversationActions.Message a VersionedParcelable, we'd need
    to make Person a VersionedParcelable first.
    
    BUG: 130212350
    Test: run PersonTest
    Change-Id: Idd1049b2a4186ba343aba52e2fa79ac4cf4de32d

--- core/src/main/java/androidx/core/app/Person.java
+++ core/src/main/java/androidx/core/app/Person.java
@@ -16,6 +16,7 @@
 
 package androidx.core.app;
 
+import static androidx.annotation.RestrictTo.Scope.LIBRARY_GROUP;
 import static androidx.annotation.RestrictTo.Scope.LIBRARY_GROUP_PREFIX;
 
 import android.os.Bundle;
@@ -26,12 +27,16 @@ import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 import androidx.annotation.RestrictTo;
 import androidx.core.graphics.drawable.IconCompat;
+import androidx.versionedparcelable.ParcelField;
+import androidx.versionedparcelable.VersionedParcelable;
+import androidx.versionedparcelable.VersionedParcelize;
 
 /**
  * Provides an immutable reference to an entity that appears repeatedly on different surfaces of the
  * platform. For example, this could represent the sender of a message.
  */
-public class Person {
+@VersionedParcelize(jetifyAs = "android.support.v4.app.Person")
+public class Person implements VersionedParcelable {
     private static final String NAME_KEY = "name";
     private static final String ICON_KEY = "icon";
     private static final String URI_KEY = "uri";
@@ -98,18 +103,39 @@ public class Person {
                 .build();
     }
 
-    @SuppressWarnings("WeakerAccess") /* synthetic access */
-    @Nullable CharSequence mName;
-    @SuppressWarnings("WeakerAccess") /* synthetic access */
-    @Nullable IconCompat mIcon;
-    @SuppressWarnings("WeakerAccess") /* synthetic access */
-    @Nullable String mUri;
-    @SuppressWarnings("WeakerAccess") /* synthetic access */
-    @Nullable String mKey;
-    @SuppressWarnings("WeakerAccess") /* synthetic access */
-    boolean mIsBot;
-    @SuppressWarnings("WeakerAccess") /* synthetic access */
-    boolean mIsImportant;
+    /** @hide */
+    @ParcelField(1)
+    @Nullable
+    @RestrictTo(LIBRARY_GROUP)
+    public CharSequence mName;
+
+    /** @hide */
+    @ParcelField(2)
+    @Nullable
+    @RestrictTo(LIBRARY_GROUP)
+    public IconCompat mIcon;
+
+    /** @hide */
+    @ParcelField(3)
+    @Nullable
+    @RestrictTo(LIBRARY_GROUP)
+    public String mUri;
+
+    /** @hide */
+    @ParcelField(4)
+    @Nullable
+    @RestrictTo(LIBRARY_GROUP)
+    public String mKey;
+
+    /** @hide */
+    @ParcelField(5)
+    @RestrictTo(LIBRARY_GROUP)
+    public boolean mIsBot;
+
+    /** @hide */
+    @ParcelField(6)
+    @RestrictTo(LIBRARY_GROUP)
+    public boolean mIsImportant;
 
     @SuppressWarnings("WeakerAccess") /* synthetic access */
     Person(Builder builder) {
@@ -120,6 +146,13 @@ public class Person {
         mIsBot = builder.mIsBot;
         mIsImportant = builder.mIsImportant;
     }
+
+    /**
+     * Used by VersionedParcelable.
+     * @hide
+     */
+    @RestrictTo(LIBRARY_GROUP)
+    public Person() {}
 
     /**
      * Writes and returns a new {@link Bundle} that represents this {@link Person}. This bundle can
