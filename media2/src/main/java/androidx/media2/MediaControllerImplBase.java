211ded90f36aa1c878365a2afe0a25e832f88bdd
Gyumin Sim <gyumin@google.com>
2019-04-10 16:38:48 +0900

    Add MediaController#getVideoSize()
    Bug: 130272742
    Test: ./gradlew :media2:check :media2:connectedCheck &&
          ./media2/version-compat-tests/runtest.sh 1
    Change-Id: I42ed0b143b24372d6002a696063f06b079d04329

--- media2/src/main/java/androidx/media2/MediaControllerImplBase.java
+++ media2/src/main/java/androidx/media2/MediaControllerImplBase.java
@@ -149,6 +149,8 @@ class MediaControllerImplBase implements MediaControllerImpl {
     private PendingIntent mSessionActivity;
     @GuardedBy("mLock")
     private SessionCommandGroup mAllowedCommands;
+    @GuardedBy("mLock")
+    private VideoSize mVideoSize = new VideoSize(0, 0);
 
     // Assignment should be used with the lock hold, but should be used without a lock to prevent
     // potential deadlock.
@@ -769,6 +771,13 @@ class MediaControllerImplBase implements MediaControllerImpl {
     }
 
     @Override
+    public @NonNull VideoSize getVideoSize() {
+        synchronized (mLock) {
+            return mVideoSize;
+        }
+    }
+
+    @Override
     public @NonNull Context getContext() {
         return mContext;
     }
@@ -1060,6 +1069,21 @@ class MediaControllerImplBase implements MediaControllerImpl {
         });
     }
 
+    void notifyVideoSizeChanged(final MediaItem item, final VideoSize videoSize) {
+        synchronized (mLock) {
+            mVideoSize = videoSize;
+        }
+        mCallbackExecutor.execute(new Runnable() {
+            @Override
+            public void run() {
+                if (!mInstance.isConnected()) {
+                    return;
+                }
+                mCallback.onVideoSizeChanged(mInstance, item, videoSize);
+            }
+        });
+    }
+
     // Should be used without a lock to prevent potential deadlock.
     void onConnectedNotLocked(IMediaSession sessionBinder,
             final SessionCommandGroup allowedCommands,
@@ -1076,7 +1100,8 @@ class MediaControllerImplBase implements MediaControllerImpl {
             final PendingIntent sessionActivity,
             final int currentMediaItemIndex,
             final int previousMediaItemIndex,
-            final int nextMediaItemIndex) {
+            final int nextMediaItemIndex,
+            final VideoSize videoSize) {
         if (DEBUG) {
             Log.d(TAG, "onConnectedNotLocked sessionBinder=" + sessionBinder
                     + ", allowedCommands=" + allowedCommands);
@@ -1115,6 +1140,7 @@ class MediaControllerImplBase implements MediaControllerImpl {
                 mCurrentMediaItemIndex = currentMediaItemIndex;
                 mPreviousMediaItemIndex = previousMediaItemIndex;
                 mNextMediaItemIndex = nextMediaItemIndex;
+                mVideoSize = videoSize;
                 try {
                     // Implementation for the local binder is no-op,
                     // so can be used without worrying about deadlock.
