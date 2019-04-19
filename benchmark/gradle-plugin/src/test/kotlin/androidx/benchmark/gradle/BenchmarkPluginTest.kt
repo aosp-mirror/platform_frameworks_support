95e5122c7059d01535f502423d74d4e96e8adc1e
Dustin Lam <dustinlam@google.com>
2019-04-18 19:43:02 -0700

    Throw error preventing usage in non-AndroidX projects
    This will throw an error when running any gradle task failing the build
    immediately with the following studio / cli output.
    
    ```
    FAILURE: Build completed with 2 failures.
    
    1: Task failed with an exception.
    -----------
    * Where:
    Build file .../build.gradle' line: #
    
    * What went wrong:
    A problem occurred evaluating project ':benchmark'.
    > Failed to apply plugin [id 'androidx.benchmark']
       > androidx.benchmark can only be used in AndroidX enabled
          projects! To enable AndroidX, add android.useAndroidX=true to your project's
          gradle.properties file. If your project has dependencies on legacy artifacts that
          need to be migrated to AndroidX, you can enable the Jetifier to do so by adding
          android.enableJetifier=true to your project's gradle.properties file. For more
          information, visit https://developer.android.com/jetpack/androidx/migrate.
    
    ...
    
    BUILD FAILED in 0s
    ```
    
    Test: ./gradlew benchmark:cC
    Change-Id: I67960660b25bd433e9591a31bc165562658b29b8

--- benchmark/gradle-plugin/src/test/kotlin/androidx/benchmark/gradle/BenchmarkPluginTest.kt
+++ benchmark/gradle-plugin/src/test/kotlin/androidx/benchmark/gradle/BenchmarkPluginTest.kt
@@ -19,6 +19,8 @@ package androidx.benchmark.gradle
 import org.gradle.api.Project
 import org.gradle.api.internal.plugins.PluginApplicationException
 import org.gradle.testfixtures.ProjectBuilder
+import org.gradle.testkit.runner.BuildResult
+import org.gradle.testkit.runner.GradleRunner
 import org.junit.Assert
 import org.junit.Before
 import org.junit.Rule
@@ -27,6 +29,8 @@ import org.junit.rules.TemporaryFolder
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
 import java.io.File
+import java.util.Properties
+import kotlin.streams.toList
 
 @RunWith(JUnit4::class)
 class BenchmarkPluginTest {
@@ -34,17 +38,53 @@ class BenchmarkPluginTest {
     @get:Rule
     val testProjectDir = TemporaryFolder()
 
+    private val pluginClasspathStream =
+        BenchmarkPluginTest::class.java.classLoader.getResourceAsStream("plugin-classpath.txt")
+            ?: throw IllegalStateException(
+                "Did not find plugin classpath resource, run `testClasses` build task."
+            )
+    private val pluginClasspath = pluginClasspathStream.bufferedReader().lines().map { File(it) }
+
+    private lateinit var gradlePropFile: File
     private lateinit var localPropFile: File
     private lateinit var project: Project
+
+    private lateinit var compileSdkVersion: String
+    private lateinit var buildToolsVersion: String
+    private lateinit var prebuiltsRepo: String
+    private lateinit var minSdkVersion: String
 
     @Before
     fun setUp() {
         testProjectDir.root.mkdirs()
 
-        localPropFile = File(testProjectDir.root, "local.properties")
-        localPropFile.createNewFile()
-        localPropFile.writeText("sdk.dir=/usr/test/android/home")
+        // TODO: Consolidate mock gradle project logic with safeargs plugin test logic.
+        val stream = BenchmarkPluginTest::class.java.classLoader.getResourceAsStream("sdk.prop")
+        val properties = Properties()
+        properties.load(stream)
 
+        compileSdkVersion = properties.getProperty("compileSdkVersion")
+        buildToolsVersion = properties.getProperty("buildToolsVersion")
+        prebuiltsRepo = properties.getProperty("prebuiltsRepo")
+        minSdkVersion = properties.getProperty("minSdkVersion")
+
+        // copy local.properties
+        localPropFile = File("../../local.properties")
+        localPropFile.copyTo(
+            File(testProjectDir.root, "local.properties"),
+            overwrite = true
+        )
+
+        gradlePropFile = File(testProjectDir.root, "gradle.properties")
+        gradlePropFile.createNewFile()
+        gradlePropFile.writer().use {
+            val props = Properties()
+            props.setProperty("android.useAndroidX", "true")
+            props.setProperty("android.enableJetifier", "true")
+            props.store(it, null)
+        }
+
+        File("src/test/test-data", "app-project").copyRecursively(testProjectDir.root)
         project = ProjectBuilder.builder()
             .withProjectDir(testProjectDir.root)
             .build()
@@ -52,11 +92,39 @@ class BenchmarkPluginTest {
 
     @Test
     fun applyPluginAppProject() {
-        project.apply { it.plugin("com.android.application") }
-        project.apply { it.plugin("androidx.benchmark") }
+        val buildFile = File(testProjectDir.root, "build.gradle")
+        buildFile.createNewFile()
+        buildFile.writeText(
+            """
+            plugins {
+                id 'com.android.application'
+                id 'androidx.benchmark'
+            }
 
-        Assert.assertNotNull(project.tasks.findByPath("lockClocks"))
-        Assert.assertNotNull(project.tasks.findByPath("unlockClocks"))
+            repositories {
+                maven { url "$prebuiltsRepo/androidx/external" }
+                maven { url "$prebuiltsRepo/androidx/internal" }
+            }
+
+            android {
+                compileSdkVersion $compileSdkVersion
+                buildToolsVersion "$buildToolsVersion"
+
+                defaultConfig {
+                    minSdkVersion $minSdkVersion
+                }
+            }
+        """.trimIndent()
+        )
+
+        val project: BuildResult = GradleRunner.create()
+            .withProjectDir(testProjectDir.root)
+            .withPluginClasspath(pluginClasspath.toList())
+            .withArguments("tasks")
+            .build()
+
+        Assert.assertTrue(project.output.contains("lockClocks"))
+        Assert.assertTrue(project.output.contains("unlockClocks"))
     }
 
     @Test
@@ -72,6 +140,18 @@ class BenchmarkPluginTest {
     fun applyPluginNonAndroidProject() {
         project.apply { it.plugin("java-library") }
         project.apply { it.plugin("androidx.benchmark") }
+
+        Assert.assertNotNull(project.tasks.findByPath("lockClocks"))
+        Assert.assertNotNull(project.tasks.findByPath("unlockClocks"))
+    }
+
+    @Test
+    fun applyPluginNonAndroidXProject() {
+        project.apply { it.plugin("com.android.library") }
+        project.apply { it.plugin("androidx.benchmark") }
+
+        project.setProperty("android.useAndroidX", "false")
+        project.setProperty("android.enableJetifier", "false")
 
         Assert.assertNotNull(project.tasks.findByPath("lockClocks"))
         Assert.assertNotNull(project.tasks.findByPath("unlockClocks"))
