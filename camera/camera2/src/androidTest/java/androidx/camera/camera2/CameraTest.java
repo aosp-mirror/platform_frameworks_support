9b9f049451319705c17b5b709381a1a5547251c7
Scott Nien <scottnien@google.com>
2019-04-23 01:21:42 +0800

    Fix Surface was abandoned crash when quickly bind/unbind use cases.
    When doing bind/unbind quickly, addOnline/RemoveOnline actions are queued in
    the camera thread while UseCase.clear() is called right after unbind call.
    This cause surface to be released too early. This CL contains 2 parts:
    (1) Aggregate addOnline/removeOnline actions to eliminate unnessary action
       and to speed up the process duration.
    (2) Add attach count to DeferrableSurface in UseCase when added to online in
       order to ensure the surface is not released when use case is online. The
       attach count is added immediately in Camera.addOnlineUsecase(), while
       reducing of the attach count happens after Camera.removeOnlineUsecase is
       finished in camera thread.
    
    Bug: 130081253, 130730218
    Test: camera2 androidTest: CameraTest
    Change-Id: Ie5141a72990920e522b6095fdc4613de6a810e27

--- camera/camera2/src/androidTest/java/androidx/camera/camera2/CameraTest.java
+++ camera/camera2/src/androidTest/java/androidx/camera/camera2/CameraTest.java
@@ -16,11 +16,14 @@
 
 package androidx.camera.camera2;
 
+import static com.google.common.truth.Truth.assertThat;
+
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.timeout;
 import static org.mockito.Mockito.verify;
 
+import android.Manifest;
 import android.graphics.ImageFormat;
 import android.hardware.camera2.CameraDevice;
 import android.media.ImageReader;
@@ -29,41 +32,55 @@ import android.os.Handler;
 import android.os.HandlerThread;
 import android.util.Size;
 
-import androidx.camera.core.BaseCamera;
 import androidx.camera.core.CameraDeviceConfig;
 import androidx.camera.core.CameraFactory;
 import androidx.camera.core.CameraX.LensFacing;
+import androidx.camera.core.DeferrableSurface;
 import androidx.camera.core.ImmediateSurface;
 import androidx.camera.core.SessionConfig;
 import androidx.camera.core.UseCase;
+import androidx.camera.testing.CameraUtil;
 import androidx.camera.testing.fakes.FakeUseCase;
 import androidx.camera.testing.fakes.FakeUseCaseConfig;
 import androidx.test.core.app.ApplicationProvider;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.SmallTest;
+import androidx.test.rule.GrantPermissionRule;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.Semaphore;
 
 @SmallTest
 @RunWith(AndroidJUnit4.class)
 public final class CameraTest {
+
     private static final LensFacing DEFAULT_LENS_FACING = LensFacing.BACK;
     static CameraFactory sCameraFactory;
 
-    BaseCamera mCamera;
+    Camera mCamera;
 
     TestUseCase mFakeUseCase;
     OnImageAvailableListener mMockOnImageAvailableListener;
     String mCameraId;
+
+    Semaphore mSemaphore;
+    HandlerThread mCameraHandlerThread;
+    Handler mCameraHandler;
+
+    @Rule
+    public GrantPermissionRule mRuntimePermissionRule = GrantPermissionRule.grant(
+            Manifest.permission.CAMERA);
 
     private static String getCameraIdForLensFacingUnchecked(LensFacing lensFacing) {
         try {
@@ -93,7 +110,7 @@ public final class CameraTest {
         suggestedResolutionMap.put(mCameraId, new Size(640, 480));
         mFakeUseCase.updateSuggestedResolution(suggestedResolutionMap);
 
-        mCamera = sCameraFactory.getCamera(mCameraId);
+        mCamera = (Camera) sCameraFactory.getCamera(mCameraId);
     }
 
     @After
@@ -116,6 +133,11 @@ public final class CameraTest {
             mFakeUseCase.close();
             mFakeUseCase = null;
         }
+
+        if (mCameraHandlerThread != null) {
+            mCameraHandlerThread.quitSafely();
+        }
+
     }
 
     @Test
@@ -201,6 +223,283 @@ public final class CameraTest {
         mCamera.onUseCaseActive(mFakeUseCase);
 
         verify(mMockOnImageAvailableListener, never()).onImageAvailable(any(ImageReader.class));
+    }
+
+    private void setupAggregateTest() {
+        mCameraHandlerThread = new HandlerThread("cameraThread");
+        mCameraHandlerThread.start();
+        mCameraHandler = new Handler(mCameraHandlerThread.getLooper());
+        mSemaphore = new Semaphore(0);
+        mCamera = new Camera(CameraUtil.getCameraManager(), mCameraId, mCameraHandler);
+        //Block the handler until semaphore is released.
+    }
+
+    // Block handler so that we can verify the aggregation.
+    private void blockHandler() {
+        mCameraHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    mSemaphore.acquire();
+                } catch (InterruptedException e) {
+
+                }
+            }
+        });
+    }
+
+    // unblock handler so that aggregated action can be executed.
+    private void unblockHandler() {
+        mSemaphore.release();
+    }
+
+    // wait until aggregated action is done.
+    private void waitHandlerIdle() throws InterruptedException {
+        final Object idleLock = new Object();
+        // If the posted runnable runs, it means the previous runnables are already executed.
+        mCameraHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                synchronized (idleLock) {
+                    idleLock.notify();
+                }
+            }
+        });
+
+        synchronized (idleLock) {
+            idleLock.wait(1000);
+        }
+
+        Thread.sleep(50);
+    }
+
+    @Test
+    public void addOnline_SameUseCases() {
+        setupAggregateTest();
+
+        // Block handler so that we can verify the aggregation.
+        blockHandler();
+
+        UseCase useCase1 = createUseCase();
+        mCamera.addOnlineUseCase(Arrays.asList(useCase1));
+        mCamera.addOnlineUseCase(Arrays.asList(useCase1));
+
+        assertThat(getUseCaseSurface(useCase1).getAttachedCount()).isEqualTo(1);
+        assertThat(mCamera.mUseCaseAggregator.getPendingOnline()).containsExactly(useCase1);
+        assertThat(mCamera.isUseCaseOnline(useCase1)).isFalse();
+    }
+
+    @Test
+    public void addOnline_alreadyOnline() throws InterruptedException {
+        setupAggregateTest();
+
+        // Block handler so that we can verify the aggregation.
+        blockHandler();
+
+        UseCase useCase1 = createUseCase();
+        mCamera.addOnlineUseCase(Arrays.asList(useCase1));
+        assertThat(getUseCaseSurface(useCase1).getAttachedCount()).isEqualTo(1);
+        unblockHandler();
+        waitHandlerIdle();
+
+        //Usecase1 is online now.
+        assertThat(mCamera.isUseCaseOnline(useCase1)).isTrue();
+
+        blockHandler();
+        mCamera.addOnlineUseCase(Arrays.asList(useCase1));
+
+        //no pending online use cases since UseCase1 is already online.
+        assertThat(mCamera.mUseCaseAggregator.getPendingOnline()).isEmpty();
+
+        unblockHandler();
+        // Surface is attached when (1) UseCase added to online (2) Camera session opened
+        // So here we need to wait until camera close before we start to verify the attach count
+        mCamera.close();
+        waitHandlerIdle();
+
+        // Surface is only attached once.
+        assertThat(getUseCaseSurface(useCase1).getAttachedCount()).isEqualTo(1);
+    }
+
+    @Test
+    public void addOnline_twoUseCases() throws InterruptedException {
+        setupAggregateTest();
+        // Block handler so that we can verify the aggregation.
+        blockHandler();
+
+        UseCase useCase1 = createUseCase();
+        mCamera.addOnlineUseCase(Arrays.asList(useCase1));
+        UseCase useCase2 = createUseCase();
+        mCamera.addOnlineUseCase(Arrays.asList(useCase2));
+
+        assertThat(getUseCaseSurface(useCase1).getAttachedCount()).isEqualTo(1);
+        assertThat(getUseCaseSurface(useCase2).getAttachedCount()).isEqualTo(1);
+        assertThat(mCamera.isUseCaseOnline(useCase1)).isFalse();
+        assertThat(mCamera.isUseCaseOnline(useCase2)).isFalse();
+        assertThat(mCamera.mUseCaseAggregator.getPendingOnline()).containsExactly(useCase1,
+                useCase2);
+
+        // unblock handler so that aggregated action can be executed.
+        unblockHandler();
+        // wait until aggregated action is done.
+        waitHandlerIdle();
+
+        // aggregator executes the action and clear the queue.
+        assertThat(mCamera.mUseCaseAggregator.getPendingOnline()).isEmpty();
+        assertThat(mCamera.isUseCaseOnline(useCase1)).isTrue();
+        assertThat(mCamera.isUseCaseOnline(useCase2)).isTrue();
+    }
+
+    @Test
+    public void addOnline_fromPendingOffline() throws InterruptedException {
+
+        setupAggregateTest();
+        blockHandler();
+
+        // First make UseCase online
+        UseCase useCase = createUseCase();
+        mCamera.addOnlineUseCase(Arrays.asList(useCase));
+        unblockHandler();
+        waitHandlerIdle();
+
+        blockHandler();
+        // Then put useCase in pendingOffline.
+        mCamera.removeOnlineUseCase(Arrays.asList(useCase));
+
+        // Then add the same UseCase .
+        mCamera.addOnlineUseCase(Arrays.asList(useCase));
+
+        assertThat(mCamera.mUseCaseAggregator.getPendingOffline()).isEmpty();
+        assertThat(mCamera.mUseCaseAggregator.getPendingOnline()).isEmpty();
+
+        unblockHandler();
+        waitHandlerIdle();
+
+        assertThat(mCamera.isUseCaseOnline(useCase)).isTrue();
+
+        mCamera.close();
+        waitHandlerIdle();
+        assertThat(getUseCaseSurface(useCase).getAttachedCount()).isEqualTo(1);
+
+    }
+
+    @Test
+    public void removeOnline_notOnline() throws InterruptedException {
+        setupAggregateTest();
+        blockHandler();
+
+        UseCase useCase1 = createUseCase();
+        mCamera.removeOnlineUseCase(Arrays.asList(useCase1));
+
+        // useCase2 is not online , should not be added to pendingOffline
+        assertThat(mCamera.mUseCaseAggregator.getPendingOffline()).isEmpty();
+        assertThat(getUseCaseSurface(useCase1).getAttachedCount()).isEqualTo(0);
+
+    }
+
+    @Test
+    public void removeOnline_fromPendingOnline() throws InterruptedException {
+        setupAggregateTest();
+        blockHandler();
+
+        UseCase useCase1 = createUseCase();
+        mCamera.addOnlineUseCase(Arrays.asList(useCase1));
+        assertThat(mCamera.mUseCaseAggregator.getPendingOnline()).containsExactly(useCase1);
+        assertThat(getUseCaseSurface(useCase1).getAttachedCount()).isEqualTo(1);
+
+        mCamera.removeOnlineUseCase(Arrays.asList(useCase1));
+
+        // useCase is pending for online, it should just remove it from pendingOnline instead of
+        // being added to pendingOffline.
+        assertThat(mCamera.mUseCaseAggregator.getPendingOffline()).isEmpty();
+        assertThat(mCamera.mUseCaseAggregator.getPendingOnline()).isEmpty();
+
+        // Should detach the surface since we remove it from pending Online .
+        assertThat(getUseCaseSurface(useCase1).getAttachedCount()).isEqualTo(0);
+    }
+
+    @Test
+    public void removeOnline_fromOnlineUseCases() throws InterruptedException {
+        setupAggregateTest();
+        blockHandler();
+
+        UseCase useCase1 = createUseCase();
+        UseCase useCase2 = createUseCase();
+        mCamera.addOnlineUseCase(Arrays.asList(useCase1, useCase2));
+
+        unblockHandler();
+        waitHandlerIdle();
+
+        assertThat(mCamera.isUseCaseOnline(useCase1)).isTrue();
+        assertThat(mCamera.isUseCaseOnline(useCase2)).isTrue();
+
+        blockHandler();
+        mCamera.removeOnlineUseCase(Arrays.asList(useCase1));
+
+        assertThat(mCamera.mUseCaseAggregator.getPendingOffline()).containsExactly(useCase1);
+
+        unblockHandler();
+        waitHandlerIdle();
+
+        assertThat(mCamera.mUseCaseAggregator.getPendingOffline()).isEmpty();
+        assertThat(mCamera.isUseCaseOnline(useCase1)).isFalse();
+        assertThat(mCamera.isUseCaseOnline(useCase2)).isTrue();
+
+        // Surface is attached when (1) UseCase added to online (2) Camera session opened
+        // So here we need to wait until camera close before we start to verify the attach count
+        mCamera.close();
+        waitHandlerIdle();
+
+        assertThat(getUseCaseSurface(useCase1).getAttachedCount()).isEqualTo(0);
+        assertThat(getUseCaseSurface(useCase2).getAttachedCount()).isEqualTo(1);
+
+
+    }
+
+    @Test
+    public void removeOnline_twoSameUseCase() throws InterruptedException {
+        setupAggregateTest();
+        blockHandler();
+
+        UseCase useCase1 = createUseCase();
+        UseCase useCase2 = createUseCase();
+        mCamera.addOnlineUseCase(Arrays.asList(useCase1, useCase2));
+
+        unblockHandler();
+        waitHandlerIdle();
+
+        // remove twice
+        blockHandler();
+        mCamera.removeOnlineUseCase(Arrays.asList(useCase1));
+        mCamera.removeOnlineUseCase(Arrays.asList(useCase1));
+
+        assertThat(mCamera.mUseCaseAggregator.getPendingOffline()).containsExactly(useCase1);
+
+        unblockHandler();
+        // Surface is attached when (1) UseCase added to online (2) Camera session opened
+        // So here we need to wait until camera close before we start to verify the attach count
+        mCamera.close();
+        waitHandlerIdle();
+
+        assertThat(getUseCaseSurface(useCase1).getAttachedCount()).isEqualTo(0);
+    }
+
+    private UseCase createUseCase() {
+        FakeUseCaseConfig config =
+                new FakeUseCaseConfig.Builder()
+                        .setTargetName("UseCase")
+                        .setLensFacing(DEFAULT_LENS_FACING)
+                        .build();
+
+        TestUseCase testUseCase = new TestUseCase(config, mMockOnImageAvailableListener);
+        Map<String, Size> suggestedResolutionMap = new HashMap<>();
+        suggestedResolutionMap.put(mCameraId, new Size(640, 480));
+        testUseCase.updateSuggestedResolution(suggestedResolutionMap);
+        return testUseCase;
+    }
+
+    private DeferrableSurface getUseCaseSurface(UseCase useCase) {
+        return useCase.getSessionConfig(mCameraId).getSurfaces().get(0);
     }
 
     private static class TestUseCase extends FakeUseCase {
