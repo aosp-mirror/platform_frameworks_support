1e3313d33e3a4031f582b2f40341f4e78b9cd118
Kyunglyul Hyun <klhyun@google.com>
2019-04-22 14:38:28 +0900

    MediaRouter: Adds setDynamicDescriptors to DGRouteController
    Hides OnDynamicRoutesChangedListener and
    setOnDynamicRoutesChangedListener from DynamicGroupRouteController and
    adds setDyanmicDescriptors to DynamicGroupRouteController
    
    The objective was to avoid managing listeners from provider.
    
    Bug: 130864689
    Test: ran support v7 demos manually
    Change-Id: I9c01003f4d0456c71e79dc5002d2310d90a4af0b

--- mediarouter/src/main/java/androidx/mediarouter/media/MediaRouteProvider.java
+++ mediarouter/src/main/java/androidx/mediarouter/media/MediaRouteProvider.java
@@ -16,6 +16,7 @@
 
 package androidx.mediarouter.media;
 
+import static androidx.annotation.RestrictTo.Scope.LIBRARY_GROUP;
 import static androidx.annotation.RestrictTo.Scope.LIBRARY_GROUP_PREFIX;
 
 import android.content.ComponentName;
@@ -25,6 +26,7 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 
+import androidx.annotation.GuardedBy;
 import androidx.annotation.IntDef;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -34,6 +36,7 @@ import androidx.mediarouter.media.MediaRouter.ControlRequestCallback;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.concurrent.Executor;
@@ -457,6 +460,15 @@ public abstract class MediaRouteProvider {
      * from the group by the user dynamically.
      */
     public abstract static class DynamicGroupRouteController extends RouteController {
+        private final Object mLock = new Object();
+
+        @GuardedBy("mLock")
+        private Executor mExecutor;
+        @GuardedBy("mLock")
+        private OnDynamicRoutesChangedListener mListener;
+        @GuardedBy("mLock")
+        private Collection<DynamicRouteDescriptor> mPendingRoutes;
+
         /**
          * Gets the title of the groupable routes section which will be shown to the user.
          * It is provided by {@link MediaRouteProvider}.
@@ -495,10 +507,52 @@ public abstract class MediaRouteProvider {
 
         /**
          * Called by {@link MediaRouter} to set the listener.
+         * @hide
          */
-        public abstract void setOnDynamicRoutesChangedListener(
+        @RestrictTo(LIBRARY_GROUP)
+        public final void setOnDynamicRoutesChangedListener(
                 @NonNull Executor executor,
-                @NonNull OnDynamicRoutesChangedListener listener);
+                @NonNull OnDynamicRoutesChangedListener listener) {
+            synchronized (mLock) {
+                if (executor == null) {
+                    throw new NullPointerException("Executor shouldn't be null.");
+                }
+                if (listener == null) {
+                    throw new NullPointerException("Listener shouldn't be null.");
+                }
+                mExecutor = executor;
+                mListener = listener;
+
+                if (mPendingRoutes != null && !mPendingRoutes.isEmpty()) {
+                    mExecutor.execute(new Runnable() {
+                        @Override
+                        public void run() {
+                            mListener.onRoutesChanged(DynamicGroupRouteController.this,
+                                    mPendingRoutes);
+                        }
+                    });
+                }
+            }
+        }
+
+        /**
+         * Sets the dynamic descriptors of routes published by the provider.
+         */
+        public final void setDynamicDescriptors(final Collection<DynamicRouteDescriptor> routes) {
+            synchronized (mLock) {
+                if (mExecutor != null) {
+                    mExecutor.execute(new Runnable() {
+                        @Override
+                        public void run() {
+                            mListener.onRoutesChanged(
+                                    DynamicGroupRouteController.this, routes);
+                        }
+                    });
+                } else {
+                    mPendingRoutes = new ArrayList<>(routes);
+                }
+            }
+        }
 
         /**
          * Used to notify media router each route's property changes regarding this
@@ -514,7 +568,9 @@ public abstract class MediaRouteProvider {
          *     <li> a route is turned off.</li>
          * </ul>
          * </p>
+         * @hide
          */
+        @RestrictTo(LIBRARY_GROUP)
         public interface OnDynamicRoutesChangedListener {
             /**
              * The provider should call this method when routes' properties change.
