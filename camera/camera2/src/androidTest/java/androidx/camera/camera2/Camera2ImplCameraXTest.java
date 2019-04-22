a50b00e74f110b76cb80853b460c0329ffff98de
AL Ho <hoal@google.com>
2019-04-22 11:50:51 +0800

    Fix ImageCaptureTest and Camera2ImplCameraXTest presubmit test failures.
    Reomove @FlakyTest.
    Test: manual test with emulator.
    
    Change-Id: I342b33c7f9a9f323bdb69330567060ca960abf8e

--- camera/camera2/src/androidTest/java/androidx/camera/camera2/Camera2ImplCameraXTest.java
+++ camera/camera2/src/androidTest/java/androidx/camera/camera2/Camera2ImplCameraXTest.java
@@ -44,7 +44,6 @@ import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.Observer;
 import androidx.test.core.app.ApplicationProvider;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
-import androidx.test.filters.FlakyTest;
 import androidx.test.filters.SmallTest;
 import androidx.test.rule.GrantPermissionRule;
 
@@ -61,7 +60,6 @@ import java.util.concurrent.atomic.AtomicLong;
  * Contains tests for {@link androidx.camera.core.CameraX} which require an actual implementation to
  * run.
  */
-@FlakyTest
 @SmallTest
 @RunWith(AndroidJUnit4.class)
 public final class Camera2ImplCameraXTest {
