4d83a828056039df34d8926baccab5c9d9a4cd11
Franklin Wu <nilknarfuw@google.com>
2019-04-23 18:22:35 -0700

    Preview processing implementation
    Added implementation in core and camera2 which allows for processing of
    preview stream related data.
    The ImageInfoProcessor only allow for updating of CaptureConfig
    parameters, does not include a step to modify the output image buffers.
    
    Bug: b/130193125
    Test: ./gradlew camera:integration-tests:camera-testapp-extensions:build
          manual test
    
    Change-Id: I48b0b5328df93b7a5cef930ed9f313909f22ba88

--- camera/extensions/src/main/java/androidx/camera/extensions/PreviewExtender.java
+++ camera/extensions/src/main/java/androidx/camera/extensions/PreviewExtender.java
@@ -18,11 +18,19 @@ package androidx.camera.extensions;
 
 import android.hardware.camera2.CameraCharacteristics;
 import android.hardware.camera2.CaptureRequest;
+import android.hardware.camera2.CaptureResult;
+import android.hardware.camera2.TotalCaptureResult;
 import android.util.Pair;
 
 import androidx.camera.camera2.Camera2Config;
+import androidx.camera.camera2.impl.Camera2CameraCaptureResultConverter;
+import androidx.camera.core.CameraCaptureResult;
+import androidx.camera.core.CameraCaptureResults;
 import androidx.camera.core.CameraX;
+import androidx.camera.core.CaptureStage;
 import androidx.camera.core.Config;
+import androidx.camera.core.ImageInfo;
+import androidx.camera.core.ImageInfoProcessor;
 import androidx.camera.core.PreviewConfig;
 import androidx.camera.extensions.impl.CaptureStageImpl;
 import androidx.camera.extensions.impl.PreviewExtenderImpl;
@@ -32,7 +40,7 @@ import androidx.camera.extensions.impl.PreviewExtenderImpl;
  */
 public abstract class PreviewExtender {
     private PreviewConfig.Builder mBuilder;
-    private PreviewExtenderImpl mImpl;
+    PreviewExtenderImpl mImpl;
 
     void init(PreviewConfig.Builder builder, PreviewExtenderImpl implementation) {
         mBuilder = builder;
@@ -58,23 +66,64 @@ public abstract class PreviewExtender {
         CameraCharacteristics cameraCharacteristics = CameraUtil.getCameraCharacteristics(cameraId);
         mImpl.enableExtension(cameraId, cameraCharacteristics);
 
-        CaptureStageImpl captureStage = mImpl.getCaptureStage();
+        switch (mImpl.getProcessorType()) {
+            case PROCESSOR_TYPE_NONE:
+                CaptureStageImpl captureStage = mImpl.getCaptureStage();
 
-        Camera2Config.Builder camera2ConfigurationBuilder =
-                new Camera2Config.Builder();
+                Camera2Config.Builder camera2ConfigurationBuilder =
+                        new Camera2Config.Builder();
 
-        for (Pair<CaptureRequest.Key, Object> captureParameter : captureStage.getParameters()) {
-            camera2ConfigurationBuilder.setCaptureRequestOption(captureParameter.first,
-                    captureParameter.second);
-        }
+                for (Pair<CaptureRequest.Key, Object> captureParameter :
+                        captureStage.getParameters()) {
+                    camera2ConfigurationBuilder.setCaptureRequestOption(captureParameter.first,
+                            captureParameter.second);
+                }
 
-        Camera2Config camera2Config = camera2ConfigurationBuilder.build();
+                final Camera2Config camera2Config = camera2ConfigurationBuilder.build();
 
-        for (Config.Option<?> option : camera2Config.listOptions()) {
-            @SuppressWarnings("unchecked") // Options/values are being copied directly
-                    Config.Option<Object> objectOpt = (Config.Option<Object>) option;
-            mBuilder.getMutableConfig().insertOption(objectOpt,
-                    camera2Config.retrieveOption(objectOpt));
+                for (Config.Option<?> option : camera2Config.listOptions()) {
+                    @SuppressWarnings("unchecked") // Options/values are being copied directly
+                            Config.Option<Object> objectOpt = (Config.Option<Object>) option;
+                    mBuilder.getMutableConfig().insertOption(objectOpt,
+                            camera2Config.retrieveOption(objectOpt));
+                }
+                break;
+            case PROCESSOR_TYPE_REQUEST_UPDATE_ONLY:
+                mBuilder.setImageInfoProcessor(new ImageInfoProcessor() {
+                    @Override
+                    public CaptureStage getCaptureStage() {
+                        return new AdaptingCaptureStage(mImpl.getCaptureStage());
+                    }
+
+                    @Override
+                    public boolean process(ImageInfo imageInfo) {
+                        CameraCaptureResult result =
+                                CameraCaptureResults.retrieveCameraCaptureResult(imageInfo);
+                        if (result == null) {
+                            return false;
+                        }
+
+                        CaptureResult captureResult =
+                                Camera2CameraCaptureResultConverter.getCaptureResult(result);
+                        if (captureResult == null) {
+                            return false;
+                        }
+
+                        TotalCaptureResult totalCaptureResult = (TotalCaptureResult) captureResult;
+                        if (totalCaptureResult == null) {
+                            return false;
+                        }
+
+                        CaptureStageImpl captureStageImpl =
+                                mImpl.getRequestUpdatePreviewProcessor().process(
+                                        totalCaptureResult);
+                        if (captureStageImpl == null) {
+                            return false;
+                        }
+
+                        return true;
+                    }
+                });
         }
     }
 }
