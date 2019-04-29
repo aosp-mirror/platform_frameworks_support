2a57223e635a38943b8b4a11e6b8f479426a5307
Insun Kang <insun@google.com>
2019-04-29 15:33:50 +0900

    Handle the case that video is rendered without proper TrackInfo
    1. Handles the edge case that video is rendred without proper trackInfo
    2. Minor fix : prevent TrackInfo#toString causing NPE when mFormat is null
    
    Bug: 131313002
    Bug: 131632602
    Test: ./gradlew :media2:media2-widget:connectedCheck, manually with
    :media2:integration-tests:testapp:installDebug
    
    Change-Id: Ib360ecaf09b7a1d7ce7d0e16e0d0d1c4448cdb37

--- media2/widget/src/main/java/androidx/media2/widget/VideoView.java
+++ media2/widget/src/main/java/androidx/media2/widget/VideoView.java
@@ -777,6 +777,10 @@ public class VideoView extends SelectiveLayout {
 
     // TODO: move this method inside callback to make sure it runs inside the callback thread.
     Bundle extractTrackInfoData() {
+        if (!isMediaPrepared()) {
+            throw new IllegalStateException(
+                    "extractTrackInfo() is unexpectedly called. Media item is not prepared");
+        }
         List<MediaPlayer.TrackInfo> trackInfos = mMediaPlayer.getTrackInfo();
         mVideoTrackCount = 0;
         mAudioTrackInfos = new ArrayList<>();
@@ -807,6 +811,13 @@ public class VideoView extends SelectiveLayout {
         // Re-select originally selected subtitle track since SubtitleController has been reset.
         if (selectedSubtitleTrackInfo != null) {
             selectSubtitleTrack(selectedSubtitleTrackInfo);
+        }
+        VideoSize videoSize = mMediaPlayer.getVideoSize();
+        boolean hasActualVideo = videoSize.getHeight() != 0 && videoSize.getWidth() != 0;
+        if (mVideoTrackCount == 0 && hasActualVideo) {
+            Log.w(TAG, "video track count is zero, but it renders video. size: "
+                    + videoSize.getWidth() + "/" + videoSize.getHeight());
+            mVideoTrackCount = 1;
         }
 
         Bundle data = new Bundle();
