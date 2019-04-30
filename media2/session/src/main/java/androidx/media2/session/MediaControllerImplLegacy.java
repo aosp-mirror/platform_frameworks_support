0ecb3a3c95458d6ae5653d5f7e9a30f04bdd99d2
Jin Seok Park <jinpark@google.com>
2019-04-30 18:46:55 +0900

    Add TrackInfo APIs in SessionPlayer/MediaController
    1. Add SessionPlayer.PlayerCallbacks
       onTrackInfoChanged/onTrack(De)Selected
    2. Make TrackInfo Parcelable
    3. Add selectTrack/deselectTrack in MediaController
    4. Add tests
    
    Bug: 130332752
    Test: ./gradlew -p media2 check connectedCheck &&
          ./media2/session/version-compat-tests/runtest.sh 1
    Change-Id: Ifd58e975096835068a7bc76bdd9caa02ab22546b

--- media2/session/src/main/java/androidx/media2/session/MediaControllerImplLegacy.java
+++ media2/session/src/main/java/androidx/media2/session/MediaControllerImplLegacy.java
@@ -809,6 +809,24 @@ class MediaControllerImplLegacy implements MediaController.MediaControllerImpl {
         return new VideoSize(0, 0);
     }
 
+    @Nullable
+    public List<SessionPlayer.TrackInfo> getTrackInfo() {
+        Log.w(TAG, "Session doesn't support getting TrackInfo");
+        return null;
+    }
+
+    @Override
+    public ListenableFuture<SessionResult> selectTrack(SessionPlayer.TrackInfo trackInfo) {
+        Log.w(TAG, "Session doesn't support selecting track");
+        return createFutureWithResult(RESULT_ERROR_NOT_SUPPORTED);
+    }
+
+    @Override
+    public ListenableFuture<SessionResult> deselectTrack(SessionPlayer.TrackInfo trackInfo) {
+        Log.w(TAG, "Session doesn't support deselecting track");
+        return createFutureWithResult(RESULT_ERROR_NOT_SUPPORTED);
+    }
+
     @Override
     public @NonNull Context getContext() {
         return mContext;
