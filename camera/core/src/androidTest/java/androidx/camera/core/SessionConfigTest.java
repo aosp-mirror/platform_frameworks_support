285f3c52826f8683fb97ea21ff92afcf328e35f1
leo huang <leohuang@google.com>
2019-05-01 00:31:32 +0800

    Remove mCaptureRequestParameters and API addCharacteristics() from CaptureConfig
    Bug: 131688042
    Test: ./gradlew camera:camera-core:test; ./gradlewcamera:camera-camera2:test
    Change-Id: Id57aa49834439537a10a7f03e99151518e88d97f

--- camera/core/src/androidTest/java/androidx/camera/core/SessionConfigTest.java
+++ camera/core/src/androidTest/java/androidx/camera/core/SessionConfigTest.java
@@ -22,8 +22,6 @@ import static org.mockito.Mockito.mock;
 
 import android.hardware.camera2.CameraCaptureSession;
 import android.hardware.camera2.CameraDevice;
-import android.hardware.camera2.CaptureRequest;
-import android.hardware.camera2.CaptureRequest.Key;
 import android.view.Surface;
 
 import androidx.test.ext.junit.runners.AndroidJUnit4;
@@ -36,7 +34,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import java.util.List;
-import java.util.Map;
 
 @SmallTest
 @RunWith(AndroidJUnit4.class)
@@ -132,26 +129,6 @@ public class SessionConfigTest {
     }
 
     @Test
-    public void builderAddCharacteristic() {
-        SessionConfig.Builder builder = new SessionConfig.Builder();
-
-        builder.addCharacteristic(
-                CaptureRequest.CONTROL_AF_MODE, CaptureRequest.CONTROL_AF_MODE_AUTO);
-        SessionConfig sessionConfig = builder.build();
-
-        Map<Key<?>, CaptureRequestParameter<?>> parameterMap =
-                sessionConfig.getCameraCharacteristics();
-
-        assertThat(parameterMap.containsKey(CaptureRequest.CONTROL_AF_MODE)).isTrue();
-        assertThat(parameterMap)
-                .containsEntry(
-                        CaptureRequest.CONTROL_AF_MODE,
-                        CaptureRequestParameter.create(
-                                CaptureRequest.CONTROL_AF_MODE,
-                                CaptureRequest.CONTROL_AF_MODE_AUTO));
-    }
-
-    @Test
     public void conflictingTemplate() {
         SessionConfig.Builder builderPreview = new SessionConfig.Builder();
         builderPreview.setTemplateType(CameraDevice.TEMPLATE_PREVIEW);
@@ -169,38 +146,14 @@ public class SessionConfigTest {
     }
 
     @Test
-    public void conflictingCharacteristics() {
-        SessionConfig.Builder builderAfAuto = new SessionConfig.Builder();
-        builderAfAuto.addCharacteristic(
-                CaptureRequest.CONTROL_AF_MODE, CaptureRequest.CONTROL_AF_MODE_AUTO);
-        SessionConfig sessionConfigAfAuto = builderAfAuto.build();
-        SessionConfig.Builder builderAfOff = new SessionConfig.Builder();
-        builderAfOff.addCharacteristic(
-                CaptureRequest.CONTROL_AF_MODE, CaptureRequest.CONTROL_AF_MODE_OFF);
-        SessionConfig sessionConfigAfOff = builderAfOff.build();
-
-        SessionConfig.ValidatingBuilder validatingBuilder = new SessionConfig.ValidatingBuilder();
-
-        validatingBuilder.add(sessionConfigAfAuto);
-        validatingBuilder.add(sessionConfigAfOff);
-
-        assertThat(validatingBuilder.isValid()).isFalse();
-    }
-
-    @Test
     public void combineTwoSessionsValid() {
         SessionConfig.Builder builder0 = new SessionConfig.Builder();
         builder0.addSurface(mMockSurface0);
         builder0.setTemplateType(CameraDevice.TEMPLATE_PREVIEW);
-        builder0.addCharacteristic(
-                CaptureRequest.CONTROL_AF_MODE, CaptureRequest.CONTROL_AF_MODE_AUTO);
 
         SessionConfig.Builder builder1 = new SessionConfig.Builder();
         builder1.addSurface(mMockSurface1);
         builder1.setTemplateType(CameraDevice.TEMPLATE_PREVIEW);
-        builder1.addCharacteristic(
-                CaptureRequest.CONTROL_AE_ANTIBANDING_MODE,
-                CaptureRequest.CONTROL_AE_ANTIBANDING_MODE_AUTO);
 
         SessionConfig.ValidatingBuilder validatingBuilder = new SessionConfig.ValidatingBuilder();
         validatingBuilder.add(builder0.build());
@@ -214,15 +167,10 @@ public class SessionConfigTest {
         SessionConfig.Builder builder0 = new SessionConfig.Builder();
         builder0.addSurface(mMockSurface0);
         builder0.setTemplateType(CameraDevice.TEMPLATE_PREVIEW);
-        builder0.addCharacteristic(
-                CaptureRequest.CONTROL_AF_MODE, CaptureRequest.CONTROL_AF_MODE_AUTO);
 
         SessionConfig.Builder builder1 = new SessionConfig.Builder();
         builder1.addSurface(mMockSurface1);
         builder1.setTemplateType(CameraDevice.TEMPLATE_PREVIEW);
-        builder1.addCharacteristic(
-                CaptureRequest.CONTROL_AE_ANTIBANDING_MODE,
-                CaptureRequest.CONTROL_AE_ANTIBANDING_MODE_AUTO);
 
         SessionConfig.ValidatingBuilder validatingBuilder = new SessionConfig.ValidatingBuilder();
         validatingBuilder.add(builder0.build());
@@ -238,15 +186,10 @@ public class SessionConfigTest {
         SessionConfig.Builder builder0 = new SessionConfig.Builder();
         builder0.addSurface(mMockSurface0);
         builder0.setTemplateType(CameraDevice.TEMPLATE_PREVIEW);
-        builder0.addCharacteristic(
-                CaptureRequest.CONTROL_AF_MODE, CaptureRequest.CONTROL_AF_MODE_AUTO);
 
         SessionConfig.Builder builder1 = new SessionConfig.Builder();
         builder1.addSurface(mMockSurface1);
         builder1.setTemplateType(CameraDevice.TEMPLATE_PREVIEW);
-        builder1.addCharacteristic(
-                CaptureRequest.CONTROL_AE_ANTIBANDING_MODE,
-                CaptureRequest.CONTROL_AE_ANTIBANDING_MODE_AUTO);
 
         SessionConfig.ValidatingBuilder validatingBuilder = new SessionConfig.ValidatingBuilder();
         validatingBuilder.add(builder0.build());
@@ -256,41 +199,6 @@ public class SessionConfigTest {
 
         List<DeferrableSurface> surfaces = sessionConfig.getSurfaces();
         assertThat(surfaces).containsExactly(mMockSurface0, mMockSurface1);
-    }
-
-    @Test
-    public void combineTwoSessionsCharacteristics() {
-        SessionConfig.Builder builder0 = new SessionConfig.Builder();
-        builder0.addSurface(mMockSurface0);
-        builder0.setTemplateType(CameraDevice.TEMPLATE_PREVIEW);
-        builder0.addCharacteristic(
-                CaptureRequest.CONTROL_AF_MODE, CaptureRequest.CONTROL_AF_MODE_AUTO);
-
-        SessionConfig.Builder builder1 = new SessionConfig.Builder();
-        builder1.addSurface(mMockSurface1);
-        builder1.setTemplateType(CameraDevice.TEMPLATE_PREVIEW);
-        builder1.addCharacteristic(
-                CaptureRequest.CONTROL_AE_ANTIBANDING_MODE,
-                CaptureRequest.CONTROL_AE_ANTIBANDING_MODE_AUTO);
-
-        SessionConfig.ValidatingBuilder validatingBuilder = new SessionConfig.ValidatingBuilder();
-        validatingBuilder.add(builder0.build());
-        validatingBuilder.add(builder1.build());
-
-        SessionConfig sessionConfig = validatingBuilder.build();
-
-        Map<Key<?>, CaptureRequestParameter<?>> parameterMap =
-                sessionConfig.getCameraCharacteristics();
-        assertThat(parameterMap)
-                .containsExactly(
-                        CaptureRequest.CONTROL_AF_MODE,
-                        CaptureRequestParameter.create(
-                                CaptureRequest.CONTROL_AF_MODE,
-                                CaptureRequest.CONTROL_AF_MODE_AUTO),
-                        CaptureRequest.CONTROL_AE_ANTIBANDING_MODE,
-                        CaptureRequestParameter.create(
-                                CaptureRequest.CONTROL_AE_ANTIBANDING_MODE,
-                                CaptureRequest.CONTROL_AE_ANTIBANDING_MODE_AUTO));
     }
 
     @Test
