ef2df14d122178f0b8ebd55b0ee587ba813ac70f
Trevor McGuire <trevormcguire@google.com>
2019-05-06 14:01:20 -0700

    Add compat implementation of CameraDeviceCompat#createCaptureSession()
     This enables an API similar to the API28
     CameraDevice#createCaptureSession(SessionConfiguration) API, back to
     API level 21.
    
     This is the only way to use a @CallbackExecutor on the
     createCaptureSession APIs, so this enables using an Executor on APIs
     back to API level 21.
    
     Some features throw exceptions not captured in the original API. Namely
     trying to create a high-speed capture session or a reprocessable
     capture session on API < 23.
    
    Bug: 121160431
    Test: ./gradlew bOS
    Change-Id: I0d1427c049c4b26c6d741fe8a22bdb64d96577ea

--- camera/camera2/src/main/java/androidx/camera/camera2/impl/compat/CameraDeviceCompat.java
+++ camera/camera2/src/main/java/androidx/camera/camera2/impl/compat/CameraDeviceCompat.java
@@ -17,16 +17,23 @@
 package androidx.camera.camera2.impl.compat;
 
 import android.annotation.TargetApi;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCaptureSession;
+import android.hardware.camera2.CameraDevice;
+import android.os.Build;
+import android.view.Surface;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.annotation.RestrictTo;
 import androidx.annotation.RestrictTo.Scope;
+import androidx.camera.camera2.impl.compat.params.SessionConfigurationCompat;
+
+import java.util.concurrent.Executor;
 
 /**
  * Helper for accessing features in CameraDevice in a backwards compatible fashion.
- *
- * @hide Will be unhidden once some methods are implemented
  */
-@RestrictTo(Scope.LIBRARY)
 @TargetApi(21)
 public final class CameraDeviceCompat {
 
@@ -38,7 +45,6 @@ public final class CameraDeviceCompat {
     @RestrictTo(Scope.LIBRARY)
     public static final int SESSION_OPERATION_MODE_NORMAL =
             0; // ICameraDeviceUser.NORMAL_MODE;
-
     /**
      * Constrained high-speed operation mode.
      *
@@ -47,4 +53,135 @@ public final class CameraDeviceCompat {
     @RestrictTo(Scope.LIBRARY)
     public static final int SESSION_OPERATION_MODE_CONSTRAINED_HIGH_SPEED =
             1; // ICameraDeviceUser.CONSTRAINED_HIGH_SPEED_MODE;
+
+    private static final CameraDeviceCompatImpl IMPL = chooseImplementation();
+
+    /**
+     * Create a new {@link CameraCaptureSession} using a {@link SessionConfigurationCompat}
+     * helper object that aggregates all supported parameters.
+     *
+     * @param config A session configuration (see {@link SessionConfigurationCompat}).
+     * @throws IllegalArgumentException In case the session configuration
+     *                                  is invalid; or the output configurations are empty; or
+     *                                  the session configuration executor is invalid.
+     * @throws CameraAccessException    In case the camera device is no longer connected or has
+     *                                  encountered a fatal error.
+     */
+    public static void createCaptureSession(@NonNull CameraDevice device,
+            @NonNull SessionConfigurationCompat config) throws CameraAccessException {
+        IMPL.createCaptureSession(device, config);
+    }
+
+    private static CameraDeviceCompatImpl chooseImplementation() {
+        if (Build.VERSION.SDK_INT >= 28) {
+            return new CameraDeviceCompatApi28Impl();
+        } else if (Build.VERSION.SDK_INT >= 24) {
+            return new CameraDeviceCompatApi24Impl();
+        } else if (Build.VERSION.SDK_INT >= 23) {
+            return new CameraDeviceCompatApi23Impl();
+        }
+
+        return new CameraDeviceCompatBaseImpl();
+    }
+
+    // Class is not a wrapper. Should not be instantiated.
+    private CameraDeviceCompat() {}
+
+    interface CameraDeviceCompatImpl {
+        void createCaptureSession(@NonNull CameraDevice device,
+                @NonNull SessionConfigurationCompat config) throws CameraAccessException;
+    }
+
+    static class StateCallbackExecutorWrapper extends CameraCaptureSession.StateCallback {
+
+        CameraCaptureSession.StateCallback mWrappedCallback;
+        private Executor mExecutor;
+
+        StateCallbackExecutorWrapper(@NonNull Executor executor,
+                @NonNull CameraCaptureSession.StateCallback wrappedCallback) {
+            mExecutor = executor;
+            mWrappedCallback = wrappedCallback;
+        }
+
+        @Override
+        public void onConfigured(@NonNull final CameraCaptureSession session) {
+            mExecutor.execute(new Runnable() {
+
+                @Override
+                public void run() {
+                    mWrappedCallback.onConfigured(session);
+                }
+            });
+        }
+
+        @Override
+        public void onConfigureFailed(@NonNull final CameraCaptureSession session) {
+            mExecutor.execute(new Runnable() {
+
+                @Override
+                public void run() {
+                    mWrappedCallback.onConfigureFailed(session);
+                }
+            });
+        }
+
+        @Override
+        public void onReady(@NonNull final CameraCaptureSession session) {
+            mExecutor.execute(new Runnable() {
+
+                @Override
+                public void run() {
+                    mWrappedCallback.onReady(session);
+                }
+            });
+        }
+
+        @Override
+        public void onActive(@NonNull final CameraCaptureSession session) {
+            mExecutor.execute(new Runnable() {
+
+                @Override
+                public void run() {
+                    mWrappedCallback.onActive(session);
+                }
+            });
+        }
+
+        @RequiresApi(26)
+        @Override
+        public void onCaptureQueueEmpty(@NonNull final CameraCaptureSession session) {
+            mExecutor.execute(new Runnable() {
+
+                @Override
+                public void run() {
+                    mWrappedCallback.onCaptureQueueEmpty(session);
+                }
+            });
+        }
+
+
+        @Override
+        public void onClosed(@NonNull final CameraCaptureSession session) {
+            mExecutor.execute(new Runnable() {
+
+                @Override
+                public void run() {
+                    mWrappedCallback.onClosed(session);
+                }
+            });
+        }
+
+        @RequiresApi(23)
+        @Override
+        public void onSurfacePrepared(@NonNull final CameraCaptureSession session,
+                @NonNull final Surface surface) {
+            mExecutor.execute(new Runnable() {
+
+                @Override
+                public void run() {
+                    mWrappedCallback.onSurfacePrepared(session, surface);
+                }
+            });
+        }
+    }
 }
