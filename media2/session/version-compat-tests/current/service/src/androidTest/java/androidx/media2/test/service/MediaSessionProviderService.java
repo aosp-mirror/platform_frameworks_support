1ed1ca82b93d560ac54cd8609897414cb58a86b9
Gyumin Sim <gyumin@google.com>
2019-05-07 18:44:57 +0900

    Add hidden MediaController#setSurface(Surface)
    Bug: 130515451
    Test: ./gradlew -p media2 check connectedCheck &&
          ./media2/session/version-compat-tests/runtest.sh 1
    Change-Id: Ia88f55492131502c6d360a58bc7deafbef78d8bd

--- media2/session/version-compat-tests/current/service/src/androidTest/java/androidx/media2/test/service/MediaSessionProviderService.java
+++ media2/session/version-compat-tests/current/service/src/androidTest/java/androidx/media2/test/service/MediaSessionProviderService.java
@@ -527,5 +527,12 @@ public class MediaSessionProviderService extends Service {
             VideoSize videoSizeObj = MediaParcelUtils.fromParcelable(videoSize);
             player.notifyVideoSizeChanged(videoSizeObj);
         }
+
+        @Override
+        public boolean getSurfaceExists(String sessionId) {
+            MediaSession session = mSessionMap.get(sessionId);
+            MockPlayer player = (MockPlayer) session.getPlayer();
+            return player.getSurfaceExists();
+        }
     }
 }
