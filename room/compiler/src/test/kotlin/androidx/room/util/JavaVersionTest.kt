515fca80ca9a1b2efaf65d0cad959039c75e2ca7
Hung Nguyen <hungnv@google.com>
2019-05-08 15:49:58 +0100

    Make Room annotation processor incremental.
    This CL introduces an annotation processor option called
        `room.incremental`
    which the user can set to try out incremental annotation processing with
    Room. It currently defaults to false.
    
    The user can pass the option through the Android Gradle plugin's DSL,
    for example:
    
    android {
        buildTypes {
            debug {
                javaCompileOptions.annotationProcessorOptions {
                    argument 'room.incremental', 'true'
                }
    	}
        }
    }
    
    Caveat: There is a JDK bug
    https://bugs.openjdk.java.net/browse/JDK-8007720 that prevents Room from
    being incremental (details are discussed in the code comment).
    Therefore, if the JDK version being used does not already have this fix,
    Room cannot be incremental and we will print out a warning to ask the
    user to use a JDK version having the fix.
    
    Note that Room also currently does not track the annotation processor
    option `room.schemaLocation` properly as an @OutputDirectory, and this
    could impact build correctness. However, this issue impacts both
    non-incremental and incremental builds, and it already existed before,
    so we will fix it separately (tracked at bug 132245929).
    
    Future work: With this CL, Room will be an aggregating annotation
    processor. Further work is being planned to make Room an isolating
    annotation processor, which will improve build speed even more (tracked
    at bug 130223419).
    
    Bug: 112110217
    Test: To be added in the next patches,
          ./gradlew room:room-compiler:test
    Change-Id: I0076e596b1c05bcc51afea21a4f6a9a90fb6c7be

---
+++ room/compiler/src/test/kotlin/androidx/room/util/JavaVersionTest.kt
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.room.util
+
+import org.junit.Assert.fail
+import org.junit.Test
+
+class JavaVersionTest {
+
+    @Test
+    fun testTryParse() {
+        assert(JavaVersion.tryParse("11.0.1+13-LTS") == JavaVersion(11, 0, null))
+        assert(JavaVersion.tryParse("1.8.0_202-release-1483-b39-5396753") == JavaVersion(8, 0, 202))
+        assert(
+            JavaVersion.tryParse("1.8.0_181-google-v7-238857965-238857965")
+                    == JavaVersion(8, 0, 181)
+        )
+        assert(JavaVersion.tryParse("a.b.c") == null)
+    }
+
+    @Test
+    fun testParse() {
+        assert(JavaVersion.parse("11.0.1+13-LTS") == JavaVersion(11, 0, null))
+        assert(JavaVersion.parse("1.8.0_202-release-1483-b39-5396753") == JavaVersion(8, 0, 202))
+        assert(
+            JavaVersion.parse("1.8.0_181-google-v7-238857965-238857965")
+                    == JavaVersion(8, 0, 181)
+        )
+        try {
+            JavaVersion.parse("a.b.c")
+            fail("Expected IllegalArgumentException")
+        } catch (e: IllegalArgumentException) {
+            assert(e.message == "Unable to parse Java version a.b.c")
+        }
+    }
+
+    @Test
+    fun testComparison() {
+        assert(JavaVersion(8, 0, 203) > JavaVersion(8, 0, 202))
+        assert(JavaVersion(8, 0, 202) == JavaVersion(8, 0, 202))
+        assert(JavaVersion(8, 0, 152) < JavaVersion(8, 0, 202))
+    }
+}
