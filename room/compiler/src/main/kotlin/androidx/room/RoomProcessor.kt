636fba3d1dd4f7153be1ae9af063e03df65f8394
Hung Nguyen <hungnv@google.com>
2019-05-08 15:49:58 +0100

    Make Room annotation processor incremental.
    This CL introduces an annotation processor option called
        `room.incremental`
    which the user can set to try out incremental annotation processing with
    Room. It is currently off by default.
    
    The user can pass the option through the Android Gradle plugin's DSL,
    for example:
    
    android {
        defaultConfig
            javaCompileOptions {
    	    annotationProcessorOptions {
                    argument 'room.incremental', 'true'
                }
    	}
        }
    }
    
    Caveat: There is a JDK bug
    https://bugs.openjdk.java.net/browse/JDK-8007720 that prevents Room from
    being incremental (details are discussed in the code comment).
    Therefore, if the JDK version being used does not already have this fix,
    Room cannot be incremental and we will print out a warning to ask the
    user to use a JDK version having the fix.
    
    Note that Room also currently does not track the annotation processor
    option `room.schemaLocation` properly as an @OutputDirectory, and this
    could impact build correctness. However, this issue impacts both
    non-incremental and incremental builds, and it already existed before,
    so we will fix it separately (tracked at bug 132245929).
    
    Future work: With this CL, Room will be an aggregating annotation
    processor. More work is required to make Room an isolating annotation
    processor, which will improve build speed even more. Specifically, Room
    would need to write schema files using the
    javax.annotation.processing.Filer API.
    
    Bug: 112110217
    Test: To be added in the next patches,
          ./gradlew room:room-compiler:test
    Change-Id: I0076e596b1c05bcc51afea21a4f6a9a90fb6c7be

--- room/compiler/src/main/kotlin/androidx/room/RoomProcessor.kt
+++ room/compiler/src/main/kotlin/androidx/room/RoomProcessor.kt
@@ -20,6 +20,7 @@ import androidx.room.processor.Context
 import androidx.room.processor.DatabaseProcessor
 import androidx.room.processor.MissingTypeException
 import androidx.room.processor.ProcessorErrors
+import androidx.room.util.SimpleJavaVersion
 import androidx.room.vo.DaoMethod
 import androidx.room.vo.Warning
 import androidx.room.writer.DaoWriter
@@ -36,12 +37,88 @@ import javax.lang.model.element.Element
  * The annotation processor for Room.
  */
 class RoomProcessor : BasicAnnotationProcessor() {
+
+    /**
+     * Annotation processor option to tell Gradle that Room is an aggregating annotation processor.
+     */
+    private val AGGREGATING_ANNOTATION_PROCESSORS_INDICATOR =
+        "org.gradle.annotation.processing.aggregating"
+
+    /** Helper variable to avoid reporting the warning twice. */
+    private var jdkVersionHasBugReported: Boolean = false
+
     override fun initSteps(): MutableIterable<ProcessingStep>? {
         return mutableListOf(DatabaseProcessingStep(processingEnv))
     }
 
     override fun getSupportedOptions(): MutableSet<String> {
-        return Context.ARG_OPTIONS.toMutableSet()
+        val supportedOptions = Context.ARG_OPTIONS.toMutableSet()
+
+        if (Context.BooleanProcessorOptions.INCREMENTAL.getValue(processingEnv)) {
+            if (methodParametersVisibleInClassFiles()) {
+                // Room can be incremental
+                supportedOptions.add(AGGREGATING_ANNOTATION_PROCESSORS_INDICATOR)
+            } else {
+                if (!jdkVersionHasBugReported) {
+                    Context(processingEnv).logger.w(
+                        Warning.JDK_VERSION_HAS_BUG, ProcessorErrors.JDK_VERSION_HAS_BUG
+                    )
+                    jdkVersionHasBugReported = true
+                }
+            }
+        }
+
+        return supportedOptions
+    }
+
+    /**
+     * Returns `true` if the method parameters in class files can be accessed by Room.
+     *
+     * Context: Room requires access to the real parameter names of constructors (see
+     * PojoProcessor.getParamNames). Room uses the ExecutableElement.getParemters() API on the
+     * constructor element to do this.
+     *
+     * When Room is not yet incremental, the above API is working as expected. However, if we make
+     * Room incremental, during an incremental compile Gradle may want to pass class files instead
+     * source files to annotation processors (to avoid recompiling the source files that haven't
+     * changed). Due to JDK bug https://bugs.openjdk.java.net/browse/JDK-8007720, the class files
+     * may lose the real parameter names of constructors, which would break Room.
+     *
+     * The above JDK bug was fixed in JDK 11. The fix was also cherry-picked back into the
+     * embedded JDK that was shipped with Android Studio 3.5+.
+     *
+     * Therefore, for Room to be incremental, we need to check whether the JDK being used has the
+     * fix: Either it is JDK 11+ or it is an embedded JDK that has the cherry-picked fix (version
+     * 1.8.0_202-release-1483-b39-5509098 or higher).
+     */
+    private fun methodParametersVisibleInClassFiles(): Boolean {
+        val currentJavaVersion = SimpleJavaVersion.getCurrentVersion() ?: return false
+
+        if (currentJavaVersion >= SimpleJavaVersion.VERSION_11_0_0) {
+            return true
+        } else {
+            val isEmbeddedJdk =
+                System.getProperty("java.vendor")?.contains("JetBrains", ignoreCase = true)
+                    ?: false
+            // We are interested in 3 ranges of Android Studio (AS) versions:
+            //    1. AS 3.5.0-alpha09 and lower use JDK 1.8.0_152 or lower.
+            //    2. AS 3.5.0-alpha10 up to 3.5.0-beta01 use JDK 1.8.0_202-release-1483-b39-5396753.
+            //    3. AS 3.5.0-beta02 and higher use JDK 1.8.0_202-release-1483-b39-5509098 or
+            //       higher, which have the cherry-picked JDK fix.
+            // Therefore, if the JDK version is 1.8.0_202, we need to filter out those exist in
+            // range #2.
+            if (isEmbeddedJdk && (currentJavaVersion > SimpleJavaVersion.VERSION_1_8_0_202)) {
+                return true
+            } else if (isEmbeddedJdk &&
+                    (currentJavaVersion == SimpleJavaVersion.VERSION_1_8_0_202)
+            ) {
+                return System.getProperty("java.runtime.version")
+                    ?.let { it != "1.8.0_202-release-1483-b39-5396753" }
+                    ?: false
+            } else {
+                return false
+            }
+        }
     }
 
     override fun getSupportedSourceVersion(): SourceVersion {
