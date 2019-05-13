085e0e70e5468cc13853fa35957b6292e17aa1d7
Nick Anthony <nickanthony@google.com>
2019-05-13 16:36:22 -0400

    Allow add_release_commit_tags.sh to understand groupId and artifactIds
    Need to account for lines like:
    <SHA>:<groupId>:<artifactId>:<version>
    
    Also move tag to it's own function to account for duplicate tag failures
    
    Test: ./add_release_commit_tags.sh AndroidX-Release-2019-05-07.txt
    Change-Id: Ib6889899e9c02da935b9817a0cb4dd83224531ee

--- development/history/add_release_commit_tags.sh
+++ development/history/add_release_commit_tags.sh
@@ -9,9 +9,9 @@ cd ../..
 
 function usage() {
 	echo "usage: ./add_release_commit_tags.sh <release-list-file>"
-  	echo "Adds git tags to last commit for a given release on release date YYYY-MM-DD (ISO date format).  Expects release date YYYY-MM-DD as first list of commit list."
-  	echo "Expected format for each artifactId in the <release-list-file> is:<SHA:artifactId:version>"
-  	exit 1
+	echo "Adds git tags to last commit for a given release on release date YYYY-MM-DD (ISO date format).  Expects release date YYYY-MM-DD as first list of commit list."
+	echo "Expected format for each artifactId in the <release-list-file> is:<SHA:artifactId:version>"
+	exit 1
 }
 
 # # Check for invalid usage
@@ -29,6 +29,16 @@ function validateDate() {
 	fi
 }
 
+function addGitTag() {
+	exists="$(git tag -l $1 | wc -l)"
+	if [ $exists == 0 ]; then
+		git tag -a "$1" "$2" -m "$3"
+		echo "Added tag!"
+	else
+		echo "Tag already exists"
+	fi
+}	
+
 # Iterate through commit log files and create tags
 commit_list="$script_dir/$1"
 first_line=1
@@ -41,15 +51,31 @@ while read -r line; do
 		first_line=0
 		continue
 	fi
-    sha="$(echo "$line" | cut -d':' -f1)"
-    artifactId="$(echo "$line" | cut -d':' -f2)"
-    version="$(echo "$line" | cut -d':' -f3)"
-    # Add tag for the release
-    echo "Adding tag $date-release-$artifactId to SHA: $sha"
-    git tag -a "$date-release-$artifactId" "$sha" -m "Inclusive cutoff commit for $artifactId for the $date release"
-    # Add tag for the artifact version
-    echo "Adding tag $artifactId-$version to SHA: $sha"
-    git tag -a "$artifactId-$version" "$sha" -m "Inclusive cutoff commit for $artifactId-$version"
+	sha="$(echo "$line" | cut -d':' -f1)"
+	numberColons="$(echo "$line" | tr -cd ':' | wc -c)"
+	if [ $numberColons == 3 ]; then
+		line_has_groupId_and_artifactId=1
+	elif [ $numberColons == 2 ]; then
+		line_has_groupId_and_artifactId=0
+	else
+		echo "The following line is malformatted: "
+		echo "$line"
+		echo "Cannot resolve line, exiting"
+		exit 1
+	fi
+	if [ $line_has_groupId_and_artifactId == 1 ]; then
+		artifactId="$(echo "$line" | cut -d':' -f3)"
+		version="$(echo "$line" | cut -d':' -f4)"
+	else
+		artifactId="$(echo "$line" | cut -d':' -f2)"
+		version="$(echo "$line" | cut -d':' -f3)"
+	fi
+	# Add tag for the release
+	echo "Adding tag $date-release-$artifactId to SHA: $sha..."
+	addGitTag "$date-release-$artifactId" "$sha" "Inclusive cutoff commit for $artifactId for the $date release"
+	# Add tag for the artifact version
+	echo "Adding tag $artifactId-$version to SHA: $sha.."
+	addGitTag "$artifactId-$version" "$sha" "Inclusive cutoff commit for $artifactId-$version"
 done < "$commit_list"
 
 exit
