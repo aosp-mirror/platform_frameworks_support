46ff59a6c217fcf662c35110a1df7be885917c50
Qingqing Deng <qqd@google.com>
2019-05-13 10:10:37 -0700

    [Text Selection] Drag.
    Test: ./gradlew test
    Change-Id: Ibb2e3b6c4eabadb70e19517f99034c8eadbd1f9f

--- ui/framework/src/main/java/androidx/ui/core/Text.kt
+++ ui/framework/src/main/java/androidx/ui/core/Text.kt
@@ -16,6 +16,7 @@
 package androidx.ui.core
 
 import android.content.Context
+import android.util.Log
 import androidx.ui.engine.geometry.Offset
 import androidx.ui.engine.text.TextAlign
 import androidx.ui.engine.text.TextDirection
@@ -38,6 +39,7 @@ import androidx.compose.state
 import androidx.compose.memo
 import androidx.compose.onDispose
 import androidx.compose.unaryPlus
+import androidx.ui.engine.text.TextAffinity
 
 private val DefaultTextAlign: TextAlign = TextAlign.Start
 private val DefaultTextDirection: TextDirection = TextDirection.Ltr
@@ -47,7 +49,6 @@ private val DefaultMaxLines: Int? = null
 
 /** The default selection color if none is specified. */
 private val DefaultSelectionColor = Color(0x6633B5E5)
-
 
 @Composable
 fun Text(
@@ -224,10 +225,14 @@ internal fun Text(
                         selectionStart =
                             TextPosition(wordBoundary.start, selectionStart.affinity)
                         selectionEnd = TextPosition(wordBoundary.end, selectionEnd.affinity)
+                    } else {
+                        selectionEnd = TextPosition(selectionEnd.offset + 1, TextAffinity.upstream)
                     }
 
                     internalSelection.value =
                         TextSelection(selectionStart.offset, selectionEnd.offset)
+
+                    selectionEnd = TextPosition(selectionEnd.offset - 1, TextAffinity.upstream)
 
                     // TODO(qqd): Determine a set of coordinates around a character that we need.
                     // Clean up the lower layer's getCaretForTextPosition methods.
