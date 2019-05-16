77e72074e21418a00f31d6dd7cea6739a64ed25b
Nader Jawad <njawad@google.com>
2019-05-16 13:43:13 -0700

    Created layer of abstraction between Image/Canvas and their android platform equivalents
    Created an interface for both Image and Canvas to support
    host side testing. Moved previous implementations of Image and Canvas
    to AndroidImage and AndroidCanvas
    Created some mapping functions to underlying framework APIs
    
    Test: Re-ran crane test + added Image tests
    Bug: N/A
    Change-Id: Ib030e1e44fd32fa4e41f0a35df01767e1e98bd02

--- ui/text/src/main/java/androidx/ui/rendering/paragraph/RenderParagraph.kt
+++ ui/text/src/main/java/androidx/ui/rendering/paragraph/RenderParagraph.kt
@@ -22,6 +22,7 @@ import androidx.ui.core.IntPxSize
 import androidx.ui.core.constrain
 import androidx.ui.core.px
 import androidx.ui.core.round
+import androidx.ui.core.toFrameworkRectF
 import androidx.ui.engine.geometry.Offset
 import androidx.ui.engine.geometry.Rect
 import androidx.ui.engine.geometry.Size
@@ -346,9 +347,11 @@ class RenderParagraph(
             if (overflowShader != null) {
                 // This layer limits what the shader below blends with to be just the text
                 // (as opposed to the text and its background).
-                canvas.saveLayer(bounds, Paint())
+                // TODO (njawad) replace with save lambda when multi children DrawNodes are supported
+                canvas.nativeCanvas.saveLayer(bounds.toFrameworkRectF(), android.graphics.Paint())
             } else {
-                canvas.save()
+                // TODO (njawad) replace with save lambda when multi children DrawNodes are supported
+                canvas.nativeCanvas.save()
             }
             canvas.clipRect(bounds)
         }
@@ -361,7 +364,8 @@ class RenderParagraph(
                 paint.shader = overflowShader
                 canvas.drawRect(Offset.zero.and(size), paint)
             }
-            canvas.restore()
+            // TODO (njawad) replace with save lambda when multi children DrawNodes are supported
+            canvas.nativeCanvas.restore()
         }
     }
 
