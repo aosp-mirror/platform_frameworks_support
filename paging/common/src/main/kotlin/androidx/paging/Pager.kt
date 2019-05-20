02398a5de35773989fe528ccb4748c26361f85c7
Dustin Lam <dustinlam@google.com>
2019-05-20 08:05:34 -0700

    Convert paging-common to .kt
    Test: ./gradlew paging:paging-common:check
    Change-Id: Ia89cb55fdc2e74e681e299103aba1fba5b8b5abc

---
+++ paging/common/src/main/kotlin/androidx/paging/Pager.kt
@@ -0,0 +1,326 @@
+/*
+ * Copyright 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.paging
+
+import androidx.paging.futures.FutureCallback
+import androidx.paging.futures.Futures
+
+import com.google.common.util.concurrent.ListenableFuture
+
+import java.util.concurrent.Executor
+import java.util.concurrent.atomic.AtomicBoolean
+
+internal class Pager<K, V>(
+    val mConfig: PagedList.Config/* synthetic access */,
+    val mSource: DataSource<K, V>/* synthetic access */,
+    val mNotifyExecutor: Executor/* synthetic access */,
+    private val mFetchExecutor: Executor,
+    val mPageConsumer: PageConsumer<V>/* synthetic access */,
+    adjacentProvider: AdjacentProvider<V>?,
+    result: DataSource.BaseResult<V>
+) {
+
+    private val mTotalCount: Int
+
+    private val adjacentProvider: AdjacentProvider<V>
+
+    private var mPrevKey: K? = null
+
+    private var mNextKey: K? = null
+
+    private val mDetached = AtomicBoolean(false)
+
+    var mLoadStateManager: PagedList.LoadStateManager = object : PagedList.LoadStateManager() {
+        override fun onStateChanged(
+            type: PagedList.LoadType,
+            state: PagedList.LoadState,
+            error: Throwable?
+        ) {
+            mPageConsumer.onStateChanged(type, state, error)
+        }
+    }
+
+    val isDetached: Boolean
+        get() = mDetached.get()
+
+    init {
+        this.adjacentProvider = adjacentProvider ?: SimpleAdjacentProvider()
+        @Suppress("UNCHECKED_CAST")
+        mPrevKey = result.prevKey as K?
+        @Suppress("UNCHECKED_CAST")
+        mNextKey = result.nextKey as K?
+        this.adjacentProvider.onPageResultResolution(PagedList.LoadType.REFRESH, result)
+        mTotalCount = result.totalCount()
+
+        // TODO: move this validation to tiled paging impl, once that's added back
+        if (mSource.type === DataSource.KeyType.POSITIONAL && mConfig.enablePlaceholders) {
+            result.validateForInitialTiling(mConfig.pageSize)
+        }
+    }
+
+    private fun listenTo(
+        type: PagedList.LoadType,
+        future: ListenableFuture<out DataSource.BaseResult<V>>
+    ) {
+        // First listen on the BG thread if the DataSource is invalid, since it can be expensive
+        future.addListener(Runnable {
+            // if invalid, drop result on the floor
+            if (mSource.isInvalid()) {
+                detach()
+                return@Runnable
+            }
+
+            // Source has been verified to be valid after producing data, so sent data to UI
+            Futures.addCallback(
+                future,
+                object : FutureCallback<DataSource.BaseResult<V>> {
+                    override fun onSuccess(value: DataSource.BaseResult<V>) {
+                        onLoadSuccess(type, value)
+                    }
+
+                    override fun onError(throwable: Throwable) {
+                        onLoadError(type, throwable)
+                    }
+                },
+                mNotifyExecutor
+            )
+        }, mFetchExecutor)
+    }
+
+    internal interface PageConsumer<V> {
+        // return true if we need to fetch more
+        fun onPageResult(
+            type: PagedList.LoadType,
+            pageResult: DataSource.BaseResult<V>
+        ): Boolean
+
+        fun onStateChanged(
+            type: PagedList.LoadType,
+            state: PagedList.LoadState,
+            error: Throwable?
+        )
+    }
+
+    internal interface AdjacentProvider<V> {
+        val firstLoadedItem: V?
+
+        val lastLoadedItem: V?
+
+        val firstLoadedItemIndex: Int
+
+        val lastLoadedItemIndex: Int
+
+        /**
+         * Notify the AdjacentProvider of new loaded data, to update first/last item/index.
+         *
+         * NOTE: this data may not be committed (e.g. it may be dropped due to max size). Up to the
+         * implementation of the AdjacentProvider to handle this (generally by ignoring this
+         * call if dropping is supported).
+         */
+        fun onPageResultResolution(
+            type: PagedList.LoadType,
+            result: DataSource.BaseResult<V>
+        )
+    }
+
+    fun onLoadSuccess(type: PagedList.LoadType, value: DataSource.BaseResult<V>) {
+        if (isDetached) {
+            // abort!
+            return
+        }
+
+        adjacentProvider.onPageResultResolution(type, value)
+
+        if (mPageConsumer.onPageResult(type, value)) {
+            if (type == PagedList.LoadType.START) {
+                @Suppress("UNCHECKED_CAST")
+                mPrevKey = value.prevKey as K?
+                schedulePrepend()
+            } else if (type == PagedList.LoadType.END) {
+                @Suppress("UNCHECKED_CAST")
+                mNextKey = value.nextKey as K?
+                scheduleAppend()
+            } else {
+                throw IllegalStateException("Can only fetch more during append/prepend")
+            }
+        } else {
+            val state =
+                if (value.data.isEmpty()) PagedList.LoadState.DONE else PagedList.LoadState.IDLE
+            mLoadStateManager.setState(type, state, null)
+        }
+    } /* synthetic accessor */
+
+    fun onLoadError(type: PagedList.LoadType, throwable: Throwable) {
+        if (isDetached) {
+            // abort!
+            return
+        }
+        // TODO: handle nesting
+        val state = if (mSource.isRetryableError(throwable))
+            PagedList.LoadState.RETRYABLE_ERROR
+        else
+            PagedList.LoadState.ERROR
+        mLoadStateManager.setState(type, state, throwable)
+    } /* synthetic accessor */
+
+    fun trySchedulePrepend() {
+        if (mLoadStateManager.start == PagedList.LoadState.IDLE) {
+            schedulePrepend()
+        }
+    }
+
+    fun tryScheduleAppend() {
+        if (mLoadStateManager.end == PagedList.LoadState.IDLE) {
+            scheduleAppend()
+        }
+    }
+
+    private fun canPrepend(): Boolean {
+        return if (mTotalCount == DataSource.BaseResult.TOTAL_COUNT_UNKNOWN) {
+            // don't know count / position from initial load, so be conservative, return true
+            true
+        } else adjacentProvider.firstLoadedItemIndex > 0
+
+        // position is known, do we have space left?
+    }
+
+    private fun canAppend(): Boolean {
+        return if (mTotalCount == DataSource.BaseResult.TOTAL_COUNT_UNKNOWN) {
+            // don't know count / position from initial load, so be conservative, return true
+            true
+        } else adjacentProvider.lastLoadedItemIndex < mTotalCount - 1
+
+        // count is known, do we have space left?
+    }
+
+    private fun schedulePrepend() {
+        if (!canPrepend()) {
+            onLoadSuccess(PagedList.LoadType.START, DataSource.BaseResult.empty())
+            return
+        }
+
+        @Suppress("UNCHECKED_CAST")
+        val key = when (mSource.type) {
+            DataSource.KeyType.POSITIONAL ->
+                (adjacentProvider.firstLoadedItemIndex - 1) as K
+            DataSource.KeyType.PAGE_KEYED -> mPrevKey
+            DataSource.KeyType.ITEM_KEYED -> mSource.getKey(adjacentProvider.firstLoadedItem!!)
+        }
+
+        mLoadStateManager.setState(PagedList.LoadType.START, PagedList.LoadState.LOADING, null)
+        listenTo(
+            PagedList.LoadType.START, mSource.load(
+                DataSource.Params(
+                    DataSource.LoadType.START,
+                    key,
+                    mConfig.initialLoadSizeHint,
+                    mConfig.enablePlaceholders,
+                    mConfig.pageSize
+                )
+            )
+        )
+    }
+
+    private fun scheduleAppend() {
+        if (!canAppend()) {
+            onLoadSuccess(PagedList.LoadType.END, DataSource.BaseResult.empty())
+            return
+        }
+
+        @Suppress("UNCHECKED_CAST")
+        val key = when (mSource.type) {
+            DataSource.KeyType.POSITIONAL -> (adjacentProvider.lastLoadedItemIndex + 1) as K
+            DataSource.KeyType.PAGE_KEYED -> mNextKey
+            DataSource.KeyType.ITEM_KEYED -> mSource.getKey(adjacentProvider.lastLoadedItem!!)
+        }
+
+        mLoadStateManager.setState(PagedList.LoadType.END, PagedList.LoadState.LOADING, null)
+        listenTo(
+            PagedList.LoadType.END, mSource.load(
+                DataSource.Params(
+                    DataSource.LoadType.END,
+                    key,
+                    mConfig.initialLoadSizeHint,
+                    mConfig.enablePlaceholders,
+                    mConfig.pageSize
+                )
+            )
+        )
+    }
+
+    fun retry() {
+        if (mLoadStateManager.start == PagedList.LoadState.RETRYABLE_ERROR) {
+            schedulePrepend()
+        }
+        if (mLoadStateManager.end == PagedList.LoadState.RETRYABLE_ERROR) {
+            scheduleAppend()
+        }
+    }
+
+    fun detach() {
+        mDetached.set(true)
+    }
+
+    internal class SimpleAdjacentProvider<V> : AdjacentProvider<V> {
+        override var firstLoadedItemIndex: Int = 0
+            private set
+        override var lastLoadedItemIndex: Int = 0
+            private set
+
+        override var firstLoadedItem: V? = null
+            private set
+        override var lastLoadedItem: V? = null
+            private set
+
+        var counted: Boolean = false
+        var leadingUnloadedCount: Int = 0
+        var trailingUnloadedCount: Int = 0
+
+        override fun onPageResultResolution(
+            type: PagedList.LoadType,
+            result: DataSource.BaseResult<V>
+        ) {
+            if (result.data.isEmpty()) {
+                return
+            }
+            if (type == PagedList.LoadType.START) {
+                firstLoadedItemIndex -= result.data.size
+                firstLoadedItem = result.data[0]
+                if (counted) {
+                    leadingUnloadedCount -= result.data.size
+                }
+            } else if (type == PagedList.LoadType.END) {
+                lastLoadedItemIndex += result.data.size
+                lastLoadedItem = result.data.last()
+                if (counted) {
+                    trailingUnloadedCount -= result.data.size
+                }
+            } else {
+                firstLoadedItemIndex = result.leadingNulls + result.offset
+                lastLoadedItemIndex = firstLoadedItemIndex + result.data.size - 1
+                firstLoadedItem = result.data[0]
+                lastLoadedItem = result.data.last()
+
+                if (result.counted) {
+                    counted = true
+                    leadingUnloadedCount = result.leadingNulls
+                    trailingUnloadedCount = result.trailingNulls
+                }
+            }
+        }
+    }
+}
---
+++ paging/common/src/main/kotlin/androidx/paging/WrapperItemKeyedDataSource.kt
@@ -0,0 +1,98 @@
+/*
+ * Copyright 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.paging
+
+import androidx.arch.core.util.Function
+import java.util.IdentityHashMap
+
+internal class WrapperItemKeyedDataSource<K : Any, A : Any, B : Any>(
+    private val mSource: ItemKeyedDataSource<K, A>,
+    @JvmField internal val mListFunction: Function<List<A>, List<B>>
+) : ItemKeyedDataSource<K, B>() {
+
+    private val mKeyMap = IdentityHashMap<B, K>()
+
+    override fun addInvalidatedCallback(onInvalidatedCallback: InvalidatedCallback) {
+        mSource.addInvalidatedCallback(onInvalidatedCallback)
+    }
+
+    override fun removeInvalidatedCallback(onInvalidatedCallback: InvalidatedCallback) {
+        mSource.removeInvalidatedCallback(onInvalidatedCallback)
+    }
+
+    override fun invalidate() {
+        mSource.invalidate()
+    }
+
+    override fun isInvalid() = mSource.isInvalid()
+
+    fun convertWithStashedKeys(source: List<A>): List<B> {
+        val dest = convert(mListFunction, source)
+        synchronized(mKeyMap) {
+            // synchronize on mKeyMap, since multiple loads may occur simultaneously.
+            // Note: manually sync avoids locking per-item (e.g. Collections.synchronizedMap)
+            for (i in dest.indices) {
+                mKeyMap[dest[i]] = mSource.getKey(source[i])
+            }
+        }
+        return dest
+    }
+
+    override fun loadInitial(params: LoadInitialParams<K>, callback: LoadInitialCallback<B>) {
+        mSource.loadInitial(params, object : ItemKeyedDataSource.LoadInitialCallback<A>() {
+            override fun onResult(data: List<A>, position: Int, totalCount: Int) {
+                callback.onResult(convertWithStashedKeys(data), position, totalCount)
+            }
+
+            override fun onResult(data: List<A>) {
+                callback.onResult(convertWithStashedKeys(data))
+            }
+
+            override fun onError(error: Throwable) {
+                callback.onError(error)
+            }
+        })
+    }
+
+    override fun loadAfter(params: LoadParams<K>, callback: LoadCallback<B>) {
+        mSource.loadAfter(params, object : ItemKeyedDataSource.LoadCallback<A>() {
+            override fun onResult(data: List<A>) {
+                callback.onResult(convertWithStashedKeys(data))
+            }
+
+            override fun onError(error: Throwable) {
+                callback.onError(error)
+            }
+        })
+    }
+
+    override fun loadBefore(params: LoadParams<K>, callback: LoadCallback<B>) {
+        mSource.loadBefore(params, object : ItemKeyedDataSource.LoadCallback<A>() {
+            override fun onResult(data: List<A>) {
+                callback.onResult(convertWithStashedKeys(data))
+            }
+
+            override fun onError(error: Throwable) {
+                callback.onError(error)
+            }
+        })
+    }
+
+    override fun getKey(item: B): K = synchronized(mKeyMap) {
+        return mKeyMap[item]!!
+    }
+}
