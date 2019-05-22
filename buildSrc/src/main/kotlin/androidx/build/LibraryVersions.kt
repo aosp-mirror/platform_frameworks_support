7ac303463d329e77b84e0f2554c448bc7b53e646
Jakub Gielzak <jgielzak@google.com>
2019-05-22 12:11:11 +0100

    Moving ViewPager2 to beta00
    In preparation for beta01 launch.
    
    Bug: 132259681
    Test: ./gradlew viewpager2:checkReleaseReady
    
    Change-Id: Ice655f4463329bc78382b94d9dcd0164e07b0547

--- buildSrc/src/main/kotlin/androidx/build/LibraryVersions.kt
+++ buildSrc/src/main/kotlin/androidx/build/LibraryVersions.kt
@@ -100,7 +100,7 @@ object LibraryVersions {
     val VECTORDRAWABLE_ANIMATED = Version("1.1.0-beta02")
     val VERSIONED_PARCELABLE = Version("1.1.0-beta02")
     val VIEWPAGER = Version("1.1.0-alpha01")
-    val VIEWPAGER2 = Version("1.0.0-alpha05")
+    val VIEWPAGER2 = Version("1.0.0-beta00")
     val WEAR = Version("1.1.0-alpha01")
     val WEBKIT = Version("1.1.0-alpha02")
     val WORKMANAGER = Version("2.1.0-alpha02")
