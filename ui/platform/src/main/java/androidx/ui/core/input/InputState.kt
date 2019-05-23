e9aaab085064c606cfe17fea1ae48b2616630d4a
Seigo Nonaka <nona@google.com>
2019-05-23 16:47:13 -0700

    Introduce RecordingInputConnection
    The IME events will be converted to EditOperations.
    The first version of RecordingInputConnection implements
    following callbacks:
    - beginBatchEdit
    - endBatchEdit
    - commitText
    - getTextBeforeCursor
    - getTextAfterCursor
    - getSelectedText
    
    In addition to this, we ignore all styles came from IME since
    practically IME don't send styled text and we have different
    style mechanism.
    
    BUG: 113819017
    Test: RecordingInputConnectionTest
    Change-Id: I86b677ed4eca78111f25a0e3b427d28ce02147e9

---
+++ ui/platform/src/main/java/androidx/ui/core/input/InputState.kt
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.ui.core.input
+
+import androidx.ui.core.TextRange
+import androidx.ui.core.substring
+
+/**
+ * Represent a state to be visible from IME
+ */
+internal data class InputState(
+    /**
+     * A text visible to IME
+     */
+    val text: String,
+
+    /**
+     * A selection range visible to IME.
+     * The selection range must be valid range in the given text.
+     */
+    val selection: TextRange,
+
+    /**
+     * A composition range visible to IME.
+     * If null, there is no composition range.
+     * If non-null, the composition range must be valid range in the given text.
+     */
+    val composition: TextRange? = null
+) {
+
+    /**
+     * Helper function for getting text before selection range.
+     */
+    fun getTextBeforeSelection(maxChars: Int): String =
+        text.substring(Math.max(0, selection.start - maxChars), selection.start)
+
+    /**
+     * Helper function for getting text after selection range.
+     */
+    fun getTextAfterSelection(maxChars: Int): String =
+        text.substring(selection.end, Math.min(selection.end + maxChars, text.length))
+
+    /**
+     * Helper function for getting text currently selected.
+     */
+    fun getSelectedText(): String = text.substring(selection)
+}
