265546acf70a8894c8ea907e0c6218b2099caab8
Insun Kang <insun@google.com>
2019-05-23 10:33:22 +0900

    Clean up "timed text" related code from androidx.media2.player
    Timed text track type has been redundantly remained in androidx.media2.
    This CL cleaned it up.
    
    Bug: 133283938
    Test: ./gradlew -p media2 check :media2:integration-tests:testapp:check
    Change-Id: I7bbeb6279afaa7f143380848d47e637a3bc72839

--- media2/player/src/main/java/androidx/media2/player/MediaPlayer2.java
+++ media2/player/src/main/java/androidx/media2/player/MediaPlayer2.java
@@ -770,8 +770,6 @@ public abstract class MediaPlayer2 {
         public static final int MEDIA_TRACK_TYPE_VIDEO = 1;
         public static final int MEDIA_TRACK_TYPE_AUDIO = 2;
 
-        public static final int MEDIA_TRACK_TYPE_TIMEDTEXT = 3;
-
         public static final int MEDIA_TRACK_TYPE_SUBTITLE = 4;
         public static final int MEDIA_TRACK_TYPE_METADATA = 5;
 
