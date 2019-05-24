791fe0626a011ff305ceb5a24f2dfc0f49b964b3
Jelle Fresen <jellefresen@google.com>
2019-05-24 15:43:29 +0100

    Make touch slop configurable in ViewPager2
    Default is set to TOUCH_SLOP_PAGING, users can modify to
    TOUCH_SLOP_CONTINUOUS. Added a demo activity where both touch slops can
    be set.
    
    Bug: 133032785
    Test: ./gradlew viewpager2:connectedCheck \
            -Pandroid.testInstrumentationRunnerArguments.class=\
            androidx.viewpager2.widget.SetScrollingTouchSlopTest
    
    Change-Id: Ibb4896c92ca7d50a8c150a30e85f5dc666b02cb6

---
+++ viewpager2/integration-tests/testapp/src/main/java/androidx/viewpager2/integration/testapp/TouchSlopActivity.kt
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.viewpager2.integration.testapp
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Switch
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.integration.testapp.cards.CardViewAdapter
+import androidx.viewpager2.widget.ViewPager2
+
+class TouchSlopActivity : FragmentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_touch_slop)
+
+        val viewPager: ViewPager2 = findViewById(R.id.view_pager)
+        viewPager.adapter = CardViewAdapter()
+
+        val switch: Switch = findViewById(R.id.use_paging_slop)
+        switch.isChecked = true // VP default is TOUCH_SLOP_PAGING
+        switch.setOnCheckedChangeListener { _, isChecked ->
+            viewPager.setScrollingTouchSlop(when (isChecked) {
+                true -> ViewPager2.TOUCH_SLOP_PAGING
+                false -> ViewPager2.TOUCH_SLOP_CONTINUOUS
+            })
+        }
+    }
+}
