bc8c334dc5b9e8e4b65bfe71a99880e5462f4675
Kyunglyul Hyun <klhyun@google.com>
2019-05-24 14:56:22 +0900

    Exifinterface: prevent file descriptor leak
    This CL closes duplicated file descriptors to prevent fd leak.
    
    Bug: 133251455
    Test: ./gradlew exifinterface:connectedCheck
    Change-Id: I344977458b0f90bdd0566ba86cfd3e7271b26207

--- exifinterface/src/main/java/androidx/exifinterface/media/ExifInterface.java
+++ exifinterface/src/main/java/androidx/exifinterface/media/ExifInterface.java
@@ -23,6 +23,7 @@ import android.location.Location;
 import android.media.MediaDataSource;
 import android.media.MediaMetadataRetriever;
 import android.os.Build;
+import android.system.ErrnoException;
 import android.system.Os;
 import android.system.OsConstants;
 import android.util.Log;
@@ -3791,6 +3792,7 @@ public class ExifInterface {
         }
         mAssetInputStream = null;
         mFilename = null;
+        boolean isFdDuped = false;
         if (Build.VERSION.SDK_INT >= 21 && isSeekableFD(fileDescriptor)) {
             mSeekableFileDescriptor = fileDescriptor;
             // Keep the original file descriptor in order to save attributes when it's seekable.
@@ -3798,6 +3800,7 @@ public class ExifInterface {
             // feature won't be working.
             try {
                 fileDescriptor = Os.dup(fileDescriptor);
+                isFdDuped = true;
             } catch (Exception e) {
                 throw new IOException("Failed to duplicate file descriptor", e);
             }
@@ -3810,6 +3813,9 @@ public class ExifInterface {
             loadAttributes(in);
         } finally {
             closeQuietly(in);
+            if (isFdDuped) {
+                closeFileDescriptor(fileDescriptor);
+            }
         }
     }
 
@@ -4533,6 +4539,7 @@ public class ExifInterface {
 
         // Read the thumbnail.
         InputStream in = null;
+        FileDescriptor newFileDescriptor = null;
         try {
             if (mAssetInputStream != null) {
                 in = mAssetInputStream;
@@ -4545,9 +4552,9 @@ public class ExifInterface {
             } else if (mFilename != null) {
                 in = new FileInputStream(mFilename);
             } else if (Build.VERSION.SDK_INT >= 21 && mSeekableFileDescriptor != null) {
-                FileDescriptor fileDescriptor = Os.dup(mSeekableFileDescriptor);
-                Os.lseek(fileDescriptor, 0, OsConstants.SEEK_SET);
-                in = new FileInputStream(fileDescriptor);
+                newFileDescriptor = Os.dup(mSeekableFileDescriptor);
+                Os.lseek(newFileDescriptor, 0, OsConstants.SEEK_SET);
+                in = new FileInputStream(newFileDescriptor);
             }
             if (in == null) {
                 // Should not be reached this.
@@ -4567,6 +4574,9 @@ public class ExifInterface {
             Log.d(TAG, "Encountered exception while getting thumbnail", e);
         } finally {
             closeQuietly(in);
+            if (newFileDescriptor != null) {
+                closeFileDescriptor(newFileDescriptor);
+            }
         }
         return null;
     }
@@ -7017,6 +7027,23 @@ public class ExifInterface {
                 throw rethrown;
             } catch (Exception ignored) {
             }
+        }
+    }
+
+    /**
+     * Closes a file descriptor that has been duplicated.
+     */
+    private static void closeFileDescriptor(FileDescriptor fd) {
+        // Os.dup and Os.close was introduced in API 21 so this method shouldn't be called
+        // in API < 21.
+        if (Build.VERSION.SDK_INT >= 21) {
+            try {
+                Os.close(fd);
+            } catch (ErrnoException ex) {
+                Log.e(TAG, "Error closing fd.");
+            }
+        } else {
+            Log.e(TAG, "closeFileDescriptor is called in API < 21, which must be wrong.");
         }
     }
 
