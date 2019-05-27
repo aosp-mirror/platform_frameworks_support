9ebcb156f49367f1c565c796a370f29bb96069b0
Jin Seok Park <jinpark@google.com>
2019-05-27 14:05:19 +0900

    Remove extracting metadata from VideoView
    Since VideoView cannot know the subclass of MediaItem (if it's a
    UriMediaItem or FileMediaItem), it cannot retrieve the metadata
    by using MediaMetadataRetriever. Thus, this CL removes the logic of
    extracting metadata from VideoView.
    
    Also adds Javadoc for MediaControlView and VideoView on what
    metadata keys are supported.
    
    Also update integration test app to extract metadata and call
    MediaItem#setMetadata.
    
    Bug: 133283493
    Test: ./gradlew :media2:media2-widget:check \
                    :media2:media2-widget:connectedCheck \
                    :media2:integration-tests:testapp:check \
                    :media2:integration-tests:testapp:installDebug
    Change-Id: I37025a2e154440801073ea677fa7d73cdb2fb9b8

--- media2/widget/src/main/java/androidx/media2/widget/MediaControlView.java
+++ media2/widget/src/main/java/androidx/media2/widget/MediaControlView.java
@@ -53,6 +53,7 @@ import androidx.annotation.VisibleForTesting;
 import androidx.appcompat.app.AlertDialog;
 import androidx.core.content.ContextCompat;
 import androidx.media2.common.MediaItem;
+import androidx.media2.common.MediaMetadata;
 import androidx.media2.common.SessionPlayer;
 import androidx.media2.common.SessionPlayer.TrackInfo;
 import androidx.media2.common.UriMediaItem;
@@ -90,7 +91,22 @@ import java.util.Locale;
  * In addition, the following customizations are supported:
  * 1) Set focus to the play/pause button by calling {@link #requestPlayButtonFocus()}.
  * 2) Set full screen behavior by calling {@link #setOnFullScreenListener(OnFullScreenListener)}
+ * <p>
+ * <em> Displaying metadata : </em>
+ * MediaControlView supports displaying metadata by calling
+ * {@link MediaItem#setMetadata(MediaMetadata)}.
  *
+ * Metadata display is different for two different media types: music, and non-music.
+ * For music, the following metadata are supported:
+ * {@link MediaMetadata#METADATA_KEY_TITLE}, {@link MediaMetadata#METADATA_KEY_ARTIST},
+ * and {@link MediaMetadata#METADATA_KEY_ALBUM_ART}.
+ * If values for these keys are not set, the following default values will be shown, respectively:
+ * {@link androidx.media2.widget.R.string#mcv2_music_title_unknown_text}
+ * {@link androidx.media2.widget.R.string#mcv2_music_artist_unknown_text}
+ * {@link androidx.media2.widget.R.drawable#ic_default_album_image}
+ *
+ * For non-music, only {@link MediaMetadata#METADATA_KEY_TITLE} metadata is supported. Also, no
+ * default value is provided for this media type.
  */
 public class MediaControlView extends ViewGroup {
     private static final String TAG = "MediaControlView";
@@ -2053,6 +2069,7 @@ public class MediaControlView extends ViewGroup {
             }
 
             updateTracks(player, trackInfos);
+            updateMetadata();
         }
 
         @Override
