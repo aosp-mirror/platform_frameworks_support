e735530553da13577ce61bf36860066bd8c1ceec
WenHung_Teng <wenhungteng@google.com>
2019-06-07 00:10:20 +0800

    Make sure getCaptureStage was called after init()
    (1) Remove the check of the CaptureStages in ImageCapture constructor
    since the check is already appled in issueTakePicture().
    (2) Add test to verify the getCaptureStage() has not been invoked
    before init().
    (3) Add test for event callbacks.
    
    Bug: 134477673
    Test: ./gradlew bOS, and manually run the test.
    Change-Id: I3d66bba99ee0c5145a3047e3951ebe6a8983a823

---
+++ camera/extensions/src/androidTest/java/androidx/camera/extensions/PreviewExtenderTest.java
@@ -0,0 +1,126 @@
+/*
+ * Copyright 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.camera.extensions;
+
+import static org.junit.Assume.assumeTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.inOrder;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.timeout;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+
+import android.Manifest;
+import android.content.Context;
+import android.graphics.SurfaceTexture;
+import android.hardware.camera2.CameraCharacteristics;
+
+import androidx.camera.camera2.Camera2Config;
+import androidx.camera.camera2.impl.CameraEventCallbacks;
+import androidx.camera.core.CameraX;
+import androidx.camera.core.Preview;
+import androidx.camera.core.PreviewConfig;
+import androidx.camera.extensions.impl.PreviewExtenderImpl;
+import androidx.camera.testing.CameraUtil;
+import androidx.camera.testing.fakes.FakeLifecycleOwner;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.MediumTest;
+import androidx.test.rule.GrantPermissionRule;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InOrder;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.FutureTask;
+
+@RunWith(AndroidJUnit4.class)
+public class PreviewExtenderTest {
+
+    @Rule
+    public GrantPermissionRule mRuntimePermissionRule = GrantPermissionRule.grant(
+            Manifest.permission.CAMERA);
+
+    @Before
+    public void setUp() {
+        assumeTrue(CameraUtil.deviceHasCamera());
+    }
+
+    @Test
+    @MediumTest
+    public void extenderLifeCycleTest_noMoreInvokeBeforeAndAfterInitDeInit() {
+        FakeLifecycleOwner lifecycle = new FakeLifecycleOwner();
+
+        PreviewExtenderImpl mockPreviewExtenderImpl = mock(PreviewExtenderImpl.class);
+
+        PreviewExtender.PreviewExtenderAdapter previewExtenderAdapter =
+                new PreviewExtender.PreviewExtenderAdapter(mockPreviewExtenderImpl);
+        PreviewConfig.Builder configBuilder = new PreviewConfig.Builder().setUseCaseEventListener(
+                previewExtenderAdapter);
+        new Camera2Config.Extender(configBuilder).setCameraEventCallback(
+                new CameraEventCallbacks(previewExtenderAdapter));
+
+        Preview useCase = new Preview(configBuilder.build());
+
+        CameraX.bindToLifecycle(lifecycle, useCase);
+        lifecycle.startAndResume();
+
+        final SurfaceTextureCallable surfaceTextureCallable0 = new SurfaceTextureCallable();
+        final FutureTask<SurfaceTexture> future0 = new FutureTask<>(surfaceTextureCallable0);
+        useCase.setOnPreviewOutputUpdateListener(
+                new Preview.OnPreviewOutputUpdateListener() {
+                    @Override
+                    public void onUpdated(Preview.PreviewOutput previewOutput) {
+                        surfaceTextureCallable0.setSurfaceTexture(
+                                previewOutput.getSurfaceTexture());
+                        future0.run();
+                    }
+                });
+
+        // To verify the call in order after bind to life cycle.
+        InOrder inOrder = inOrder(mockPreviewExtenderImpl);
+        inOrder.verify(mockPreviewExtenderImpl, timeout(3000)).onInit(any(String.class), any(
+                CameraCharacteristics.class), any(Context.class));
+        inOrder.verify(mockPreviewExtenderImpl, timeout(3000)).onPresetSession();
+        inOrder.verify(mockPreviewExtenderImpl, timeout(3000)).onEnableSession();
+        inOrder.verify(mockPreviewExtenderImpl, timeout(3000)).getCaptureStage();
+
+        // Unbind the use case to test the onDisableSession and onDeInit.
+        CameraX.unbind(useCase);
+
+        // To verify the onDisableSession and onDeInit.
+        inOrder.verify(mockPreviewExtenderImpl, timeout(3000)).onDisableSession();
+        inOrder.verify(mockPreviewExtenderImpl, timeout(3000)).onDeInit();
+
+        // To verify there is no any other calls on the mock.
+        verifyNoMoreInteractions(mockPreviewExtenderImpl);
+    }
+
+    private static final class SurfaceTextureCallable implements Callable<SurfaceTexture> {
+        SurfaceTexture mSurfaceTexture;
+
+        void setSurfaceTexture(SurfaceTexture surfaceTexture) {
+            this.mSurfaceTexture = surfaceTexture;
+        }
+
+        @Override
+        public SurfaceTexture call() {
+            return mSurfaceTexture;
+        }
+    }
+}
