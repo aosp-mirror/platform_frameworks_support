7cff7f69b6eb6da7e0344c4fe5726b0e1f28b0fa
Franklin Wu <nilknarfuw@google.com>
2019-06-07 17:35:23 -0700

    Added sample implementation using PROCESSOR_TYPE_IMAGE_PROCESSOR
    Added a sample implementation of using OpenGL to write a Image to
    Surface. This assumes YUV and takes just the Y channel and writes it to
    RGB channels.
    
    Bug: b/130193125
    Test: manual test
    Change-Id: Ib021cd1fa1ea77f2cb615a690115d04c9b8973c6

--- camera/integration-tests/extensionstestlib/src/main/java/androidx/camera/extensions/impl/HdrPreviewExtenderImpl.java
+++ camera/integration-tests/extensionstestlib/src/main/java/androidx/camera/extensions/impl/HdrPreviewExtenderImpl.java
@@ -20,7 +20,6 @@ import android.content.Context;
 import android.hardware.camera2.CameraCharacteristics;
 import android.hardware.camera2.TotalCaptureResult;
 import android.media.Image;
-import android.util.Log;
 import android.util.Size;
 import android.view.Surface;
 
@@ -31,7 +30,6 @@ import android.view.Surface;
  * don't need to implement this, unless this is used for related testing usage.
  */
 public final class HdrPreviewExtenderImpl implements PreviewExtenderImpl {
-    private static final String TAG = "HDRImpl";
     private static final int DEFAULT_STAGE_ID = 0;
 
     public HdrPreviewExtenderImpl() { }
@@ -67,20 +65,33 @@ public final class HdrPreviewExtenderImpl implements PreviewExtenderImpl {
     }
 
     private PreviewImageProcessorImpl mProcessor = new PreviewImageProcessorImpl() {
+        Surface mSurface;
+        Size mSize;
+
+        GLImage2SurfaceRenderer mRenderer = new GLImage2SurfaceRenderer();
+
+        private void setWindowSurface() {
+            if (mSurface != null && mSize != null) {
+                mRenderer.setWindowSurface(mSurface, mSize.getWidth(), mSize.getHeight());
+            }
+        }
 
         @Override
         public void onOutputSurface(Surface surface, int imageFormat) {
+            mSurface = surface;
+            setWindowSurface();
         }
 
         @Override
         public void process(Image image, TotalCaptureResult result) {
-            Log.d(TAG, "process image");
+            mRenderer.renderTexture(image);
         }
 
-        Size mSize;
         @Override
         public void onResolutionUpdate(Size size) {
             mSize = size;
+            setWindowSurface();
+            mRenderer.setInput(size);
         }
 
         @Override
