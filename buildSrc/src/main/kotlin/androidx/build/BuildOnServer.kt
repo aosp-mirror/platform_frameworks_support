b7d36c1fb8e416290bd3cde300bafabdd233c1db
Nick Anthony <nickanthony@google.com>
2019-06-13 07:21:02 -0400

    Create buildOnServer task action that creates the aggregate buildInfo File
    Aggregates the output of every project's _build_info.txt file
    
    This taskaction depends on the output of every project's createLibraryBuildInfoFile task.
    However, given everything I've tried, I found that constructing it as a standalone task
    would mean it would mean depending on and configuring every createLibraryBuildInfoFile task.
    So moving to buildOnServer to avoid eager task configuration.
    
    Test: ./gradlew buildOnServer && subl ../../out/dist/androidx_aggregate_build_info.txt
    Change-Id: I770b5e62dc44139f409adaa4248f437768d92857

--- buildSrc/src/main/kotlin/androidx/build/BuildOnServer.kt
+++ buildSrc/src/main/kotlin/androidx/build/BuildOnServer.kt
@@ -16,11 +16,15 @@
 
 package androidx.build
 
+import androidx.build.jetpad.LibraryBuildInfoFile
+import com.google.gson.Gson
 import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.InputFiles
+import org.gradle.api.tasks.OutputFile
 import org.gradle.api.tasks.TaskAction
 import java.io.File
 import java.io.FileNotFoundException
+import java.util.ArrayList
 
 /**
  * Task for building all of Androidx libraries and documentation
@@ -72,6 +76,69 @@ open class BuildOnServer : DefaultTask() {
         if (missingFiles.isNotEmpty()) {
             val missingFileString = missingFiles.reduce { acc, s -> "$acc, $s" }
             throw FileNotFoundException("buildOnServer required output missing: $missingFileString")
+        }
+    }
+
+    @OutputFile
+    val outputFile = File(project.getDistributionDirectory(),
+        getAndroidxAggregateBuildInfoFilename())
+
+    private fun getAndroidxAggregateBuildInfoFilename(): String {
+        return "androidx_aggregate_build_info.txt"
+    }
+
+    private data class AllLibraryBuildInfoFiles(
+        val artifacts: ArrayList<LibraryBuildInfoFile>
+    )
+
+    /**
+     * Reads in file and checks that json is valid
+     */
+    private fun jsonFileIsValid(jsonFile: File, artifactList: MutableList<String>): Boolean {
+        if (!jsonFile.exists()) {
+            return(false)
+        }
+        val gson = Gson()
+        val jsonString: String = jsonFile.readText(Charsets.UTF_8)
+        val aggregateBuildInfoFile = gson.fromJson(jsonString, AllLibraryBuildInfoFiles::class.java)
+        aggregateBuildInfoFile.artifacts.forEach { artifact ->
+            if (!(artifactList.contains("${artifact.groupId}_${artifact.artifactId}"))) {
+                println("Failed to find ${artifact.artifactId} in artifact list!")
+                return false
+            }
+        }
+        return true
+    }
+
+    /**
+     * Create the output file to contain the final complete AndroidX project build info graph
+     * file.  Iterate through the list of project-specific build info files, and collects
+     * all dependencies as a JSON string. Finally, write this complete dependency graph to a text
+     * file as json list of every project's build information
+     */
+    @TaskAction
+    fun createAndroidxAggregateBuildInfoFile() {
+        // Loop through each file in the list of libraryBuildInfoFiles and collect all build info
+        // data from each of these $groupId-$artifactId-_build_info.txt files
+        var output = StringBuilder()
+        output.append("{ \"artifacts\": [\n")
+        var artifactList = mutableListOf<String>()
+        val distFiles = project.getDistributionDirectory().listFiles()
+        distFiles.filter {
+            ((it.isFile and (it.name != outputFile.name))
+                    and (it.name.contains("_build_info.txt")))
+        }.forEach { file ->
+            println(file)
+            var fileText: String = file.readText(Charsets.UTF_8)
+            output.append("$fileText,")
+            artifactList.add(file.name.replace("_build_info.txt", ""))
+        }
+        // Remove final ',' from list (so a null object doesn't get added to list
+        output.setLength(output.length - 1)
+        output.append("]}")
+        outputFile.writeText(output.toString(), Charsets.UTF_8)
+        if (!jsonFileIsValid(outputFile, artifactList)) {
+            throw RuntimeException("JSON written to $outputFile was invalid.")
         }
     }
 }
