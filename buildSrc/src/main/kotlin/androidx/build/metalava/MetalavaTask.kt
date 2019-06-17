41f3e8a9db49b3015b748bc171056092acb39aff
Aurimas Liutikas <aurimas@google.com>
2019-06-17 15:00:11 -0700

    Start running Metalava with --api-lint
    Also update the API baseline to suppress existing API lint issues.
    
    Bug: 135478003
    Test: ./gradlew checkApi --> passes
    Change-Id: Ic5ba99f426973c7d756d04a23598636c7252ab1d

--- buildSrc/src/main/kotlin/androidx/build/metalava/MetalavaTask.kt
+++ buildSrc/src/main/kotlin/androidx/build/metalava/MetalavaTask.kt
@@ -51,8 +51,16 @@ abstract class MetalavaTask : DefaultTask() {
             it.main = "com.android.tools.metalava.Driver"
             it.args = listOf(
                 "--no-banner",
+                "--api-lint",
                 "--error",
-                "DeprecationMismatch" // Enforce deprecation mismatch
+                "DeprecationMismatch", // Enforce deprecation mismatch
+                "--hide",
+                "Enum,IntentBuilderName,OnNameExpected,TopLevelBuilder,MissingBuild," +
+                        "BuilderSetStyle,SetterReturnsThis,PackageLayering,OverlappingConstants" +
+                        "IllegalStateException,ListenerLast,ExecutorRegistration,StreamFiles" +
+                        "ParcelableList,AbstractInner,NotCloseable,ArrayReturn,UserHandle" +
+                        "UserHandleName,MethodNameTense,UseIcu,NoByteOrShort,CommonArgsFirst" +
+                        "SamShouldBeLast,MissingJvmStatic"
             ) + args
         }
     }
