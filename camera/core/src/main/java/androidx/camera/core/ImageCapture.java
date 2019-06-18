3ebb3320bb880b9f5d7b5d3a7733dec6910f2c85
leo huang <leohuang@google.com>
2019-06-18 17:04:01 +0800

    Fix take picture in a row fails.
    Root cause:
    It takes time to save image and the image buffer resource is hold durring image
    saving task. It will get "maxImages" exception if image buffer is not available.
    
    Solution
    Issue next image capture request when dispatched image is closed to ensure the
    image buffer is available. Callback error when fail to acquire image or take
    picture.
    
    Bug: 134360132
    Test: ./gradlew camera:camera-camera2:connectedAndroidTest -Pandroid.testInstrumentationRunnerArguments.class=androidx.camera.camera2.ImageCaptureTest
    Change-Id: I20b96231af95967e33cf4589b91c8a616e5b985a

--- camera/core/src/main/java/androidx/camera/core/ImageCapture.java
+++ camera/core/src/main/java/androidx/camera/core/ImageCapture.java
@@ -41,6 +41,7 @@ import androidx.camera.core.CameraCaptureMetaData.AfState;
 import androidx.camera.core.CameraCaptureMetaData.AwbState;
 import androidx.camera.core.CameraCaptureResult.EmptyCameraCaptureResult;
 import androidx.camera.core.CameraX.LensFacing;
+import androidx.camera.core.ForwardingImageProxy.OnImageCloseListener;
 import androidx.camera.core.ImageOutputConfig.RotationValue;
 import androidx.camera.core.impl.utils.executor.CameraXExecutors;
 import androidx.camera.core.impl.utils.futures.AsyncFunction;
@@ -82,7 +83,7 @@ import java.util.concurrent.atomic.AtomicInteger;
  * via an {@link ImageCapture.OnImageCapturedListener}.
  *
  */
-public class ImageCapture extends UseCase {
+public class ImageCapture extends UseCase implements OnImageCloseListener {
     /**
      * Provides a static configuration with implementation-agnostic options.
      *
@@ -491,6 +492,30 @@ public class ImageCapture extends UseCase {
         }
     }
 
+    /**
+     * {@inheritDoc}
+     *
+     * <p>Issues next image capture request when dispatched image is closed, which can ensure the
+     * image buffer in ImageReader is always available.
+     *
+     * @hide
+     */
+    @RestrictTo(Scope.LIBRARY_GROUP)
+    @Override
+    public void onImageClose(final ImageProxy image) {
+        if (Looper.getMainLooper() != Looper.myLooper()) {
+            mMainHandler.post(new Runnable() {
+                @Override
+                public void run() {
+                    onImageClose(image);
+                }
+            });
+            return;
+        }
+        mImageCaptureRequests.poll();
+        issueImageCaptureRequests();
+    }
+
     /** Issues saved ImageCaptureRequest. */
     @UiThread
     void issueImageCaptureRequests() {
@@ -536,7 +561,23 @@ public class ImageCapture extends UseCase {
 
                             @Override
                             public void onFailure(Throwable t) {
-                                Log.e(TAG, "takePictureInternal onFailure", t);
+                                final String message = "Failed to take picture.";
+                                final Throwable cause = t;
+                                Log.e(TAG, message, t);
+
+                                mMainHandler.post(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        ImageCaptureRequest imageCaptureRequest =
+                                                mImageCaptureRequests.peek();
+                                        if (imageCaptureRequest != null) {
+                                            imageCaptureRequest.callbackError(
+                                                    UseCaseError.UNKNOWN_ERROR, message, cause);
+                                            mImageCaptureRequests.poll();
+                                            ImageCapture.this.issueImageCaptureRequests();
+                                        }
+                                    }
+                                });
                             }
                         },
                         mExecutor);
@@ -623,18 +664,24 @@ public class ImageCapture extends UseCase {
                         ImageCaptureRequest imageCaptureRequest = mImageCaptureRequests.peek();
                         if (imageCaptureRequest != null) {
                             ImageProxy image = null;
+                            String message = null;
+                            Throwable cause = null;
                             try {
                                 image = imageReader.acquireLatestImage();
                             } catch (IllegalStateException e) {
-                                Log.e(TAG, "Failed to acquire latest image.", e);
+                                message = "Failed to acquire latest image.";
+                                cause = e;
+                                Log.e(TAG, message, e);
                             } finally {
                                 if (image != null) {
-                                    // Remove the first listener from the queue
+                                    SingleCloseImageProxy wrappedImage = new SingleCloseImageProxy(
+                                            image);
+                                    wrappedImage.addOnImageCloseListener(ImageCapture.this);
+                                    imageCaptureRequest.dispatchImage(wrappedImage);
+                                } else {
+                                    imageCaptureRequest.callbackError(
+                                            UseCaseError.UNKNOWN_ERROR, message, cause);
                                     mImageCaptureRequests.poll();
-
-                                    // Inform the listener
-                                    imageCaptureRequest.dispatchImage(image);
-
                                     ImageCapture.this.issueImageCaptureRequests();
                                 }
                             }
@@ -1213,6 +1260,7 @@ public class ImageCapture extends UseCase {
              * @param captureResult the camera capture result.
              * @return the check result, return null to continue checking.
              */
+            @Nullable
             T check(@NonNull CameraCaptureResult captureResult);
         }
 
@@ -1273,6 +1321,24 @@ public class ImageCapture extends UseCase {
             }
 
             mListener.onCaptureSuccess(image, mRotationDegrees);
+        }
+
+        void callbackError(final UseCaseError useCaseError, final String message,
+                final Throwable cause) {
+            if (mHandler != null && Looper.myLooper() != mHandler.getLooper()) {
+                boolean posted = mHandler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        ImageCaptureRequest.this.callbackError(useCaseError, message, cause);
+                    }
+                });
+                if (!posted) {
+                    Log.e(TAG, "Unable to post to the supplied handler.");
+                }
+                return;
+            }
+
+            mListener.onError(useCaseError, message, cause);
         }
     }
 }
