91fb91a67cc7b5307a76b4ac90ed646732432912
Chaohui Wang <chaohuiw@google.com>
2019-06-25 19:35:14 +0800

    Performance for ads-identifier client by keeping the connection.
    Test: TODO.
    Change-Id: Iedcf0c2d8f783c343758721ad454e92b874dc831

--- ads/ads-identifier/src/main/java/androidx/ads/identifier/AdvertisingIdClient.java
+++ ads/ads-identifier/src/main/java/androidx/ads/identifier/AdvertisingIdClient.java
@@ -19,7 +19,7 @@ package androidx.ads.identifier;
 import static androidx.ads.identifier.AdvertisingIdUtils.GET_AD_ID_ACTION;
 import static androidx.ads.identifier.AdvertisingIdUtils.getAdIdProviders;
 
-import android.app.Service;
+import android.annotation.SuppressLint;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
@@ -28,10 +28,11 @@ import android.content.pm.ResolveInfo;
 import android.content.pm.ServiceInfo;
 import android.os.Looper;
 import android.os.RemoteException;
-import android.util.Log;
+import android.util.Pair;
 
-import androidx.ads.identifier.internal.BlockingServiceConnection;
+import androidx.ads.identifier.internal.HoldingConnectionClient;
 import androidx.ads.identifier.provider.IAdvertisingIdService;
+import androidx.annotation.GuardedBy;
 import androidx.annotation.Nullable;
 import androidx.annotation.VisibleForTesting;
 import androidx.core.util.Preconditions;
@@ -41,7 +42,6 @@ import java.nio.charset.Charset;
 import java.util.List;
 import java.util.Locale;
 import java.util.UUID;
-import java.util.concurrent.TimeUnit;
 
 /**
  * Helper library for retrieval of advertising ID and related information such as the limit ad
@@ -49,50 +49,49 @@ import java.util.concurrent.TimeUnit;
  */
 public class AdvertisingIdClient {
 
-    private static final String TAG = "AdvertisingIdClient";
+    private static final Object sLock = new Object();
 
-    private static final long SERVICE_CONNECTION_TIMEOUT_SECONDS = 10;
-
+    @SuppressLint("StaticFieldLeak") // Application Context only.
+    @GuardedBy("sLock")
     @Nullable
-    private BlockingServiceConnection mConnection;
+    private static HoldingConnectionClient sHoldingConnectionClient;
 
-    @Nullable
-    private IAdvertisingIdService mService;
-
-    private final Context mContext;
-
-    private ComponentName mComponentName;
-
-    /** Constructs a new {@link AdvertisingIdClient} object. */
-    @VisibleForTesting
-    AdvertisingIdClient(Context context) {
-        Preconditions.checkNotNull(context);
-        mContext = context.getApplicationContext();
+    private AdvertisingIdClient() {
     }
 
-    private void start() throws IOException, AdvertisingIdNotAvailableException {
-        if (mConnection == null) {
-            mComponentName = getProviderComponentName(mContext);
-            mConnection = getServiceConnection();
-            mService = getAdvertisingIdService(mConnection);
+    private static Pair<IAdvertisingIdService, Intent> getServiceWithIntent(Context context)
+            throws IOException, AdvertisingIdNotAvailableException {
+        synchronized (sLock) {
+            if (sHoldingConnectionClient == null) {
+                sHoldingConnectionClient =
+                        new HoldingConnectionClient(context, () -> getIntent(context),
+                                IAdvertisingIdService.Stub::asInterface);
+            }
+            return sHoldingConnectionClient.getServiceWithIntent();
         }
     }
 
-    /** Returns the advertising ID info using {@link AdvertisingIdInfo}. */
-    @VisibleForTesting
-    AdvertisingIdInfo getInfo() throws IOException, AdvertisingIdNotAvailableException {
-        if (mConnection == null) {
-            start();
-        }
+    private static Intent getIntent(Context context) throws AdvertisingIdNotAvailableException {
+        Intent intent = new Intent(GET_AD_ID_ACTION);
+        intent.setComponent(getProviderComponentName(context));
+        return intent;
+    }
+
+    private static AdvertisingIdInfo getAdvertisingIdInfo(
+            Pair<IAdvertisingIdService, Intent> serviceWithIntent)
+            throws IOException, AdvertisingIdNotAvailableException {
+        IAdvertisingIdService service = serviceWithIntent.first;
+        ComponentName componentName = serviceWithIntent.second.getComponent();
+
         try {
-            String id = mService.getId();
+            String id = service.getId();
             if (id == null || id.trim().isEmpty()) {
                 throw new AdvertisingIdNotAvailableException();
             }
             return AdvertisingIdInfo.builder()
                     .setId(normalizeId(id))
-                    .setProviderPackageName(mComponentName.getPackageName())
-                    .setLimitAdTrackingEnabled(mService.isLimitAdTrackingEnabled())
+                    .setProviderPackageName(componentName.getPackageName())
+                    .setLimitAdTrackingEnabled(service.isLimitAdTrackingEnabled())
                     .build();
         } catch (RemoteException e) {
             throw new IOException("Remote exception", e);
@@ -123,24 +122,14 @@ public class AdvertisingIdClient {
         }
     }
 
-    /** Closes the connection. */
-    @VisibleForTesting
-    void finish() {
-        if (mConnection == null) {
-            return;
-        }
-        try {
-            mContext.unbindService(mConnection);
-        } catch (Throwable e) {
-            // This could happen when the thread takes too long to disconnect from the service.
-            Log.i(TAG, "AdvertisingIdClient unbindService failed.", e);
-        }
-    }
-
     /** Closes the connection before the client is finalized. */
     @Override
     protected void finalize() throws Throwable {
-        finish();
+        synchronized (sLock) {
+            if (sHoldingConnectionClient != null) {
+                sHoldingConnectionClient.finish();
+            }
+        }
         super.finalize();
     }
 
@@ -154,46 +143,6 @@ public class AdvertisingIdClient {
             throw new AdvertisingIdNotAvailableException();
         }
         return new ComponentName(serviceInfo.packageName, serviceInfo.name);
-    }
-
-    /**
-     * Retrieves BlockingServiceConnection which must be unbound after use.
-     *
-     * @throws IOException when unable to bind service successfully.
-     */
-    @VisibleForTesting
-    BlockingServiceConnection getServiceConnection() throws IOException {
-        Intent intent = new Intent(GET_AD_ID_ACTION);
-        intent.setComponent(mComponentName);
-
-        final BlockingServiceConnection bsc = new BlockingServiceConnection();
-        try {
-            if (mContext.bindService(intent, bsc, Service.BIND_AUTO_CREATE)) {
-                return bsc;
-            }
-        } catch (Throwable e) {
-            throw new IOException(e);
-        }
-
-        throw new IOException("Connection failure");
-    }
-
-    /**
-     * Get the AdvertisingIdService from the blocking queue. This should wait until
-     * onServiceConnected event with a {@link #SERVICE_CONNECTION_TIMEOUT_SECONDS} second timeout.
-     *
-     * @throws IOException if connection failed or the timeout period has expired.
-     */
-    private static IAdvertisingIdService getAdvertisingIdService(BlockingServiceConnection bsc)
-            throws IOException {
-        try {
-            // Block until the bind is complete, or timeout period is over.
-            return IAdvertisingIdService.Stub.asInterface(
-                    bsc.getServiceWithTimeout(
-                            SERVICE_CONNECTION_TIMEOUT_SECONDS, TimeUnit.SECONDS));
-        } catch (Throwable e) {
-            throw new IOException(e);
-        }
     }
 
     /**
@@ -236,12 +185,17 @@ public class AdvertisingIdClient {
      */
     public static AdvertisingIdInfo getAdvertisingIdInfo(Context context)
             throws IOException, IllegalStateException, AdvertisingIdNotAvailableException {
+        Preconditions.checkNotNull(context);
         checkNotMainThread();
-        AdvertisingIdClient client = new AdvertisingIdClient(context);
+
         try {
-            return client.getInfo();
+            return getAdvertisingIdInfo(getServiceWithIntent(context.getApplicationContext()));
         } finally {
-            client.finish();
+            synchronized (sLock) {
+                if (sHoldingConnectionClient != null) {
+                    sHoldingConnectionClient.scheduleAutoDisconnect();
+                }
+            }
         }
     }
 }
---
+++ ads/ads-identifier/src/main/java/androidx/ads/identifier/internal/IntentCallback.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.ads.identifier.internal;
+
+import android.content.Intent;
+
+import androidx.ads.identifier.AdvertisingIdNotAvailableException;
+
+/**
+ * Represents a callback function to return a {@link Intent}.
+ */
+@FunctionalInterface
+public interface IntentCallback {
+    /**
+     * Gets the intent for the connection.
+     *
+     * @return the intent.
+     */
+    Intent getIntent() throws AdvertisingIdNotAvailableException;
+}
