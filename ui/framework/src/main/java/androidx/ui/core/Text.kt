7ddf46ee686f34ffbf0bc0d6e261c12be46ea093
Siyamed Sinir <siyamed@google.com>
2019-06-27 16:23:31 -0700

    Use SP for fontSize
    Test: ./gradlew ui-text:text
    Test: ./gradlew ui-text:connectedAndroidTest
    Test: ./gradlew ui-framework:test
    Test: ./gradlew ui-framework:connectedAndroidTest
    Test: ./gradlew ui-material:test
    Test: ./gradlew ui-material:connectedAndroidTest
    Test: ./gradlew ui-animation:test
    Test: ./gradlew ui-animation:connectedAndroidTest
    Test: ./gradlew ui-layout:test
    Test: ./gradlew ui-layout:connectedAndroidTest
    
    Test: Manual on demo app
    
    Bug: 127345041
    Change-Id: Iac6c9f5915fd557f1e80c7a3577b42db42539baa

--- ui/framework/src/main/java/androidx/ui/core/Text.kt
+++ ui/framework/src/main/java/androidx/ui/core/Text.kt
@@ -191,8 +191,12 @@ fun Text(
     // TODO(Migration/siyamed): This is temporary and should be removed when resource
     //  system is resolved.
     val context = composer.composer.context
+    val density = +ambient(DensityAmbient)
+
     mergedStyle.fontFamily?.context = context
-    text.textStyles.forEach { it.style.fontFamily?.context = context }
+    text.textStyles.forEach {
+        it.style.fontFamily?.context = context
+    }
 
     Semantics(label = text.text) {
         val textPainter = +memo(
@@ -202,7 +206,8 @@ fun Text(
             softWrap,
             overflow,
             textScaleFactor,
-            maxLines
+            maxLines,
+            density
         ) {
             TextPainter(
                 text = text,
@@ -211,7 +216,8 @@ fun Text(
                 softWrap = softWrap,
                 overflow = overflow,
                 textScaleFactor = textScaleFactor,
-                maxLines = maxLines
+                maxLines = maxLines,
+                density = density
             )
         }
 
