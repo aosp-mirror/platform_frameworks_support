305d7a0772fa78eee27e022078b3d456a51d9871
Jelle Fresen <jellefresen@google.com>
2019-07-01 11:45:15 +0100

    Fix OnPageChangeCallback events for empty adapters
    When all items of the adapter of ViewPager2 are removed, an
    onPageSelected and an onPageScrolled is sent out, similar to when the
    ViewPager2 is populated for the first time. For API parity with
    ViewPager, the position in those events must be 0 even though there may
    not be any page.
    
    Bug: 136447132
    Test: ./gradlew viewpager2:connectedCheck
    
    Change-Id: Ibee24d5e3871310b1d6908835698eec5a9436c61

--- viewpager2/src/androidTest/java/androidx/viewpager2/widget/AdapterTest.kt
+++ viewpager2/src/androidTest/java/androidx/viewpager2/widget/AdapterTest.kt
@@ -16,25 +16,194 @@
 
 package androidx.viewpager2.widget
 
+import androidx.recyclerview.widget.RecyclerView
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
+import androidx.testutils.PollingCheck
+import androidx.viewpager2.widget.AdapterTest.Event.OnPageScrollStateChangedEvent
+import androidx.viewpager2.widget.AdapterTest.Event.OnPageScrolledEvent
+import androidx.viewpager2.widget.AdapterTest.Event.OnPageSelectedEvent
+import org.hamcrest.CoreMatchers.equalTo
+import org.junit.Assert.assertThat
 import org.junit.Test
 import org.junit.runner.RunWith
-import java.util.concurrent.TimeUnit
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.TimeUnit.SECONDS
 
 @LargeTest
 @RunWith(AndroidJUnit4::class)
 class AdapterTest : BaseTest() {
+    private val pageCount = 5
+
+    private lateinit var test: Context
+    private lateinit var dataSet: MutableList<String>
+    private lateinit var adapter: RecyclerView.Adapter<RecyclerView.ViewHolder>
+
+    override fun setUp() {
+        super.setUp()
+        test = setUpTest(ViewPager2.ORIENTATION_HORIZONTAL)
+    }
+
     @Test
     fun test_setAdapter() {
-        val test = setUpTest(ViewPager2.ORIENTATION_HORIZONTAL)
         test.setAdapterSync(viewAdapterProvider(stringSequence(5)))
         test.assertBasicState(0)
-        test.viewPager.setCurrentItemSync(1, false, 2, TimeUnit.SECONDS)
+        test.viewPager.setCurrentItemSync(1, false, 2, SECONDS)
         test.assertBasicState(1)
         activityTestRule.runOnUiThread {
             test.viewPager.adapter = test.viewPager.adapter
         }
         test.assertBasicState(0)
+    }
+
+    @Test
+    fun test_removeAllLookingAt0() {
+        val recorder = test.viewPager.addNewRecordingCallback()
+        setUpAdapterSync(pageCount)
+        clearDataSet()
+
+        // check events
+        assertThat(recorder.allEvents, equalTo(
+            eventsForPage(0)            // for setting the adapter
+                .plus(eventsForPage(0)) // for clearing it
+        ))
+    }
+
+    @Test
+    fun test_removeAllLookingAt1() {
+        val recorder = test.viewPager.addNewRecordingCallback()
+        setUpAdapterSync(pageCount, initialPage = 1)
+        clearDataSet()
+
+        // check events
+        assertThat(recorder.allEvents, equalTo(
+            eventsForPage(0)            // for setting the adapter
+                .plus(eventsForPage(1)) // for going to page 1
+                .plus(eventsForPage(0)) // for clearing it
+        ))
+    }
+
+    @Test
+    fun test_addItemsWhileEmpty() {
+        val recorder = test.viewPager.addNewRecordingCallback()
+        setUpAdapterSync(0)
+        fillDataSet()
+
+        // check events
+        assertThat(recorder.allEvents, equalTo(
+            eventsForPage(0) // for populating the adapter
+        ))
+    }
+
+    @Test
+    fun test_removeAllAddAllRemoveAgain() {
+        val recorder = test.viewPager.addNewRecordingCallback()
+        setUpAdapterSync(pageCount)
+
+        clearDataSet()
+        fillDataSet()
+        clearDataSet()
+
+        // check events
+        assertThat(recorder.allEvents, equalTo(
+            eventsForPage(0)            // for setting the adapter
+                .plus(eventsForPage(0)) // for clearing it
+                .plus(eventsForPage(0)) // for repopulating it
+                .plus(eventsForPage(0)) // for clearing it again
+        ))
+    }
+
+    private fun eventsForPage(page: Int): List<Event> {
+        return listOf(
+            OnPageSelectedEvent(page),
+            OnPageScrolledEvent(page, 0f, 0)
+        )
+    }
+
+    private fun setUpAdapterSync(pageCount: Int, initialPage: Int? = null) {
+        dataSet = MutableList(pageCount) { "$it" }
+        test.setAdapterSync(viewAdapterProvider(dataSet))
+        adapter = test.viewPager.adapter!!
+
+        if (initialPage != null) {
+            test.viewPager.setCurrentItemSync(initialPage, false, 2, SECONDS)
+        }
+    }
+
+    private fun clearDataSet() {
+        modifyDataSet {
+            dataSet.clear()
+            adapter.notifyItemRangeRemoved(0, pageCount)
+        }
+    }
+
+    private fun fillDataSet() {
+        modifyDataSet {
+            dataSet.addAll(List(pageCount) { "$it" })
+            adapter.notifyItemRangeInserted(0, pageCount)
+        }
+    }
+
+    private fun modifyDataSet(block: () -> Unit) {
+        val layoutChangedLatch = test.viewPager.addWaitForLayoutChangeLatch()
+        activityTestRule.runOnUiThread {
+            block()
+        }
+        layoutChangedLatch.await(1, SECONDS)
+
+        // Let animations run
+        val animationLatch = CountDownLatch(1)
+        test.viewPager.recyclerView.itemAnimator!!.isRunning {
+            animationLatch.countDown()
+        }
+        animationLatch.await(1, SECONDS)
+
+        // Wait until VP2 has stabilized
+        val adapter = test.viewPager.adapter
+        if (adapter != null && adapter.itemCount > 0) {
+            PollingCheck.waitFor(1000) { test.viewPager.currentCompletelyVisibleItem != -1 }
+        }
+    }
+
+    private fun ViewPager2.addNewRecordingCallback(): RecordingCallback {
+        return RecordingCallback().also { registerOnPageChangeCallback(it) }
+    }
+
+    private sealed class Event {
+        data class OnPageScrolledEvent(
+            val position: Int,
+            val positionOffset: Float,
+            val positionOffsetPixels: Int
+        ) : Event()
+        data class OnPageSelectedEvent(val position: Int) : Event()
+        data class OnPageScrollStateChangedEvent(val state: Int) : Event()
+    }
+
+    private class RecordingCallback : ViewPager2.OnPageChangeCallback() {
+        private val events = mutableListOf<Event>()
+
+        val allEvents get() = events.toList()
+
+        override fun onPageScrolled(
+            position: Int,
+            positionOffset: Float,
+            positionOffsetPixels: Int
+        ) {
+            synchronized(events) {
+                events.add(OnPageScrolledEvent(position, positionOffset, positionOffsetPixels))
+            }
+        }
+
+        override fun onPageSelected(position: Int) {
+            synchronized(events) {
+                events.add(OnPageSelectedEvent(position))
+            }
+        }
+
+        override fun onPageScrollStateChanged(state: Int) {
+            synchronized(events) {
+                events.add(OnPageScrollStateChangedEvent(state))
+            }
+        }
     }
 }
